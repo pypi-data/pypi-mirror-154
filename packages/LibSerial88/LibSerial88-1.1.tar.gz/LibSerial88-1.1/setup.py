b4ede58861611b03bc2b78bfdbe386dbfd3466f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:22 +0000

    Add package LibSerial88/1.1/LibSerial88-1.1.tar.gz

---
+++ packages/LibSerial88/LibSerial88-1.1.tar.gz/LibSerial88-1.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'LibSerial88',       
+  packages = ['LibSerial'],   
+  version = '1.1',      
+  license='MIT',       
+  description = 'Biblioteca LibSerial88',   
+  author = 'Fernando Simplicio',                  
+  author_email = 'fernando@microgenios.com.br',     
+  url = 'https://github.com/microgenios/libserial88',  
+  download_url = 'https://github.com/microgenios/libserial88/archive/refs/heads/main.zip',    
+  keywords = ['Serial', 'Senai', 'ComPort'],  
+  install_requires=[           
+          'datetime',         
+      ],
+  classifiers=[
+    #"3 - Alpha", "4 - Beta" or "5 - Production/Stable"   
+    'Development Status :: 5 - Production/Stable',   
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
