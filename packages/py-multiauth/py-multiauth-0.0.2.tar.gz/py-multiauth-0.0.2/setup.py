2812954c0fb852920cb3cbbe6d0b0c803dbcef7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:48 +0000

    Add package py-multiauth/0.0.2/py-multiauth-0.0.2.tar.gz

---
+++ packages/py-multiauth/py-multiauth-0.0.2.tar.gz/py-multiauth-0.0.2/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['multiauth',
+ 'multiauth.providers',
+ 'multiauth.types',
+ 'multiauth.types.providers']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Authlib>=1.0.1,<2.0.0',
+ 'PyJWT>=2.4.0,<3.0.0',
+ 'graphql-core>=3.2.1,<4.0.0',
+ 'pycognito>=2022.5.0,<2023.0.0',
+ 'pydash>=5.1.0,<6.0.0']
+
+setup_kwargs = {
+    'name': 'py-multiauth',
+    'version': '0.0.2',
+    'description': 'Python package to interact with multiple authentication services',
+    'long_description': '# py-multiauth\n',
+    'author': 'Escape Technologies SAS',
+    'author_email': 'ping@escape.tech',
+    'maintainer': 'Antoine Carossio',
+    'maintainer_email': 'antoine@escape.tech',
+    'url': 'https://escape.tech/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<3.11',
+}
+
+
+setup(**setup_kwargs)
