f0d5ec6ffb3e23e9c6071ddd449b4d43a445c92d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:35 +0000

    Add package pygbag/0.0.6/pygbag-0.0.6-py3-none-any.whl

---
+++ packages/pygbag/pygbag-0.0.6-py3-none-any.whl/pygbag/pack.py
@@ -0,0 +1,104 @@
+import sys, os
+import zipfile
+from pathlib import Path
+
+COUNTER = 0
+TRUNCATE = 0
+ASSETS = []
+HAS_STATIC = False
+HAS_MAIN = False
+
+
+def pack_files(zf, pushpopd, newpath):
+    global COUNTER, TRUNCATE, ASSETS, HAS_STATIC, HAS_MAIN
+
+    if str(newpath).find("/.git") >= 0:
+        return
+
+    os.chdir(newpath)
+
+    for dirname, dirnames, filenames in os.walk(newpath):
+        p_dirname = Path(dirname).as_posix()
+        # do not put git subfolders
+        if p_dirname.find("/.git") >= 0:
+            continue
+
+        if p_dirname.endswith("/build"):
+            continue
+
+        if p_dirname.endswith("/static"):
+            HAS_STATIC = True
+            continue
+
+        try:
+            dispname = Path(dirname[TRUNCATE:] or "/").as_posix()
+            if dispname.startswith("/build"):
+                continue
+            os.chdir(dirname)
+            print(f"now in .{dispname}")
+
+        except:
+            print("Invalid Folder :", pushpopd, newpath)
+
+        for f in filenames:
+            # do not pack ourself
+            if f.endswith(".apk"):
+                continue
+
+            if f.endswith(".gitignore"):
+                continue
+
+            if not os.path.isfile(f):
+                continue
+
+            if Path(f).as_posix().endswith("/main.py"):
+                HAS_MAIN = True
+
+            # ext = f.rsplit(".", 1)[-1].lower()
+            # folders to skip __pycache__
+            # extensions to skip : pyc pyx pyd pyi
+
+            src = os.path.join(os.getcwd(), f)
+            src = f"assets{src[TRUNCATE:]}"
+
+            if not src in ASSETS:
+                zf.write(f, src)
+                # print(src)
+                ASSETS.append(src)
+
+                COUNTER += 1
+
+        for subdir in dirnames:
+            # do not archive static web files
+            if subdir == "static":
+                HAS_STATIC = True
+                continue
+
+            if subdir == "build":
+                continue
+
+            if subdir != ".git":
+                pack_files(zf, os.getcwd(), subdir)
+
+    os.chdir(pushpopd)
+
+
+def archive(apkname, target_folder, build_dir=None):
+    global COUNTER, TRUNCATE, ASSETS, HAS_MAIN, HAS_STATIC
+    TRUNCATE = len(target_folder.as_posix())
+    if build_dir:
+        apkname = build_dir.joinpath(apkname).as_posix()
+
+    try:
+        with zipfile.ZipFile(
+            apkname, mode="x", compression=zipfile.ZIP_DEFLATED, compresslevel=9
+        ) as zf:
+            pack_files(zf, Path.cwd(), target_folder)
+    except TypeError:
+        # 3.6 does not support compresslevel
+        with zipfile.ZipFile(apkname, mode="x", compression=zipfile.ZIP_DEFLATED) as zf:
+            pack_files(zf, Path.cwd(), target_folder)
+    print(COUNTER)
+
+    if not (HAS_MAIN or HAS_STATIC):
+        print("Warning : this apk has no startup file (main.py or static )")
