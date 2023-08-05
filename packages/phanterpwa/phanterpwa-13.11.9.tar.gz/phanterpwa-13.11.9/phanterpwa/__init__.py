8dfd6f41dd01cc91161b5d5803122768f56c0c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:13 +0000

    Add package phanterpwa/13.11.9/phanterpwa-13.11.9.tar.gz

---
+++ packages/phanterpwa/phanterpwa-13.11.9.tar.gz/phanterpwa-13.11.9/phanterpwa/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# author: PhanterJR<junior.conex@gmail.com>
+# license: MIT
+
+__author__ = "PhanterJR<junior.conex@gmail.com>"
+__version__ = "13.11.9"
+__install_requeriments__ = [
+    "psutil",
+    "tornado",
+    "libsass",
+    "transcrypt==3.7.16",
+    "pillow",
+    "itsdangerous==1.1",
+    "requests-oauthlib",
+    "pydal>=19.04",
+    "passlib",
+    "appdirs>=1.4.3"
+]
