ad341bf69979b27464c7673e96c6253b79ded14b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:38:08 +0000

    Add package flit_ext/0.0.2/flit_ext-0.0.2-py2.py3-none-any.whl

---
+++ packages/flit_ext/flit_ext-0.0.2-py2.py3-none-any.whl/flit_ext/__init__.py
@@ -0,0 +1,25 @@
+"""Flit with extensions"""
+
+import tomli
+from flit_core import buildapi  # noqa: F401
+from setuptools_scm import ScmVersion, get_version
+
+__version__ = "0.0.2"
+
+
+try:
+    with open("pyproject.toml", "rb") as f:
+        pyproject = tomli.load(f)
+except OSError:
+    pass
+else:
+    try:
+        setuptools_scm_config = pyproject["tool"]["setuptools_scm"]
+    except KeyError:
+        pass
+    else:
+        get_version(**setuptools_scm_config)
+
+
+def local_version(version: ScmVersion):
+    return version.format_choice("+{node}", "+{node}.d{time:%Y%m%d}")
