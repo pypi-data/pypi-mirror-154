962046eabe1df6ba5f4c9c1d5ecc47cb6dec64eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:35 +0000

    Add package libttp/0.1.2/libttp-0.1.2.tar.gz

---
+++ packages/libttp/libttp-0.1.2.tar.gz/libttp-0.1.2/setup.py
@@ -0,0 +1,28 @@
+# python setup.py build_ext --inplace
+import os
+from sys import platform
+print("Found platform:", platform)
+
+from setuptools import find_packages, setup
+from distutils.core import setup
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+import numpy as np
+
+print(find_packages())
+setup(
+    name='libttp',
+    version='0.1.2',
+    url='https://github.com/VicidominiLab/libttp',
+    license='CC-BY-NC-4.0',
+    author='Mattia Donato',
+    author_email='mattia.donato@iit.it',
+    description='Libraries for reading the data from time-tagging module (BrightEyes-TTM, https://github.com/VicidominiLab/BrightEyes-TTM)',
+    ext_modules=cythonize("libttp/ttpCython.pyx", compiler_directives={'language_level' : "3"},
+include_path=[np.get_include()]
+),
+    packages=find_packages(),
+    include_dirs=[np.get_include()]
+)
+
+
