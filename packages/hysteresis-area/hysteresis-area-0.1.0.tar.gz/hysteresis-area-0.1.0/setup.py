6eb8a8122e1fc70e8fb2420b136c4430db20a25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:48 +0000

    Add package hysteresis-area/0.1.0/hysteresis-area-0.1.0.tar.gz

---
+++ packages/hysteresis-area/hysteresis-area-0.1.0.tar.gz/hysteresis-area-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hysteresis_area']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['opencv-contrib-python>=4.5.5,<5.0.0', 'opencv-python>=4.5.5,<5.0.0']
+
+setup_kwargs = {
+    'name': 'hysteresis-area',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'k4w411p0wer',
+    'author_email': 'alexandrvasyutin@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<3.11',
+}
+
+
+setup(**setup_kwargs)
