4dc2df272062bd608e7f870853e489acd74ab22f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:35 +0000

    Add package pyte/0.8.1/pyte-0.8.1.tar.gz

---
+++ packages/pyte/pyte-0.8.1.tar.gz/pyte-0.8.1/setup.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+
+import os
+
+from setuptools import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+DESCRIPTION = "Simple VTXXX-compatible terminal emulator."
+
+try:
+    with open(os.path.join(here, "README")) as f:
+        LONG_DESCRIPTION = f.read()
+except IOError:
+    LONG_DESCRIPTION = ""
+
+
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Environment :: Console",
+    "Intended Audience :: Developers",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Topic :: Terminals :: Terminal Emulators/X Terminals",
+]
+
+
+setup(name="pyte",
+      version="0.8.1",
+      packages=["pyte"],
+      install_requires=["wcwidth"],
+      setup_requires=["pytest-runner"],
+      tests_require=["pytest"],
+      platforms=["any"],
+
+      author="Sergei Lebedev",
+      author_email="superbobry@gmail.com",
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      classifiers=CLASSIFIERS,
+      keywords=["vt102", "vte", "terminal emulator"],
+      url="https://github.com/selectel/pyte")
