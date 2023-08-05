a6f5e23d8ff82d6d2b6f8e3996db9d59b9e3ff5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:32 +0000

    Add package pypi-package-tttt/0.0.1/pypi-package-tttt-0.0.1.tar.gz

---
+++ packages/pypi-package-tttt/pypi-package-tttt-0.0.1.tar.gz/pypi-package-tttt-0.0.1/pypi-package-tttt/__init__.py
@@ -0,0 +1,1 @@
+name="pypi-package-tttt"
---
+++ packages/pypi-package-tttt/pypi-package-tttt-0.0.1.tar.gz/pypi-package-tttt-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="pypi-package-tttt",
+    version="0.0.1",
+    author="zzqq2199",
+    author_email="zhouquanjs@qq.com",
+    description="A small example package",
+    packages=setuptools.find_packages()
+)
