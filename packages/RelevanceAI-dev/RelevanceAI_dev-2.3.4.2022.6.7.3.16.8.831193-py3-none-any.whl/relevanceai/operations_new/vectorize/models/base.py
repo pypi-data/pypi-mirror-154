f34c8210c02c27602ed710db6dcb55a324a157a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:20 +0000

    Add package RelevanceAI-dev/2.3.4.2022.6.7.3.16.8.831193/RelevanceAI_dev-2.3.4.2022.6.7.3.16.8.831193-py3-none-any.whl

---
+++ packages/RelevanceAI-dev/RelevanceAI_dev-2.3.4.2022.6.7.3.16.8.831193-py3-none-any.whl/relevanceai/operations_new/vectorize/models/base.py
@@ -0,0 +1,47 @@
+from abc import abstractmethod
+from pydoc import doc
+from typing import Any, Dict, List
+
+from relevanceai.utils import DocUtils
+
+
+class VectorizeModelBase(DocUtils):
+    model_name: str
+    has_printed_vector_field_name: dict = {}
+
+    def _get_model_name(self, url):
+        model_name = "_".join(url.split("/google/")[-1].split("/")[:-1])
+        return model_name
+
+    def vector_name(self, field):
+        return f"{field}_{self.model_name}_vector_".replace("/", "_")
+
+    @abstractmethod
+    def encode(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def bulk_encode(self, *args, **kwargs):
+        pass
+
+    def print_vector_field_name(self, field: str):
+        # Store a simple dictionary to test vectorizing
+        if field not in self.has_printed_vector_field_name:
+            print(f"Vector field is `{self.vector_name(field)}`")
+            self.has_printed_vector_field_name[field] = True
+
+    def encode_documents(
+        self, documents: List[Dict[str, Any]], fields: List[str], bias_value=0
+    ):
+        for field in fields:
+            self.print_vector_field_name(field)
+            values = self.get_field_across_documents(field=field, docs=documents)
+            vectors = self.bulk_encode(values)
+
+            self.set_field_across_documents(
+                field=self.vector_name(field),
+                values=vectors,
+                docs=documents,
+            )
+
+        return documents
