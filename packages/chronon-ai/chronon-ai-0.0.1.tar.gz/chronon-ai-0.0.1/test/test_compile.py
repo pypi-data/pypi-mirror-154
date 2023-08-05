bf1172243320602db06f10738c5f1335439b4e14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:03 +0000

    Add package chronon-ai/0.0.1/chronon-ai-0.0.1.tar.gz

---
+++ packages/chronon-ai/chronon-ai-0.0.1.tar.gz/chronon-ai-0.0.1/test/test_compile.py
@@ -0,0 +1,45 @@
+"""
+Run the flow for materialize.
+"""
+from ai.chronon.repo.compile import extract_and_convert
+from click.testing import CliRunner
+
+
+def test_basic_compile():
+    runner = CliRunner()
+    result = runner.invoke(extract_and_convert, [
+        '--chronon_root=test/sample',
+        '--input_path=joins/sample_team/'
+    ])
+    assert result.exit_code == 0
+    result = runner.invoke(extract_and_convert, [
+        '--chronon_root=test/sample',
+        '--input_path=joins/sample_team'
+    ])
+    assert result.exit_code == 0
+    result = runner.invoke(extract_and_convert, [
+        '--chronon_root=test/sample',
+        '--input_path=joins/sample_team/sample_join.py'
+    ])
+    assert result.exit_code == 0
+
+
+def test_debug_compile():
+    runner = CliRunner()
+    result = runner.invoke(extract_and_convert, [
+        '--chronon_root=test/sample',
+        '--input_path=joins/sample_team/',
+        '--debug'
+    ])
+    assert result.exit_code == 0
+
+
+def test_failed_compile():
+    """
+    Should fail as it fails to find teams.
+    """
+    runner = CliRunner()
+    result = runner.invoke(extract_and_convert, [
+        '--input_path=joins/sample_team/',
+    ])
+    assert result.exit_code != 0
