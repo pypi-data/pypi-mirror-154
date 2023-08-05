3d1a948f9d6ab941ca01c0de7ef99788b967e95e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:49 +0000

    Add package rdata/0.8/rdata-0.8.tar.gz

---
+++ packages/rdata/rdata-0.8.tar.gz/rdata-0.8/rdata/parser/__init__.py
@@ -0,0 +1,12 @@
+"""Utilities for parsing a rdata file."""
+
+from ._parser import (
+    DEFAULT_ALTREP_MAP,
+    CharFlags,
+    RData,
+    RObject,
+    RObjectInfo,
+    RObjectType,
+    parse_data,
+    parse_file,
+)
