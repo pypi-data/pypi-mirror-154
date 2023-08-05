0edba0994b63c53476029cc555c3a5ec41a708f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:11 +0000

    Add package DataframePacking/22.6.6/DataframePacking-22.6.6.tar.gz

---
+++ packages/DataframePacking/DataframePacking-22.6.6.tar.gz/DataframePacking-22.6.6/README.md
@@ -0,0 +1,5 @@
+A package to save and load dictionaries containing multiple Pandas dataframes to/from a tar file.
+
+D. Estes McKnight, 2022.
+
+Created off the back of Rob deCarvalho's method to save a dict of dataframes to a tar file. (https://gist.github.com/robdmc/56c998bfafb8f1037744e6493238768e)
---
+++ packages/DataframePacking/DataframePacking-22.6.6.tar.gz/DataframePacking-22.6.6/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='DataframePacking',
+    version='22.06.06',
+    #packages=['things_labels', 'things_labels.data', 'things_labels.data_labelling'],
+    packages = find_packages(exclude=["*_old*", "*old_", "tests*", "venv*"]),
+    license='',
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        #"": ["*.csv", "*.tsv"],
+    },
+    # entry_points={
+    #     "console_scripts": [
+    #         "label_ages = things_labels.data_labelling.label_ages:main",
+    #         "label_animism = things_labels.data_labelling.label_animism:main",
+    #         "label_object_names_in_triplet = things_labels.data_labelling.label_object_names_in_triplet:main"
+    #     ]
+    # },
+    install_requires=[
+        "pandas>=1.1.0"
+    ],
+)
+
+
+
