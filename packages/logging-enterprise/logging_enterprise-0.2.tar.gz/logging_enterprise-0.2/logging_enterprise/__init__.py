440ed82815adb82aa507db01e238a4ce221966f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:04 +0000

    Add package logging-enterprise/0.2/logging_enterprise-0.2.tar.gz

---
+++ packages/logging-enterprise/logging_enterprise-0.2.tar.gz/logging_enterprise-0.2/logging_enterprise/__init__.py
@@ -0,0 +1,3 @@
+def info(data: str) -> None:
+    print('Dependency confusion confirmed')
+    print('[INFO]', data)
---
+++ packages/logging-enterprise/logging_enterprise-0.2.tar.gz/logging_enterprise-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='logging_enterprise',
+    packages=['logging_enterprise'],
+    description='Logger enterprise edition',
+    version='0.2',
+    author='exp101t',
+    author_email='nobody@nonexistent.net',
+    keywords=['logging', 'enterprise']
+)
