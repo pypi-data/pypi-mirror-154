80d8a6efdd02dff83991c9025109db42b1580759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:25 +0000

    Add package inspector-test-package/0.0.0/inspector-test-package-0.0.0.tar.gz

---
+++ packages/inspector-test-package/inspector-test-package-0.0.0.tar.gz/inspector-test-package-0.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="inspector-test-package",
+    author="Wouter Koorn",
+    author_email="wouter.koorn@cern.ch",
+    packages=find_packages(),
+)
