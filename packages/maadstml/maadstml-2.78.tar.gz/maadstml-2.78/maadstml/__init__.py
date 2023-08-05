26c6bbbb5234551a62d821b4c81a09c4dfc58178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:57 +0000

    Add package maadstml/2.78/maadstml-2.78.tar.gz

---
+++ packages/maadstml/maadstml-2.78.tar.gz/maadstml-2.78/maadstml/__init__.py
@@ -0,0 +1,54 @@
+
+from .sendfiles import viperstats
+
+from .sendfiles import viperlisttopics
+
+from .sendfiles import vipersubscribeconsumer
+
+from .sendfiles import viperunsubscribeconsumer
+
+from .sendfiles import viperproducetotopic
+from .sendfiles import viperproducetotopicbulk
+from .sendfiles import viperconsumefromtopic
+from .sendfiles import viperhpdepredict
+
+from .sendfiles import viperhpdeoptimize
+
+from .sendfiles import viperhpdetraining
+
+from .sendfiles import viperproducetotopicstream
+
+from .sendfiles import viperpreprocessproducetotopicstream
+
+from .sendfiles import viperpreprocessbatch
+
+from .sendfiles import vipercreatetrainingdata
+    
+from .sendfiles import viperstreamquery
+
+from .sendfiles import vipercreatetopic
+from .sendfiles import viperconsumefromstreamtopic
+from .sendfiles import vipercreatejointopicstreams
+    
+from .sendfiles import vipercreateconsumergroup
+from .sendfiles import viperconsumergroupconsumefromtopic
+    
+from .sendfiles import vipermodifyconsumerdetails
+    
+from .sendfiles import vipermodifytopicdetails
+    
+from .sendfiles import viperactivatetopic
+    
+from .sendfiles import viperdeactivatetopic
+    
+from .sendfiles import vipergroupactivate
+    
+from .sendfiles import vipergroupdeactivate
+
+from .sendfiles import viperanomalytrain
+from .sendfiles import viperanomalypredict
+
+from .sendfiles import areyoubusy
+
+
+name = "maadstml"
