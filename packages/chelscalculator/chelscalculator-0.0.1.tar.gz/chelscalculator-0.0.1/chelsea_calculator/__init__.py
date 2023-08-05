7b022bd8db4198aa6d4829a92f90ec37861c412d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:37 +0000

    Add package chelscalculator/0.0.1/chelscalculator-0.0.1.tar.gz

---
+++ packages/chelscalculator/chelscalculator-0.0.1.tar.gz/chelscalculator-0.0.1/chelsea_calculator/__init__.py
@@ -0,0 +1,14 @@
+def add_numbers(num1, num2):
+    return num1 + num2
+
+def sub_numbers(num1, num2):
+    return num1 - num2
+
+def mult_numbers(num1, num2):
+    return num1 * num2
+
+def divide_numbers(num1, num2):
+    return num1 / num2
+
+
+print(add_numbers(5,6))
