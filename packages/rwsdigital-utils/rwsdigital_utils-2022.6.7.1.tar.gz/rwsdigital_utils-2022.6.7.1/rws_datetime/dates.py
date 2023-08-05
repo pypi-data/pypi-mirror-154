1a75d9224a0111edaf80ccc497978d49d504c977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:17 +0000

    Add package rwsdigital-utils/2022.6.7.1/rwsdigital_utils-2022.6.7.1.tar.gz

---
+++ packages/rwsdigital-utils/rwsdigital_utils-2022.6.7.1.tar.gz/rwsdigital_utils-2022.6.7.1/rws_datetime/dates.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from datetime import timedelta
+
+def str_to_datetime(date_string):
+    most_used_formats = ["%Y/%m/%d %H.%M.%S",
+                         "%Y/%m/%d %H:%M:%S",
+                         "%Y-%m-%d %H.%M.%S",
+                         "%Y-%m-%d %H:%M:%S",
+                         "%d/%m/%Y %H.%M.%S",
+                         "%d/%m/%Y %H:%M:%S",
+                         "%d-%m-%Y %H.%M.%S",
+                         "%d-%m-%Y %H:%M:%S",
+                         "%Y/%m/%d %H.%M",
+                         "%Y/%m/%d %H:%M",
+                         "%Y-%m-%d %H.%M",
+                         "%Y-%m-%d %H:%M",
+                         "%d/%m/%Y %H.%M",
+                         "%d/%m/%Y %H:%M",
+                         "%d-%m-%Y %H.%M",
+                         "%d-%m-%Y %H:%M",
+                         "%Y/%m/%d",
+                         "%Y-%m-%d",
+                         "%d/%m/%Y",
+                         "%d-%m-%Y"]
+    for format_ in most_used_formats:
+        try:
+            res = datetime.strptime(date_string, format_)
+            return res
+        except ValueError as e:
+            continue
+    raise ValueError("Formato data non supportato ({})".format(date_string))
+
+
+def str_to_date(date_string):
+    return str_to_datetime(date_string).date()
