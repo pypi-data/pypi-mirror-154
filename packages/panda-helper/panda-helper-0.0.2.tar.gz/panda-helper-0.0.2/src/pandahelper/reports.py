d8e2adcbdb22b0ac7381357c12a0e371ef818844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:57 +0000

    Add package panda-helper/0.0.2/panda-helper-0.0.2.tar.gz

---
+++ packages/panda-helper/panda-helper-0.0.2.tar.gz/panda-helper-0.0.2/src/pandahelper/reports.py
@@ -0,0 +1,241 @@
+"""Panda-Helper Classes and associated helper functions."""
+
+import pandas as pd
+import scipy.stats
+from tabulate import tabulate
+
+
+def abbreviate_df(df, first=20, last=5):
+    """Returns a shortened DataFrame or Series.
+
+    Returned dataframe will contain the input numbers of the first and last
+    rows. Most useful when input is sorted.
+
+    Args:
+        df (pd.DataFrame or pd.Series): pd.DataFrame or pd.Series
+        first (int): First n rows of input to be included in the shortened
+            Dataframe or Series. Default value is 20.
+        last (int): Last n rows of input to be included in the shortened
+            Dataframe or Series. Default value is 5.
+
+    Returns:
+        pd.DataFrame or pd.Series: Shortened DataFrame or Series.
+
+    Raises:
+        ValueError: If first or last are negative.
+        TypeError: If df is not a pd.DataFrame or pd.Series.
+    """
+    if first < 0 or last < 0:
+        raise ValueError("'first' and 'last' parameters cannot be negative")
+    if not isinstance(df, (pd.DataFrame, pd.Series)):
+        raise TypeError(f"{df} is not pd.Series or pd.DataFrame")
+    if len(df) <= first + last:
+        abbrev = df
+    else:
+        abbrev = pd.concat([df.iloc[:first], df.iloc[(len(df) - last) : len(df)]])
+    return abbrev
+
+
+def abbreviate_string(s, limit=60):
+    """Returns first x characters of a string.
+
+    Args:
+        s (str): String to be shortened
+        limit (int): Maximum length of string. Default value is 60.
+
+    Returns:
+        str: Shortened string.
+
+    Raises:
+        TypeError: If input is not a string.
+    """
+    if not isinstance(s, str):
+        raise TypeError("Input is not a string")
+    return s[:limit]
+
+
+def distribution_stats(series):
+    """Returns Series distribution statistics.
+
+    Args:
+        series (pd.Series): Series used to calculate distribution statistics.
+
+    Returns:
+        dict: Key-value pairs with name of statistic and calculated value.
+
+    Raises:
+        TypeError: If input is not a numeric pd.Series.
+    """
+    if not isinstance(series, pd.Series):
+        raise TypeError(f"{series}, is not pd.Series")
+    if not pd.api.types.is_numeric_dtype(series.dtype):
+        raise TypeError(f"{series}, is not numeric")
+    mad = scipy.stats.median_abs_deviation(series, nan_policy="omit")
+    stats = {
+        "count": series.count(),
+        "min": series.min(),
+        "1%": series.quantile(0.01),
+        "5%": series.quantile(0.05),
+        "25%": series.quantile(0.25),
+        "50%": series.quantile(0.50),
+        "75%": series.quantile(0.75),
+        "95%": series.quantile(0.95),
+        "99%": series.quantile(0.99),
+        "max": series.max(),
+        "median": series.median(),
+        "mean": series.mean(),
+        "median absolute deviation": mad,
+        "standard deviation": series.std(),
+        "skew": series.skew(),
+    }
+    return stats
+
+
+def frequency_table(series):
+    """Returns value counts and relative frequency.
+
+    Args:
+        series (pd.Series): Series used to calculate value counts and relative
+            frequencies.
+
+    Returns:
+        pd.DataFrame: DataFrame containing values as the row index with value
+            counts and counts as a percentage of total count.
+
+    Raises:
+        TypeError: If input is not a pd.Series.
+    """
+    if not isinstance(series, pd.Series):
+        raise TypeError(f"{series}, is not pd.Series")
+    freq = series.value_counts()  # excludes nulls
+    freq.name = "Count"
+    counts = series.value_counts(normalize=True)
+    percent = pd.Series([f"{x:.2%}" for x in counts], index=counts.index)
+    percent.name = "% of Total"
+    output = pd.concat([freq, percent], axis=1)
+    output.index = [abbreviate_string(str(x), limit=60) for x in output.index]
+    return output
+
+
+class DataFrameProfile:
+    """DataFrame-level data profile.
+
+    Prepares pretty-printed DataFrame-level data profile that can be displayed
+    or saved.
+
+    Attributes:
+        name (str): Name of DataFrame profile if provided. Default value is "".
+        shape (tuple): Dataframe shape.
+        dtypes (pd.Series): Data types of Series within DataFrame.
+        num_duplicates (int): Number of duplicated rows.
+        nulls_per_row (pd.Series): Count of null values per row.
+        nulls_stats (list): Distribution statistics on nulls per row.
+    """
+
+    def __init__(self, df, name=""):
+        """Initializes DataFrameProfile.
+
+        Args:
+            df (pd.DataFrame): DataFrame to profile.
+            name (str, optional): Name to assign to profile.
+
+        Raises:
+            TypeError: If input is not a pd.DataFrame.
+        """
+        if not isinstance(df, pd.DataFrame):
+            raise TypeError(f"{df}, is not pd.DataFrame")
+        self.name = name
+        self.shape = df.shape
+        self.dtypes = list(zip(df.dtypes.index, df.dtypes.values))
+        self.num_duplicates = sum(df.duplicated(keep="first"))
+        self.nulls_per_row = df.isna().sum(axis=1)
+        self.null_stats = list(distribution_stats(self.nulls_per_row).items())
+
+    def __repr__(self):
+        """Printable version of profile."""
+        df_info = [
+            ("DF Shape", self.shape),
+            ("Duplicated Rows", self.num_duplicates),
+        ]
+        if self.name:
+            df_info.insert(0, ("DF Name", self.name))
+        df_table = tabulate(df_info, headers=["DataFrame-Level Info", ""])
+        dtype_table = tabulate(self.dtypes, headers=["Series Name", "Data Type"])
+        null_table = tabulate(self.null_stats, headers=["Summary of Nulls Per Row", ""])
+        output = ["".join([x, "\n\n"]) for x in [df_table, dtype_table, null_table]]
+        return "".join(output)
+
+    def save_report(self, path):
+        """Saves profile to provided path.
+
+        Args:
+            path (str): Where to save profile.
+        """
+        with open(path, "w+", encoding="utf-8") as fh:
+            fh.write(str(self))
+
+
+class SeriesProfile:
+    """Series-level data profile.
+
+    Prepares pretty-printed Series-level data profile that can be displayed
+    or saved.
+
+    Attributes:
+        name (str): Name of Series.
+        dtype (np.dtype): Data types of Series within DataFrame.
+        count (int): Count of non-null values.
+        num_unique (int): Number of unique values.
+        num_nulls (int): Number of null values.
+        frequency (pd.DataFrame): Table of value counts and relative frequency
+            as a DataFrame
+        stats (list): Distribution statistics for numeric Series.
+    """
+
+    def __init__(self, series):
+        """Initializes SeriesProfile.
+
+        Args:
+            series (pd.Series): DataFrame to profile.
+
+        Raises:
+            TypeError: If input is not a pd.Series.
+        """
+        if not isinstance(series, pd.Series):
+            raise TypeError(f"{series}, is not pd.DataFrame")
+        self.name = series.name
+        self.dtype = series.dtype
+        self.count = series.count()  # counts non-null values
+        self.num_unique = series.nunique()
+        self.num_nulls = series.size - self.count  # NAs, NANs, but not " "
+        self.frequency = frequency_table(series)
+        self.stats = None
+        if pd.api.types.is_numeric_dtype(self.dtype):
+            self.stats = list(distribution_stats(series).items())
+
+    def __repr__(self):
+        """Printable version of profile."""
+        series_info = [
+            ("Data Type", self.dtype),
+            ("Count", self.count),
+            ("Unique Values", self.num_unique),
+            ("Null Values", self.num_nulls),
+        ]
+        series_table = tabulate(series_info, headers=[f"{self.name} Info", ""])
+        freq_info = abbreviate_df(self.frequency, first=20, last=5)
+        freq_table = tabulate(freq_info, headers=["Value", "Count", "% of total"])
+        stats_table = ""
+        if self.stats is not None:
+            stats_table = tabulate(self.stats, headers=["Statistic", "Value"])
+
+        output = ["".join([x, "\n\n"]) for x in [series_table, freq_table, stats_table]]
+        return "".join(output)
+
+    def save_report(self, path):
+        """Saves profile to provided path.
+
+        Args:
+            path (str): Where to save profile.
+        """
+        with open(path, "w+", encoding="utf-8") as fh:
+            fh.write(str(self))
---
+++ packages/panda-helper/panda-helper-0.0.2.tar.gz/panda-helper-0.0.2/tests/test_reports.py
@@ -0,0 +1,227 @@
+""" Tests for helper functions and profile classes in reports.py"""
+import os
+import math
+import numbers
+import tempfile
+import filecmp
+import numpy as np
+import pandas as pd
+import pytest
+from pandahelper import reports
+
+TEST_DATA_DIR = "tests/test_data"
+TEST_DATA_FILE = "sample_collisions.csv"
+TEST_DF = pd.read_csv(os.path.join(TEST_DATA_DIR, TEST_DATA_FILE))
+TEST_CAT_SERIES = TEST_DF["BOROUGH"]
+TEST_NUM_SERIES = TEST_DF["NUMBER OF PERSONS INJURED"]
+
+
+def test_abbreviate_df_invalid_input():
+    """Invalid data type inputs should raise Type Error"""
+    invalid_types = [
+        "data",
+        34,
+        34.5,
+        {"data": "dictionary"},
+        [["col_name", 1], ["col_name2", 2]],
+        (("col_name", 3), ("col_name2", 4)),
+        np.array([1, 2, 3]),
+    ]
+    for invalid in invalid_types:
+        with pytest.raises(TypeError):
+            reports.abbreviate_df(invalid)
+
+
+def test_abbreviate_df_valid_output():
+    """Valid input should lead to valid and consistent output"""
+    output = reports.abbreviate_df(TEST_DF, 5, 5)
+    assert isinstance(output, (pd.DataFrame, pd.Series))
+    assert output.iloc[1]["LOCATION"] == "(40.7504, -73.985214)"
+    assert output.iloc[8]["COLLISION_ID"] == 3676178
+
+
+def test_abbreviate_df_most_plus_least_greater_sum():
+    """Returns itself if (first + last) parameters exceed object length"""
+    first = 300
+    last = 300
+    assert len(TEST_DF) < first + last
+    assert TEST_DF.equals(reports.abbreviate_df(TEST_DF, first, last))
+
+
+def test_abbreviate_df_negative_params():
+    """Negative parameters raise Value Error"""
+    invalid_params = [
+        {"first": -5, "last": 5},
+        {"first": 5, "last": -5},
+        {"first": -5, "last": -5},
+    ]
+    for invalid in invalid_params:
+        with pytest.raises(ValueError):
+            reports.abbreviate_df(TEST_DF, **invalid)
+
+
+def test_abbreviate_string_correct():
+    """Correctly abbreviates string"""
+    test_str = "test" * 10
+    assert reports.abbreviate_string(test_str, limit=4) == "test"
+
+
+def test_abbreviate_string_invalid():
+    """Input of invalid data type raises Type Error"""
+    invalid_types = [
+        34,
+        34.5,
+        {"data": "dictionary"},
+        [["col_name", 1], ["col_name2", 2]],
+        (("col_name", 3), ("col_name2", 4)),
+        np.array([1, 2, 3]),
+        TEST_DF,
+    ]
+    for invalid in invalid_types:
+        with pytest.raises(TypeError):
+            reports.abbreviate_string(invalid)
+
+
+def test_distribution_stats_valid():
+    """Valid data returns dictionary containing all statistics with numeric values"""
+    output = reports.distribution_stats(TEST_DF["NUMBER OF PERSONS INJURED"])
+    assert isinstance(output, dict)
+    stats = {
+        "count": 200,
+        "1%": 0,
+        "5%": 0.0,
+        "25%": 0.0,
+        "50%": 0.0,
+        "75%": 0.0,
+        "95%": 2.0,
+        "99%": 3.0,
+        "max": 8,
+        "median": 0.0,
+        "mean": 0.3,
+        "median absolute deviation": 0.0,
+        "standard deviation": 0.8082489292651694,
+        "skew": 5.341196456568395,
+    }
+    for key, val in stats.items():
+        assert key in output
+        assert isinstance(output[key], numbers.Number)
+        assert math.isclose(output[key], val)
+
+
+def test_distribution_stats_invalid():
+    """Invalid data type raises Type error"""
+    invalid_types = [
+        TEST_DF,
+        TEST_CAT_SERIES,
+        "data",
+        34,
+        34.5,
+        {"data": "dictionary"},
+        [["col_name", 1], ["col_name2", 2]],
+        (("col_name", 3), ("col_name2", 4)),
+        np.array([1, 2, 3]),
+    ]
+    for invalid in invalid_types:
+        with pytest.raises(TypeError):
+            reports.distribution_stats(invalid)
+
+
+def test_frequency_table_valid():
+    """Frequency gives expected output"""
+    d_index = ["0", "1", "2", "3", "8"]
+    expected_data = {
+        "Count": [160, 29, 7, 3, 1],
+        "% of Total": ["80.00%", "14.50%", "3.50%", "1.50%", "0.50%"],
+    }
+    expected_df = pd.DataFrame(expected_data, index=d_index)
+    table = reports.frequency_table(TEST_DF["NUMBER OF PERSONS INJURED"])
+    assert isinstance(table, pd.DataFrame)
+    assert expected_df.equals(table)
+
+
+def test_frequency_table_invalid():
+    """Invalid data type raises Type error"""
+    invalid_types = [
+        TEST_DF,
+        "data",
+        34,
+        34.5,
+        {"data": "dictionary"},
+        [["col_name", 1], ["col_name2", 2]],
+        (("col_name", 3), ("col_name2", 4)),
+        np.array([1, 2, 3]),
+    ]
+    for invalid in invalid_types:
+        with pytest.raises(TypeError):
+            reports.frequency_table(invalid)
+
+
+def test_DataFrameProfile_valid():
+    """Generated DataFrame profile should match test profile"""
+    compare_profile_name = "test_df_profile_name.txt"
+    compare_profile_no_name = "test_df_profile_no_name.txt"
+    compare_files = [
+        os.path.join(TEST_DATA_DIR, compare_profile_name),
+        os.path.join(TEST_DATA_DIR, compare_profile_no_name),
+    ]
+    names = ["test_name", ""]
+    with tempfile.TemporaryDirectory() as tmp:
+        for name, compare_file in zip(names, compare_files):
+            test_file = os.path.join(tmp, "temp.txt")
+            reports.DataFrameProfile(TEST_DF, name=name).save_report(test_file)
+            assert filecmp.cmp(compare_file, test_file, shallow=False)
+
+
+def test_DataFrameProfile_invalid():
+    """DataFrame profile should not accept invalid data types"""
+    invalid_types = [
+        TEST_CAT_SERIES,
+        TEST_NUM_SERIES,
+        "data",
+        34,
+        34.5,
+        {"data": "dictionary"},
+        [["col_name", 1], ["col_name2", 2]],
+        (("col_name", 3), ("col_name2", 4)),
+        np.array([1, 2, 3]),
+    ]
+    for invalid in invalid_types:
+        with pytest.raises(TypeError):
+            reports.DataFrameProfile(invalid)
+
+
+def test_SeriesProfile_valid_numerical():
+    """Generated Series profile for numerical data should match test profile"""
+    comparison_profile = "test_series_injured_profile.txt"
+    compare_file = os.path.join(TEST_DATA_DIR, comparison_profile)
+    with tempfile.TemporaryDirectory() as tmp:
+        test_file = os.path.join(tmp, "temp.txt")
+        reports.SeriesProfile(TEST_NUM_SERIES).save_report(test_file)
+        assert filecmp.cmp(compare_file, test_file, shallow=False)
+
+
+def test_SeriesProfile_valid_categorical():
+    """Generated Series profile for categorical should match test profile"""
+    comparison_profile = "test_series_borough_profile.txt"
+    compare_file = os.path.join(TEST_DATA_DIR, comparison_profile)
+    with tempfile.TemporaryDirectory() as tmp:
+        test_file = os.path.join(tmp, "temp.txt")
+        reports.SeriesProfile(TEST_CAT_SERIES).save_report(test_file)
+        assert filecmp.cmp(compare_file, test_file, shallow=False)
+
+
+def test_SeriesProfile_invalid():
+    """DataFrame profile should not accept invalid data types"""
+    invalid_types = [
+        TEST_DF,
+        "data",
+        34,
+        34.5,
+        {"data": "dictionary"},
+        [["col_name", 1], ["col_name2", 2]],
+        (("col_name", 3), ("col_name2", 4)),
+        np.array([1, 2, 3]),
+    ]
+    for invalid in invalid_types:
+        with pytest.raises(TypeError):
+            reports.SeriesProfile(invalid)
