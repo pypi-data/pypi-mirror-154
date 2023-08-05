38ef745332d45e24a1450aa9db6fabccede869e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:53 +0000

    Add package sgtpyutils/1.7.21/sgtpyutils-1.7.21.tar.gz

---
+++ packages/sgtpyutils/sgtpyutils-1.7.21.tar.gz/sgtpyutils-1.7.21/sgtpyutils/network/SimpleCaculateService/__init__.py
@@ -0,0 +1,27 @@
+import json
+from ..SimpleTcpService import SimpleTcpService
+from ...extensions import clazz
+
+
+class SimpleCaculateService(SimpleTcpService):
+    def handle_caculate(self, data: bytes):
+        results = {}
+        result = {}
+        try:
+            data = data.decode()
+            first_line = data.find('\n')
+            # have at least 2 lines
+            if first_line > -1 and data.find('\n', first_line+1) > -1:
+                exec(data, results)
+                keys = [x for x in results if not x.startswith('_')]
+                for x in keys:
+                    result[x] = results[x]
+                result = json.dumps(result)
+            else:
+                result = eval(data)
+        except Exception as e:
+            result = f'Error: {e}'
+        self.server.send(f'{result}\n'.encode())
+
+    def __init__(self, host: str = '127.0.0.1'):
+        return super().__init__(self.handle_caculate, host)
