3bb870120660db6124e1becefbae9d309302257f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:19 +0000

    Add package DeepPhysX.Torch/22.6/DeepPhysX.Torch-22.6.tar.gz

---
+++ packages/DeepPhysX.Torch/DeepPhysX.Torch-22.6.tar.gz/DeepPhysX.Torch-22.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from os.path import join
+
+PROJECT = 'DeepPhysX'
+PACKAGE = 'Torch'
+DEPENDENCIES = ['DeepPhysX', 'torch', 'psutil']
+
+# Configure packages and subpackages list and dependencies list
+packages = [f'{PROJECT}.{PACKAGE}']
+packages_dir = {f'{PROJECT}.{PACKAGE}': 'src'}
+for sub_package in find_packages(where='src'):
+    packages.append(f'{PROJECT}.{PACKAGE}.{sub_package}')
+    packages_dir[f'{PROJECT}.{PACKAGE}.{sub_package}'] = join('src', *sub_package.split('.'))
+
+# Extract README.md content
+with open('README.md') as f:
+    long_description = f.read()
+
+# Installation
+setup(name=f'{PROJECT}.{PACKAGE}',
+      version='22.06',
+      description='A PyTorch compatible package for DeepPhysX framework.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Mimesis',
+      author_email='robin.enjalbert@inria.fr',
+      url='https://github.com/mimesis-inria/DeepPhysX_Torch',
+      packages=packages,
+      package_dir=packages_dir,
+      install_requires=DEPENDENCIES)
