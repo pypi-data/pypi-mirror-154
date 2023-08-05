22c2527259ad35cbddb5ea355a450490e989e4b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:21 +0000

    Add package arya-api-framework/0.1.4/arya_api_framework-0.1.4-py3-none-any.whl

---
+++ packages/arya-api-framework/arya_api_framework-0.1.4-py3-none-any.whl/arya_api_framework/sync_framework/utils.py
@@ -0,0 +1,33 @@
+from os import PathLike
+from typing import Union
+from functools import wraps
+import time
+import logging
+
+try:
+    from ratelimit import RateLimitException
+except ImportError:
+    pass
+
+_log = logging.getLogger('arya_api_framework.Sync')
+
+
+def chunk_file_reader(file: Union[str, PathLike[str]]):
+    with open(file, 'rb') as f:
+        chunk = f.read(64 * 1024)
+
+        while chunk:
+            yield chunk
+            chunk = f.read(64 * 1024)
+
+
+def sleep_and_retry(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        while True:
+            try:
+                return func(*args, **kwargs)
+            except RateLimitException as exception:
+                _log.info(f"Applying rate limit: Sleeping for {exception.period_remaining}s")
+                time.sleep(exception.period_remaining)
+    return wrapper
