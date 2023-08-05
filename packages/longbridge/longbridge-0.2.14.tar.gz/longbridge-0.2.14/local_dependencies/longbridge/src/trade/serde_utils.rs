3214ed41dba99d4810092fbbc4b977cb58fcbe8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:09 +0000

    Add package longbridge/0.2.14/longbridge-0.2.14.tar.gz

---
+++ packages/longbridge/longbridge-0.2.14.tar.gz/longbridge-0.2.14/local_dependencies/longbridge/src/trade/serde_utils.rs
@@ -0,0 +1,244 @@
+#![allow(dead_code)]
+
+use std::{fmt::Display, str::FromStr};
+
+use serde::{de::Error, Deserialize, Deserializer, Serializer};
+use time::{Date, OffsetDateTime};
+
+pub(crate) mod timestamp {
+    use super::*;
+
+    pub(crate) fn deserialize<'de, D>(deserializer: D) -> Result<OffsetDateTime, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let value = String::deserialize(deserializer)?;
+        let value = value
+            .parse::<i64>()
+            .map_err(|_| Error::custom("invalid timestamp"))?;
+        OffsetDateTime::from_unix_timestamp(value).map_err(|_| Error::custom("invalid timestamp"))
+    }
+
+    pub(crate) fn serialize<S>(datetime: &OffsetDateTime, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&datetime.unix_timestamp().to_string())
+    }
+}
+
+pub(crate) mod timestamp_opt {
+    use super::*;
+
+    pub(crate) fn deserialize<'de, D>(deserializer: D) -> Result<Option<OffsetDateTime>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let value = String::deserialize(deserializer)?;
+        let value = value
+            .parse::<i64>()
+            .map_err(|_| Error::custom("invalid timestamp"))?;
+        if value != 0 {
+            let datetime = OffsetDateTime::from_unix_timestamp(value)
+                .map_err(|_| Error::custom("invalid timestamp"))?;
+            Ok(Some(datetime))
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub(crate) fn serialize<S>(
+        datetime: &Option<OffsetDateTime>,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match datetime {
+            Some(datetime) => serializer.serialize_str(&datetime.unix_timestamp().to_string()),
+            None => serializer.serialize_none(),
+        }
+    }
+}
+
+pub(crate) mod date_opt {
+    use super::*;
+
+    pub(crate) fn deserialize<'de, D>(deserializer: D) -> Result<Option<Date>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let value = String::deserialize(deserializer)?;
+        if !value.is_empty() {
+            let datetime = Date::parse(
+                &value,
+                time::macros::format_description!("[year]-[month]-[day]"),
+            )
+            .map_err(|_| Error::custom("invalid timestamp"))?;
+            Ok(Some(datetime))
+        } else {
+            Ok(None)
+        }
+    }
+
+    pub(crate) fn serialize<S>(datetime: &Option<Date>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match datetime {
+            Some(date) => serializer.serialize_str(
+                &date
+                    .format(time::macros::format_description!("[year]-[month]-[day]"))
+                    .unwrap(),
+            ),
+            None => serializer.serialize_none(),
+        }
+    }
+}
+
+pub(crate) mod number_str_opt {
+    use num_traits::Num;
+
+    use super::*;
+
+    pub(crate) fn serialize<S, T>(value: &Option<T>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+        T: Num + Display,
+    {
+        match value {
+            Some(value) => serializer.collect_str(value),
+            None => serializer.serialize_none(),
+        }
+    }
+
+    pub(crate) fn deserialize<'de, D, T>(deserializer: D) -> Result<Option<T>, D::Error>
+    where
+        D: Deserializer<'de>,
+        T: Num,
+        T::FromStrRadixErr: Display,
+    {
+        let value = String::deserialize(deserializer)?;
+        if !value.is_empty() {
+            let n = <T as Num>::from_str_radix(&value, 10)
+                .map_err(|err| D::Error::custom(err.to_string()))?;
+            Ok(Some(n))
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+pub(crate) mod decimal_empty_is_0 {
+    use rust_decimal::Decimal;
+
+    use super::*;
+
+    pub(crate) fn deserialize<'de, D>(deserializer: D) -> Result<Decimal, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let value = String::deserialize(deserializer)?;
+        if !value.is_empty() {
+            let n = value
+                .parse::<Decimal>()
+                .map_err(|err| Error::custom(err.to_string()))?;
+            Ok(n)
+        } else {
+            Ok(Decimal::ZERO)
+        }
+    }
+}
+
+pub(crate) mod decimal_opt_empty_is_none {
+    use rust_decimal::Decimal;
+
+    use super::*;
+
+    pub(crate) fn deserialize<'de, D>(deserializer: D) -> Result<Option<Decimal>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let value = String::deserialize(deserializer)?;
+        if !value.is_empty() {
+            let n = value
+                .parse::<Decimal>()
+                .map_err(|err| Error::custom(err.to_string()))?;
+            Ok(Some(n))
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+pub(crate) mod decimal_opt_0_is_none {
+    use rust_decimal::Decimal;
+
+    use super::*;
+
+    pub(crate) fn deserialize<'de, D>(deserializer: D) -> Result<Option<Decimal>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let value = String::deserialize(deserializer)?;
+        let n = value
+            .parse::<Decimal>()
+            .map_err(|err| Error::custom(err.to_string()))?;
+        if !n.is_zero() {
+            Ok(Some(n))
+        } else {
+            Ok(None)
+        }
+    }
+}
+
+pub(crate) mod trigger_status {
+    use super::*;
+    use crate::trade::TriggerStatus;
+
+    pub(crate) fn deserialize<'de, D>(deserializer: D) -> Result<Option<TriggerStatus>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let value = String::deserialize(deserializer)?;
+        match value.as_str() {
+            "NOT_USED" => Ok(None),
+            _ => Ok(Some(
+                TriggerStatus::from_str(value.as_str()).unwrap_or_default(),
+            )),
+        }
+    }
+}
+
+pub(crate) mod outside_rth {
+    use super::*;
+    use crate::trade::OutsideRTH;
+
+    pub(crate) fn deserialize<'de, D>(deserializer: D) -> Result<Option<OutsideRTH>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let value = String::deserialize(deserializer)?;
+        match value.as_str() {
+            "UnknownOutsideRth" => Ok(None),
+            _ => Ok(Some(
+                OutsideRTH::from_str(value.as_str()).unwrap_or_default(),
+            )),
+        }
+    }
+}
+
+pub(crate) mod cash_flow_symbol {
+    use super::*;
+
+    pub(crate) fn deserialize<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let value = String::deserialize(deserializer)?;
+        match value.as_str() {
+            "." => Ok(None),
+            _ => Ok(Some(value)),
+        }
+    }
+}
---
+++ packages/longbridge/longbridge-0.2.14.tar.gz/longbridge-0.2.14/src/quote/context.rs
@@ -0,0 +1,260 @@
+use std::sync::Arc;
+
+use longbridge::blocking::QuoteContextSync;
+use pyo3::prelude::*;
+
+use crate::{
+    config::Config,
+    error::ErrorNewType,
+    quote::{
+        push::handle_push_event,
+        types::{
+            AdjustType, Candlestick, IntradayLine, IssuerInfo, MarketTradingDays,
+            MarketTradingSession, OptionQuote, ParticipantInfo, Period, RealtimeQuote,
+            SecurityBrokers, SecurityDepth, SecurityQuote, SecurityStaticInfo, StrikePriceInfo,
+            SubType, SubTypes, Subscription, Trade, WarrantQuote,
+        },
+    },
+    time::PyDateWrapper,
+    types::Market,
+};
+
+#[pyclass]
+pub(crate) struct QuoteContext(QuoteContextSync);
+
+#[pymethods]
+impl QuoteContext {
+    #[new]
+    fn new(config: &Config, handler: Option<PyObject>) -> PyResult<Self> {
+        let ctx = QuoteContextSync::try_new(Arc::new(config.0.clone()), move |event| {
+            if let Some(handler) = &handler {
+                handle_push_event(handler, event);
+            }
+        })
+        .map_err(ErrorNewType)?;
+        Ok(Self(ctx))
+    }
+
+    /// Subscribe
+    #[args(is_first_push = false)]
+    fn subscribe(
+        &self,
+        symbols: Vec<String>,
+        sub_types: Vec<SubType>,
+        is_first_push: bool,
+    ) -> PyResult<()> {
+        self.0
+            .subscribe(symbols, SubTypes(sub_types), is_first_push)
+            .map_err(ErrorNewType)?;
+        Ok(())
+    }
+
+    /// Unsubscribe
+    fn unsubscribe(&self, symbols: Vec<String>, sub_types: Vec<SubType>) -> PyResult<()> {
+        self.0
+            .unsubscribe(symbols, SubTypes(sub_types))
+            .map_err(ErrorNewType)?;
+        Ok(())
+    }
+
+    /// Get subscription information
+    fn subscriptions(&self) -> PyResult<Vec<Subscription>> {
+        self.0
+            .subscriptions()
+            .map_err(ErrorNewType)?
+            .into_iter()
+            .map(TryInto::try_into)
+            .collect()
+    }
+
+    /// Get basic information of securities
+    fn static_info(&self, symbols: Vec<String>) -> PyResult<Vec<SecurityStaticInfo>> {
+        self.0
+            .static_info(symbols)
+            .map_err(ErrorNewType)?
+            .into_iter()
+            .map(TryInto::try_into)
+            .collect()
+    }
+
+    /// Get quote of securities
+    fn quote(&self, symbols: Vec<String>) -> PyResult<Vec<SecurityQuote>> {
+        self.0
+            .quote(symbols)
+            .map_err(ErrorNewType)?
+            .into_iter()
+            .map(TryInto::try_into)
+            .collect()
+    }
+
+    /// Get quote of option securities
+    fn option_quote(&self, symbols: Vec<String>) -> PyResult<Vec<OptionQuote>> {
+        self.0
+            .option_quote(symbols)
+            .map_err(ErrorNewType)?
+            .into_iter()
+            .map(TryInto::try_into)
+            .collect()
+    }
+
+    /// Get quote of warrant securities
+    fn warrant_quote(&self, symbols: Vec<String>) -> PyResult<Vec<WarrantQuote>> {
+        self.0
+            .warrant_quote(symbols)
+            .map_err(ErrorNewType)?
+            .into_iter()
+            .map(TryInto::try_into)
+            .collect()
+    }
+
+    /// Get security depth
+    fn depth(&self, symbol: String) -> PyResult<SecurityDepth> {
+        self.0.depth(symbol).map_err(ErrorNewType)?.try_into()
+    }
+
+    /// Get security brokers
+    fn brokers(&self, symbol: String) -> PyResult<SecurityBrokers> {
+        self.0.brokers(symbol).map_err(ErrorNewType)?.try_into()
+    }
+
+    /// Get participants
+    fn participants(&self) -> PyResult<Vec<ParticipantInfo>> {
+        self.0
+            .participants()
+            .map_err(ErrorNewType)?
+            .into_iter()
+            .map(TryInto::try_into)
+            .collect()
+    }
+
+    /// Get security trades
+    fn trades(&self, symbol: String, count: usize) -> PyResult<Vec<Trade>> {
+        self.0
+            .trades(symbol, count)
+            .map_err(ErrorNewType)?
+            .into_iter()
+            .map(TryInto::try_into)
+            .collect()
+    }
+
+    /// Get security intraday
+    fn intraday(&self, symbol: String) -> PyResult<Vec<IntradayLine>> {
+        self.0
+            .intraday(symbol)
+            .map_err(ErrorNewType)?
+            .into_iter()
+            .map(TryInto::try_into)
+            .collect()
+    }
+
+    /// Get security candlesticks
+    fn candlesticks(
+        &self,
+        symbol: String,
+        period: Period,
+        count: usize,
+        adjust_type: AdjustType,
+    ) -> PyResult<Vec<Candlestick>> {
+        self.0
+            .candlesticks(symbol, period.into(), count, adjust_type.into())
+            .map_err(ErrorNewType)?
+            .into_iter()
+            .map(TryInto::try_into)
+            .collect()
+    }
+
+    /// Get option chain expiry date list
+    fn option_chain_expiry_date_list(&self, symbol: String) -> PyResult<Vec<PyDateWrapper>> {
+        Ok(self
+            .0
+            .option_chain_expiry_date_list(symbol)
+            .map_err(ErrorNewType)?
+            .into_iter()
+            .map(Into::into)
+            .collect())
+    }
+
+    /// Get option chain info by date
+    fn option_chain_info_by_date(
+        &self,
+        symbol: String,
+        expiry_date: PyDateWrapper,
+    ) -> PyResult<Vec<StrikePriceInfo>> {
+        self.0
+            .option_chain_info_by_date(symbol, expiry_date.0)
+            .map_err(ErrorNewType)?
+            .into_iter()
+            .map(TryInto::try_into)
+            .collect()
+    }
+
+    /// Get warrant issuers
+    fn warrant_issuers(&self) -> PyResult<Vec<IssuerInfo>> {
+        self.0
+            .warrant_issuers()
+            .map_err(ErrorNewType)?
+            .into_iter()
+            .map(TryInto::try_into)
+            .collect()
+    }
+
+    /// Get trading session of the day
+    fn trading_session(&self) -> PyResult<Vec<MarketTradingSession>> {
+        self.0
+            .trading_session()
+            .map_err(ErrorNewType)?
+            .into_iter()
+            .map(TryInto::try_into)
+            .collect()
+    }
+
+    /// Get trading session of the day
+    fn trading_days(
+        &self,
+        market: Market,
+        begin: PyDateWrapper,
+        end: PyDateWrapper,
+    ) -> PyResult<MarketTradingDays> {
+        self.0
+            .trading_days(market.into(), begin.0, end.0)
+            .map_err(ErrorNewType)?
+            .try_into()
+    }
+
+    /// Get real-time quote
+    fn realtime_quote(&self, symbols: Vec<String>) -> PyResult<Vec<RealtimeQuote>> {
+        self.0
+            .realtime_quote(symbols)
+            .map_err(ErrorNewType)?
+            .into_iter()
+            .map(TryInto::try_into)
+            .collect()
+    }
+
+    /// Get real-time depth
+    fn realtime_depth(&self, symbol: String) -> PyResult<SecurityDepth> {
+        self.0
+            .realtime_depth(symbol)
+            .map_err(ErrorNewType)?
+            .try_into()
+    }
+
+    /// Get real-time brokers
+    fn realtime_brokers(&self, symbol: String) -> PyResult<SecurityBrokers> {
+        self.0
+            .realtime_brokers(symbol)
+            .map_err(ErrorNewType)?
+            .try_into()
+    }
+
+    /// Get real-time trades
+    #[args(count = 500)]
+    fn realtime_trades(&self, symbol: String, count: usize) -> PyResult<Vec<Trade>> {
+        self.0
+            .realtime_trades(symbol, count)
+            .map_err(ErrorNewType)?
+            .into_iter()
+            .map(TryInto::try_into)
+            .collect()
+    }
+}
