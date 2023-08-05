0aa538e5ea44c4a86bff371f40559ed0e4dd367a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:19:16 +0000

    Add package linora/1.4.0/linora-1.4.0.tar.gz

---
+++ packages/linora/linora-1.4.0.tar.gz/linora-1.4.0/linora/param_search/__init__.py
@@ -0,0 +1,13 @@
+# from linora.param_search import GEClassifier
+# from linora.param_search import GERegressor
+from linora.param_search import LGBMClassifier
+from linora.param_search import LGBMRegressor
+from linora.param_search import XGBClassifier
+from linora.param_search import XGBRegressor
+# from linora.param_search import XGBRanker
+
+from linora.param_search._HyperParameters import *
+from linora.param_search._Search import *
+
+from linora.param_search._config import __xgboost_version__, __lightgbm_version__
+
