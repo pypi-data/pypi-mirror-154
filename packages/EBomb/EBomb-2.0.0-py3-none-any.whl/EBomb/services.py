f21690f726dbae8ab3a64c1df56b0215607eb38f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:19:23 +0000

    Add package EBomb/2.0.0/EBomb-2.0.0-py3-none-any.whl

---
+++ packages/EBomb/EBomb-2.0.0-py3-none-any.whl/EBomb/services.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+import json
+import pathlib
+import urllib.request
+from http.client import HTTPResponse
+from urllib.parse import urlparse
+
+import EasyProxies
+import spys.me
+from requests import Response, request
+
+from EBomb.ua import random_ua
+
+__all__ = ('Service', 'services', 'JSON_DB_FILE_PATH')
+
+JSON_DB_FILE_PATH = pathlib.Path(__file__).parent / 'services.json'
+
+
+class Service:
+    __slots__ = ('url', 'method', 'netloc')
+
+    def __init__(self, url: str, method: str):
+        self.netloc = urlparse(url).netloc
+        self.url = url
+        self.method = method.upper()
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__}({self.netloc!r}, {self.method!r})>'
+
+    def request(self, mail: str, proxies: EasyProxies.ProxyDescriptor | spys.me.BaseProxyView = None) -> Response:
+        if isinstance(proxies, EasyProxies.ProxyDescriptor):
+            proxies = proxies.as_requests_proxy if proxies else None
+        else:
+            proxies = f'socks5://{proxies}'
+            proxies = {'http': proxies, 'https': proxies}
+
+        for ua_n in ('user-agent', 'User-Agent'):
+            resp = request(self.method, self.url.replace('%s', mail), headers={ua_n: random_ua()},
+                           proxies=proxies, timeout=10)
+            if resp.status_code == 403:
+                continue
+            break
+        return resp
+
+
+def _get_services_from_json():
+    if not (JSON_DB_FILE_PATH.is_file() and JSON_DB_FILE_PATH.stat().st_size):
+        download: HTTPResponse
+        with urllib.request.urlopen('https://github.com/NIKDISSV-Forever/EBomb/blob/main/EBomb/services.json'
+                                    '?raw=true') as download:
+            JSON_DB_FILE_PATH.write_bytes(download.read())
+    with open(JSON_DB_FILE_PATH, 'rb') as to_load:
+        return json.load(to_load)
+
+
+services = [Service(**serv) for serv in _get_services_from_json()]
