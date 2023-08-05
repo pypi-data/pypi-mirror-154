b66f41b56472ce3059a853d6cc08d0b5ea1c1660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:17 +0000

    Add package az-creds/0.0.4/az_creds-0.0.4.tar.gz

---
+++ packages/az-creds/az_creds-0.0.4.tar.gz/az_creds-0.0.4/README.md
@@ -0,0 +1,24 @@
+# FETCH_AKS_CREDENTIALS Project
+
+This is a program which is responsible to fetch the AKS credentials using azure-cli.
+
+# Installing the package
+
+```
+pip install az_creds
+```
+
+# Dependency
+
+```
+azure-cli       # To Install Azure CLI
+```
+
+# Usage:
+
+Open your terminal and run the following command:
+
+```
+az_creds
+```
+
---
+++ packages/az-creds/az_creds-0.0.4.tar.gz/az_creds-0.0.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Fetch credentials of aks using azure-cli'
+LONG_DESCRIPTION = 'A package that allows to fetch credentials of aks using azure-cli'
+
+# Setting up
+setup(
+    name='az_creds',
+    version=VERSION,
+    license='GNU',
+    author='Jilani Sayyad',
+    author_email='sayyedjilani88@gmail.com',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jilanisayyad/fetch_credentials',
+    keywords=['get_creds', 'fetch_credentials', 'azure', 'aks', 'azure-cli'],
+    install_requires=[
+        'simple-term-menu',
+        'pyyaml'
+    ],
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'az_creds=fetch_app.credentials:main'
+        ]
+    }
+)
