131f5794e36d131c10f42b5b6c2f16469264c808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:45 +0000

    Add package macq/0.2.6/macq-0.2.6-py3-none-any.whl

---
+++ packages/macq/macq-0.2.6-py3-none-any.whl/macq/extract/extract.py
@@ -0,0 +1,70 @@
+""".. include:: ../../docs/templates/extract/extract.md"""
+
+from dataclasses import dataclass
+from enum import Enum, auto
+from ..trace import Action, State
+from ..observation import ObservationLists
+from .model import Model
+from .observer import Observer
+from .slaf import SLAF
+from .amdn import AMDN
+from .arms import ARMS
+
+
+@dataclass
+class SAS:
+    pre_state: State
+    action: Action
+    post_state: State
+
+
+class modes(Enum):
+    """Model extraction techniques.
+
+    An Enum where each value represents a model extraction technique.
+    """
+
+    OBSERVER = auto()
+    SLAF = auto()
+    AMDN = auto()
+    ARMS = auto()
+
+
+class Extract:
+    """Extracts models from observations.
+
+    The Extract class uses an extraction method to retrieve an action Model
+    from state observations.
+    """
+
+    def __new__(
+        cls, obs_lists: ObservationLists, mode: modes, debug: bool = False, **kwargs
+    ) -> Model:
+        """Extracts a Model object.
+
+        Extracts a model from the observations using the specified extraction
+        technique.
+
+        Args:
+            obs_lists (ObservationList):
+                The state observations to extract the model from.
+            mode (Enum):
+                The extraction technique to use.
+            **kwargs: (keyword arguments)
+                Any extra arguments to supply to the extraction technique.
+
+        Returns:
+            A Model object. The model's characteristics are determined by the
+            extraction technique used.
+        """
+        techniques = {
+            modes.OBSERVER: Observer,
+            modes.SLAF: SLAF,
+            modes.AMDN: AMDN,
+            modes.ARMS: ARMS,
+        }
+        if mode == modes.SLAF:
+            if len(obs_lists) != 1:
+                raise Exception("The SLAF extraction technique only takes one trace.")
+
+        return techniques[mode](obs_lists, debug, **kwargs)
---
+++ packages/macq/macq-0.2.6-py3-none-any.whl/macq/extract/learned_action.py
@@ -0,0 +1,98 @@
+from __future__ import annotations
+from typing import List, Set
+
+
+class LearnedAction:
+    def __init__(self, name: str, obj_params: List[str], **kwargs):
+        self.name = name
+        self.obj_params = obj_params
+        if "cost" in kwargs:
+            self.cost = kwargs["cost"]
+
+        self.precond = set() if "precond" not in kwargs else kwargs["precond"]
+        self.add = set() if "add" not in kwargs else kwargs["add"]
+        self.delete = set() if "delete" not in kwargs else kwargs["delete"]
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, LearnedAction)
+            and self.name == other.name
+            and self.obj_params == other.obj_params
+        )
+
+    def __hash__(self):
+        # Order of obj_params is important!
+        return hash(self.details())
+
+    def details(self):
+        # obj_params can be either a list of strings or a list of PlanningObject depending on the token type and extraction method used to learn the action
+        try:
+            string = f"({self.name} {' '.join(self.obj_params)})"
+        except TypeError:
+            string = f"({self.name} {' '.join([o.details() for o in self.obj_params])})"
+
+        return string
+
+    def update_precond(self, fluents: Set[str]):
+        """Adds preconditions to the action.
+
+        Args:
+            fluents (set):
+                The set of fluents to be added to the action's preconditions.
+        """
+        self.precond.update(fluents)
+
+    def update_add(self, fluents: Set[str]):
+        """Adds add effects to the action.
+
+        Args:
+            fluents (set):
+                The set of fluents to be added to the action's add effects.
+        """
+        self.add.update(fluents)
+
+    def update_delete(self, fluents: Set[str]):
+        """Adds delete effects to the action.
+
+        Args:
+            fluents (set):
+                The set of fluents to be added to the action's delete effects.
+        """
+        self.delete.update(fluents)
+
+    def clear(self):
+        self.precond = set()
+        self.add = set()
+        self.delete = set()
+
+    def compare(self, orig_action: LearnedAction):
+        """Compares the learned action to an original, ground truth action."""
+        precond_diff = orig_action.precond.difference(self.precond)
+        add_diff = orig_action.add.difference(self.add)
+        delete_diff = orig_action.delete.difference(self.delete)
+        return precond_diff, add_diff, delete_diff
+
+    def _serialize(self):
+        return dict(
+            name=self.name,
+            obj_params=self.obj_params,
+            cost=self.cost,
+            precond=list(self.precond),
+            add=list(self.add),
+            delete=list(self.delete),
+        )
+
+    @classmethod
+    def _deserialize(cls, data):
+        """Converts a json object to an Action."""
+        precond = set(data["precond"])
+        add = set(data["add"])
+        delete = set(data["delete"])
+        return cls(
+            data["name"],
+            data["obj_params"],
+            cost=data["cost"],
+            precond=precond,
+            add=add,
+            delete=delete,
+        )
