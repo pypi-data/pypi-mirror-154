39d2b50211b0c48884c97409110e988801923995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:23 +0000

    Add package sinta-scraper/1.3.0/sinta_scraper-1.3.0-py3-none-any.whl

---
+++ packages/sinta-scraper/sinta_scraper-1.3.0-py3-none-any.whl/utils/utils.py
@@ -0,0 +1,40 @@
+import json
+
+import pandas as pd
+from dict2xml import dict2xml
+from string_utils.validation import is_integer, is_decimal
+
+
+def format_output(obj, output_format):
+    if output_format == 'json':
+        return json.dumps(obj)
+    elif output_format == 'json-pretty':
+        return json.dumps(obj, indent=4)
+    elif output_format == 'xml':
+        return dict2xml(obj, wrap='author')
+    elif output_format == 'xml-pretty':
+        return dict2xml(obj, wrap='author', indent='    ')
+    elif output_format == 'dataframe':
+        return pd.DataFrame.from_dict(obj)
+    else:
+        return obj
+
+
+def cast(string: str):
+    if string is None:
+        return None
+
+    string = string.strip()
+
+    if is_integer(string):
+        return int(string)
+    elif is_decimal(string):
+        return float(string)
+    elif string == '-':
+        return None
+    else:
+        return string
+
+
+def listify(param) -> list:
+    return [param] if type(param) not in [list, tuple] else param
