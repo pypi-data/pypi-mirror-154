522608ec5865c53f60e7f8db95fcbcd68a23ad20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:13 +0000

    Add package sinta-scraper/1.2.1/sinta-scraper-1.2.1.tar.gz

---
+++ packages/sinta-scraper/sinta-scraper-1.2.1.tar.gz/sinta-scraper-1.2.1/sinta/author_scraper.py
@@ -0,0 +1,89 @@
+import re
+from concurrent.futures import ThreadPoolExecutor
+
+from bs4 import BeautifulSoup
+from requests import get
+
+import utils
+from utils.config import get_config
+
+
+def author(author_ids, output_format='dictionary'):
+    if type(author_ids) is not list and type(author_ids) is not tuple:
+        author_ids = [author_ids]
+
+    worker_result = []
+
+    with ThreadPoolExecutor() as executor:
+        for author_id in author_ids:
+            executor.submit(worker, author_id, worker_result)
+
+    if len(worker_result) == 1:
+        worker_result = worker_result[0]
+
+    return utils.format_output(worker_result, output_format)
+
+
+def worker(author_id, worker_result):
+    domain = get_config()['domain']
+    url = f'{domain}/authors/detail?id={author_id}&view=overview'
+    html = get(url)
+    soup = BeautifulSoup(html.content, 'html.parser')
+
+    name = soup.select('.au-name')[0].text.strip()
+    areas = [area.text.strip() for area in soup.select('.area-item')]
+    scores_soup = soup.select('.stat2-val')
+
+    index_score_names = ['documents', 'citations', 'h-index', 'i10-index', 'g-index']
+    index_scores = soup.select('.stat-num-pub')
+
+    scopus = {index_score_names[i]: utils.cast(index_scores[i + 16].text) for i in range(len(index_score_names))}
+    scopus_outputs_names = ['articles', 'conferences', 'others']
+    scopus_outputs = {scopus_outputs_names[i]: utils.cast(soup.select('.stat-num-pub')[i].text) for i in range(3)}
+    scopus_quartiles = {f'Q{i}': utils.cast(soup.select('.stat-num-pub')[i + 3].text) for i in range(1, 5)}
+    scopus = {**scopus, **scopus_outputs, **scopus_quartiles}
+    scopus['undefined'] = utils.cast(soup.select('.stat-num-pub')[8].text)
+
+    scholar = {index_score_names[i]: utils.cast(index_scores[i + 21].text) for i in range(len(index_score_names))}
+    wos = {index_score_names[i]: utils.cast(index_scores[i + 26].text) for i in range(len(index_score_names))}
+
+    sinta = {f'S{i}': utils.cast(index_scores[i + 9].text) for i in range(6)}
+    sinta['uncategorized'] = utils.cast(index_scores[15].text)
+
+    score_names = ['overall', '3_years', 'overall_v2', '3_years_v2']
+    scores = {score_name: float(scores_soup[i].text) for i, score_name in enumerate(score_names)}
+
+    books = utils.cast(scores_soup[4].text)
+    ipr = utils.cast(scores_soup[7].text)
+
+    rank_names = ['national', '3_years_national', 'ipr', 'affiliation', '3_years_affiliation']
+    ranks = {rank_names[i]: utils.cast(scores_soup[i + 5].text) for i in [0, 1, 3, 4]}
+
+    affil = soup.select('.au-affil > a')
+    dept = soup.select('.au-department')[0].text.strip()
+    affil_name = affil[0].text.strip()
+    affil_url = 'http://sinta.ristekbrin.go.id' + affil[0]['href']
+    affil_id = re.search(r'id=(\d+)', affil_url).group(1)
+
+    result_data = {
+        'id': author_id,
+        'name': name,
+        'url': url,
+        'affiliation': {
+            'id': affil_id,
+            'name': affil_name,
+            'url': affil_url
+        },
+        'department': dept,
+        'areas': areas,
+        'score': scores,
+        'rank': ranks,
+        'scopus': scopus,
+        'scholar': scholar,
+        'wos': wos,
+        'sinta': sinta,
+        'books': books,
+        'ipr': ipr
+    }
+
+    worker_result.append(result_data)
---
+++ packages/sinta-scraper/sinta-scraper-1.2.1.tar.gz/sinta-scraper-1.2.1/sinta/scopus_documents_scraper.py
@@ -0,0 +1,111 @@
+import re
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+
+from bs4 import BeautifulSoup
+from requests import get
+from string_utils.validation import is_integer
+
+from sinta.dept_scraper import dept_authors
+from utils.config import get_config
+from utils.utils import cast, format_output, listify
+
+
+def author_scopus(author_id, output_format='dictionary', min_date=None, max_date=None):
+    domain = get_config()['domain']
+    url = f'{domain}/authors/detail?id={author_id}&view=documentsscopus'
+    html = get(url)
+    soup = BeautifulSoup(html.content, 'html.parser')
+    page_info = soup.select('.uk-width-large-1-2.table-footer')
+    n_page = cast(page_info[0].text.strip().split()[3])
+    worker_result = parse(soup)
+    date_format = '%Y-%m-%d'
+
+    with ThreadPoolExecutor() as executor:
+        for page in range(2, n_page + 1):
+            executor.submit(worker, author_id, page, worker_result)
+
+    if min_date is not None:
+        min_date_parsed = datetime.strptime(min_date, date_format)
+        worker_result = [doc for doc in worker_result if datetime.strptime(doc['date'], date_format) >= min_date_parsed]
+
+    if max_date is not None:
+        max_date_parsed = datetime.strptime(max_date, date_format)
+        worker_result = [doc for doc in worker_result if datetime.strptime(doc['date'], date_format) <= max_date_parsed]
+
+    return format_output(worker_result, output_format)
+
+
+def author_scopus_journal(author_id, output_format='dictionary', min_date=None, max_date=None):
+    docs = author_scopus(author_id, min_date=min_date, max_date=max_date)
+    journal_docs = [doc for doc in docs if doc['type'] == 'Journal']
+
+    return format_output(journal_docs, output_format)
+
+
+def author_scopus_conference(author_id, output_format='dictionary', min_date=None, max_date=None):
+    docs = author_scopus(author_id, min_date=min_date, max_date=max_date)
+    journal_docs = [doc for doc in docs if doc['type'].startswith('Conference')]
+
+    return format_output(journal_docs, output_format)
+
+
+def worker(author_id, page, worker_result):
+    domain = get_config()['domain']
+    url = f'{domain}/authors/detail?page={page}&id={author_id}&view=documentsscopus'
+    html = get(url)
+    soup = BeautifulSoup(html.content, 'html.parser')
+    data = parse(soup)
+
+    worker_result.extend(data)
+
+
+def parse(soup):
+    rows = soup.select('table.uk-table tr')
+    result = []
+
+    for row in rows:
+        link = row.select('a.paper-link')
+
+        if not link:
+            continue
+
+        link = link[0]
+        info1 = row.select('.index-val')
+        quartile = info1[0].text.strip()
+        citations = info1[1].text.strip()
+        info2 = row.select('dd.indexed-by')[0].text.strip().split('|')
+
+        result.append({
+            'title': link.text,
+            'url': link['href'],
+            'publisher': info2[0].strip(),
+            'date': info2[3].strip(),
+            'type': info2[4].strip(),
+            'quartile': cast(quartile[1]) if re.search(r'^Q[1-4]{1}$', quartile) else '-',
+            'citations': cast(citations) if is_integer(citations) else 0
+        })
+
+    return result
+
+
+def dept_scopus(dept_ids, affil_id, output_format='dictionary', max_workers=None):
+    dept_ids = listify(dept_ids)
+
+    authors = []
+    worker_result = []
+
+    for dept_id in dept_ids:
+        authors.extend(dept_authors(dept_id, affil_id))
+
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        for author in authors:
+            executor.submit(dept_scopus_docs_worker, author['id'], worker_result)
+
+    return format_output(worker_result, output_format)
+
+
+def dept_scopus_docs_worker(author_id, worker_result):
+    researches = author_scopus(author_id)
+
+    worker_result.extend(researches)
