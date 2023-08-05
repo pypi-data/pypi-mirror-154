f4d506cc51bcd69d4870e237d62a3e191a489c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:08 +0000

    Add package py2graphql/0.17.0/py2graphql-0.17.0.tar.gz

---
+++ packages/py2graphql/py2graphql-0.17.0.tar.gz/py2graphql-0.17.0/py2graphql/__init__.py
@@ -0,0 +1,17 @@
+from .core import (
+    Client,
+    Mutation,
+    Query,
+)
+from .exception import (
+    GraphQLError,
+    GraphQLEndpointError,
+    InfinityNotSupportedError,
+    UnserializableTypeError,
+    ValuesRequiresArgumentsError,
+)
+from .types import (
+    Aliased,
+    Literal,
+    Variable,
+)
