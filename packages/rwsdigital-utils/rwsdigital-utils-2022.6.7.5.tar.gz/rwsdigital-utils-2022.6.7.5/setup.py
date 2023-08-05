222d739868a05be5f504cf45faa6eb3df8d312a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:39 +0000

    Add package rwsdigital-utils/2022.6.7.5/rwsdigital-utils-2022.6.7.5.tar.gz

---
+++ packages/rwsdigital-utils/rwsdigital-utils-2022.6.7.5.tar.gz/rwsdigital-utils-2022.6.7.5/setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+import os
+import setuptools
+
+setuptools.setup(
+    name="rwsdigital-utils",
+    version="2022.6.7.5",
+    description="Odoo Development Utils by RWS Digital",
+    readme="README.md",
+    author="Salvatore Castaldo",
+    author_email = "salvatore@rwsdigital.com",
+    license="MIT License",
+    url = "https://github.com/sCast17/rwsdigital_utils",
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    )
