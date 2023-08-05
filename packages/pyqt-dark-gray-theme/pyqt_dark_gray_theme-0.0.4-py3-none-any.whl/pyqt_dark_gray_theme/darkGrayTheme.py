82568877cff4ce76ebc265b7324070e9f642393f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:09 +0000

    Add package pyqt-dark-gray-theme/0.0.4/pyqt_dark_gray_theme-0.0.4-py3-none-any.whl

---
+++ packages/pyqt-dark-gray-theme/pyqt_dark_gray_theme-0.0.4-py3-none-any.whl/pyqt_dark_gray_theme/darkGrayTheme.py
@@ -0,0 +1,23 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMenuBar, QToolButton
+
+from qt_sass_theme.qtSassTheme import QtSassTheme
+
+t = QtSassTheme()
+
+def getThemeStyle() -> str:
+    return t.getThemeStyle()
+
+def getIconButtonStyle() -> str:
+    return t.getIconButtonStyle()
+
+def getIconTextButtonStyle() -> str:
+    return t.getIconTextButtonStyle()
+
+def getMenuBarStyle(menu_bar: QMenuBar) -> str:
+    tool_button = menu_bar.findChild(QToolButton)
+    tool_button.setArrowType(Qt.RightArrow)
+    return t.getMenuBarStyle()
+
+def getMainWidgetStyle() -> str:
+    return t.getMainWidgetStyle()
