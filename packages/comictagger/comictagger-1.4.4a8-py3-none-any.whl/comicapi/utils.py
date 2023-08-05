9fa97788a7bd86dd6137a1e8463a11381aed52fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:47 +0000

    Add package comictagger/1.4.4a8/comictagger-1.4.4a8-py3-none-any.whl

---
+++ packages/comictagger/comictagger-1.4.4a8-py3-none-any.whl/comicapi/utils.py
@@ -0,0 +1,245 @@
+"""Some generic utilities"""
+# Copyright 2012-2014 Anthony Beville
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import annotations
+
+import json
+import logging
+import os
+import pathlib
+import re
+import unicodedata
+from collections import defaultdict
+from shutil import which  # noqa: F401
+from typing import Any, Mapping
+
+import pycountry
+
+logger = logging.getLogger(__name__)
+
+
+class UtilsVars:
+    already_fixed_encoding = False
+
+
+def get_recursive_filelist(pathlist: list[str]) -> list[str]:
+    """Get a recursive list of of all files under all path items in the list"""
+
+    filelist = []
+    for p in pathlist:
+        # if path is a folder, walk it recursively, and all files underneath
+        if not isinstance(p, str):
+            # it's probably a QString
+            p = str(p)
+
+        if os.path.isdir(p):
+            for root, _, files in os.walk(p):
+                for f in files:
+                    if not isinstance(f, str):
+                        # it's probably a QString
+                        f = str(f)
+                    filelist.append(os.path.join(root, f))
+        else:
+            filelist.append(p)
+
+    return filelist
+
+
+def list_to_string(lst: list[str | Any]) -> str:
+    string = ""
+    if lst is not None:
+        for item in lst:
+            if len(string) > 0:
+                string += ", "
+            string += item
+    return string
+
+
+def add_to_path(dirname: str) -> None:
+    if dirname is not None and dirname != "":
+
+        # verify that path doesn't already contain the given dirname
+        tmpdirname = re.escape(dirname)
+        pattern = r"(^|{sep}){dir}({sep}|$)".format(dir=tmpdirname, sep=os.pathsep)
+
+        match = re.search(pattern, os.environ["PATH"])
+        if not match:
+            os.environ["PATH"] = dirname + os.pathsep + os.environ["PATH"]
+
+
+def xlate(data: Any, is_int: bool = False) -> Any:
+    if data is None or data == "":
+        return None
+    if is_int:
+        i = str(data).translate(defaultdict(lambda: None, zip((ord(c) for c in "1234567890"), "1234567890")))
+        if i == "0":
+            return "0"
+        if i == "":
+            return None
+        return int(i)
+
+    return str(data)
+
+
+def remove_articles(text: str) -> str:
+    text = text.lower()
+    articles = [
+        "&",
+        "a",
+        "am",
+        "an",
+        "and",
+        "as",
+        "at",
+        "be",
+        "but",
+        "by",
+        "for",
+        "if",
+        "is",
+        "issue",
+        "it",
+        "it's",
+        "its",
+        "itself",
+        "of",
+        "or",
+        "so",
+        "the",
+        "the",
+        "with",
+    ]
+    new_text = ""
+    for word in text.split(" "):
+        if word not in articles:
+            new_text += word + " "
+
+    new_text = new_text[:-1]
+
+    return new_text
+
+
+def sanitize_title(text: str, basic: bool = False) -> str:
+    # normalize unicode and convert to ascii. Does not work for everything eg ½ to 1⁄2 not 1/2
+    text = unicodedata.normalize("NFKD", text)
+    # comicvine keeps apostrophes a part of the word
+    text = text.replace("'", "")
+    text = text.replace('"', "")
+    if not basic:
+        # comicvine ignores punctuation and accents, TODO: only remove punctuation accents and similar
+        text = re.sub(r"[^A-Za-z0-9]+", " ", text)
+        # remove extra space and articles and all lower case
+        text = remove_articles(text).casefold().strip()
+
+    return text
+
+
+def unique_file(file_name: str) -> str:
+    counter = 1
+    file_name_parts = os.path.splitext(file_name)
+    while True:
+        if not os.path.lexists(file_name):
+            return file_name
+        file_name = file_name_parts[0] + " (" + str(counter) + ")" + file_name_parts[1]
+        counter += 1
+
+
+languages: dict[str | None, str | None] = defaultdict(lambda: None)
+
+countries: dict[str | None, str | None] = defaultdict(lambda: None)
+
+for c in pycountry.countries:
+    if "alpha_2" in c._fields:
+        countries[c.alpha_2] = c.name
+
+for lng in pycountry.languages:
+    if "alpha_2" in lng._fields:
+        languages[lng.alpha_2] = lng.name
+
+
+def get_language_from_iso(iso: str | None) -> str | None:
+    return languages[iso]
+
+
+def get_language(string: str | None) -> str | None:
+    if string is None:
+        return None
+
+    lang = get_language_from_iso(string)
+
+    if lang is None:
+        try:
+            return str(pycountry.languages.lookup(string).name)
+        except LookupError:
+            return None
+    return lang
+
+
+def get_publisher(publisher: str) -> tuple[str, str]:
+    if publisher is None:
+        return ("", "")
+    imprint = ""
+
+    for pub in publishers.values():
+        imprint, publisher, ok = pub[publisher]
+        if ok:
+            break
+
+    return (imprint, publisher)
+
+
+def update_publishers(new_publishers: Mapping[str, Mapping[str, str]]) -> None:
+    for publisher in new_publishers:
+        if publisher in publishers:
+            publishers[publisher].update(new_publishers[publisher])
+        else:
+            publishers[publisher] = ImprintDict(publisher, new_publishers[publisher])
+
+
+class ImprintDict(dict):
+    """
+    ImprintDict takes a publisher and a dict or mapping of lowercased
+    imprint names to the proper imprint name. Retrieving a value from an
+    ImprintDict returns a tuple of (imprint, publisher, keyExists).
+    if the key does not exist the key is returned as the publisher unchanged
+    """
+
+    def __init__(self, publisher: str, mapping=(), **kwargs) -> None:
+        super().__init__(mapping, **kwargs)
+        self.publisher = publisher
+
+    def __missing__(self, key: str) -> None:
+        return None
+
+    def __getitem__(self, k: str) -> tuple[str, str, bool]:
+        item = super().__getitem__(k.casefold())
+        if k.casefold() == self.publisher.casefold():
+            return ("", self.publisher, True)
+        if item is None:
+            return ("", k, False)
+        else:
+            return (item, self.publisher, True)
+
+    def copy(self) -> ImprintDict:
+        return ImprintDict(self.publisher, super().copy())
+
+
+publishers: dict[str, ImprintDict] = {}
+
+
+def load_publishers() -> None:
+    try:
+        update_publishers(json.loads((pathlib.Path(__file__).parent / "data" / "publishers.json").read_text("utf-8")))
+    except Exception:
+        logger.exception("Failed to load publishers.json; The are no publishers or imprints loaded")
---
+++ packages/comictagger/comictagger-1.4.4a8-py3-none-any.whl/comictaggerlib/renamewindow.py
@@ -0,0 +1,209 @@
+"""A PyQT4 dialog to confirm rename"""
+#
+# Copyright 2012-2014 Anthony Beville
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import annotations
+
+import logging
+import os
+import shutil
+from typing import TypedDict
+
+from PyQt5 import QtCore, QtWidgets, uic
+
+from comicapi import utils
+from comicapi.comicarchive import ComicArchive, MetaDataStyle
+from comictaggerlib.filerenamer import FileRenamer
+from comictaggerlib.settings import ComicTaggerSettings
+from comictaggerlib.settingswindow import SettingsWindow
+from comictaggerlib.ui.qtutils import center_window_on_parent
+
+logger = logging.getLogger(__name__)
+
+
+class RenameItem(TypedDict):
+    archive: ComicArchive
+    new_name: str
+
+
+class RenameWindow(QtWidgets.QDialog):
+    def __init__(
+        self,
+        parent: QtWidgets.QWidget,
+        comic_archive_list: list[ComicArchive],
+        data_style: int,
+        settings: ComicTaggerSettings,
+    ) -> None:
+        super().__init__(parent)
+
+        uic.loadUi(ComicTaggerSettings.get_ui_file("renamewindow.ui"), self)
+        self.label.setText(f"Preview (based on {MetaDataStyle.name[data_style]} tags):")
+
+        self.setWindowFlags(
+            QtCore.Qt.WindowType(
+                self.windowFlags()
+                | QtCore.Qt.WindowType.WindowSystemMenuHint
+                | QtCore.Qt.WindowType.WindowMaximizeButtonHint
+            )
+        )
+
+        self.settings = settings
+        self.comic_archive_list = comic_archive_list
+        self.data_style = data_style
+        self.rename_list: list[RenameItem] = []
+
+        self.btnSettings.clicked.connect(self.modify_settings)
+        self.renamer = FileRenamer(None, platform="universal" if self.settings.rename_strict else "auto")
+
+        self.config_renamer()
+        self.do_preview()
+
+    def config_renamer(self) -> None:
+        self.renamer.set_template(self.settings.rename_template)
+        self.renamer.set_issue_zero_padding(self.settings.rename_issue_number_padding)
+        self.renamer.set_smart_cleanup(self.settings.rename_use_smart_string_cleanup)
+
+    def do_preview(self) -> None:
+        self.twList.setRowCount(0)
+
+        self.twList.setSortingEnabled(False)
+
+        for ca in self.comic_archive_list:
+
+            new_ext = ca.path.suffix  # default
+            if self.settings.rename_extension_based_on_archive:
+                if ca.is_sevenzip():
+                    new_ext = ".cb7"
+                elif ca.is_zip():
+                    new_ext = ".cbz"
+                elif ca.is_rar():
+                    new_ext = ".cbr"
+
+            md = ca.read_metadata(self.data_style)
+            if md.is_empty:
+                md = ca.metadata_from_filename(
+                    self.settings.complicated_parser,
+                    self.settings.remove_c2c,
+                    self.settings.remove_fcbd,
+                    self.settings.remove_publisher,
+                )
+            self.renamer.set_metadata(md)
+            self.renamer.move = self.settings.rename_move_dir
+
+            try:
+                new_name = self.renamer.determine_name(new_ext)
+            except Exception as e:
+                QtWidgets.QMessageBox.critical(
+                    self,
+                    "Invalid format string!",
+                    "Your rename template is invalid!"
+                    f"<br/><br/>{e}<br/><br/>"
+                    "Please consult the template help in the "
+                    "settings and the documentation on the format at "
+                    "<a href='https://docs.python.org/3/library/string.html#format-string-syntax'>"
+                    "https://docs.python.org/3/library/string.html#format-string-syntax</a>",
+                )
+                return
+
+            row = self.twList.rowCount()
+            self.twList.insertRow(row)
+            folder_item = QtWidgets.QTableWidgetItem()
+            old_name_item = QtWidgets.QTableWidgetItem()
+            new_name_item = QtWidgets.QTableWidgetItem()
+
+            item_text = os.path.split(ca.path)[0]
+            folder_item.setFlags(QtCore.Qt.ItemFlag.ItemIsSelectable | QtCore.Qt.ItemFlag.ItemIsEnabled)
+            self.twList.setItem(row, 0, folder_item)
+            folder_item.setText(item_text)
+            folder_item.setData(QtCore.Qt.ItemDataRole.ToolTipRole, item_text)
+
+            item_text = os.path.split(ca.path)[1]
+            old_name_item.setFlags(QtCore.Qt.ItemFlag.ItemIsSelectable | QtCore.Qt.ItemFlag.ItemIsEnabled)
+            self.twList.setItem(row, 1, old_name_item)
+            old_name_item.setText(item_text)
+            old_name_item.setData(QtCore.Qt.ItemDataRole.ToolTipRole, item_text)
+
+            new_name_item.setFlags(QtCore.Qt.ItemFlag.ItemIsSelectable | QtCore.Qt.ItemFlag.ItemIsEnabled)
+            self.twList.setItem(row, 2, new_name_item)
+            new_name_item.setText(new_name)
+            new_name_item.setData(QtCore.Qt.ItemDataRole.ToolTipRole, new_name)
+
+            dict_item = RenameItem(
+                {
+                    "archive": ca,
+                    "new_name": new_name,
+                }
+            )
+            self.rename_list.append(dict_item)
+
+        # Adjust column sizes
+        self.twList.setVisible(False)
+        self.twList.resizeColumnsToContents()
+        self.twList.setVisible(True)
+        if self.twList.columnWidth(0) > 200:
+            self.twList.setColumnWidth(0, 200)
+
+        self.twList.setSortingEnabled(True)
+
+    def modify_settings(self) -> None:
+        settingswin = SettingsWindow(self, self.settings)
+        settingswin.setModal(True)
+        settingswin.show_rename_tab()
+        settingswin.exec()
+        if settingswin.result():
+            self.config_renamer()
+            self.do_preview()
+
+    def accept(self) -> None:
+
+        prog_dialog = QtWidgets.QProgressDialog("", "Cancel", 0, len(self.rename_list), self)
+        prog_dialog.setWindowTitle("Renaming Archives")
+        prog_dialog.setWindowModality(QtCore.Qt.WindowModality.WindowModal)
+        prog_dialog.setMinimumDuration(100)
+        center_window_on_parent(prog_dialog)
+        QtCore.QCoreApplication.processEvents()
+
+        for idx, item in enumerate(self.rename_list):
+
+            QtCore.QCoreApplication.processEvents()
+            if prog_dialog.wasCanceled():
+                break
+            idx += 1
+            prog_dialog.setValue(idx)
+            prog_dialog.setLabelText(item["new_name"])
+            center_window_on_parent(prog_dialog)
+            QtCore.QCoreApplication.processEvents()
+
+            folder = os.path.dirname(os.path.abspath(item["archive"].path))
+            if self.settings.rename_move_dir and len(self.settings.rename_dir.strip()) > 3:
+                folder = self.settings.rename_dir.strip()
+
+            new_abs_path = utils.unique_file(os.path.join(folder, item["new_name"]))
+
+            if os.path.join(folder, item["new_name"]) == item["archive"].path:
+                logger.info(item["new_name"], "Filename is already good!")
+                continue
+
+            if not item["archive"].is_writable(check_rar_status=False):
+                continue
+
+            os.makedirs(os.path.dirname(new_abs_path), 0o777, True)
+            shutil.move(item["archive"].path, new_abs_path)
+
+            item["archive"].rename(new_abs_path)
+
+        prog_dialog.hide()
+        QtCore.QCoreApplication.processEvents()
+
+        QtWidgets.QDialog.accept(self)
