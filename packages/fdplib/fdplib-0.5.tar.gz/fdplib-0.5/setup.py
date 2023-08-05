2837156cfdd9bcaa1880c1c0824f499de51a6ea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:47 +0000

    Add package fdplib/0.5/fdplib-0.5.tar.gz

---
+++ packages/fdplib/fdplib-0.5.tar.gz/fdplib-0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import os
+
+
+setup(
+    name='fdplib',
+    url='https://github.com/illini-motorsports/fdplib',
+    version = 0.5,
+    author='cmmeyer1800',
+    author_email='collinmmeyer@gmail.com',
+    python_requires = ">=3.6",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages= find_packages(
+        where='src'
+    ),
+    package_dir={'': 'src'},
+    install_requires=[
+        "tqdm>=4.62.3"
+    ]
+)
