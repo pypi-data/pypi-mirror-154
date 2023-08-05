dda8d59239e20ded1910d416872824620f31baf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:34 +0000

    Add package toxassign/0.5.5/ToxAssign-0.5.5-py3-none-any.whl

---
+++ packages/toxassign/ToxAssign-0.5.5-py3-none-any.whl/toxassign/Automation.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+from toxassign import PubChemClass
+from toxassign import Format
+from toxassign import Match
+from toxassign import Merge
+from toxassign import Writer
+import os
+import logging
+
+__author__ = "Samuel Breuer"
+__copyright__ = "Copyright 2021, MOST"
+__credits__ = ["Samuel Breuer", "Simeon Schum", "Joshua Pearce", "Lucy Toppen"]
+__license__ = "MIT"
+__version__ = "0.2.0"
+__maintainer__ = "Samuel Breuer"
+__email__ = "swbreuer@mtu.edu"
+__status__ = "Development"
+
+
+def automate(filepath):
+    pub = PubChemClass()
+    form = Format(pub)
+    mat = Match(pub)
+    os.chdir(filepath)
+    # run against all data files in folder
+    for root, subDirs, files in os.walk("."):
+        for file in files:
+            pub = PubChemClass()
+            format = Format()
+            match = Match()
+            sign = file[0]
+            cmpd = file.replace(f"{sign}", "").replace(".csv", "")
+
+            # all data files are .csv and contain either - or + as their first character, all other files are not data
+            if (file[-1] != "v") | ((sign != "-") & (sign != "+")):
+                continue
+
+            # create storage folder
+            # if not os.path.exists(cmpd):
+                # os.mkdir(cmpd)
+            # call the pubchem record, classify, sort toxic records, match unfound records
+
+            try:
+                print(f"{sign}{cmpd}")
+                pub.main(cmpd, sign)
+                form.toxfilter()
+                mat.match()
+                Writer.write_chem(cmpd, sign, form, mat, pub)
+
+            except Exception as e:
+                logging.exception(e)
+                pass
+
+    # collate all universal records into total files
+    Merge.unfound_merge()
+    Merge.unchecked_merge()
+    Merge.toxic_merge()
+    Format.toxfilter(".", "totalTox")
+
+
+def main():
+    automate(".")
+
+
+if __name__ == "__main__":
+    automate(".")
---
+++ packages/toxassign/ToxAssign-0.5.5-py3-none-any.whl/toxassign/Merge.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+import pandas as pd
+from toxassign import Format
+import os
+import logging
+
+
+# collate unfound files together
+def unfound_merge():
+    merge_main("UnfoundCopy", "Unfound")
+
+
+# collate unchecked files together
+def unchecked_merge():
+    print("---- UNCHECKED MERGE ----")
+    merge_main("Unchecked", "Unchecked")
+
+
+# collate toxic files together
+def toxic_merge():
+    print("---- TOXIC MERGE ----")
+    checked = pd.DataFrame(columns=['name', 'toxicity'])
+
+    for subDir in os.scandir("."):
+        # track whether directory has changed
+        chdir = False
+        # merge files into one object
+        try:
+            os.chdir(f"./{subDir.name}")
+            chdir = True
+            # read in toxic file
+            unfound = pd.read_csv(f"./+ SetToxic.txt", delimiter="\t",
+                                  engine='python', names=["name", "toxicity"])
+            # merge unfound into checked
+            checked = unfound.merge(checked, how="outer", on="name")
+            # read in toxic file
+            unfound = pd.read_csv(f"./+ SetToxic.txt", delimiter="\t",
+                                  engine='python', names=["name", "toxicity"])
+            # repeat
+            checked = unfound.merge(checked, how="outer", on="name")
+            os.chdir("../src")
+            chdir = False
+        except Exception as e:
+            logging.exception(e)
+            if chdir:
+                os.chdir("../src")
+
+    # write new collated object to file
+
+    with open(f"./totalTox.txt", "w+") as fTotalTox:
+        for index, value in checked.iterrows():
+            fTotalTox.write(str(value["name"]) + "\t" + str(value["toxicity"]) + "\n")
+    # format new collated file
+    Format.toxfilter(".", "totalTox")
+
+
+def merge_main(infile, outfile):
+    checked = pd.DataFrame(columns=['Compound'])
+
+    for subdir in os.scandir("."):  # merge files into one object
+        try:
+            os.chdir(f"./{subdir.name}")  # move into data directory
+            # read in toxic file
+            unfound = pd.read_csv(f"./+ Set{infile}.txt", delimiter="\t", engine='python', names=["Compound"])
+            checked = unfound.merge(checked, how="outer")  # merge unfound into checked
+            unfound = pd.read_csv(f"./- Set{infile}.txt", delimiter="\t", engine='python', names=["Compound"])  # repeat
+            checked = unfound.merge(checked, how="outer")
+        finally:
+            # move out of dir at end
+            os.chdir("..")
+
+    # write object to file
+    print(os.getcwd())
+    with open(f"./total{outfile}.txt", "w+") as fTotalUnfound:
+        for index, value in checked.iterrows():
+            fTotalUnfound.write(str(value["Compound"]) + "\n")
