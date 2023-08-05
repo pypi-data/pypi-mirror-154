7b6afbe01d5a7a0d4ae028d7af607d96b0c5d476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:45 +0000

    Add package Abhi-pdf/8.0/Abhi_pdf-8.0.tar.gz

---
+++ packages/Abhi-pdf/Abhi_pdf-8.0.tar.gz/Abhi_pdf-8.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from setuptools import setup, find_packages
+setup(
+    name='Abhi_pdf',
+    version='8.0',
+    author='Tector Pro',
+    description='Testing the efficientcy of Inspector',
+    packages=find_packages(),
+)
