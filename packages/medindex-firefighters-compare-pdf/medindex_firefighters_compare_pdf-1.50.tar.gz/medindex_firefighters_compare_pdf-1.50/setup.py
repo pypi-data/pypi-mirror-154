7e9abaa53d810b120c5d50444a0cd8ed87a464ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:21 +0000

    Add package medindex-firefighters-compare-pdf/1.50/medindex_firefighters_compare_pdf-1.50.tar.gz

---
+++ packages/medindex-firefighters-compare-pdf/medindex_firefighters_compare_pdf-1.50.tar.gz/medindex_firefighters_compare_pdf-1.50/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+def get_install_requires():
+    with open('requirements.txt', 'r') as file:
+        return [line.strip() for line in file if line.strip()]
+
+
+DESCRIPTION = "A package for internal usage in The Medindex ru for pdf image compare"
+
+
+setuptools.setup(
+    name="medindex_firefighters_compare_pdf",
+    version="1.50",
+    author='kurzeneva.o',
+    author_email='kurzeneva.o@medindex.ru',
+    description=DESCRIPTION,
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+    install_requires=get_install_requires(),
+)
