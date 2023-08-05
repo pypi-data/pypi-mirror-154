a77d7c730d9731fca75e18acdf9e4ba59e955ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:01 +0000

    Add package flasktts/0.0.7/flasktts-0.0.7.tar.gz

---
+++ packages/flasktts/flasktts-0.0.7.tar.gz/flasktts-0.0.7/setup.py
@@ -0,0 +1,34 @@
+import setuptools, os
+
+readme_path = os.path.join(os.getcwd(), "README.md")
+if os.path.exists(readme_path):
+    with open(readme_path, "r") as f:
+        long_description = f.read()
+else:
+    long_description = 'flasktts'
+
+setuptools.setup(
+    name="flasktts",
+    version="0.0.7",
+    author="Kristof",
+    description="flasktts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kkristof200/py_flask_tts",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'fastpunct>=2.0.2',
+        'Flask>=1.1.2',
+        'kcu>=0.0.73',
+        'kffmpeg>=0.2.46',
+        'pyttsx3>=2.90',
+        'setuptools>=62.3.2',
+        'Unidecode>=1.3.2'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
