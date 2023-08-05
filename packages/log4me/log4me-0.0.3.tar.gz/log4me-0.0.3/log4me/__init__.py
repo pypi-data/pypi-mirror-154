e799b037317c291dada3c5b623ea2d40836a0472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:02 +0000

    Add package log4me/0.0.3/log4me-0.0.3.tar.gz

---
+++ packages/log4me/log4me-0.0.3.tar.gz/log4me-0.0.3/log4me/__init__.py
@@ -0,0 +1,72 @@
+# Imports
+import datetime, os
+
+# Set the color theme
+class bcolors:
+    INFO = '\033[94m'
+    SUCCESS = '\033[92m'
+    WARNING = '\033[93m'
+    ERROR = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# Create the class Logger
+class Logger:
+    # Init with a name and a path for the logs
+    def __init__(self, name, path):
+        if name == "":
+            raise ValueError('Give the logger a name!')
+        if path == "":
+            raise ValueError('Provide a path')
+        self.name = name
+        self.path = path
+        self.createDict()
+    def createDict(self):
+        try:
+            # Create target Directory
+            tmpString = self.path
+            d = {"/":"", ".":""}
+            tmp = self.replace_all(tmpString, d)
+            os.mkdir(tmp)
+        except FileExistsError:
+            pass
+
+    def getTime(self):
+        return datetime.datetime.now()
+    
+    # Get the log string
+    def info(self, head, message):
+        string = f"{self.getTime()} [{bcolors.INFO + head + bcolors.ENDC}] {message}"
+        self.writeToLog(string + "\n")
+        print(string)
+
+    def success(self, head, message):
+        string = f"{self.getTime()} [{bcolors.SUCCESS + head + bcolors.ENDC}] {message}"
+        self.writeToLog(string + "\n")
+        print(string)
+
+    def warning(self, head, message):
+        string = f"{self.getTime()} [{bcolors.WARNING + head + bcolors.ENDC}] {message}"
+        self.writeToLog(string + "\n")
+        print(string)
+
+    def error(self, head, message):
+        string = f"{self.getTime()} [{bcolors.ERROR + head + bcolors.ENDC}] {message}"
+        self.writeToLog(string + "\n")
+        print(string)
+
+    def replace_all(self, text, dic):
+        for i, j in dic.items():
+            text = text.replace(i, j)
+        return text
+    # 
+    def writeToLog(self, string):
+        # Replace the color theme 
+        d = { "[94m": "", "[92m": "", "[93m": "", "[91m": "", "[0m": "", "[91m": "", "[94m": ""}
+        new = self.replace_all(string, d)
+        
+        # Write it to file
+        f = open(f"{self.path}{self.name}.log", "a")
+        f.write(new)
+        f.close()
---
+++ packages/log4me/log4me-0.0.3.tar.gz/log4me-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='log4me',
+  version='0.0.3',
+  description='Basic logger for personal python projects',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  long_description_content_type="text/markdown",
+  url='https://github.com/LucasHahmann/log4me',  
+  author='Lucas Hahmann',
+  author_email='lucas@hahmann-trier.de',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='logger', 
+  packages=find_packages(),
+  install_requires=['datetime', 'os'] 
+)
