40e399d309306122eab75b8b686fcf486646dbb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:24 +0000

    Add package oceanengine/0.0.15/oceanengine-0.0.15.tar.gz

---
+++ packages/oceanengine/oceanengine-0.0.15.tar.gz/oceanengine-0.0.15/oceanengine/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# coding = utf8
+"""
+@ Author : ZeroSeeker
+@ e-mail : zeroseeker@foxmail.com
+@ GitHub : https://github.com/ZeroSeeker
+@ Gitee : https://gitee.com/ZeroSeeker
+"""
+from .ad import *
+from .business import *
+from .e import *
+from . import business
+from . import ad
+from . import e
+"""
+code:
+    0:正常
+    40001:未登录
+"""
