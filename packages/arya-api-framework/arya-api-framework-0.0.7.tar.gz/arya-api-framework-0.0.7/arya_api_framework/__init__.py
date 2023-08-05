f8881a265e9f9179bf7dcacf97106b9f13d9c55c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:55 +0000

    Add package arya-api-framework/0.0.7/arya-api-framework-0.0.7.tar.gz

---
+++ packages/arya-api-framework/arya-api-framework-0.0.7.tar.gz/arya-api-framework-0.0.7/arya_api_framework/__init__.py
@@ -0,0 +1,7 @@
+from .async_framework import AsyncClient
+from .sync_framework import SyncClient
+
+__all__ = {
+    "AsyncClient",
+    "SyncClient"
+}
---
+++ packages/arya-api-framework/arya-api-framework-0.0.7.tar.gz/arya-api-framework-0.0.7/arya_api_framework/framework.py
@@ -0,0 +1,57 @@
+import abc
+from typing import Any, Optional
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+from .errors import MISSING
+
+
+class ClientInit(type):
+    def __call__(cls, *args, **kwargs) -> Any:
+        uri = kwargs.get('uri', MISSING)
+        if uri is MISSING and len(args) > 0:
+            uri = args[0]
+        headers = kwargs.get('headers', MISSING)
+        cookies = kwargs.get('cookies', MISSING)
+        parameters = kwargs.get('parameters', MISSING)
+        error_responses = kwargs.get('error_responses', MISSING)
+        bearer_token = kwargs.get('bearer_token', MISSING)
+
+        obj = type.__call__(cls, uri=uri, headers=headers, cookies=cookies, parameters=parameters,
+                            error_responses=error_responses, bearer_token=bearer_token)
+        if hasattr(obj, '__post_init__'):
+            obj.__post_init__(*args, **kwargs)
+        return obj
+
+
+class Response(BaseModel, abc.ABC):
+    request_base_: Optional[str] = None
+    request_received_at_: Optional[datetime] = Field(default_factory=datetime.utcnow)
+
+
+class PaginatedResponse(Response, abc.ABC):
+    @property
+    @abc.abstractmethod
+    def is_paginating(self) -> bool:
+        return False
+
+    @property
+    @abc.abstractmethod
+    def next(self) -> Optional[int]:
+        return None
+
+    @property
+    @abc.abstractmethod
+    def end(self) -> Optional[int]:
+        return None
+
+    @property
+    @abc.abstractmethod
+    def back(self) -> Optional[int]:
+        return None
+
+    @property
+    @abc.abstractmethod
+    def start(self) -> Optional[int]:
+        return None
