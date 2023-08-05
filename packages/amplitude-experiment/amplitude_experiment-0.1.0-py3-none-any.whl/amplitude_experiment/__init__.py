0db041b8988a6f0749b189c4227813d6b4078bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:17 +0000

    Add package amplitude-experiment/0.1.0/amplitude_experiment-0.1.0-py3-none-any.whl

---
+++ packages/amplitude-experiment/amplitude_experiment-0.1.0-py3-none-any.whl/amplitude_experiment/__init__.py
@@ -0,0 +1,12 @@
+"""
+The official Amplitude Experiment Python Server SDK
+.. include:: ../../README.md
+"""
+
+from .client import Client
+from .config import Config
+from .variant import Variant
+from .user import User
+from .version import __version__
+from .factory import Experiment
+from .cookie import AmplitudeCookie
