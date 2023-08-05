e5b6c0ab9a16f521fee683e4de91c928f39b5c22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:56 +0000

    Add package vandal/3.8.1/vandal-3.8.1.tar.gz

---
+++ packages/vandal/vandal-3.8.1.tar.gz/vandal-3.8.1/vandal/objects/__init__.py
@@ -0,0 +1,25 @@
+# metaclass.
+from logistics.plugins.metaclass import Meta
+
+# import all relevant contents from the associated module.
+from vandal.objects.montecarlo import (
+    MonteCarlo,
+    MCapp,
+)
+
+from vandal.objects.eoq import(
+    EOQ,
+    EOQapp,
+)
+
+from vandal.objects.dijkstra import Dijkstra
+
+# all relevant contents.
+__all__ = [
+    'MonteCarlo',
+    'EOQ',
+    'Dijkstra',
+    'MCapp',
+    'EOQapp',
+    'Meta',
+]
