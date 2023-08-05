172ad12f334e058704d15b90f1c9120ad6809f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:12 +0000

    Add package SQcircuit/0.0.7/SQcircuit-0.0.7.tar.gz

---
+++ packages/SQcircuit/SQcircuit-0.0.7.tar.gz/SQcircuit-0.0.7/SQcircuit/tests/test_qubits.py
@@ -0,0 +1,51 @@
+"""
+test_qubits contains the test cases for well-known qubits.
+"""
+
+from SQcircuit.tests.conftest import QubitTest
+
+
+class TestZeroPi(QubitTest):
+    """
+    class for testing the zero-pi qubit
+    """
+    @classmethod
+    def setup_class(cls):
+        cls.fileName = "zeroPi_1"
+
+
+class TestInductivelyShunted(QubitTest):
+    """
+    class for testing the zero-pi qubit
+    """
+    @classmethod
+    def setup_class(cls):
+        cls.fileName = "inductivelyShunted_1"
+
+
+class TestFluxonium(QubitTest):
+    """
+    class for testing the zero-pi qubit
+    """
+    @classmethod
+    def setup_class(cls):
+        cls.fileName = "Fluxonium_1"
+
+
+class TestFluxonium2(QubitTest):
+    """
+    class for testing the zero-pi qubit
+    """
+    @classmethod
+    def setup_class(cls):
+        cls.fileName = "Fluxonium_2"
+
+
+class TestTransmon(QubitTest):
+    """
+    class for testing the zero-pi qubit
+    """
+    @classmethod
+    def setup_class(cls):
+        cls.fileName = "Transmon_1"
+
