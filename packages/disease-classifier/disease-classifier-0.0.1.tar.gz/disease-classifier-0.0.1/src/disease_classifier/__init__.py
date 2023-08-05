0386289ccdbf950e818e7dedf26161b1c0c16dd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:23 +0000

    Add package disease-classifier/0.0.1/disease-classifier-0.0.1.tar.gz

---
+++ packages/disease-classifier/disease-classifier-0.0.1.tar.gz/disease-classifier-0.0.1/src/disease_classifier/__init__.py
@@ -0,0 +1,8 @@
+
+__version__ = "0.0.1"
+
+from ._widget import Disease_classifier
+#from .UI import MyWidget
+from . import aid_cv2_dnn
+from .background_program import bin_2_rtdc
+from . import image_processing
