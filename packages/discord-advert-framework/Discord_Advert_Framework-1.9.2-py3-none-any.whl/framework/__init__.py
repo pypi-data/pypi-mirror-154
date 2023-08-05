156ea706baa09c59b573bcc427e71942c6a8a30c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:47 +0000

    Add package discord-advert-framework/1.9.2/Discord_Advert_Framework-1.9.2-py3-none-any.whl

---
+++ packages/discord-advert-framework/Discord_Advert_Framework-1.9.2-py3-none-any.whl/framework/__init__.py
@@ -0,0 +1,16 @@
+"""
+    DISCORD ADVERTISEMENT FRAMEWORK (DAF)
+    Author      :: David Hozic
+    Copyright   :: Copyright (c) 2022 David Hozic
+    Version     :: V1.9
+"""
+import _discord as discord
+from .client import *
+from .const import *
+from .core import *
+from .dtypes import *
+from .guild import *
+from .message import *
+from .tracing import *
+from .sql import *
+
