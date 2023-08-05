01d5a1a74a336ef15eac014c88a424e71ce1b246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:18:16 +0000

    Add package pipeline-telemetry/0.4.4/pipeline-telemetry-0.4.4.tar.gz

---
+++ packages/pipeline-telemetry/pipeline-telemetry-0.4.4.tar.gz/pipeline-telemetry-0.4.4/tests/test_storage/test_storage.py
@@ -0,0 +1,154 @@
+"""Module to test storage module.
+"""
+import sqlite3
+from datetime import datetime
+
+from freezegun import freeze_time
+
+from pipeline_telemetry.settings.settings import DEFAULT_TRAFIC_LIGHT_COLOR
+from pipeline_telemetry.storage.generic import AbstractTelemetryStorage
+from pipeline_telemetry.storage.memory import TelemetryInMemoryStorage
+from pipeline_telemetry.storage.mongo import TelemetryMongoModel, \
+    TelemetryMongoStorage
+from pipeline_telemetry.storage.mongo_connection import get_mongo_db_port
+
+
+def test_abstract_strorage_class_exists():
+    """Test abstract storage class exists."""
+    assert AbstractTelemetryStorage
+
+
+def test_in_memory_strorage_class_exists():
+    """Test that in memory storage class exists."""
+    assert TelemetryInMemoryStorage
+
+
+def test_in_memory_strorage_creates_db_in_memory():
+    """Test in memory storage instance has db_in_memory and db_cursor"""
+    in_memory_storage = TelemetryInMemoryStorage()
+    assert in_memory_storage.db_in_memory
+    assert in_memory_storage.db_cursor
+    assert isinstance(in_memory_storage.db_in_memory, sqlite3.Connection)
+    assert isinstance(in_memory_storage.db_cursor, sqlite3.Cursor)
+
+
+def test_in_memory_strorage_is_only_created_once():
+    """Test that once the database is created it will not be reinitialized"""
+    in_memory_storage = TelemetryInMemoryStorage()
+    db = in_memory_storage.db_in_memory
+    TelemetryInMemoryStorage.initialize_db()
+    assert in_memory_storage.db_in_memory is db
+    new_in_memory_storage = TelemetryInMemoryStorage()
+    assert new_in_memory_storage.db_in_memory is db
+
+
+def test_store_telemetry_stores_object():
+    """Test in memory storage instance has db_in_memory and db_cursor"""
+    telemetry = {"source_name": "test"}
+    # Table reset for each test is needed as the table is a class property
+    TelemetryInMemoryStorage._define_db_table(TelemetryInMemoryStorage.db_cursor)
+    in_memory_storage = TelemetryInMemoryStorage()
+    in_memory_storage.store_telemetry(telemetry)
+    all_in_memory_objects = in_memory_storage.db_cursor.execute(
+        "SELECT * FROM telemetry "
+    )
+    assert len(all_in_memory_objects.fetchall()) == 1
+
+
+@freeze_time("2022-01-18 18:00:00.123456")
+def test_store_telemetry_stores_object_with_create_date(mocker):
+    """Test in memory storage instance has db_in_memory and db_cursor"""
+    telemetry = {"source_name": "test", "start_date_time": datetime.now()}
+    # Table reset for each test is needed as the table is a class property
+    TelemetryInMemoryStorage._define_db_table(TelemetryInMemoryStorage.db_cursor)
+    in_memory_storage = TelemetryInMemoryStorage()
+    in_memory_storage.store_telemetry(telemetry)
+    in_memory_record = in_memory_storage.db_cursor.execute(
+        "SELECT * FROM telemetry LIMIT 1"
+    ).fetchone()
+    print(in_memory_record)
+    assert in_memory_record[5] == "2022-01-18 18:00:00.123456"
+
+
+def test_telemetry_mongo_model_class_exists():
+    """Test TelemetryMongoModel class exists."""
+    assert TelemetryMongoModel
+
+
+def test_telemetry_mongo_storage_class_exists():
+    """Test TelemetryMongoStorage class exists."""
+    assert TelemetryMongoStorage
+
+
+def test_store_telemetry_method_processes_and_saves_telemetry(mocker):
+    """
+    Test store_telemetry method calls _telemetry_model_kwargs and then creates
+    and saves a mongo object.
+    """
+    mongo_module_path = "pipeline_telemetry.storage.mongo."
+    mocker.patch(mongo_module_path + "TelemetryMongoModel.save", return_value=True)
+    mocker.patch(
+        mongo_module_path + "TelemetryMongoStorage._telemetry_model_kwargs",
+        return_value={},
+    )
+    _telemetry_model_kwargs_spy = mocker.spy(
+        TelemetryMongoStorage, "_telemetry_model_kwargs"
+    )
+    _telemetry_mongo_model_new_spy = mocker.spy(TelemetryMongoModel, "__new__")
+    _telemetry_mongo_model_save_spy = mocker.spy(TelemetryMongoModel, "save")
+    TelemetryMongoStorage().store_telemetry(telemetry={"source_name": "test"})
+    assert _telemetry_model_kwargs_spy.called
+    assert _telemetry_mongo_model_new_spy.called
+    assert _telemetry_mongo_model_save_spy.called
+
+
+def test_telemetry_model_kwargs_method():
+    """Test _telemetry_model_kwargs method returns correct kwargs."""
+    result = TelemetryMongoStorage()._telemetry_model_kwargs(
+        {
+            "telemetry_type": "test telemetry type",
+            "category": "test",
+            "sub_category": "sub_test",
+            "source_name": "tst_source_name",
+            "process_type": "tst_process_type",
+            "start_date_time": "tst_start_date_time",
+            "run_time_in_seconds": "tst_run_time_in_seconds",
+            "io_time_in_seconds": 1.1,
+            "field1": {"a": 1},
+            "field2": "value",
+            "traffic_light": DEFAULT_TRAFIC_LIGHT_COLOR,
+        }
+    )
+
+    assert result == {
+        "telemetry_type": "test telemetry type",
+        "category": "test",
+        "sub_category": "sub_test",
+        "source_name": "tst_source_name",
+        "process_type": "tst_process_type",
+        "start_date_time": "tst_start_date_time",
+        "run_time_in_seconds": "tst_run_time_in_seconds",
+        "io_time_in_seconds": 1.1,
+        "telemetry": {"field1": {"a": 1}, "field2": "value"},
+        "traffic_light": DEFAULT_TRAFIC_LIGHT_COLOR,
+    }
+
+
+def test_get_mongo_db_port_returns_none():
+    """Test _get_mongo_db_port returns None if no env variable is set."""
+    assert get_mongo_db_port() is None
+
+
+def test_get_mongo_db_port_returns_port_int(mocker):
+    """Test _get_mongo_db_port returns port nr as intno env variable is set."""
+    mocker.patch("os.getenv", return_value="12345")
+    assert get_mongo_db_port() == 12345
+
+
+def test_default_access_params():
+    """Test default_access_params are set poperly."""
+    from pipeline_telemetry.storage import mongo_connection as mc
+
+    def_params = mc.MONGO_ACCESS_PARAMS
+    assert def_params["port"] == mc.DEFAULT_MONGO_DB_PORT
+    assert def_params["host"] == mc.DEFAULT_MONGO_DB_HOST
---
+++ packages/pipeline-telemetry/pipeline-telemetry-0.4.4.tar.gz/pipeline-telemetry-0.4.4/tests/test_telemetry.py
@@ -0,0 +1,424 @@
+"""
+Module to test telemetry main class for pipeline telemetry module.
+"""
+from datetime import datetime
+
+import pytest
+from test_data import DEFAULT_TELEMETRY_PARAMS
+
+from pipeline_telemetry.main import Telemetry
+from pipeline_telemetry.settings import exceptions
+from pipeline_telemetry.settings import settings as st
+from pipeline_telemetry.settings.data_class import ProcessType
+from pipeline_telemetry.storage.generic import AbstractTelemetryStorage
+from pipeline_telemetry.storage.memory import TelemetryInMemoryStorage
+
+# pylint: disable=protected-access
+
+
+def test_telemetry_exists():
+    """check that Telemetry class exists"""
+    assert Telemetry
+
+
+def test_telemetry_instance_creation():
+    """check that Telemetry instance can be created using default_params"""
+    assert Telemetry(**DEFAULT_TELEMETRY_PARAMS).source_name == "load_weather_data"
+
+
+def test_telemetry_instance_creation_raises_excption():
+    """
+    Check that Telemetry instance creation raises exception with non registered
+    ProcessType.
+    """
+    process_type = ProcessType(process_type="not_registered", subtypes=["test"])
+    telemetry_params = {
+        "category": "WEATHER",
+        "sub_category": "DAILY_PREDICTIONS",
+        "source_name": "load_weather_data",
+        "process_type": process_type,
+    }
+    with pytest.raises(exceptions.ProcessTypeNotRegistered):
+        Telemetry(**telemetry_params)
+
+
+def test_telemetry_instance_has_telemetry_property():
+    """check that Telemetry instance has a telemetry property"""
+    assert Telemetry(**DEFAULT_TELEMETRY_PARAMS).telemetry
+
+
+def test_telemetry_instance_has_sub_process_types_property():
+    """
+    check that Telemetry instance has a sub_process_types property that return
+    a dict with all sub_process_types
+    """
+    assert isinstance(Telemetry(**DEFAULT_TELEMETRY_PARAMS).sub_process_types, list)
+    assert (
+        Telemetry(**DEFAULT_TELEMETRY_PARAMS).sub_process_types
+        == st.DEFAULT_CREATE_DATA_SUB_PROCESS_TYPES
+    )
+
+
+def test_telemetry_instance_raises_exception_with_invalid_process_type():
+    """
+    check that Telemetry instance creation raises exception when invalid
+    process_type is used
+    """
+    telemetry_params = DEFAULT_TELEMETRY_PARAMS | {"process_type": "invalid"}
+    with pytest.raises(exceptions.ProcessTypeMustBeOfClassProcessType):
+        Telemetry(**telemetry_params)
+
+
+def test_telemetry_instance_sets_start_time():
+    """
+    check that Telemetry instance has a start_date_time
+    """
+    telemetry = Telemetry(**DEFAULT_TELEMETRY_PARAMS).telemetry
+    assert isinstance(telemetry.get("start_date_time"), datetime)
+
+
+def test_telemetry_instance_sets_io_time_to_zero():
+    """
+    check that Telemetry instance initially has zero io_time.
+    """
+    telemetry = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    assert telemetry.get(st.IO_TIME_KEY) == 0
+
+
+def test_telemetry_instance_increase_io_time():
+    """
+    check increase_io_time method increase the io_time of a Telemetry instance
+    with given float value.
+    """
+    telemetry = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry.increase_io_time(1.1)
+    assert telemetry.get(st.IO_TIME_KEY) == 1.1
+
+
+def test_increase_new_sub_process_base_count_to_telemetry():
+    """
+    Check that a new sub process base count can be added to a Telemetry
+    instance.
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst.increase_sub_process_base_count("RETRIEVE_RAW_DATA")
+    assert "RETRIEVE_RAW_DATA" in telemetry_inst.telemetry
+    assert telemetry_inst.get("RETRIEVE_RAW_DATA")["base_counter"] == 1
+
+
+def test_increase_non_exsiting_sub_process_raises_exception():
+    """
+    Check that a add a based count for a non existing sub process raises
+    an excpetion
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    with pytest.raises(exceptions.InvalidSubProcess):
+        telemetry_inst.increase_sub_process_base_count("Non exsiting")
+
+
+def test_increase_sub_process_base_count_to_telemetry():
+    """
+    Check that an existing sub process base count can be inreased.
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst.increase_sub_process_base_count("RETRIEVE_RAW_DATA")
+    assert telemetry_inst.get("RETRIEVE_RAW_DATA")["base_counter"] == 1
+    telemetry_inst.increase_sub_process_base_count("RETRIEVE_RAW_DATA")
+    assert telemetry_inst.get("RETRIEVE_RAW_DATA")["base_counter"] == 2
+
+
+def test_increase_sub_process_count_not_allowed_with_closed_telemetry():
+    """
+    check that a sub process base count can be added to th eTelemetry instance
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst.increase_sub_process_base_count("RETRIEVE_RAW_DATA")
+    telemetry_inst.save_and_close()
+    with pytest.raises(exceptions.TelemetryObjectAlreadyClosed):
+        telemetry_inst.increase_sub_process_base_count("RETRIEVE_RAW_DATA")
+
+
+def test_can_not_close_closed_telemetry_instance():
+    """
+    check that a closed telemetry instance can not be closed again
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst.save_and_close()
+    with pytest.raises(exceptions.TelemetryObjectAlreadyClosed):
+        telemetry_inst.save_and_close()
+
+
+def test_increase_sub_process_fail_not_allowed_with_closed_telemetry():
+    """
+    check that a sub process base count can be added to th eTelemetry instance
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst.increase_sub_process_base_count("RETRIEVE_RAW_DATA")
+    telemetry_inst.save_and_close()
+    with pytest.raises(exceptions.TelemetryObjectAlreadyClosed):
+        telemetry_inst.increase_sub_process_fail_count("RETRIEVE_RAW_DATA")
+
+
+def test_sub_process_custom_count_not_allowed_with_closed_telemetry():
+    """
+    check that a sub process custom count method raises an excpetion
+    when telementry object has been closed
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst.increase_sub_process_base_count("RETRIEVE_RAW_DATA")
+    telemetry_inst.save_and_close()
+    with pytest.raises(exceptions.TelemetryObjectAlreadyClosed):
+        telemetry_inst.increase_sub_process_custom_count(
+            sub_process="RETRIEVE_RAW_DATA", custom_counter="test_counter"
+        )
+
+
+def test_custom_count_not_allowed_with_closed_telemetry():
+    """
+    check that a custom count method raises an excpetion
+    when telementry object has been closed
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst.save_and_close()
+    with pytest.raises(exceptions.TelemetryObjectAlreadyClosed):
+        telemetry_inst.increase_custom_count(custom_counter="test_counter")
+
+
+def test_add_sub_process_fail_count_to_telemetry():
+    """
+    check that a sub process fail count can be increased
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst.increase_sub_process_base_count("RETRIEVE_RAW_DATA")
+    telemetry_inst.increase_sub_process_fail_count("RETRIEVE_RAW_DATA")
+    assert telemetry_inst.telemetry["RETRIEVE_RAW_DATA"][st.FAIL_COUNT_KEY] == 1
+
+
+def test_add_sub_process_fail_count_raises_excpetion():
+    """
+    check that a sub process fail count raises an excpetion when sub_process
+    has not yet been initialized by increase_sub_process_base_count(sub_process)
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    with pytest.raises(exceptions.BaseCountForSubProcessNotAdded):
+        telemetry_inst.increase_sub_process_fail_count("RETRIEVE_RAW_DATA")
+
+
+def test_add_custom_counter_to_telemetry():
+    """
+    check that a custom counter can be added to the telemetry instance
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst.increase_custom_count("network_error")
+    assert telemetry_inst.telemetry["network_error"] == 1
+
+
+def test_add_sub_process_custom_counter_to_telemetry():
+    """
+    Check that a custom counter can be added to a subprocess.
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst.increase_sub_process_base_count("RETRIEVE_RAW_DATA")
+    telemetry_inst.increase_sub_process_custom_count(
+        custom_counter="network_error", sub_process="RETRIEVE_RAW_DATA"
+    )
+    assert telemetry_inst.get("RETRIEVE_RAW_DATA")["network_error"] == 1
+
+
+def test_increase_existing_sub_process_custom_counter_to_telemetry():
+    """
+    Check that a existing custom counter can be icnrease
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst.increase_sub_process_base_count("RETRIEVE_RAW_DATA")
+    for _ in [1, 2]:
+        telemetry_inst.increase_sub_process_custom_count(
+            custom_counter="network_error", sub_process="RETRIEVE_RAW_DATA"
+        )
+
+    assert telemetry_inst.get("RETRIEVE_RAW_DATA")["network_error"] == 2
+
+
+def test_increase_custom_counter_to_telemetry():
+    """
+    check that a custom counter once created can be increased with any value
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst.increase_custom_count("network_error")
+    telemetry_inst.increase_custom_count("network_error", 2)
+    assert telemetry_inst.telemetry["network_error"] == 3
+
+
+def test_increase_sub_process_custom_count_raises_excpetion():
+    """
+    check that increase_sub_process_custom_count method raises an excpetion
+    when sub_process has not yet been initialized
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    with pytest.raises(exceptions.BaseCountForSubProcessNotAdded):
+        telemetry_inst.increase_sub_process_custom_count(
+            sub_process="RETRIEVE_RAW_DATA", custom_counter="test_counter"
+        )
+
+
+def test_increase_sub_process_custom_count_with_two():
+    """
+    check that increase_sub_process_custom_count method creates counter
+    with initial value 2
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst.increase_sub_process_base_count("RETRIEVE_RAW_DATA")
+    telemetry_inst.increase_sub_process_custom_count(
+        sub_process="RETRIEVE_RAW_DATA", custom_counter="test_counter", increment=2
+    )
+    assert telemetry_inst.telemetry["RETRIEVE_RAW_DATA"]["test_counter"] == 2
+
+
+def test_close_telemetry_instance_sets_run_time(mocker):
+    """
+    check that closing the telemetry sets the run_time
+    """
+    mocker.patch(
+        (
+            "pipeline_telemetry.storage.memory."
+            "TelemetryInMemoryStorage.store_telemetry"
+        ),
+        return_value=None,
+    )
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst.increase_sub_process_base_count("RETRIEVE_RAW_DATA")
+    telemetry_result = telemetry_inst.save_and_close()
+    assert float(telemetry_result.get("run_time_in_seconds")) > 0
+
+
+def test_close_telemetry_instance_calls_store_telemetry(mocker):
+    """
+    check that closing the telemetry sets the run_time
+    """
+    mocker.patch(
+        (
+            "pipeline_telemetry.storage.memory."
+            "TelemetryInMemoryStorage.store_telemetry"
+        ),
+        return_value=None,
+    )
+    _store_telemetry_spy = mocker.spy(TelemetryInMemoryStorage, "store_telemetry")
+    Telemetry(**DEFAULT_TELEMETRY_PARAMS).save_and_close()
+    assert _store_telemetry_spy.called
+
+
+def test_get_storage_class_returns_in_memory_storage_by_default():
+    """
+    check that _get_storage_class method returns TelemetryInMemoryStorage
+    instance if no storage class is provided
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    assert isinstance(telemetry_inst._get_storage_class(None), TelemetryInMemoryStorage)
+
+
+def test_get_storage_class_raises_exception_if_invalid_class_is_provided():
+    """
+    check that _get_storage_class raises exception if storage class is provided
+    that is not a subclass of AbstractTelemetryStorage
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    with pytest.raises(exceptions.StorageClassOfIncorrectType):
+        telemetry_inst._get_storage_class(str)
+
+
+def test_get_storage_class_returns_instance_of_storage_class():
+    """
+    check that _get_storage_class method returns an instance of
+    Storage class provided
+    """
+    # pylint: disable=too-few-public-methods
+    class TestStorage(AbstractTelemetryStorage):
+        """test class"""
+
+        def store_telemetry(self, telemetry):
+            """test method"""
+            return None
+
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    assert isinstance(telemetry_inst._get_storage_class(TestStorage), TestStorage)
+
+
+def test_storage_class_close_method_closes_db():
+    """
+    check that _get_storage_class method returns an instance of
+    Storage class provided
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    storage_instance = telemetry_inst._get_storage_class(None)
+    storage_instance.close_db()
+    assert storage_instance.db_in_memory is None
+    assert storage_instance.db_cursor is None
+
+
+def test_new_telemetry_has_default_traffic_light_color():
+    """New telemetry instance should have default trafic light color."""
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    assert telemetry_inst.traffic_light == st.DEFAULT_TRAFIC_LIGHT_COLOR
+
+
+def test_set_telemetry_traffic_light_to_orange():
+    """New telemetry instance traffic light property can be set to orange."""
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst.set_orange_traffic_light()
+    assert telemetry_inst.traffic_light == st.TRAFIC_LIGHT_COLOR_ORANGE
+
+
+def test_set_telemetry_traffic_light_to_red():
+    """New telemetry instance traffic light property can be set to red."""
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst.set_red_traffic_light()
+    assert telemetry_inst.traffic_light == st.TRAFIC_LIGHT_COLOR_RED
+
+
+def test_sub_process_is_initialized_returns_false():
+    """
+    Check that _sub_process_is_initialized returns False when sub process is not
+    yet initialized
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    assert not telemetry_inst._sub_process_is_initialized("RETRIEVE_RAW_DATA")
+
+
+def test_sub_process_is_initialized_returns_true():
+    """
+    Check that _sub_process_is_initialized returns true when sub process is
+    initialized
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst._initialize_sub_process("RETRIEVE_RAW_DATA")
+    assert telemetry_inst._sub_process_is_initialized("RETRIEVE_RAW_DATA")
+
+
+def test_sub_process_not_yet_initialized_returns_false():
+    """
+    Check that _sub_process_not_yet_initialized returns False when sub process
+    initialized
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    telemetry_inst._initialize_sub_process("RETRIEVE_RAW_DATA")
+    assert not telemetry_inst._sub_process_not_yet_initialized("RETRIEVE_RAW_DATA")
+
+
+def test_sub_process_not_yet_initialized_returns_true():
+    """
+    Check that _sub_process_not_yet_initialized returns false when sub process
+    is not yet initialized
+    """
+    telemetry_inst = Telemetry(**DEFAULT_TELEMETRY_PARAMS)
+    assert telemetry_inst._sub_process_not_yet_initialized("RETRIEVE_RAW_DATA")
+
+
+def test_instanciating_telemetry_with_invalid_telemetry_type():
+    """
+    Test that instanciating a telemetry object with invalid telemetry type
+    raises an exception.
+    """
+    telemetry_params = DEFAULT_TELEMETRY_PARAMS.copy()
+    telemetry_params['telemetry_type'] = 'invalid_type'
+    with pytest.raises(exceptions.InvalidTelemetryType):
+        Telemetry(**telemetry_params)
