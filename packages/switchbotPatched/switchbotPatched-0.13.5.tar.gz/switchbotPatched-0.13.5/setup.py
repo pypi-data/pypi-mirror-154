01f9ce82015937bb6c20bf891022115e85b4bbf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:44 +0000

    Add package switchbotPatched/0.13.5/switchbotPatched-0.13.5.tar.gz

---
+++ packages/switchbotPatched/switchbotPatched-0.13.5.tar.gz/switchbotPatched-0.13.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name = 'switchbotPatched',
+    packages = ['switchbotPatched'],
+    install_requires=['bluepy'],
+    version = '0.13.5',
+    description = 'A Unofficial library to communicate with Switchbot',
+    author='Ninad',
+    author_email='ninadpchaudhari@gmail.com',
+    url='https://github.com/Switchbot-Python/pySwitchbot-patched',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+)
