3cc3b0646035c998e2ec47756735c8dedde68c1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:28 +0000

    Add package midi-to-noteblock/0.0.1/midi-to-noteblock-0.0.1.tar.gz

---
+++ packages/midi-to-noteblock/midi-to-noteblock-0.0.1.tar.gz/midi-to-noteblock-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="midi-to-noteblock",
+    version="0.0.1",
+    license="MIT",
+    author="Eugene Matsumura",
+    url="https://github.com/SushiWaUmai/midi-to-noteblock",
+    keywords="example project",
+    install_requires=["NBT", "pretty-midi"],
+)
