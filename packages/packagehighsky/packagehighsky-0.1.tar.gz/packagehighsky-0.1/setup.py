589cb1b901f33904ebbd4fc3c7b6b4b35a90b574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:18:34 +0000

    Add package packagehighsky/0.1/packagehighsky-0.1.tar.gz

---
+++ packages/packagehighsky/packagehighsky-0.1.tar.gz/packagehighsky-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='packagehighsky',
+      version='0.1',
+      description='square root',
+      license="MIT",
+      author='Sachin R',
+      packages=find_packages('packagehighsky'),
+      zip_safe=False)
