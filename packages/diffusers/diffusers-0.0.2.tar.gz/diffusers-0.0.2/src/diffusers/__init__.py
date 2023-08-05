6e1c95da03db212fa5068d129cfb38c1fbe518c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:07 +0000

    Add package diffusers/0.0.2/diffusers-0.0.2.tar.gz

---
+++ packages/diffusers/diffusers-0.0.2.tar.gz/diffusers-0.0.2/src/diffusers/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+# There's no way to ignore "F401 '...' imported but unused" warnings in this
+# module, but to preserve other warnings. So, don't check this module at all.
+
+__version__ = "0.0.1"
+
+from .modeling_utils import ModelMixin
+from .models.unet import UNetModel
+from .models.unet_glide import UNetGLIDEModel
+from .pipeline_utils import DiffusionPipeline
+from .schedulers.gaussian_ddpm import GaussianDDPMScheduler
