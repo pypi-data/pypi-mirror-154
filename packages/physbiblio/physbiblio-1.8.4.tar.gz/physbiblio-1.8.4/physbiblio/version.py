6e90de2089666e583d31c2b949aa0715c0bce728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:27 +0000

    Add package physbiblio/1.8.4/physbiblio-1.8.4.tar.gz

---
+++ packages/physbiblio/physbiblio-1.8.4.tar.gz/physbiblio-1.8.4/physbiblio/version.py
@@ -0,0 +1,8 @@
+__version__ = "1.8.4"
+__version_date__ = "2021"
+
+__recent_changes__ = """<br>* improved warning for 'clean spare PDF' function<br>
+* add old/alternative bibtex keys in the lower-right info panel<br>
+* allow to search bibtexs by "old_keys"<br>
+* Ctrl+return/enter accepts category or experiments form<br>
+"""
