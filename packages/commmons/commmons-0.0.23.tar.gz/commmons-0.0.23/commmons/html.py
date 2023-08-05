53bf194672dbfd31c0f11efa900d8a3c1814bc17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:56 +0000

    Add package commmons/0.0.23/commmons-0.0.23.tar.gz

---
+++ packages/commmons/commmons-0.0.23.tar.gz/commmons-0.0.23/commmons/html.py
@@ -0,0 +1,18 @@
+import requests
+from lxml import html
+from lxml.html import HtmlElement
+
+__all__ = [
+    "html_from_url",
+    "html_to_string"
+]
+
+
+def html_from_url(url, **kwargs) -> HtmlElement:
+    r = requests.get(url, **kwargs)
+    r.raise_for_status()
+    return html.fromstring(r.text)
+
+
+def html_to_string(e: HtmlElement):
+    return html.tostring(e, encoding="utf-8").decode("utf-8")
