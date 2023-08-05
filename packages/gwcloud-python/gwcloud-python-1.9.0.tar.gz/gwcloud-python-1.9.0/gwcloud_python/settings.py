6f4125608cfdd7a670b11839843b61e1afc2544b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:18 +0000

    Add package gwcloud-python/1.9.0/gwcloud-python-1.9.0.tar.gz

---
+++ packages/gwcloud-python/gwcloud-python-1.9.0.tar.gz/gwcloud-python-1.9.0/gwcloud_python/settings.py
@@ -0,0 +1,3 @@
+GWCLOUD_ENDPOINT = 'https://gwcloud.org.au/bilby/graphql'
+GWCLOUD_FILE_DOWNLOAD_ENDPOINT = 'https://gwcloud.org.au/job/apiv1/file/?fileId='
+GWCLOUD_UPLOADED_JOB_FILE_DOWNLOAD_ENDPOINT = 'https://gwcloud.org.au/bilby/file_download/?fileId='
