87159a9ce6bc9087d708cffe22295c18eb508244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:42 +0000

    Add package dapi-rpc-aggregate/0.0.13/dapi_rpc_aggregate-0.0.13-py3-none-any.whl

---
+++ packages/dapi-rpc-aggregate/dapi_rpc_aggregate-0.0.13-py3-none-any.whl/dapi-rpc-aggregate/functions.py
@@ -0,0 +1,19 @@
+import requests
+
+def get_token_balance():
+    return 0
+
+def get_token_balances_for_user():
+    return 1
+
+def get_token_balance_for_users():
+    return 2
+
+def calculate_reserves_amount_from_pair():
+    return 3
+
+def get_reserves_from_pair():
+    return 4
+
+def calculate_token_price_from_pair():
+    return 5
