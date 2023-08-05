965b3b1de8dd2b3f8aad7d583eb486130e9e5acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:38 +0000

    Add package ptrmetagen/1.0.8/ptrmetagen-1.0.8-py3-none-any.whl

---
+++ packages/ptrmetagen/ptrmetagen-1.0.8-py3-none-any.whl/metagen/__init__.py
@@ -0,0 +1,3 @@
+from metagen.generator import Generator
+from metagen.metadata import Application, Configuration, Scope, Place, Period, LayerTemplate, Attribute, SpatialWMS, \
+    RelationSpatial, SpatialAttribute, SpatialWMTS, SpatialVector, Style, Case, RelationAttribute, Tag
---
+++ packages/ptrmetagen/ptrmetagen-1.0.8-py3-none-any.whl/metagen/components/_general.py
@@ -0,0 +1,27 @@
+from typing import Union, Dict
+from uuid import UUID
+from abc import ABC
+from pydantic import BaseModel
+
+from metagen.base import Leaf, BaseModelWithDynamicKey, set_key_from_input
+
+
+class BaseModelArbitrary(BaseModel):
+    pass
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+class Component(BaseModel, ABC):
+    pass
+
+
+class Filter(BaseModelWithDynamicKey):
+    __root__: Dict[str, UUID]
+
+    @classmethod
+    def set(cls, key_name: str, key: Union[str, UUID, Leaf]):
+        uuid = set_key_from_input(key)
+        return cls(**{key_name: uuid})
+
