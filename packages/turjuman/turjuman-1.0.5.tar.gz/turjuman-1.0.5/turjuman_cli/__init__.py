3554c62f6554ef2fb721180cf35a938369a72ca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:55 +0000

    Add package turjuman/1.0.5/turjuman-1.0.5.tar.gz

---
+++ packages/turjuman/turjuman-1.0.5.tar.gz/turjuman-1.0.5/turjuman_cli/__init__.py
@@ -0,0 +1,10 @@
+
+from turjuman import *
+from turjuman.copyright import *
+
+__author__ = author
+__copyright__ = copyright
+__license__ = license
+__version__ = version
+__email__ = email
+__status__ = status
