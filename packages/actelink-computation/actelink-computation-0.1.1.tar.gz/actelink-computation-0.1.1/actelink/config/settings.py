840692e4056b96ba3bad4b676523964e881dd192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:54 +0000

    Add package actelink-computation/0.1.1/actelink-computation-0.1.1.tar.gz

---
+++ packages/actelink-computation/actelink-computation-0.1.1.tar.gz/actelink-computation-0.1.1/actelink/config/settings.py
@@ -0,0 +1,8 @@
+# default settings
+class Settings:
+    SECRET_KEY                  = 'test'
+    HOST                        = '0.0.0.0'
+    PORT                        = 5001
+
+    #VARIABLE_ENGINE_URL         = "http://127.0.0.1:7109"
+    #VARIABLE_ENGINE_API_KEY     = "65421efac845f08a"
