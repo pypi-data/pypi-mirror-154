59efc0164c6f207703bddadae847df9cfbe98de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:12 +0000

    Add package beancount-reds-importers/0.5.0/beancount_reds_importers-0.5.0-py3-none-any.whl

---
+++ packages/beancount-reds-importers/beancount_reds_importers-0.5.0-py3-none-any.whl/beancount_reds_importers/libreader/ofxreader.py
@@ -0,0 +1,76 @@
+"""Ofx importer module for beancount to be used along with investment/banking/other importer modules in
+beancount_reds_importers."""
+
+import datetime
+from ofxparse import OfxParser
+from collections import namedtuple
+from beancount.ingest import importer
+from beancount_reds_importers.libreader import reader
+
+
+class Importer(reader.Reader, importer.ImporterProtocol):
+    FILE_EXT = 'fx'
+
+    def initialize_reader(self, file):
+        if getattr(self, 'file', None) != file:
+            self.file = file
+            self.ofx = OfxParser.parse(open(file.name))
+            self.ofx_account = None
+            self.reader_ready = False
+            for acc in self.ofx.accounts:
+                # account identifying info fieldname varies across institutions
+                # self.acc_num_field can be overridden in self.custom_init() if needed
+                acc_num_field = getattr(self, 'account_number_field', 'account_id')
+                if self.match_account_number(getattr(acc, acc_num_field),
+                                             self.config['account_number']):
+                    self.ofx_account = acc
+                    self.reader_ready = True
+            if self.reader_ready:
+                self.currency = self.ofx_account.statement.currency.upper()
+                self.includes_balances = True
+
+    def match_account_number(self, file_account, config_account):
+        """We many not want to store entire credit card numbers in our config. Or a given ofx may not contain
+        the full account number. Override this method to handle these cases."""
+        return file_account == config_account
+
+    def file_date(self, file):
+        "Get the maximum date from the file."
+        return self.ofx_account.statement.end_date
+
+    def read_file(self, file):
+        pass
+
+    def get_transactions(self):
+        for ot in self.ofx_account.statement.transactions:
+            yield ot
+
+    def get_balance_statement(self):
+        date = self.get_max_transaction_date()
+        if date:
+            date += datetime.timedelta(days=1)  # See comment in get_max_transaction_date() for explanation
+            Balance = namedtuple('Balance', ['date', 'amount'])
+            yield Balance(date, self.ofx_account.statement.balance)
+
+    def get_balance_positions(self):
+        for pos in self.ofx_account.statement.positions:
+            yield pos
+
+    def get_available_cash(self):
+        return getattr(self.ofx_account.statement, 'available_cash', None)
+
+    def get_max_transaction_date(self):
+        try:
+            # date = self.ofx_account.statement.end_date.date() # this is the date of ofx download
+            # we find the last transaction's date. If we use the ofx download date (if our source is ofx), we
+            # could end up with a gap in time between the last transaction's date and balance assertion.
+            # Pending (but not yet downloaded) transactions in this gap will get downloaded the next time we
+            # do a download in the future, and cause the balance assertions to be invalid.
+
+            date = max(ot.tradeDate if hasattr(ot, 'tradeDate') else ot.date
+                       for ot in self.get_transactions()).date()
+        except TypeError:
+            return False
+        except ValueError:
+            return False
+        return date
---
+++ packages/beancount-reds-importers/beancount_reds_importers-0.5.0-py3-none-any.whl/beancount_reds_importers/workday/__init__.py
@@ -0,0 +1,71 @@
+""" Workday paycheck importer."""
+
+import datetime
+import re
+from beancount.core.number import D
+from beancount_reds_importers.libreader import xlsx_multitable_reader
+from beancount_reds_importers.libtransactionbuilder import paycheck
+
+# Workday exports paycheck stubs to a .xlsx, one paycheck per .xlsx, with multiple tables on a single sheet,
+# that this importer imports. Call this importer with a config that looks like:
+#
+# workday.Importer({'desc': "Paycheck (Acme Company)",
+#      'main_account' : 'Income:Employment',
+#      'paycheck_template': '{}' # See beancount_reds_importers/libtransactionbuilder/paycheck.py for sample template
+#      'currency' : 'PENNIES',
+#     }),
+
+
+class Importer(paycheck.Importer, xlsx_multitable_reader.Importer):
+    IMPORTER_NAME = 'Workday Paycheck'
+
+    def custom_init(self):
+        self.max_rounding_error = 0.04
+        self.filename_pattern_def = '.*_Complete'
+        self.header_identifier = '- Complete' + self.config.get('custom_header', '')
+        self.includes_balances = False
+        self.date_format = '%m/%d/%Y'
+        self.skip_head_rows = 1
+        self.skip_tail_rows = 1
+        self.funds_db_txt = 'funds_by_ticker'
+        self.header_map = {
+            "Description": 'memo',
+            "Symbol":      'security',
+            "Quantity":    'units',
+            "Price":       'unit_price',
+            }
+
+    def paycheck_date(self, input_file):
+        self.read_file(input_file)
+        d = self.alltables['Payslip Information'].namedtuples()[0].check_date
+        self.date = datetime.datetime.strptime(d, self.date_format)
+        return self.date.date()
+
+    def prepare_raw_columns(self, rdr):
+        return rdr
+
+    def convert_columns(self, rdr):
+        # fixup decimals
+        decimals = ['units']
+        for i in decimals:
+            rdr = rdr.convert(i, D)
+
+        # fixup currencies
+        def remove_non_numeric(x):
+            return re.sub("[^0-9\.]", "", x)  # noqa: W605
+        currencies = ['unit_price']
+        for i in currencies:
+            rdr = rdr.convert(i, remove_non_numeric)
+            rdr = rdr.convert(i, D)
+
+        return rdr
+
+    def prepare_tables(self):
+
+        def valid_header_label(label):
+            return label.lower().replace(' ', '_')
+
+        for section, table in self.alltables.items():
+            for header in table.header():
+                table = table.rename(header, valid_header_label(header))
+            self.alltables[section] = table
