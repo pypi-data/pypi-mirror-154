5496459d3b0e9d742abdebef30676b5f3391c9cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:55 +0000

    Add package simple-parsing/0.0.20/simple_parsing-0.0.20.tar.gz

---
+++ packages/simple-parsing/simple_parsing-0.0.20.tar.gz/simple_parsing-0.0.20/setup.py
@@ -0,0 +1,35 @@
+import sys
+import setuptools
+import versioneer
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+packages = setuptools.find_namespace_packages(include=["simple_parsing*"])
+print("PACKAGES FOUND:", packages)
+print(sys.version_info)
+
+with open("requirements.txt", "r") as req_file:
+    install_requires = req_file.read().splitlines(keepends=False)
+
+
+setuptools.setup(
+    name="simple_parsing",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Fabrice Normandin",
+    author_email="fabrice.normandin@gmail.com",
+    description="A small utility for simplifying and cleaning up argument parsing scripts.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lebrice/SimpleParsing",
+    packages=packages,
+    package_data={"simple_parsing": ["py.typed"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=install_requires,
+    setup_requires=["pre-commit"],
+)
---
+++ packages/simple-parsing/simple_parsing-0.0.20.tar.gz/simple_parsing-0.0.20/test/test_fields.py
@@ -0,0 +1,67 @@
+from dataclasses import dataclass
+
+from test.testutils import TestSetup
+
+from simple_parsing import ArgumentParser, field, ConflictResolution
+
+
+def test_cmd_false_doesnt_create_conflicts():
+    @dataclass
+    class A:
+        batch_size: int = field(default=10, cmd=False)
+
+    @dataclass
+    class B:
+        batch_size: int = 20
+
+    # @dataclass
+    # class Foo(TestSetup):
+    #     a: A = mutable_field(A)
+    #     b: B = mutable_field(B)
+
+    parser = ArgumentParser(conflict_resolution=ConflictResolution.NONE)
+    parser.add_arguments(A, "a")
+    parser.add_arguments(B, "b")
+    args = parser.parse_args("--batch_size 32".split())
+    a: A = args.a
+    b: B = args.b
+    assert a == A()
+    assert b == B(batch_size=32)
+
+
+from typing import Type, Dict, Tuple, List, Optional
+from simple_parsing.wrappers.field_wrapper import get_argparse_options_for_annotation
+import pytest
+
+from enum import Enum
+
+
+class Color(Enum):
+    blue: str = "BLUE"
+    red: str = "RED"
+    green: str = "GREEN"
+    orange: str = "ORANGE"
+
+
+from simple_parsing.wrappers.field_parsing import get_parsing_fn
+
+from simple_parsing.utils import str2bool
+from simple_parsing.wrappers.field_parsing import parse_enum
+
+@pytest.mark.parametrize(
+    "annotation, expected_options",
+    [
+        (Tuple[int, int], dict(nargs=2, type=int)),
+        (Tuple[Color, Color], dict(nargs=2, type=parse_enum(Color))),
+        (Optional[Tuple[Color, Color]], dict(nargs=2, type=parse_enum(Color), required=False)),
+        (List[str], dict(nargs="*", type=str)),
+        (Optional[List[str]], dict(nargs="*", type=str, required=False)),
+        (Optional[str], dict(nargs="?", type=str, required=False)),
+        (Optional[bool], dict(nargs="?", type=str2bool, required=False)),
+        # (Optional[Tuple[Color, str]], dict(nargs=2, type=get_parsing_fn(Tuple[Color, str]), required=False)),
+    ],
+)
+def test_generated_options_from_annotation(annotation: Type, expected_options: Dict):
+    actual_options = get_argparse_options_for_annotation(annotation)
+    for option, expected_value in expected_options.items():
+        assert actual_options[option] == expected_value
