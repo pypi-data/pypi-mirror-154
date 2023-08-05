7315a235f1521d0cb5ff2cd27998f36d36ed9e9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:48 +0000

    Add package Flask-OneID/1.0.2/Flask-OneID-1.0.2.tar.gz

---
+++ packages/Flask-OneID/Flask-OneID-1.0.2.tar.gz/Flask-OneID-1.0.2/setup.py
@@ -0,0 +1,39 @@
+"""
+Flask-OneID
+-------------
+
+This is the description for that library
+"""
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='Flask-OneID',
+    version='1.0.2',
+    url='https://github.com/Odya-LLC/flask_oneid',
+    license='MIT',
+    author='odya',
+    author_email='support@odya.uz',
+    description='OneID integration with Flask application, (only for Uzbekistan)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['flask_oneid'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',"requests"
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
