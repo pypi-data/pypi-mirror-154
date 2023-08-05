7d7a58fae61cbf11d5e9ad567c4bde46b62b84a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:22 +0000

    Add package pod-exec/0.0.2/pod-exec-0.0.2.tar.gz

---
+++ packages/pod-exec/pod-exec-0.0.2.tar.gz/pod-exec-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pod-exec",
+    version="0.0.2",
+    author="wangziling100",
+    author_email="wangziling100@163.com",
+    description="Command line controller help for executing command in a pod",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
