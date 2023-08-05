ddc65455dfb19ea8903fddc476487331e7aa577d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:11 +0000

    Add package gradcache/0.0.1b0/gradcache-0.0.1b0.tar.gz

---
+++ packages/gradcache/gradcache-0.0.1b0.tar.gz/gradcache-0.0.1b0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gradcache',
+    version='0.0.1b',
+    packages=find_packages("src"),
+    package_dir={'': 'src'},
+    url='https://github.com/luyug/GradCache',
+    license='Apache-2.0',
+    author='Luyu Gao',
+    author_email='luyug@cs.cmu.edu',
+    description=''
+)
