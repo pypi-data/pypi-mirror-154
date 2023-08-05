d8ee92fa4c94f1cb16b2a200a171f2a10809e528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:09 +0000

    Add package lacibus/1.0.1/lacibus-1.0.1.tar.gz

---
+++ packages/lacibus/lacibus-1.0.1.tar.gz/lacibus-1.0.1/src/lacibus/__init__.py
@@ -0,0 +1,9 @@
+'''
+This package provides a web client interface to the Soda data content
+management system. It has a main module - soda - that provides the .
+
+'''
+
+from . import soda
+
+__all__ = ["soda"]
