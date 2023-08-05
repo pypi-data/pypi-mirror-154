0b448574569a20b29af4403c768be7455c1c0bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:28 +0000

    Add package leveraged-multi-family-analysis/0.1.3/leveraged_multi_family_analysis-0.1.3-py3-none-any.whl

---
+++ packages/leveraged-multi-family-analysis/leveraged_multi_family_analysis-0.1.3-py3-none-any.whl/lmfa/__main__.py
@@ -0,0 +1,31 @@
+"""Leveraged Multi Family Real Estate Analysis.
+
+Usage:
+------
+
+python -m lmfa 'multifamily_2.yaml', 'multifamily_4.yaml'
+
+
+Contact:
+--------
+
+More information is available at:
+
+- https://pypi.org/project/leveraged-multi-family-analysis/
+- https://github.com/vamseeachanta/leveraged-multi-family-analysis
+
+
+Version:
+--------
+
+- leveraged-multi-family-analysis v0.1.3
+"""
+from lmfa import multifamily
+
+
+def main() -> None:
+    multifamily.run_analysis()
+
+
+if __name__ == "__main__":
+    main()
