6c06f2e46dbb76d7f3e01600e77c7bd5f3420128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:27 +0000

    Add package duality/4.7.3/duality-4.7.3.tar.gz

---
+++ packages/duality/duality-4.7.3.tar.gz/duality-4.7.3/duality/decorators/__init__.py
@@ -0,0 +1,13 @@
+# imports relevant dependencies.
+from logistics.plugins.metaclass import Meta
+
+# imports all relevant contents.
+from duality.decorators.record import Record
+from duality.decorators.track import Track
+
+# all relevant contents.
+__all__ = [
+    'Meta',
+    'Record',
+    'Track',
+]
