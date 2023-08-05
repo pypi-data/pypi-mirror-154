430420ac166c9b1fe8e054874982072f1afce679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:44 +0000

    Add package walkman-modules.convolution-reverb/0.1.1/walkman_modules.convolution_reverb-0.1.1.tar.gz

---
+++ packages/walkman-modules.convolution-reverb/walkman_modules.convolution_reverb-0.1.1.tar.gz/walkman_modules.convolution_reverb-0.1.1/setup.py
@@ -0,0 +1,39 @@
+import setuptools  # type: ignore
+
+MAJOR, MINOR, PATCH = 0, 1, 1
+VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
+"""This project uses semantic versioning.
+See https://semver.org/
+Before MAJOR = 1, there is no promise for
+backwards compatibility between minor versions.
+"""
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+extras_require = {"testing": ["nose"]}
+
+setuptools.setup(
+    name="walkman_modules.convolution_reverb",
+    version=VERSION,
+    license="GPL",
+    description="Provides walkman module to send input through a convolution reverb",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Levin Eric Zimmermann",
+    author_email="levin.eric.zimmermann@posteo.eu",
+    packages=[
+        package
+        for package in setuptools.find_namespace_packages(include=["walkman_modules.*"])
+        if package[:5] != "tests"
+    ],
+    setup_requires=[],
+    install_requires=[
+        # core package
+        "audiowalkman>=0.7.0, <1.0.0",
+        # for audio
+        "pyo==1.0.4",
+    ],
+    extras_require=extras_require,
+    python_requires="==3.8",
+)
