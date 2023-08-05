82386d5db0bf144af2b21cea8d204248d8594e73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:40 +0000

    Add package seafileapi-extended/1.0.3/seafileapi_extended-1.0.3-py3-none-any.whl

---
+++ packages/seafileapi-extended/seafileapi_extended-1.0.3-py3-none-any.whl/seafileapi_extended/exceptions.py
@@ -0,0 +1,43 @@
+"""Exceptions class"""
+
+
+class ClientHttpError(Exception):
+    """This exception is raised if the returned http response is not as
+    expected"""
+
+    def __init__(self, code, message):
+        super(ClientHttpError, self).__init__()
+        self.code = code
+        self.message = message
+
+    def __str__(self):
+        return "ClientHttpError[%s: %s]" % (self.code, self.message)
+
+
+class OperationError(Exception):
+    """Expcetion to raise when an opeartion is failed"""
+
+    pass
+
+
+class DoesNotExist(Exception):
+    """Raised when not matching resource can be found."""
+
+    def __init__(self, msg):
+        super(DoesNotExist, self).__init__()
+        self.msg = msg
+
+    def __str__(self):
+        return "DoesNotExist: %s" % self.msg
+
+
+class AuthenticationError(ClientHttpError):
+    """Authentication error occurred while retrieving access token"""
+
+
+class UserExisted(Exception):
+    pass
+
+
+class GroupExisted(Exception):
+    pass
