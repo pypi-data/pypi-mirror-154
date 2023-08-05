4b0217547651d710d02aaf8ccbb4e52c6ba634da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:15 +0000

    Add package aclick/0.2.1/aclick-0.2.1-py3-none-any.whl

---
+++ packages/aclick/aclick-0.2.1-py3-none-any.whl/aclick/__init__.py
@@ -0,0 +1,22 @@
+from . import _version, core, types, utils
+from ._version import __version__  # noqa: F401
+from .configuration import (  # noqa: F401
+    parse_configuration,
+    register_configuration_provider,
+)
+from .core import (  # noqa: F401
+    Command,
+    Context,
+    FlattenParameterRenamer,
+    Group,
+    ParameterRenamer,
+    RegexParameterRenamer,
+)
+from .decorators import (  # noqa: F401  # noqa: F401
+    command as command,
+    configuration_option as configuration_option,
+    group as group,
+)
+from .types import ClassUnion as ClassUnion, List as List  # noqa: F401  # noqa: F401
+
+del _version  # noqa: F821
