55844e59348325cb58433b9afa1676171d0d06d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:32:49 +0000

    Add package dsndxp-distributions/0.2/dsndxp_distributions-0.2.tar.gz

---
+++ packages/dsndxp-distributions/dsndxp_distributions-0.2.tar.gz/dsndxp_distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsndxp_distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['dsndxp_distributions'],
+      zip_safe=False)
