409d4d760b0d91e18e8a51809d0b83024e65f3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:04 +0000

    Add package torch-hd/3.0.1/torch_hd-3.0.1-py3-none-any.whl

---
+++ packages/torch-hd/torch_hd-3.0.1-py3-none-any.whl/torchhd/__init__.py
@@ -0,0 +1,32 @@
+import torchhd.functional as functional
+import torchhd.embeddings as embeddings
+import torchhd.structures as structures
+import torchhd.datasets as datasets
+import torchhd.utils as utils
+
+from torchhd.functional import (
+    identity_hv,
+    random_hv,
+    level_hv,
+    circular_hv,
+    bind,
+    bundle,
+    permute,
+)
+
+from torchhd.version import __version__
+
+__all__ = [
+    "functional",
+    "embeddings",
+    "structures",
+    "datasets",
+    "utils",
+    "identity_hv",
+    "random_hv",
+    "level_hv",
+    "circular_hv",
+    "bind",
+    "bundle",
+    "permute",
+]
