663556f4e043747dead812f76e5295c9b2287c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:55 +0000

    Add package simple-parsing/0.0.20/simple_parsing-0.0.20-py3-none-any.whl

---
+++ packages/simple-parsing/simple_parsing-0.0.20-py3-none-any.whl/simple_parsing/__init__.py
@@ -0,0 +1,42 @@
+"""Simple, Elegant Argument parsing.
+@author: Fabrice Normandin
+"""
+from . import helpers, utils, wrappers
+from .conflicts import ConflictResolution
+from .help_formatter import SimpleHelpFormatter
+from .helpers import (
+    MutableField,
+    Serializable,
+    choice,
+    field,
+    flag,
+    list_field,
+    mutable_field,
+    subgroups,
+    subparsers,
+)
+from .parsing import ArgumentParser, DashVariant, ParsingError
+from .utils import InconsistentArgumentError
+
+__all__ = [
+    "helpers",
+    "utils",
+    "wrappers",
+    "ConflictResolution",
+    "DashVariant" "MutableField",
+    "Serializable",
+    "SimpleHelpFormatter",
+    "choice",
+    "field",
+    "flag",
+    "list_field",
+    "mutable_field",
+    "subparsers",
+    "subgroups",
+    "ArgumentParser",
+    "InconsistentArgumentError",
+]
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
