f9055c6fc6b043988c97dd8316fd64148e797909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:19:26 +0000

    Add package popuchange/1.0.1/popuchange-1.0.1.tar.gz

---
+++ packages/popuchange/popuchange-1.0.1.tar.gz/popuchange-1.0.1/README.md
@@ -0,0 +1,8 @@
+# popuchange
+東京都の人口をを可視化できるPython Package。  
+Python Package for visualization of population in tokyo.
+
+## Example of use
+
+pip install popuchange
+python tokyopopu.py
---
+++ packages/popuchange/popuchange-1.0.1.tar.gz/popuchange-1.0.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="popuchange",
+    version="1.0.1",
+    author="takuma iwasaki",
+    author_email="s2022005@stu.musashino-u.ac.jp",
+    description='A package for visualization of population in tokyo"',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kuma2198/popuchange",
+    project_urls={
+        "Bug Tracker": "https://github.com/kuma2198/popuchange",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    py_modules=['popuchange'],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    entry_points = {
+        'console_scripts': [
+            'popuchange = popuchange:main'
+        ]
+    },
+)
