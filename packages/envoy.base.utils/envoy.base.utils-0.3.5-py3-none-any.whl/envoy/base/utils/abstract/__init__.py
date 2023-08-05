9ecd6d790fd389d33da976331949f7a8e3a13b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:00 +0000

    Add package envoy.base.utils/0.3.5/envoy.base.utils-0.3.5-py3-none-any.whl

---
+++ packages/envoy.base.utils/envoy.base.utils-0.3.5-py3-none-any.whl/envoy/base/utils/abstract/__init__.py
@@ -0,0 +1,15 @@
+
+from .project import (
+    AChangelog,
+    AChangelogEntry,
+    AChangelogs,
+    AInventories,
+    AProject)
+
+
+__all__ = (
+    "AChangelog",
+    "AChangelogEntry",
+    "AChangelogs",
+    "AInventories",
+    "AProject", )
