e8fc49d15aa7cab898d02c4a79beb8b58d98bbce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:19:10 +0000

    Add package mental-health-and-prevalence/0.0.2/mental_health_and_prevalence-0.0.2.tar.gz

---
+++ packages/mental-health-and-prevalence/mental_health_and_prevalence-0.0.2.tar.gz/mental_health_and_prevalence-0.0.2/README.md
@@ -0,0 +1,18 @@
+# Mental Health
+
+"Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act. It also helps determine how we handle stress, relate to others, and make healthy choices. 1. Mental health is important at every stage of life, from childhood and adolescence through adulthood."(Mental Health Dataset | Kaggle, License CC0: Public Domain)
+
+# prevalence-by-mental-and-substance-use-disorder.csv
+
+There are Seven columns('Schizophrenia', 'Bipolar_disorder', 'Eating_disorders', 'Anxiety_disorders', 'Drug_use_disorders', 'Depressive_disorders', 'Alcohol_use_disorders') stored  
+these symptoms preference.
+
+# prevalence-of-depression-males-vs-females.csv
+
+The prevalence-of-depression-males-vs-females.csv file include prevalense of depression separated by gender and popunation of each country.
+
+# How to install mental-health-and-prevalence==0.0.1
+
+$ pip install mental-health-and-prevalence==0.0.1
+
+<img src="https://github.com/RikoWatanabe/MentalHelse/blob/master/result.png" width=320 height=240 >
---
+++ packages/mental-health-and-prevalence/mental_health_and_prevalence-0.0.2.tar.gz/mental_health_and_prevalence-0.0.2/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mental_health_and_prevalence",
+    version="0.0.2",
+    author="riko watanabe",
+    author_email="s2022039@stu.musashino-u.ac.jp",
+    description="Trends in the incidence of psychosis worldwide",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RikoWatanabe/MentalHelse.git",
+    project_urls={
+        "Bug Tracker": "https://github.com/RikoWatanabe/MentalHelse.git",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    py_modules=['mental_health_and_prevalence'],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    entry_points = {
+        'console_scripts': [
+            'mental_health_and_prevalence = mental_health_and_prevalence:main'
+        ]
+    },
+)
