9e1c97700d2e87ab8a8a4030fdbe7e9c5d06dfe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:46 +0000

    Add package walkman-modules.convolution-reverb/0.1.2/walkman_modules.convolution_reverb-0.1.2.tar.gz

---
+++ packages/walkman-modules.convolution-reverb/walkman_modules.convolution_reverb-0.1.2.tar.gz/walkman_modules.convolution_reverb-0.1.2/README.md
@@ -0,0 +1,8 @@
+# walkman_modules.convolution_reverb
+
+[![Build Status](https://circleci.com/gh/audiowalkman/walkman_modules.convolution_reverb.svg?style=shield)](https://circleci.com/gh/audiowalkman/walkman_modules.convolution_reverb)
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+[![PyPI version](https://badge.fury.io/py/walkman_modules.convolution_reverb.svg)](https://badge.fury.io/py/walkman_modules.convolution_reverb)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
+This package adds the `convolution_reverb` module to `walkman`.
---
+++ packages/walkman-modules.convolution-reverb/walkman_modules.convolution_reverb-0.1.2.tar.gz/walkman_modules.convolution_reverb-0.1.2/walkman_modules/convolution_reverbs/__init__.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+
+import dataclasses
+import warnings
+
+import pyo
+
+import walkman
+
+
+class InvalidInputIndexWarning(Warning):
+    pass
+
+
+@dataclasses.dataclass
+class ConvolutionReverb(walkman.ModuleWithDecibel):
+    """"""
+
+    impulse_path: str = ""
+
+    def setup_pyo_object(self):
+        super().setup_pyo_object()
+
+        self._balance_signal_to = pyo.SigTo(1)
+        try:
+            pyo_input = self.input_provider.audio_input_list[0]
+        except IndexError:
+            warnings.warn(
+                f"Found no audio inputs! Using {self.get_name()} module is useless "
+                "without any audio input!"
+            )
+            pyo_input = pyo.Sig(0)
+        self._convolution_reverb = pyo.CvlVerb(
+            pyo_input,
+            self.impulse_path,
+            mul=self._decibel_signal_to,
+            bal=self._balance_signal_to,
+        ).stop()
+
+    def _play(self, duration: float = 0, delay: float = 0):
+        super()._play(duration, delay)
+        self._convolution_reverb.play(dur=duration, delay=delay)
+        self._balance_signal_to.play(dur=duration, delay=delay)
+
+    def _stop(self, wait: float = 0):
+        super()._stop(wait)
+        self._convolution_reverb.stop(wait=wait)
+        self._balance_signal_to.stop(wait=wait)
+
+    def _initialise(
+        self,
+        input_index: int = 0,
+        decibel: walkman.Parameter = -6,  # type: ignore
+        balance: walkman.Parameter = 1,  # type: ignore
+    ):
+        super()._initialise(decibel=decibel)
+
+        try:
+            pyo_input = self.input_provider.audio_input_list[input_index]
+        except IndexError:
+            warnings.warn(
+                f"There is no input with input_index = '{input_index}'!"
+                f" WALKMAN ignored current cue settings for {self.get_name()}.",
+                InvalidInputIndexWarning,
+            )
+
+        else:
+            self._convolution_reverb.setInput(pyo_input)
+            self._balance_signal_to.setValue(balance.value)
+
+    @property
+    def _pyo_object(self) -> pyo.PyoObject:
+        return self._convolution_reverb
