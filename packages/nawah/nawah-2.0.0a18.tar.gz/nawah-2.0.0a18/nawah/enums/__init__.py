b40438e59cf18814fb6feb049eb37a3d4673bb85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:59 +0000

    Add package nawah/2.0.0a18/nawah-2.0.0a18.tar.gz

---
+++ packages/nawah/nawah-2.0.0a18.tar.gz/nawah-2.0.0a18/nawah/enums/__init__.py
@@ -0,0 +1,13 @@
+'''Provides enums required for Nawah Fremework and apps'''
+
+from ._enums import (AttrType, DeleteStrategy, Event, LocaleStrategy,
+                     NawahValues, VarType)
+
+__all__ = [
+    'AttrType',
+    'DeleteStrategy',
+    'Event',
+    'LocaleStrategy',
+    'NawahValues',
+    'VarType',
+]
