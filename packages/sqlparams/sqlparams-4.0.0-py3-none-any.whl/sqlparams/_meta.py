3c4825023d45dc7254a398621eba53880f0a2a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:52 +0000

    Add package sqlparams/4.0.0/sqlparams-4.0.0-py3-none-any.whl

---
+++ packages/sqlparams/sqlparams-4.0.0-py3-none-any.whl/sqlparams/_meta.py
@@ -0,0 +1,15 @@
+"""
+This module contains project meta-data.
+"""
+
+__author__ = "Caleb P. Burns"
+__copyright__ = "Copyright © 2012-2022 by Caleb P. Burns"
+__credits__ = [
+	"khomyakov42 <https://github.com/khomyakov42>",
+	"pedermoller <https://github.com/pedermoller>",
+	"ryokash <https://github.com/ryokash>",
+	"zenwalker <https://github.com/zenwalker>",
+	"kesmit13 <https://github.com/kesmit13>",
+]
+__license__ = "MIT License"
+__version__ = "4.0.0"
