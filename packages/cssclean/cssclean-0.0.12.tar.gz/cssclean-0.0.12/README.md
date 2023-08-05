3e27b3171fd157a0e8db44fdd894a482ec7e0da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:35:40 +0000

    Add package cssclean/0.0.12/cssclean-0.0.12.tar.gz

---
+++ packages/cssclean/cssclean-0.0.12.tar.gz/cssclean-0.0.12/README.md
@@ -0,0 +1,236 @@
+# CSS Clean!
+
+[![PyPI version](https://badge.fury.io/py/cssclean.svg)](https://badge.fury.io/py/cssclean)
+
+This is a library for cleaning up css based on styles that are actually used in html files.
+Although there are [libraries that use npm](https://www.keycdn.com/blog/remove-unused-css)
+I am not a fan so I wanted something in Python.
+
+<img src="https://raw.githubusercontent.com/vsoch/cssclean/main/docs/img/logo.jpg" width="500px">
+
+## Approach
+
+We take a conservative approach to filtering, meaning:
+
+ - for any style block, one matching class or identifier is sufficient for inclusion
+ - multiple levels selectors are not combined (e.g., using "button" is sufficient to add `.button. red`.
+ - we always keep keyframes and imports. Additional parsing could be added to support checking, but they are fairly rare (imho).
+ - The library is **new** and I may have missed a case! Please [open an issue](https://github.com/vsoch/cssclean/issues) if you find something.
+
+Of course this could be improved upon but we would need more careful token parsing!
+Please [open an issue](https://github.com/vsoch/cssclean/issues) if you are interested. 
+
+## Usage
+
+### Install
+
+First, clone and install.
+
+```bash
+$ git clone https://github.com/vsoch/cssclean
+$ cd cssclean
+$ pip install -e .
+```
+
+or install from pip.
+
+```bash
+$ pip install cssclean
+```
+
+And then try running an example! 
+
+### Clean Directory
+
+Below we ask cssclean to automatically detect files (css and html) under the same test directory:
+
+```bash
+$ cssclean clean --css cssclean/tests/testdata/1 --html cssclean/tests/testdata/1
+Sheet style.css has 124 rules.
+Sheet style.css filtered down to 43 rules.
+1 files written:
+  style.css => /home/vanessa/Desktop/Code/unused-css/cssclean/tests/testdata/1/style.clean.css
+```
+
+As you can see, by default, it will write the updated css files alongside the old files but with .clean added.
+Note that you can provide individual files, one at a time too, for both of ``--css``
+and ``--html``.
+
+```bash
+$ cssclean clean --css cssclean/tests/testdata/1 --html cssclean/tests/testdata/1/home.html --html cssclean/tests/testdata/1/table.html
+```
+
+### Dry Run
+
+You can also do a dry run to print the filtered style to the screen:
+
+```bash
+$ cssclean clean --css cssclean/tests/testdata/1 --html cssclean/tests/testdata/1 --dry-run
+...
+.card.yellow {
+  background-color: var(--color-yellow);
+}
+tr {
+  background-color: white;
+}
+```
+
+### SCSS
+
+We have support for cleaning of scss files, meaning that you will generate a final cleaned
+css file from a potential directory of inputs. As an example, we can use the hierarchy under
+test data, where the main style file is ``style.scss`` that includes all the others:
+
+```bash
+$ cssclean clean --css cssclean/tests/testdata/3/style.scss --html cssclean/tests/testdata/3/index.html
+```
+```bash
+Sheet style.css has 638 rules.
+Sheet style.css filtered down to 298 rules.
+1 files written:
+  style.css => cssclean/tests/testdata/3/style.clean.css
+```
+
+All imports are required to exist since we are actually rendering the sass into css
+in the library.
+
+
+### Minify
+
+And you can ask for the same preview, but minified:
+
+```bash
+$ cssclean clean --css cssclean/tests/testdata/1 --html cssclean/tests/testdata/1 --dry-run --minify
+```
+
+Or a non-preview minified:
+
+```bash
+$ cssclean clean --css cssclean/tests/testdata/1 --html cssclean/tests/testdata/1 --outdir ./out --minify
+```
+```bash
+Sheet style.css has 124 rules.
+Sheet style.css filtered down to 43 rules.
+1 files written:
+  style.css => ./out/style.clean.min.css
+```
+
+Note that "min" is added to the saved filename, and also note that this is experimental - I'll need to do
+some more tests and reading to figure out what exactly the process is.
+
+### Output Directory
+
+You can also ask for a custom output directory:
+
+```bash
+$ mkdir out
+$ cssclean clean --css cssclean/tests/testdata/1 --html cssclean/tests/testdata/1 --outdir ./out
+```
+```bash
+Sheet style.css has 124 rules.
+Sheet style.css filtered down to 43 rules.
+1 files written:
+  style.css => ./out/style.clean.css
+```
+
+## Example
+
+As an example, I tested against the usrse site. We can render the site into ``_site``:
+
+```bash
+$ bundle exec jekyll build
+```
+
+and then run the clean in place. Here is the original style file:
+
+```bash
+$ ls -l style.css 
+-rw-rw-r-- 1 dinosaur dinosaur 263390 Jun  4 20:57 style.css
+```
+We can then run the clean in place:
+
+```bash
+$ cssclean clean --css assets/css/style.css --html _site/ --in-place
+Sheet style.css has 4080 rules.
+Sheet style.css filtered down to 1380 rules.
+1 files written:
+  style.css => assets/css/style.css
+```
+
+And what is the new size?
+
+```bash
+$ ls -l style.css 
+-rw-rw-r-- 1 dinosaur dinosaur 201347 Jun  5 15:11 style.css
+```
+
+So there is a huge reduction in size when we first remove unused stuff (-62043 bytes)!
+To summarize:
+
+ - original: ~263KB
+ - cleaned: ~201KB
+
+And importantly, the site looks the same!
+
+![docs/img/usrse-site.png](docs/img/usrse-site.png)
+
+So personally, I don't have issue with the size differences between minified and cleaned - my preference is not to
+minify so I can easily read it, but I can also imagine a workflow where you minify just for the production
+site and the development version is just cleaned. It's really up to you!
+
+## GitHub Action
+
+You can use this tool as a GitHub action! Here's how.
+
+```yaml
+  - name: Test CSS Clean
+    uses: vsoch/cssclean/action/clean@main
+    with:
+      css: cssclean/tests/testdata/1/style.css
+      html: cssclean/tests/testdata/1/table.html cssclean/tests/testdata/1/home.html
+      in_place: true
+```
+
+See the [action](action) folder, action.yml file for more parameters and descriptions.
+
+## Contributors
+
+We use the [all-contributors](https://github.com/all-contributors/all-contributors) 
+tool to generate a contributors graphic below.
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable -->
+<table>
+  <tr>
+    <td align="center"><a href="https://vsoch.github.io"><img src="https://avatars.githubusercontent.com/u/814322?v=4?s=100" width="100px;" alt=""/><br /><sub><b>Vanessasaurus</b></sub></a><br /><a href="https://github.com/vsoch/cssclean/commits?author=vsoch" title="Code">💻</a></td>
+  </tr>
+</table>
+
+<!-- markdownlint-restore -->
+<!-- prettier-ignore-end -->
+
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+## Thanks!
+
+This library wouldn't be possible without these fantastic other libraries!
+
+ - [libsass-python](https://github.com/sass/libsass-python/blob/main/sass.py)
+ - [tinycss2](https://github.com/Kozea/tinycss2)
+ - [BeautifulSoup4](https://www.crummy.com/software/BeautifulSoup/)
+
+
+## TODO
+
+ - add tests for different cases
+ - create GitHub action to detect (fail if unused)
+ - automated release
+ - some kind of summary command that shows used/unused styles, or the count
+ - can we use asp to determine used rules?
+ 
+## License
+
+This code is licensed under the MPL 2.0 [LICENSE](LICENSE).
+
+<img src="https://raw.githubusercontent.com/vsoch/cssclean/main/docs/img/vsoch.jpg" width="100px">
---
+++ packages/cssclean/cssclean-0.0.12.tar.gz/cssclean-0.0.12/cssclean/main/client.py
@@ -0,0 +1,231 @@
+__author__ = "Vanessa Sochat"
+__copyright__ = "Copyright 2022, Vanessa Sochat"
+__license__ = "MPL 2.0"
+
+import os
+import re
+
+import bs4
+import sass
+import tinycss2
+
+import cssclean.utils as utils
+from cssclean.logger import logger
+
+
+def collect_files(contenders, pattern):
+    """
+    Collect files based on allowed extension pattern
+    """
+    paths = []
+    for path in contenders or []:
+        if path == ".":
+            path = os.getcwd()
+        if (
+            os.path.isfile(path)
+            and re.search(pattern, path)
+            and ".clean" not in os.path.basename(path)
+        ):
+            paths.append(path)
+        elif os.path.isdir(path):
+            for p in utils.recursive_find(path):
+                if (
+                    os.path.isfile(p)
+                    and re.search(pattern, p)
+                    and "clean" not in os.path.basename(p)
+                ):
+                    paths.append(p)
+    return paths
+
+
+class Cleaner:
+    def clean(
+        self, css, html, outdir=None, in_place=False, dry_run=False, minify=False
+    ):
+        """
+        Perform a clean operation.
+        """
+        css = collect_files(css, "[.](css|scss)$")
+        html = collect_files(html, "[.](html|html)$")
+        self._ensure_both_filetypes(css, html)
+
+        # Read in html and derive classes and div types
+        elements = self.collect_html_elements(html)
+
+        # Also read in style components
+        styles = self.collect_styles(css)
+
+        # Filter to a final set
+        styles = self.filter_rules(elements, styles)
+
+        # Are we doing a dry run? Just print
+        if dry_run:
+            return self.do_print(styles, minify)
+
+        # Out directory takes preference to in place
+        return self.save_styles(styles, outdir, in_place=in_place, minify=minify)
+
+    def save_styles(self, styles, outdir=None, in_place=False, minify=False):
+        """
+        Save styles to file.
+        """
+        files = {}
+        for filename, sheet in styles.items():
+            rest, ext = os.path.splitext(filename)
+            outfile = rest + ".clean"
+            if minify:
+                outfile += ".min"
+            outfile += ext
+            basename = os.path.basename(outfile)
+            if outdir is not None:
+                outfile = os.path.join(outdir, basename)
+            elif in_place:
+                outfile = filename
+            if minify:
+                utils.write_file(self.minify(sheet), outfile)
+            else:
+                utils.write_file(self.write_sheet(sheet), outfile)
+            files[filename] = outfile
+        return files
+
+    def _ensure_both_filetypes(self, css, html):
+        """
+        A CSS clean requires both css/html files, and we want to give a clear
+        message to the user about what is provided and missing.
+        """
+        if not css and not html:
+            logger.exit("No css or html files detected - check paths!")
+        if not css:
+            logger.exit("No css files detected - check paths!")
+        if not html:
+            logger.exit("No html files detected - check paths!")
+
+    def write_sheet(self, sheet):
+        """
+        Write a sheet to file, not minimized
+        """
+        return "\n".join([x.serialize() for x in sheet])
+
+    def do_print(self, styles, minify=False):
+        """
+        Print styles to the screen
+        """
+        for filename, sheet in styles.items():
+            logger.info(filename)
+            if minify:
+                print(self.minify(sheet))
+                continue
+            for rule in sheet:
+                print(rule.serialize())
+
+    def minify(self, sheet):
+        """
+        Given a list of rules, minify (remove comments, whitespace, newlines)
+        """
+        sheet = [x for x in sheet if not isinstance(x, tinycss2.ast.Comment)]
+        return "".join([re.sub("[\r\n ]+", "", x.serialize()) for x in sheet])
+
+    def filter_rules(self, elements, styles):
+        """
+        Given a set of elements (classes and divs) and style sheets, filter
+        each style sheet down to classes / ids used.
+        """
+        # selectors that are used - we aren't distinguishing between classes/divs
+        # this is a lazy and more conservative approach that at worst will
+        # have styling for an extra class / div with the same name
+        selectors = elements["classes"].union(elements["divs"]).union(elements["ids"])
+
+        for filename, sheet in styles.items():
+
+            basename = os.path.basename(filename)
+            logger.info(f"Sheet {basename} has {len(sheet)} rules.")
+
+            # Updated set of rules
+            rules = []
+            for rule in sheet:
+                # We are conservative and don't try to break apart rules
+                if self.include_rule(rule, selectors):
+                    rules.append(rule)
+
+            styles[filename] = rules
+            logger.info(f"Sheet {basename} filtered down to {len(rules)} rules.")
+
+        return styles
+
+    def include_rule(self, rule, selectors):
+        """
+        Determine if a rule should be included based on known selectors.
+
+        To be conservative we don't break apart rules. We could do this
+        if we are willing to also parse the extra tokens.
+        """
+        keepers = ["root"]
+        if isinstance(rule, tinycss2.ast.Comment):
+            return True
+        if isinstance(rule, tinycss2.ast.WhitespaceToken):
+            return False
+
+        # Keep these, would require deeper parsing (open issue if interested)
+        if rule.type == "at-rule" and rule.at_keyword in ["keyframes", "import"]:
+            return True
+
+        # Check components list based on type
+        # E.g., max width - we have to check components inside
+        if rule.type == "at-rule":
+            comps = rule.content or []
+        else:
+            comps = rule.prelude or []
+        for r in comps:
+            if self._is_qualifier(r):
+                continue
+            if getattr(r, "is_identifier", False) == True and r.value in selectors:
+                return True
+            if r.type == "ident" and r.value in keepers or r.value in selectors:
+                return True
+
+    def _is_qualifier(self, r):
+        """
+        A qualifier component is not relevant to making an inclusion decision
+        """
+        if isinstance(r, tinycss2.ast.SquareBracketsBlock):
+            return True
+        if isinstance(r, tinycss2.ast.CurlyBracketsBlock):
+            return True
+        if isinstance(r, tinycss2.ast.FunctionBlock):
+            return True
+        return False
+
+    def collect_styles(self, files):
+        """
+        Assemble list of styles
+        """
+        sheets = {}
+        for filename in files:
+            if filename.endswith("scss"):
+
+                # Always read relative to file
+                with utils.workdir(os.path.dirname(filename)):
+                    css = sass.compile(filename=os.path.basename(filename))
+                converted_file = re.sub("[.]scss$", ".css", filename)
+                sheets[converted_file] = tinycss2.parse_stylesheet(css)
+            else:
+                sheets[filename] = tinycss2.parse_stylesheet(utils.read_file(filename))
+        return sheets
+
+    def collect_html_elements(self, files):
+        """
+        Collect html elements and classes using beautiful soup
+        """
+        elements = {"classes": set(), "divs": set(), "ids": set()}
+        for filename in files:
+            soup = bs4.BeautifulSoup(utils.read_file(filename), "html.parser")
+
+            for e in soup.find_all():
+                elements["divs"].add(e.name)
+
+                # Add specific classes
+                if "class" in e.attrs:
+                    [elements["classes"].add(x) for x in e["class"]]
+                if "id" in e.attrs:
+                    elements["ids"].add(e["id"])
+        return elements
