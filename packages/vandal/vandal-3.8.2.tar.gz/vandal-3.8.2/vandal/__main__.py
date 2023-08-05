f07f35b45554105fd1b85062cb99438a1acfb71c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:56 +0000

    Add package vandal/3.8.2/vandal-3.8.2.tar.gz

---
+++ packages/vandal/vandal-3.8.2.tar.gz/vandal-3.8.2/vandal/__main__.py
@@ -0,0 +1,26 @@
+# imports all relevant contents.
+import vandal
+from vandal.objects import (
+    montecarlo,
+    eoq,
+)
+
+# coloring.
+from colorama import Fore, init
+
+init()
+
+# runs the main CLI client that leads to individual apps.
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--entry', help = 'start the desired application.', type = str,
+    choices = ['montecarlo', 'eoq'])
+    args = parser.parse_args()
+
+    if args.entry == 'montecarlo':
+        print(Fore.YELLOW + '=== ENTERING MONTECARLO CLIENT... ===\n', Fore.RESET)
+        vandal.objects.montecarlo.MCapp()
+    elif args.entry == 'eoq':
+        print(Fore.YELLOW + '=== ENTERING ECONOMIC ORDER QUANTITY CLIENT... ===\n', Fore.RESET)
+        vandal.objects.eoq.EOQapp()
