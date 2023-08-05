fa4e198bc149d0d9817b304c91f3638555c0c51d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:47 +0000

    Add package bvn_values/0.1.1/bvn_values-0.1.1-py2.py3-none-any.whl

---
+++ packages/bvn_values/bvn_values-0.1.1-py2.py3-none-any.whl/bvn_values/values.py
@@ -0,0 +1,34 @@
+company_name = "BVN"
+
+
+def get_bvn_brand_colours():
+    return {
+        "white": "#fff",
+        "gray-light": "#f2f2f2",
+        "gray": "#e0e0e0",
+        "black": "#000",
+        "turquoise": "#55e0e2",
+        "turquoise-light": "#c5f5f6",
+        "purple": "#8680c7",
+        "purple-light": "#d6d3ea",
+        "greenDark": "#25b765",
+        "greenDark-light": "#d6f5e3",
+        "greenLight": "#a5f46e",
+        "greenLight-light": "#e0facc",
+        "greenLime": "#daf62f",
+        "greenLime-light": "#f6fbc6",
+        "yellow": "#fee039",
+        "yellow-light": "#fff9d3",
+        "orange-deep": "#ff9a0f",
+        "orange": "#ffb23a",
+        "orange-light": "#fff5b7",
+        "redBright": "#f9a08f",
+        "redBright-light": "#ffe3dd",
+        "red": "#f0482e",
+        "redDark": "#aa4149",
+        "redDark-light": "#f0d4d6",
+        "blueDark": "#3f70ab",
+        "blueDark-light": "#dae7f7",
+        "blue": "#22a3ef",
+        "blue-light": "#b4dff9",
+    }
