543c02f66f638332d00f3fcd96dad348c5923fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:10 +0000

    Add package neuralspace/0.2.26/neuralspace-0.2.26.tar.gz

---
+++ packages/neuralspace/neuralspace-0.2.26.tar.gz/neuralspace-0.2.26/src/neuralspace/transcription/constants.py
@@ -0,0 +1,7 @@
+TRANSCRIPTION_LANGUAGE_CATALOGUE_URL = "api/transcription/v1/supported-languages"
+TRANSCRIPTION_STREAMING_URL = "v1/live/transcribe"
+LANGUAGE_MODEL_MAP_PATH = "src/neuralspace/transcription/language-model-map.yml"
+
+DOMAIN = "domain"
+SUBURL = "suburl"
+SAMPLE_RATE = "sampleRate"
