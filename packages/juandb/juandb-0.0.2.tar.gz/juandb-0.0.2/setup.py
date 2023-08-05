c866b528f00f6c5ad5d3d0a7bbeb09ade1e82fbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:46 +0000

    Add package juandb/0.0.2/juandb-0.0.2.tar.gz

---
+++ packages/juandb/juandb-0.0.2.tar.gz/juandb-0.0.2/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+setup(
+    name='juandb',
+    packages=['juandb'],
+    version='0.0.2',
+    license='MIT',
+    description='Nice',
+    author='amiwrpremium',
+    author_email='amiwrpremium@gmail.com',
+    url='https://github.com/amiwrpremium/juan',
+    keywords=['NICE', 'JUAN'],
+    install_requires=[
+        'sqlalchemy',
+        'pymysql',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
