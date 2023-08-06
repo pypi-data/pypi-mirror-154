0c2f022f640476c371ea95ce232ea5ce538e768a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:27:08 +0000

    Add package eumdac/1.1.0/eumdac-1.1.0-py3-none-any.whl

---
+++ packages/eumdac/eumdac-1.1.0-py3-none-any.whl/eumdac/__version__.py
@@ -0,0 +1,8 @@
+"""Module containing package information for setup.py and __init__.py"""
+__title__ = "eumdac"
+__description__ = "EUMETSAT Data Access Client"
+__url__ = "https://gitlab.eumetsat.int/eumetlab/data-services/eumdac"
+__version__ = "1.1.0"
+__author__ = "EUMETSAT"
+__author_email__ = "ops@eumetsat.int"
+__license__ = "MIT"
