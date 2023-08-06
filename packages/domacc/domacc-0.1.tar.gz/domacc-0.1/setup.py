63ce208c86c5e6a8a15b4b17bf95f5b51e99d018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:09 +0000

    Add package domacc/0.1/domacc-0.1.tar.gz

---
+++ packages/domacc/domacc-0.1.tar.gz/domacc-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="domacc",
+    version="0.1",
+    description="Domestic accounting python package",
+    url="http://gitlab.com/pepe.bari/domacc",
+    author="Pepe Bari",
+    author_email="pepe.bari@gmail.com",
+    license="MIT",
+    packages=["domacc"],
+    install_requires=["pandas"],
+    zip_safe=False,
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
