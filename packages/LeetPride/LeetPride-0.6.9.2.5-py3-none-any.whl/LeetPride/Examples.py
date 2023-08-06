9c71c1b5e9bcdb4245dae73e083c31506589f5c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:22:40 +0000

    Add package LeetPride/0.6.9.2.5/LeetPride-0.6.9.2.5-py3-none-any.whl

---
+++ packages/LeetPride/LeetPride-0.6.9.2.5-py3-none-any.whl/LeetPride/Examples.py
@@ -0,0 +1,17 @@
+from LP_Examples.LPC_Class_Pride import lpccp_example_main
+from LP_Examples.LPC_Class_Mix import lpccm_example_main
+from LP_Examples.LPC_Method_1 import lpcm1_example_main
+from LP_Examples.LPC_NodeList_1 import lpcnl1_example_main
+
+
+def main() -> int:
+    res_max = 0
+    res_max = max(lpccm_example_main(), res_max)
+    res_max = max(lpccp_example_main(), res_max)
+    res_max = max(lpcnl1_example_main(), res_max)
+    res_max = max(lpcm1_example_main(), res_max)
+    return res_max
+
+
+if __name__ == '__main__':
+    exit(main())
