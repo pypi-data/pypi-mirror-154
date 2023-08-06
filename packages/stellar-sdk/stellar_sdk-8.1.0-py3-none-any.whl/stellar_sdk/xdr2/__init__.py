cfae23128832cc4fdfb891cd84ba06974450db01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:48 +0000

    Add package stellar-sdk/8.1.0/stellar_sdk-8.1.0-py3-none-any.whl

---
+++ packages/stellar-sdk/stellar_sdk-8.1.0-py3-none-any.whl/stellar_sdk/xdr2/__init__.py
@@ -0,0 +1,27 @@
+# Automatically generated on 2022-04-21T08:26:12+08:00
+# DO NOT EDIT or your changes may be overwritten
+from .base import *
+from .color import Color
+from .constants import *
+from .has_stuff import HasStuff
+from .hash import Hash
+from .hashes1 import Hashes1
+from .hashes2 import Hashes2
+from .hashes3 import Hashes3
+from .int1 import Int1
+from .int2 import Int2
+from .int3 import Int3
+from .int4 import Int4
+from .lots_of_my_structs import LotsOfMyStructs
+from .my_struct import MyStruct
+from .nester import Nester
+from .nester_nested_enum import NesterNestedEnum
+from .nester_nested_struct import NesterNestedStruct
+from .nester_nested_union import NesterNestedUnion
+from .opt_hash1 import OptHash1
+from .opt_hash2 import OptHash2
+from .str import Str
+from .str2 import Str2
+from .uint512 import Uint512
+from .uint513 import Uint513
+from .uint514 import Uint514
