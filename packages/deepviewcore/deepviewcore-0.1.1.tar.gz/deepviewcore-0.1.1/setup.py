323a71a5e578f387b17166485129d008b671a9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:47 +0000

    Add package deepviewcore/0.1.1/deepviewcore-0.1.1.tar.gz

---
+++ packages/deepviewcore/deepviewcore-0.1.1.tar.gz/deepviewcore-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['deepviewcore', 'deepviewcore.process', 'deepviewcore.process.filters']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy', 'opencv-python']
+
+setup_kwargs = {
+    'name': 'deepviewcore',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Miguel Martín',
+    'author_email': 'alu0101209777@ull.edu.es',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
