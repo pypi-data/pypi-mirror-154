4ba375e012ee708b48888acf3b4b8280fbfbdab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:14 +0000

    Add package intelmq-webinput-csv/0.5.0/intelmq_webinput_csv-0.5.0-py3-none-any.whl

---
+++ packages/intelmq-webinput-csv/intelmq_webinput_csv-0.5.0-py3-none-any.whl/intelmq_webinput_csv/version.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2017-2018 nic.at GmbH <wagner@cert.at>, 2022 Intevation GmbH <sebastian.wagner@intevation.de>
+# SPDX-License-Identifier: AGPL-3.0
+__version_info__ = (0, 5, 0)
+__version__ = '.'.join(map(str, __version_info__))
