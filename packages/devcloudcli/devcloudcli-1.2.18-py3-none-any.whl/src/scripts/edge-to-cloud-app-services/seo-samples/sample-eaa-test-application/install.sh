f17cf782a8280bd5c4eaaa75cb9af903416ed01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:28:53 +0000

    Add package devcloudcli/1.2.18/devcloudcli-1.2.18-py3-none-any.whl

---
+++ packages/devcloudcli/devcloudcli-1.2.18-py3-none-any.whl/src/scripts/edge-to-cloud-app-services/seo-samples/sample-eaa-test-application/install.sh
@@ -0,0 +1,25 @@
+#Checking if the git is present or not
+if [[ $(which git) && $(git --version) ]]; then
+        true
+else
+        echo "Git is not installed....Installing Git"
+        sudo apt-get update
+        sudo apt-get install git
+fi
+
+DIR="edgeapps"
+if [ -d "$DIR" ]; then
+  # Take action if $DIR exists. #
+  echo "Repository already present"
+else
+  echo "Cloning the git repository"
+  git clone https://github.com/smart-edge-open/edgeapps.git
+  success=$?
+  if [[ $success -eq 0 ]];then
+    echo "Repository successfully cloned.."
+    echo "Please navaigate to edgeapps/applications/sample-app"
+  else
+    echo "Something went wrong!"
+  fi
+fi
+
