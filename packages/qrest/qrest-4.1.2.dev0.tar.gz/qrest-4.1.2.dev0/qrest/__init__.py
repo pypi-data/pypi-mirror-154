6acbcbf3fb9a5698d517c212ac7e7cb72678e5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:03 +0000

    Add package qrest/4.1.2.dev0/qrest-4.1.2.dev0.tar.gz

---
+++ packages/qrest/qrest-4.1.2.dev0.tar.gz/qrest-4.1.2.dev0/qrest/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "4.1.2-dev"
+
+from .resource import JSONResource  # noqa: F401
+from .conf import APIConfig, ResourceConfig, BodyParameter, QueryParameter  # noqa: F401
+from .conf import FileParameter  # noqa: F401
+from .exception import RestClientConfigurationError  # noqa: F401
+from .resource import API  # noqa: F401
