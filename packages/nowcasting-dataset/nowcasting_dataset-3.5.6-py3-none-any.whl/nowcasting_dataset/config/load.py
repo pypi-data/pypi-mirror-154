3bc6e193e5940f5cd857225c565b826c03282823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:08 +0000

    Add package nowcasting-dataset/3.5.6/nowcasting_dataset-3.5.6-py3-none-any.whl

---
+++ packages/nowcasting-dataset/nowcasting_dataset-3.5.6-py3-none-any.whl/nowcasting_dataset/config/load.py
@@ -0,0 +1,31 @@
+""" Loading configuration functions """
+import logging
+from typing import Union
+
+import fsspec
+import yaml
+from pathy import Pathy
+
+from nowcasting_dataset.config.model import Configuration
+
+logger = logging.getLogger(__name__)
+
+
+def load_yaml_configuration(filename: Union[str, Pathy]) -> Configuration:
+    """
+    Load a yaml file which has a configuration in it
+
+    Args:
+        filename: the file name that you want to load.  Will load from local, AWS, or GCP
+            depending on the protocol suffix (e.g. 's3://bucket/config.yaml').
+
+    Returns:pydantic class
+
+    """
+    # load the file to a dictionary
+    with fsspec.open(filename, mode="r") as stream:
+        configuration = yaml.safe_load(stream)
+    # turn into pydantic class
+    configuration = Configuration(**configuration)
+
+    return configuration
