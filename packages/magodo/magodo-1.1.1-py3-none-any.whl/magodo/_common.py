731c26b0f02f9a2161cd4ef94d7e0b9fe7f80db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:39 +0000

    Add package magodo/1.1.1/magodo-1.1.1-py3-none-any.whl

---
+++ packages/magodo/magodo-1.1.1-py3-none-any.whl/magodo/_common.py
@@ -0,0 +1,11 @@
+"""Shared utility functions / constants / classes."""
+
+from __future__ import annotations
+
+from typing import Final
+
+from .types import Priority
+
+
+DEFAULT_PRIORITY: Final[Priority] = "O"
+PUNCTUATION: Final = ",.?!;"
