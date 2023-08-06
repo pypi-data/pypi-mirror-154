01da30dcc08071b64b8a7d579eb727090d452483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:22 +0000

    Add package latexpages/0.7.2/latexpages-0.7.2.zip

---
+++ packages/latexpages/latexpages-0.7.2.zip/latexpages-0.7.2/latexpages/cleaning.py
@@ -0,0 +1,62 @@
+"""Remove intermediate and/or output files."""
+
+from fnmatch import fnmatch
+import os
+import shutil
+
+from . import jobs
+from . import tools
+
+__all__ = ['clean']
+
+
+def clean(config, *, clean_output=None):
+    job = jobs.Job(config)
+    with tools.chdir(job.config_dir):
+        in_parts = list(matched_files(job.to_clean(), job.clean_parts, job.clean_except))
+        if job.clean_output or clean_output:
+            in_output = list(output_files(job.directory))
+            if not in_parts and not in_output:
+                return
+            print('\n'.join(in_parts))
+            print('\n'.join(in_output))
+            msg = (f'...delete {len(in_parts)} files matched in parts'
+                   f' and {len(in_output)} files removing {job.directory}?')
+            if tools.confirm(msg):
+                remove(in_parts, directory=job.directory)
+        elif in_parts:
+            print('\n'.join(in_parts))
+            if tools.confirm(f'...delete {len(in_parts)} files matched in parts?'):
+                remove(in_parts)
+
+
+def matched_files(dirs, patterns, except_patterns):
+    for d in dirs:
+        if os.path.isabs(d):
+            raise ValueError(f'non-relative path: {d!r}')
+
+        for f in sorted(os.listdir(d)):
+            path = os.path.join(d, f)
+            if not os.path.isfile(path):
+                continue
+
+            match = any(fnmatch(path, p) for p in patterns)
+            match = match and not any(fnmatch(path, e) for e in except_patterns)
+            if match:
+                yield path
+
+
+def output_files(directory):
+    if os.path.isabs(directory):
+        raise ValueError(f'non-relative path: {directory!r}')
+
+    for root, dirs, files in os.walk(directory):
+        for f in sorted(files):
+            yield os.path.join(root, f)
+
+
+def remove(files, *, directory=None):
+    for f in files:
+        os.remove(f)
+    if directory is not None:
+        shutil.rmtree(directory)
---
+++ packages/latexpages/latexpages-0.7.2.zip/latexpages-0.7.2/setup.py
@@ -0,0 +1,47 @@
+import pathlib
+from setuptools import setup, find_packages
+
+setup(
+    name='latexpages',
+    version='0.7.2',
+    author='Sebastian Bank',
+    author_email='sebastian.bank@uni-leipzig.de',
+    description='Combine LaTeX docs into a single PDF',
+    keywords='pdfpages parallel compilation proceedings',
+    license='MIT',
+    url='https://github.com/xflr6/latexpages',
+    project_urls={
+        'Issue Tracker': 'https://github.com/xflr6/latexpages/issues',
+        'CI': 'https://github.com/xflr6/latexpages/actions',
+    },
+    packages=find_packages(),
+    entry_points={'console_scripts': [
+        'latexpages=latexpages.__main__:main',
+        'latexpages-paginate=latexpages.__main__:main_paginate',
+        'latexpages-clean=latexpages.__main__:main_clean'
+    ]},
+    package_data={'latexpages': ['template.tex', 'settings.ini']},
+    zip_safe=False,
+    platforms='any',
+    python_requires='>=3.7',
+    extras_require={
+        'dev': ['flake8', 'pep8-naming', 'wheel', 'twine'],
+        'test': ['coverage'],
+    },
+    long_description=pathlib.Path('README.rst').read_text(encoding='utf-8'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Printing',
+        'Topic :: Text Processing :: Markup :: LaTeX',
+    ],
+)
