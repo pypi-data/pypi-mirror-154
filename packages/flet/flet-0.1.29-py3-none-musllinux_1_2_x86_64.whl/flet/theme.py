28b613d83be2697d2f735ff8cc5253506db82c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:00 +0000

    Add package flet/0.1.29/flet-0.1.29-py3-none-musllinux_1_2_x86_64.whl

---
+++ packages/flet/flet-0.1.29-py3-none-musllinux_1_2_x86_64.whl/flet/theme.py
@@ -0,0 +1,14 @@
+import dataclasses
+from dataclasses import field
+
+try:
+    from typing import Literal
+except:
+    from typing_extensions import Literal
+
+
+@dataclasses.dataclass
+class Theme:
+    color_scheme_seed: str = field(default=None)
+    brightness: Literal[None, "dark", "light"] = field(default="light")
+    use_material3: bool = field(default=False)
