1430b9ac26e0b691a8c8598595f4f52fc628cba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:34 +0000

    Add package rectvision/0.1.0/rectvision-0.1.0.tar.gz

---
+++ packages/rectvision/rectvision-0.1.0.tar.gz/rectvision-0.1.0/README.md
@@ -0,0 +1,16 @@
+# rectvision
+Flexibility to Computer Vision - Get the best out of your data
+
+RectVision is an edge intelligence platform that leverages on data, pipeline ochestration and edge deployment of AI systems / models.
+
+![image](img/rectvision.png)
+
+Edge Deployment on:
+
+* FPGAs - FPGA Modules
+* TPUs 
+* GPUs 
+* Mobile Applications - Mobile App modules
+* Drones - Drone modules
+
+`[backend api](http://164.92.64.23/api/docs/#/)`
---
+++ packages/rectvision/rectvision-0.1.0.tar.gz/rectvision-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0' 
+DESCRIPTION = 'Rectvision package'
+LONG_DESCRIPTION = 'This package allows the user to interact with the rectvision platform via code'
+
+# Setting up
+setup(
+        name="rectvision", 
+        version=VERSION,
+        author="Rectlabs",
+        author_email="<team@rectlabs.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. 
+        
+        keywords=['python']
+)
