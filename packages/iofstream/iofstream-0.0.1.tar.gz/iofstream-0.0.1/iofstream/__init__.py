5cfc08b198f8469f7ee9bdb85831e0cf994b958d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:48 +0000

    Add package iofstream/0.0.1/iofstream-0.0.1.tar.gz

---
+++ packages/iofstream/iofstream-0.0.1.tar.gz/iofstream-0.0.1/iofstream/__init__.py
@@ -0,0 +1,7 @@
+from file_functions import write_file
+from file_functions import append_file
+from file_functions import clear_file
+from file_functions import create_file
+from file_functions import write_and_read_file
+from file_functions import read_and_write_file
+from log import Log
