4748ce57e8ea93d29f1e3fa73bab6d39476562a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:27:01 +0000

    Add package hukudo/2.0.0/hukudo-2.0.0.tar.gz

---
+++ packages/hukudo/hukudo-2.0.0.tar.gz/hukudo-2.0.0/hukudo/grafana/models.py
@@ -0,0 +1,28 @@
+import json
+from pathlib import Path
+
+
+class JSONThing:
+    def __init__(self, raw):
+        self.raw = raw
+
+    def __repr__(self):
+        return str(self.raw)[:30]
+
+    @property
+    def id(self):
+        return self.raw['id']
+
+    @property
+    def uid(self):
+        return self.raw['uid']
+
+    @property
+    def title(self):
+        return self.raw['title']
+
+
+class Dashboard(JSONThing):
+    def export(self, path):
+        path = Path(path)
+        path.write_text(json.dumps(self.raw))
