1ab770cb34a13c2aa8ef0510b2c9bdf721c8af69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:04:58 +0000

    Add package Sh-tokatan/0.3.5/Sh_tokatan-0.3.5-py3-none-any.whl

---
+++ packages/Sh-tokatan/Sh_tokatan-0.3.5-py3-none-any.whl/Shotokatan/__init__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Shocurasy
+========
+
+These are functions that facilitate certain actions. Which needs to cite some parts for it to work.
+
+## Features
+
+- **crypto_key** create a key of any size.
+- **sql_create_values** generates the value within the cited table and columns.
+- **sql_create_table** create the table and its columns.
+- **file_to_binary**, onvert digital data to binary format.
+- **hex_to_rgb**. convert hex to rgb.
+- **rgb_to_hex** convert rgb to hex.
+- **cal_math** solve math accounts and show if you want.
+- **token_generate** Just generate a Token with "uuid4".
+- **write_file** It places the desired content in the file and maintains the
+     information that exists within the file..
+- **read_file** Show the current date.
+- **check_date** Show the current time.
+- **check_time** its every key piano on keyboard.
+- **keyboard_piano** its every key piano on keyboard (https://virtualpiano.net/).
+- **check_file** It will check if the file exists.
+- **console** Execute command in computer terminal.
+- **tempo** Delay execution for a given number of milliseconds.
+- **detect_pixel** Check color pixel.
+- **random_num** Random Value.
+- **openweb** Open website with a Key.
+- **pos_color** Shows color and coordinate based on mouse pointer.
+- **sorteio** makes a random choice from a list.
+- **on_off** One key for activate and other to desactivate.
+- **sorteio_porcentagem** Generate random percentage.
+- **click** clicks on the screen based on the mentioned coordinates.
+- **screen** Collects the cited coordinates of the corners. Useful to check the colors of pixels in a certain area.
+- **check_imports** Checks if pip(s) exists, if not, it does the installation if possible.
+- **check_def** Look for "def" in the file behind the return.
+
+"""
+# -**** .
+from Shotokatan.shortcut import *
