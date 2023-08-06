a7b6c25348502c2f5c7fb61167ae9f1091b00c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:24:45 +0000

    Add package albion-similog/0.3.6/albion_similog-0.3.6.tar.gz

---
+++ packages/albion-similog/albion_similog-0.3.6.tar.gz/albion_similog-0.3.6/setup.py
@@ -0,0 +1,58 @@
+from setuptools import find_packages, setup
+
+with open("albion_similog/__init__.py", "r") as f:
+    for line in f:
+        if line.startswith("__version__"):
+            version = line.strip().split("=")[1].strip(" '\"")
+            break
+    else:
+        __version__ = "0.3.6"
+
+with open("README.md", "rb") as f:
+    readme = f.read().decode("utf-8")
+
+install_requires = [
+    "cython~=0.29",
+    "numpy~=1.19",
+    "pandas~=1.3",
+    "scikit-learn~=1.0",
+    "scikit-bio~=0.5",
+    "biopython~=1.78",
+    "changepoint-cython~=0.1",  # numpy and cython must be installed to build
+]
+
+extra_requirements = {
+    "dev": [
+        "pytest~=6.2",
+        "black~=21.12b0",
+        "flake8~=3.8",
+        "pylint~=2.6",
+        "pre-commit~=2.7",
+    ]
+}
+
+setup(
+    name="albion_similog",
+    version=version,
+    description="Compute a consensus dataseries with FAMSA algorithm.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Oslandia",
+    author_email="infos@oslandia.com",
+    maintainer="Oslandia",
+    maintainer_email="infos@oslandia.com",
+    url="",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    install_requires=install_requires,
+    extras_require=extra_requirements,
+    packages=find_packages(),
+)
