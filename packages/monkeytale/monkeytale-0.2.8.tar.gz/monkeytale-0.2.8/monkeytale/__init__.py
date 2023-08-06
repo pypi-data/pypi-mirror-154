ff87ecd53166df0664ca46191b905d01a43ba28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:54 +0000

    Add package monkeytale/0.2.8/monkeytale-0.2.8.tar.gz

---
+++ packages/monkeytale/monkeytale-0.2.8.tar.gz/monkeytale-0.2.8/monkeytale/__init__.py
@@ -0,0 +1,13 @@
+import click
+
+__version__ = "0.2.8"
+
+
+@click.command()
+@click.version_option(version=__version__)
+def cli():
+    click.echo(f"{__file__}!")
+
+
+if __name__ == "__main__":
+    cli()  # pragma: no cover
