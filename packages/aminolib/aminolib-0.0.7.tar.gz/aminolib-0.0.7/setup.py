350e0b35195915c8730c345185e1419dac1be486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:55 +0000

    Add package aminolib/0.0.7/aminolib-0.0.7.tar.gz

---
+++ packages/aminolib/aminolib-0.0.7.tar.gz/aminolib-0.0.7/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+with open("README.md", "r") as stream:
+    long_description = stream.read()
+setuptools.setup(name="aminolib",license = "MIT",version="0.0.7",author="@kingzero#6580",description="Library for Amino",long_description=long_description,long_description_content_type = "text/markdown",install_requires=["datetime","flask","requests","json-minify"],keywords=["amino","aminolib","Lord","AminoAPI"],python_requires=">=3.6",url="https://github.com/EpicSans002/aminolib",project_urls={"aminolib": "https://github.com/EpicSans002/aminolib/issues",},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
