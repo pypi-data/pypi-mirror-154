ec8c90c989df696c8bd9e5fad419a1bdc4d372ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:07 +0000

    Add package fileconfig/0.6.1/fileconfig-0.6.1.zip

---
+++ packages/fileconfig/fileconfig-0.6.1.zip/fileconfig-0.6.1/setup.py
@@ -0,0 +1,37 @@
+import pathlib
+from setuptools import setup, find_packages
+
+setup(
+    name='fileconfig',
+    version='0.6.1',
+    author='Sebastian Bank',
+    author_email='sebastian.bank@uni-leipzig.de',
+    description='Config file sections as objects',
+    keywords='configuration ini file inheritance aliasing',
+    license='MIT',
+    url='https://github.com/xflr6/fileconfig',
+    project_urls={
+        'Issue Tracker': 'https://github.com/xflr6/fileconfig/issues',
+        'CI': 'https://github.com/xflr6/fileconfig/actions',
+        'Coverage': 'https://codecov.io/gh/xflr6/fileconfig',
+    },
+    packages=find_packages(),
+    platforms='any',
+    python_requires='>=3.7',
+    extras_require={
+        'dev': ['tox>=3', 'flake8', 'pep8-naming', 'wheel', 'twine'],
+        'test': ['pytest>=7', 'pytest-cov'],
+    },
+    long_description=pathlib.Path('README.rst').read_text(encoding='utf-8'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
