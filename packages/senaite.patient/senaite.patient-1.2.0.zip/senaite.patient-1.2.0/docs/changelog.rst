8509a70caac3c9768a5feb55b0a418ccba6ddb25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:37 +0000

    Add package senaite.patient/1.2.0/senaite.patient-1.2.0.zip

---
+++ packages/senaite.patient/senaite.patient-1.2.0.zip/senaite.patient-1.2.0/docs/changelog.rst
@@ -0,0 +1,36 @@
+Changelog
+=========
+
+1.2.0 (2022-06-13)
+------------------
+
+- #40 Add setting to display/hide temporary MRN icon in samples listing
+- Fix source distribution package (missing Changelog.rst file)
+
+
+1.1.0 (2022-06-10)
+------------------
+
+- #37 Allow client users to create samples and patients
+- #36 Do not display Patient root folder to users other than lab contact
+- #35 Integrate new DX address field for patients
+- #34 Fix UnicodeDecodeError when storing Patients with special characters
+- #31 Allow patient to receive result reports
+- #30 Add Patient Identifiers
+- #29 Avoid UnknownTimeZoneError when creating patient from Sample
+- #28 Remember age/dob selection on context
+- #27 Fix cannot create samples with empty Patient ID
+
+
+1.0.0 (2022-01-05)
+------------------
+
+- #25 Add Patient ID in samples
+- #24 Migrate patient objects to be folderish
+- #23 Integrate new DX date field and widget
+- #18 Unique patient ID
+- #19 Convert DoB widget to native date input field
+- #17 Added marker interface for patients
+- #14 Compatibility with Senaite catalog migration
+- #8 Added patient workflow and managed permissions
+- #7 Added upgrade step handler for senaite.patient
---
+++ packages/senaite.patient/senaite.patient-1.2.0.zip/senaite.patient-1.2.0/src/senaite/patient/vocabularies.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of SENAITE.PATIENT.
+#
+# SENAITE.PATIENT is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright 2020-2022 by it's authors.
+# Some rights reserved, see README and LICENSE.
+
+from senaite.core.api.geo import get_countries
+from senaite.patient.config import GENDERS
+from senaite.patient.config import NAME_ENTRY_MODES
+from zope.interface import implementer
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+from bika.lims import api
+
+
+@implementer(IVocabularyFactory)
+class IdentifierVocabulary(object):
+
+    def __call__(self, context):
+
+        identifiers = api.get_registry_record("senaite.patient.identifiers")
+
+        items = []
+        for identifier in identifiers:
+            # note: the key will get the submitted value
+            keyword = identifier.get("key")
+            title = identifier.get("value")
+            # value, token, title
+            term = SimpleTerm(keyword, keyword, title)
+            items.append(term)
+        return SimpleVocabulary(sorted(items, key=lambda t: t.title))
+
+
+IdentiierVocabularyFactory = IdentifierVocabulary()
+
+
+@implementer(IVocabularyFactory)
+class GenderVocabulary(object):
+
+    def __call__(self, context):
+        items = [
+            # value, token, title
+            SimpleTerm(value, value, title) for value, title in GENDERS
+        ]
+        return SimpleVocabulary(items)
+
+
+GenderVocabularyFactory = GenderVocabulary()
+
+
+@implementer(IVocabularyFactory)
+class CountryVocabulary(object):
+
+    def __call__(self, context):
+        items = []
+        for country in get_countries():
+            value = country.name
+            token = country.alpha_2
+            title = country.name
+            # value, token, title
+            term = SimpleTerm(value, token, title)
+            items.append(term)
+        return SimpleVocabulary(sorted(items, key=lambda t: t.title))
+
+
+CountryVocabularyFactory = CountryVocabulary()
+
+
+@implementer(IVocabularyFactory)
+class NameEntryModesVocabulary(object):
+
+    def __call__(self, context):
+        items = [
+            # value, token, title
+            SimpleTerm(value, value, title) for value, title in NAME_ENTRY_MODES
+        ]
+        return SimpleVocabulary(items)
+
+
+NameEntryModesVocabularyFactory = NameEntryModesVocabulary()
