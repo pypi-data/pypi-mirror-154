452d78837b3cb445dde32c8eef92a09eec5ec255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:29:51 +0000

    Add package pypdfium2/2.0.0b3/pypdfium2-2.0.0b3.tar.gz

---
+++ packages/pypdfium2/pypdfium2-2.0.0b3.tar.gz/pypdfium2-2.0.0b3/utilities/install.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+# SPDX-FileCopyrightText: 2022 geisserml <geisserml@gmail.com>
+# SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
+
+set -v
+
+python3 -m pip install . -v --no-build-isolation
+rm -f data/.presetup_done.txt
---
+++ packages/pypdfium2/pypdfium2-2.0.0b3.tar.gz/pypdfium2-2.0.0b3/utilities/setup_all.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+# SPDX-FileCopyrightText: 2022 geisserml <geisserml@gmail.com>
+# SPDX-License-Identifier: Apache-2.0 OR BSD-3-Clause
+
+set -v
+
+# build wheels
+whl_targets=(
+    "darwin_arm64"
+    "darwin_x64"
+    "linux_arm64"
+    "linux_arm32"
+    "linux_x64"
+    "linux_x86"
+    "musllinux_x64"
+    "musllinux_x86"
+    "windows_arm64"
+    "windows_x64"
+    "windows_x86"
+)
+for target in ${whl_targets[*]}; do
+    echo "$target"
+    PYP_TARGET_PLATFORM="$target" python3 -m build -n -x --wheel
+done
+
+# package source distribution
+PYP_TARGET_PLATFORM="sdist" python3 -m build -n -x --sdist
