bb309cddf65b230e2be3185c9dadb1f83c52906d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:29 +0000

    Add package hspylib-kafman/0.9.42/hspylib_kafman-0.9.42-py3-none-any.whl

---
+++ packages/hspylib-kafman/hspylib_kafman-0.9.42-py3-none-any.whl/kafman/views/__init__.py
@@ -0,0 +1,15 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-kafman v0.9.38
+#
+# Package: main.kafman.views
+"""Package initialization."""
+
+__all__ = [
+  'filters_dialog', 
+  'indexes', 
+  'main_qt_view', 
+  'promotions', 
+  'settings_dialog'
+]
+__version__ = '0.9.38'
