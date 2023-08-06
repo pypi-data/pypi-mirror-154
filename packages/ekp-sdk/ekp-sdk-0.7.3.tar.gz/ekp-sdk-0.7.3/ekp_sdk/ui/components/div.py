7ef03924ccc83f0240fcafa59f1705d016e8bf09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:27 +0000

    Add package ekp-sdk/0.7.3/ekp-sdk-0.7.3.tar.gz

---
+++ packages/ekp-sdk/ekp-sdk-0.7.3.tar.gz/ekp-sdk-0.7.3/ekp_sdk/ui/components/div.py
@@ -0,0 +1,14 @@
+from ekp_sdk.util.clean_null_terms import clean_null_terms
+
+
+def Div(children=None, class_name=None, style=None, when=None, context=None):
+    return {
+        "_type": "Div",
+        "props": clean_null_terms({
+            "className": class_name,
+            "children": children or [],
+            "style": style,
+            "when": when,
+            "context": context
+        })
+    }
