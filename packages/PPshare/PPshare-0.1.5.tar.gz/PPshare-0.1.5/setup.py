1d79463ac7185c8d9afcdf07947a7376ba6720d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:18:22 +0000

    Add package PPshare/0.1.5/PPshare-0.1.5.tar.gz

---
+++ packages/PPshare/PPshare-0.1.5.tar.gz/PPshare-0.1.5/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name = "PPshare",
+    version = "0.1.5",
+    author="Lv Yiqing",
+    author_email="344599070@qq.com",
+    description = 'a package for sharing',
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    python_requires=">=3.5.0",
+    license = "MIT Licence",
+    classifiers=[
+     "Programming Language :: Python :: 3",
+     "License :: OSI Approved :: MIT License",
+     "Operating System :: OS Independent",
+    ],
+    package_data={"": ["*.py", "*.json", "*.pk", "*.js", "*.zip"]},
+    packages =find_packages(),
+    include_package_data = True,
+    install_requires = ['pandas','requests','tqdm','beautifulsoup4','importlib','pathlib','pypinyin','py_mini_racer'],
+    platforms = "any",
+)
