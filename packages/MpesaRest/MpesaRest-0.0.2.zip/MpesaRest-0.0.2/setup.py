17c9dc55be632e37485452b4babc68b3b98c6eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:34 +0000

    Add package MpesaRest/0.0.2/MpesaRest-0.0.2.zip

---
+++ packages/MpesaRest/MpesaRest-0.0.2.zip/MpesaRest-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+setup(
+    name='MpesaRest',
+    version='0.0.2',
+    description="An interaction of the Safaricom Daraja Api with Python",
+    long_description=open('DESCRIPTION.txt').read(),
+    author='Lumuli Ken Reagan',
+    author_email='lumulikenreagan@gmail.com',
+    install_requires=[
+        "requests>=2.22.0",
+        "SQLAlchemy>=1.4.32"
+    ],
+    url="https://github.com/kenreagan/MpesaRest",
+    license="MIT",
+    classifiers=[
+      "Intended Audience :: Developers",
+      "Programming Language :: Python",
+      "Operating System :: OS Independent"
+    ],
+    py_modules=[
+        "managedb"
+    ],
+    entry_points = {
+        'console_scripts': ['MpesaRest=managedb:main']
+    }
+)
