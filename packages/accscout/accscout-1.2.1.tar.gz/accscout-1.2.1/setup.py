b0e957da6ab139c8c2129c71e7fc3248b2714d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:13 +0000

    Add package accscout/1.2.1/accscout-1.2.1.tar.gz

---
+++ packages/accscout/accscout-1.2.1.tar.gz/accscout-1.2.1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from os import getenv
+
+version = getenv('CI_COMMIT_TAG', 'v0.1')[1:]
+# Try to fall back to version file
+if version == '0.1':
+    try:
+        with open('version.txt', 'r') as f:
+            version= f.read()
+    except:
+        pass
+else:
+    with open('version.txt', 'w') as f:
+            f.write(version)
+
+setup(
+    name = "accscout",
+    version = version,
+    author = "Richard Antal Nagy",
+    author_email="nagy.richard.antal@gmail.com",
+    description="Search for certain user accounts on popular websites on the internet",
+    license = "MIT",
+    keywords = [ "accscout", "account", "security", "hacking", "OSINT", "crawler" ],
+    url = "https://gitlab.com/richardnagy/security/accscout",
+    packages=find_packages(),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Topic :: Security",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pyyaml',
+        'requests',
+    ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'accscout = accscout.__main__:main'
+        ]
+    },
+    python_requires='>=3.8',
+)
