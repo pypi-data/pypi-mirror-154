46cf5d16a3c767e8c77a76a170df782f11d0520b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:57 +0000

    Add package tabdeal-python/0.2.2/tabdeal_python-0.2.2-py3-none-any.whl

---
+++ packages/tabdeal-python/tabdeal_python-0.2.2-py3-none-any.whl/tabdeal/enums.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class SecurityTypes(Enum):
+    TRADE = "TRADE"
+    NONE = "NONE"
+
+
+class RequestTypes(Enum):
+    GET = "GET"
+    POST = "POST"
+    DELETE = "DELETE"
+    PUT = "PUT"
+
+
+class OrderSides(Enum):
+    BUY = "BUY"
+    SELL = "SELL"
+
+
+class OrderTypes(Enum):
+    MARKET = "MARKET"
+    LIMIT = "LIMIT"
+    STOP_LIMIT = "STOP_LOSS_LIMIT"
