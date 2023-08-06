3cb14ffbf28002dc12b63a04ddf2e8fb9efc57e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:25:30 +0000

    Add package CVAT-wrapper/0.0.10/CVAT_wrapper-0.0.10-py3-none-any.whl

---
+++ packages/CVAT-wrapper/CVAT_wrapper-0.0.10-py3-none-any.whl/CVAT/_put.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+# Created by antoine.desruet@epitech.eu at 5/12/22
+
+import abc
+
+from requests import Response
+
+from .Predictions.Interface import IPrediction
+from .data_types import Task
+
+
+class Put:
+    @abc.abstractmethod
+    def __init__(self):
+        raise NotImplementedError
+
+    def upload_predictions(self, task: Task, prediction: IPrediction) -> None:
+        a = prediction.export().to_json()
+        response: Response = self.session.put(
+            url=f'{self.url}/api/tasks/{task.id}/annotations',
+            json=a
+        )
+        if response.status_code != 200:
+            raise Exception(response.content)
