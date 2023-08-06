3db5670f6cfb03b02248a84e373fb283e210556f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:27:20 +0000

    Add package e360-charting/1.10.8/e360_charting-1.10.8-py3-none-any.whl

---
+++ packages/e360-charting/e360_charting-1.10.8-py3-none-any.whl/e360_charting/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '1.10.8'
+
+try:
+    # Attempts to import the client class
+    # Allowed to fail importing so the package metadata can be read for building
+    from ._charts import *  # noqa: F403
+    from .models import AnnotationModel  # noqa: F401
+    from .builders import PlotlyVisualisation, PlotlyConverter  # noqa: F401
+except (ImportError, ModuleNotFoundError):  # pragma: no cover
+    pass  # pragma: no cover
