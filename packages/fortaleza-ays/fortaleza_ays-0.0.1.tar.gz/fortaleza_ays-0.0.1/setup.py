4ee9f63338ce708fad50e4aa1af16d33e7cdef24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:58 +0000

    Add package fortaleza-ays/0.0.1/fortaleza_ays-0.0.1.tar.gz

---
+++ packages/fortaleza-ays/fortaleza_ays-0.0.1.tar.gz/fortaleza_ays-0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="fortaleza_ays",
+    version="0.0.1",
+    author="ayush",
+    description="For learning purpose and testing",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>3.5'
+)
