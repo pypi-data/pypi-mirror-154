2d7af59dd1c10469d0caff343cced8cf0e40183d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:40:48 +0000

    Add package MyDBLogger/1.0.3/MyDBLogger-1.0.3.tar.gz

---
+++ packages/MyDBLogger/MyDBLogger-1.0.3.tar.gz/MyDBLogger-1.0.3/setup.py
@@ -0,0 +1,27 @@
+
+from setuptools import setup
+
+setup(
+    name='MyDBLogger',
+    version='1.0.3',
+    packages=['MyDBLogger'],
+    url='',
+    license='MIT',
+    author='rahim',
+    author_email='zenith.rahim@gmail.com',
+    description='MyDBLogger',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords='RZlogger rest realtime pub/sub and rpc service',
+
+    install_requires=[
+    ],
+)
