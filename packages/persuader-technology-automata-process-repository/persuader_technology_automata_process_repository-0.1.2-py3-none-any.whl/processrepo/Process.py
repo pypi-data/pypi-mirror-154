df5ff55a9a081ee6bf28dd7fadf7c10f1485e64d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:55 +0000

    Add package persuader-technology-automata-process-repository/0.1.2/persuader_technology_automata_process_repository-0.1.2-py3-none-any.whl

---
+++ packages/persuader-technology-automata-process-repository/persuader_technology_automata_process_repository-0.1.2-py3-none-any.whl/processrepo/Process.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass, field
+from enum import Enum
+
+from processrepo.ProcessRunProfile import RunProfile
+
+
+class ProcessStatus(Enum):
+    INITIALIZED = 'initialized'
+    RUNNING = 'running'
+    IDLE = 'idle'
+    ERROR = 'error'
+    STOPPED = 'stopped'
+    UNKNOWN = 'unknown'
+
+    @staticmethod
+    def parse(value):
+        result = [member for name, member in ProcessStatus.__members__.items() if member.value.lower() == value.lower()]
+        return result[0]
+
+
+@dataclass
+class Process:
+    market: str
+    name: str
+    version: str
+    instant: int
+    run_profile: RunProfile = field(default=RunProfile.ASAP)
+    status: ProcessStatus = field(default=ProcessStatus.UNKNOWN)
