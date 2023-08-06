5258273e38acdef69fd4f9ee8eb5e4b495feaf62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:59:22 +0000

    Add package library1-xjl/1.0.0/library1_xjl-1.0.0.tar.gz

---
+++ packages/library1-xjl/library1_xjl-1.0.0.tar.gz/library1_xjl-1.0.0/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 12 23:34:39 2022
+
+@author: 10025
+"""
+
+from setuptools import setup
+
+setup(name = 'library1_xjl',
+      version='1.0.0', 
+      description = "homework1 library",
+      author = "Junlu Xu",
+      author_email = "jx2211@nyu.edu",
+      packages = ['library1_xjl'],
+      install_requires=[],
+      license='MIT LICENSE',
+      platforms=["all"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Software Development :: Libraries'
+      ]
+      ) 
+
+
+
