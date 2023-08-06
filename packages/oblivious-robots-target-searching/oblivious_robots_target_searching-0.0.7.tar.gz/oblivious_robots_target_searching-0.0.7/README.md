84b319ab5bd1145bdaa2248d76c10710b7a39f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:02:34 +0000

    Add package oblivious-robots-target-searching/0.0.7/oblivious_robots_target_searching-0.0.7.tar.gz

---
+++ packages/oblivious-robots-target-searching/oblivious_robots_target_searching-0.0.7.tar.gz/oblivious_robots_target_searching-0.0.7/README.md
@@ -0,0 +1,9 @@
+# Oblivious Robots Target Searching
+
+## Current Version
+
+0.0.7
+
+## Changelog
+
+- Run module directly from the terminal
---
+++ packages/oblivious-robots-target-searching/oblivious_robots_target_searching-0.0.7.tar.gz/oblivious_robots_target_searching-0.0.7/setup.py
@@ -0,0 +1,32 @@
+
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='oblivious_robots_target_searching',
+    version='0.0.7',
+    description='This package provides a powerful Playground for designing and testing algorithms for oblivious robots',
+    description_content_type='text/plain',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author="Sairyö No Developers",
+    author_email='tech@sairyonodevs.in',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Sairyo-No-Developers/oblivious-robots-target-search',
+    keywords='oblivious robots target searching graph',
+    install_requires=[
+          'scipy',
+          'networkx',
+          'matplotlib',
+          'uuid',
+          'names',
+          'pytest'
+      ],
+
+)
