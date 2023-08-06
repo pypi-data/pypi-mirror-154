4167eaf7659229ec7b90c9433e81b007b6668f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:12 +0000

    Add package pycamel/0.0.1/pycamel-0.0.1.tar.gz

---
+++ packages/pycamel/pycamel-0.0.1.tar.gz/pycamel-0.0.1/pycamel/__init__.py
@@ -0,0 +1,12 @@
+"""
+:authors: Yurii Abramenko
+:license: Apache License, Version 2.0, see LICENSE file
+:copyright: (c) 2022 SolveMeSolutions
+"""
+
+from .src.modules import *
+from .src.utils import *
+
+__author__ = 'Yurii Abramenko'
+__version__ = '0.0.1'
+__email__ = 'yura.abramenko1@gmail.com'
