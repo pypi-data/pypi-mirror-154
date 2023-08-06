82e2beb0074760039bd74437ef4672803967ca48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:27 +0000

    Add package hsluv-restored/5.0.2.post1/hsluv-restored-5.0.2.post1.tar.gz

---
+++ packages/hsluv-restored/hsluv-restored-5.0.2.post1.tar.gz/hsluv-restored-5.0.2.post1/setup.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+from setuptools import setup
+
+from hsluv import __version__
+
+setup(
+    name='hsluv-restored',
+    version=__version__+'-1',
+    description='Human-friendly HSL',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license="MIT",
+    author_email="alexei@boronine.com",
+    url="https://www.hsluv.org",
+    keywords="color hsl cie cieluv colorwheel hsluv hpluv",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    python_requires='>=3.6',
+    setup_requires=[
+        'setuptools>=38.6.0',  # for long_description_content_type
+    ],
+    py_modules=["hsluv"],
+    test_suite="tests.test_hsluv"
+)
