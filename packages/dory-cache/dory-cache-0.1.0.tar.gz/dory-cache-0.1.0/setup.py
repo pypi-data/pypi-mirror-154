c8167473bb6e4d9af359c763028c433b076c3524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:46 +0000

    Add package dory-cache/0.1.0/dory-cache-0.1.0.tar.gz

---
+++ packages/dory-cache/dory-cache-0.1.0.tar.gz/dory-cache-0.1.0/setup.py
@@ -0,0 +1,33 @@
+"""
+Setup project and PyPi package
+"""
+from setuptools import setup, find_packages
+from pathlib import Path
+import os
+
+# read the contents of your README file
+directory = Path(__file__).parent
+
+setup(
+    name="dory-cache",
+    version=os.getenv("VERSION"),
+    license="MIT",
+    author="Sören Rifé",
+    author_email="sorenrife@gmail.com",
+    long_description=(directory / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    packages=find_packages("dory"),
+    package_dir={"": "dory"},
+    python_requires=">=3.8",
+    url="https://github.com/sorenrife/dory",
+    keywords="dory smart cache",
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
