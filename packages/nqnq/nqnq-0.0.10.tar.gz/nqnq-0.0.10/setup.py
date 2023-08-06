272191b993d5cab4117347c1f5e30c505cd724ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:33 +0000

    Add package nqnq/0.0.10/nqnq-0.0.10.tar.gz

---
+++ packages/nqnq/nqnq-0.0.10.tar.gz/nqnq-0.0.10/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="nqnq",
+    version="0.0.10",
+    author="nanangqq",
+    author_email="gingggg@gmail.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    package_data={"": ["src/nq/data/*.pkl"]},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+    install_requires=["requests", "six", "pyhwp"],
+)
