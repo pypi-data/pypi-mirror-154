67bda6bbb73ac9d53025df9af361db8cfa61eafd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:19 +0000

    Add package pylocatorconvt/1.0.0/pylocatorconvt-1.0.0.tar.gz

---
+++ packages/pylocatorconvt/pylocatorconvt-1.0.0.tar.gz/pylocatorconvt-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'pylocatorconvt',
+    version          = '1.0.0',
+    description      = 'Appium Inspector Locator Creation Helper',
+    author           = 'jiwon.lee',
+    author_email     = 'easyone.jio@gmail.com',
+    url              = '',
+    download_url     = '',
+    install_requires = ['pillow'],
+	include_package_data=True,
+	packages=find_packages(),
+    keywords         = ['LOCATORCONVERTER', 'locatorconverter'],
+    python_requires  = '>=3',
+    zip_safe=False,
+    classifiers      = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+) 
