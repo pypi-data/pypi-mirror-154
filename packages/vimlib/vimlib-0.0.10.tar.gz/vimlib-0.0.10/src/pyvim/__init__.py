529c19f9d0f33f0d9e02fb036c46a6afd6718355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:48 +0000

    Add package vimlib/0.0.10/vimlib-0.0.10.tar.gz

---
+++ packages/vimlib/vimlib-0.0.10.tar.gz/vimlib-0.0.10/src/pyvim/__init__.py
@@ -0,0 +1,20 @@
+from pyvim.current import current
+from pyvim.environment import env
+from pyvim.environment import environment
+from pyvim.function import Function
+from pyvim.highlighting import highlight_groups
+from pyvim.job import Job
+from pyvim.print import echo
+from pyvim.register import Register
+
+
+__all__ = [
+    "current",
+    "env",
+    "environment",
+    "echo",
+    "highlight_groups",
+    "Function",
+    "Register",
+    "Job",
+]
