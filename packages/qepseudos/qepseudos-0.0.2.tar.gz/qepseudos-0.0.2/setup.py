8b199ee4a1ec3dba64582e2c8d11f6e901ff38aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:50:09 +0000

    Add package qepseudos/0.0.2/qepseudos-0.0.2.tar.gz

---
+++ packages/qepseudos/qepseudos-0.0.2.tar.gz/qepseudos-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import glob
+  
+with open("README.md", 'r') as f:
+    long_description = f.read()
+  
+setup(
+        name ='qepseudos',
+        version ='0.0.2',
+        author ='Harish PVV',
+        author_email ='harishpvv@gmail.com',
+        description ="Pseudo potentials for QuantumEspresso calculations",
+        long_description = long_description,
+        long_description_content_type ="text/markdown",
+        license ='MIT',
+        packages = find_packages(),
+        classifiers =(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ),
+        keywords ='quantum espresso pseudo potentials dft harishpvv',
+        zip_safe = False,
+        data_files = [("pseudos/PBE/", glob.glob("src/qepseudos/pseudos/PBE_ONCV/*.UPF")),
+                      ("pseudos/LDA/", glob.glob("src/qepseudos/pseudos/LDA_ONCV/*.UPF")),
+                      ("pseudos/PBESOL/", glob.glob("src/qepseudos/pseudos/PBESOL_ONCV/*.UPF"))],
+        include_package_data = True
+
+        )
---
+++ packages/qepseudos/qepseudos-0.0.2.tar.gz/qepseudos-0.0.2/src/qepseudos/qepseudos.py
@@ -0,0 +1,15 @@
+import os
+
+elements = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
+
+ps_dir = os.path.dirname(__file__)+"/pseudos/"
+
+pbe = {}
+lda = {}
+pbesol = {}
+
+for i in elements:
+    pbe[i] = f"{ps_dir}/PBE/{i}.UPF"
+    lda[i] = f"{ps_dir}/LDA/{i}.UPF"
+    pbesol[i] = f"{ps_dir}/PBESOL/{i}.UPF"
+
