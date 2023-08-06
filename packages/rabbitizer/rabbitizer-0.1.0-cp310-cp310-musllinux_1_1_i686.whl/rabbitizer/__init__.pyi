9c24e59c8c143c3019e34fe1e0e219369f075b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:35 +0000

    Add package rabbitizer/0.1.0/rabbitizer-0.1.0-cp310-cp310-musllinux_1_1_i686.whl

---
+++ packages/rabbitizer/rabbitizer-0.1.0-cp310-cp310-musllinux_1_1_i686.whl/rabbitizer/__init__.pyi
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: © 2022 Decompollaborate
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+from .Utils import *
+
+from .Enum import *
+from .InstrCategory import *
+from .InstrId import *
+
+from .Config import *
+
+from .rabbitizer import *
