9cbc743b0226df484c0d9d4fa8a9c820a777ff94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:53 +0000

    Add package mvinittipdf/1.1/mvinittipdf-1.1-py3-none-any.whl

---
+++ packages/mvinittipdf/mvinittipdf-1.1-py3-none-any.whl/mvinittipdf/pdf2image.py
@@ -0,0 +1,19 @@
+""" This module provides funtions to convert a PDF to image.
+
+"""
+
+class ConverterImage:
+    """
+        A simple converter for converting PDF to image.
+    """
+    def convertImage(self, path):
+        """
+        Convert the given PDF to image.
+
+        Parameters:
+        path (str): the path to a PDF file
+
+        Returns:
+        str: The content of the PDF file as image.
+        """
+        print("Pdf 2 image")
