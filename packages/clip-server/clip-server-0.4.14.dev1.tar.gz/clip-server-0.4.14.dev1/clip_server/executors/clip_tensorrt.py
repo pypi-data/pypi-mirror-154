f43190c0350b1bc099def380dc7d0fd1c21e7e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:26:02 +0000

    Add package clip-server/0.4.14.dev1/clip-server-0.4.14.dev1.tar.gz

---
+++ packages/clip-server/clip-server-0.4.14.dev1.tar.gz/clip-server-0.4.14.dev1/clip_server/executors/clip_tensorrt.py
@@ -0,0 +1,137 @@
+from multiprocessing.pool import ThreadPool
+from typing import Dict
+
+import numpy as np
+from clip_server.executors.helper import (
+    split_img_txt_da,
+    preproc_image,
+    preproc_text,
+    set_rank,
+)
+from clip_server.model import clip
+from clip_server.model.clip_trt import CLIPTensorRTModel
+from jina import Executor, requests, DocumentArray
+
+
+class CLIPEncoder(Executor):
+    def __init__(
+        self,
+        name: str = 'ViT-B/32',
+        device: str = 'cuda',
+        num_worker_preprocess: int = 4,
+        minibatch_size: int = 64,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+        self._preprocess_tensor = clip._transform_ndarray(clip.MODEL_SIZE[name])
+        self._pool = ThreadPool(processes=num_worker_preprocess)
+
+        self._minibatch_size = minibatch_size
+        self._device = device
+
+        import torch
+
+        assert self._device.startswith('cuda'), (
+            f'can not perform inference on {self._device}'
+            f' with Nvidia TensorRT as backend'
+        )
+
+        assert (
+            torch.cuda.is_available()
+        ), "CUDA/GPU is not available on Pytorch. Please check your CUDA installation"
+
+        self._model = CLIPTensorRTModel(name)
+
+        self._model.start_engines()
+
+    def _preproc_images(self, docs: 'DocumentArray'):
+        with self.monitor(
+            name='preprocess_images_seconds',
+            documentation='images preprocess time in seconds',
+        ):
+            return preproc_image(
+                docs,
+                preprocess_fn=self._preprocess_tensor,
+                device=self._device,
+                return_np=False,
+            )
+
+    def _preproc_texts(self, docs: 'DocumentArray'):
+        with self.monitor(
+            name='preprocess_texts_seconds',
+            documentation='texts preprocess time in seconds',
+        ):
+            return preproc_text(docs, device=self._device, return_np=False)
+
+    @requests(on='/rank')
+    async def rank(self, docs: 'DocumentArray', parameters: Dict, **kwargs):
+        await self.encode(docs['@r,m'])
+
+        set_rank(docs)
+
+    @requests
+    async def encode(self, docs: 'DocumentArray', **kwargs):
+        _img_da = DocumentArray()
+        _txt_da = DocumentArray()
+        for d in docs:
+            split_img_txt_da(d, _img_da, _txt_da)
+
+        # for image
+        if _img_da:
+            for minibatch, _contents in _img_da.map_batch(
+                self._preproc_images,
+                batch_size=self._minibatch_size,
+                pool=self._pool,
+            ):
+                with self.monitor(
+                    name='encode_images_seconds',
+                    documentation='images encode time in seconds',
+                ):
+                    minibatch.embeddings = (
+                        self._model.encode_image(minibatch.tensors)
+                        .detach()
+                        .cpu()
+                        .numpy()
+                        .astype(np.float32)
+                    )
+
+                # recover original content
+                try:
+                    _ = iter(_contents)
+                    for _d, _ct in zip(minibatch, _contents):
+                        _d.content = _ct
+                except TypeError:
+                    pass
+
+        # for text
+        if _txt_da:
+            for minibatch, _contents in _txt_da.map_batch(
+                self._preproc_texts,
+                batch_size=self._minibatch_size,
+                pool=self._pool,
+            ):
+                with self.monitor(
+                    name='encode_texts_seconds',
+                    documentation='texts encode time in seconds',
+                ):
+                    minibatch.embeddings = (
+                        self._model.encode_text(minibatch.tensors)
+                        .detach()
+                        .cpu()
+                        .numpy()
+                        .astype(np.float32)
+                    )
+
+                # recover original content
+                try:
+                    _ = iter(_contents)
+                    for _d, _ct in zip(minibatch, _contents):
+                        _d.content = _ct
+                except TypeError:
+                    pass
+
+        # drop tensors
+        docs.tensors = None
+
+        return docs
