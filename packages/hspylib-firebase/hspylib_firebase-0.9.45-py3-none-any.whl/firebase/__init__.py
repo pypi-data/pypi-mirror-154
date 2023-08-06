5b9ba13642f5e5f34f13c453f64f56b456ee66f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:05 +0000

    Add package hspylib-firebase/0.9.45/hspylib_firebase-0.9.45-py3-none-any.whl

---
+++ packages/hspylib-firebase/hspylib_firebase-0.9.45-py3-none-any.whl/firebase/__init__.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-firebase v0.9.43
+#
+# Package: main.firebase
+"""Package initialization."""
+
+__all__ = [
+  '__main__', 
+  'core', 
+  'entity'
+]
+__version__ = '0.9.43'
