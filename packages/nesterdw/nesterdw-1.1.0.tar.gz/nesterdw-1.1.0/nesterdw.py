58cdabe2ae1bcea28558ee2184fd60635aa58974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:31 +0000

    Add package nesterdw/1.1.0/nesterdw-1.1.0.tar.gz

---
+++ packages/nesterdw/nesterdw-1.1.0.tar.gz/nesterdw-1.1.0/nesterdw.py
@@ -0,0 +1,14 @@
+"""This is the nester.py module which contains a function called print_lol() dedicated at printing lists
+including splitting sub-lists down to elements"""
+
+def print_lol(the_list, tab_number):
+    """print_lol() printing elements of the lists,
+    splitting sub-lists down to elements.
+    Each sublist is indented by tab_number tab stops"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, tab_number+1)
+        else:
+            for tab_stop in range(tab_number):
+                print("\t", end='')
+            print(each_item)
