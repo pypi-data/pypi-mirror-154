aa2777aec3b204234aa35709b74301d92578519c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:57:07 +0000

    Add package MMK-parser/0.1.5/MMK_parser-0.1.5.tar.gz

---
+++ packages/MMK-parser/MMK_parser-0.1.5.tar.gz/MMK_parser-0.1.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='MMK_parser',
+    version='0.1.5',
+    license='MIT',
+    author="MMK_group",
+    author_email='maciekgoncerzewicz@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/gmyrianthous/example-publish-pypi',
+    keywords='example project',
+    install_requires=[
+        'bs4',
+        'requests',
+    ],
+)
