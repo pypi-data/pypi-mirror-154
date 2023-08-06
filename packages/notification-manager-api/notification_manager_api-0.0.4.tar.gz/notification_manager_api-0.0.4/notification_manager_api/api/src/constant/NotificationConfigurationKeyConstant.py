e034da72eba0c03ccad518cffc722904a959c1fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:24 +0000

    Add package notification-manager-api/0.0.4/notification_manager_api-0.0.4.tar.gz

---
+++ packages/notification-manager-api/notification_manager_api-0.0.4.tar.gz/notification_manager_api-0.0.4/notification_manager_api/api/src/constant/NotificationConfigurationKeyConstant.py
@@ -0,0 +1,9 @@
+CURRENT_NOTIFICARION_DEGREE = 'notification-manager-api.current-degree'
+
+QUEUE_KEY = 'notification-manager-api.notification-queue.queue-key'
+
+EMITTER_API_KEY = 'notification-manager-api.notification-queue.api-key'
+EMITTER_BASE_URL = 'notification-manager-api.notification-queue.base-url'
+EMITTER_TIMEOUT = 'notification-manager-api.notification-queue.timeout'
+
+NOTIFICATION_API_KEY = 'notification-manager-api.notification-queue.notification-api-key'
