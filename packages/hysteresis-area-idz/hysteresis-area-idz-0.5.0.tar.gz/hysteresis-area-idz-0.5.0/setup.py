607e4935fc0164d923dc7f682702d447d3a5773a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:40 +0000

    Add package hysteresis-area-idz/0.5.0/hysteresis-area-idz-0.5.0.tar.gz

---
+++ packages/hysteresis-area-idz/hysteresis-area-idz-0.5.0.tar.gz/hysteresis-area-idz-0.5.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hysteresis_area_idz']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.22.4,<2.0.0',
+ 'opencv-contrib-python>=4.5.5,<5.0.0',
+ 'opencv-python>=4.5.5,<5.0.0']
+
+setup_kwargs = {
+    'name': 'hysteresis-area-idz',
+    'version': '0.5.0',
+    'description': '',
+    'long_description': None,
+    'author': 'k4w411p0wer',
+    'author_email': 'alexandrvasyutin@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
