220183b694c7dfac4e1b65d006f4f8061cc301c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:17 +0000

    Add package egen111222-my-package23/1.0/egen111222_my_package23-1.0.tar.gz

---
+++ packages/egen111222-my-package23/egen111222_my_package23-1.0.tar.gz/egen111222_my_package23-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='egen111222_my_package23',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Greg Ward',
+      author_email='gward@python.net',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['my_package'],
+     )
