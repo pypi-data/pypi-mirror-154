1ffd58d59f1373f4f5fe213d9923eec2757b15da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:25:30 +0000

    Add package plum-dispatch/1.6/plum-dispatch-1.6.tar.gz

---
+++ packages/plum-dispatch/plum-dispatch-1.6.tar.gz/plum-dispatch-1.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup, Extension
+
+requirements = []
+
+setup(
+    packages=find_packages(exclude=["docs"]),
+    python_requires=">=3.7",
+    install_requires=requirements,
+    ext_modules=[Extension("plum.function", ["plum/function.py"])],
+    include_package_data=True,
+)
