3cc9aef8a17b290d9728936a21d03d053e8fa3e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:26:02 +0000

    Add package sharry-jira-tool/0.1.0/sharry-jira-tool-0.1.0.tar.gz

---
+++ packages/sharry-jira-tool/sharry-jira-tool-0.1.0.tar.gz/sharry-jira-tool-0.1.0/README.md
@@ -0,0 +1,3 @@
+# Jira Tool
+
+This is tool for product owner who need to work with Jira on sorting stories.
---
+++ packages/sharry-jira-tool/sharry-jira-tool-0.1.0.tar.gz/sharry-jira-tool-0.1.0/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import *
+
+setup(
+    name='jira-tool',
+    version='0.1.0',
+    description='Sort Jira stories.',
+    author='Sharry Xu',
+    author_email='sharry.xu@outlook.com'
+)
