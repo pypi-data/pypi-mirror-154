7e932ca3271c4bc7163ec516965e42b2fcf96101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:07 +0000

    Add package gappr10/0.1/gappr10-0.1.tar.gz

---
+++ packages/gappr10/gappr10-0.1.tar.gz/gappr10-0.1/gappr10/__init__.py
@@ -0,0 +1,6 @@
+"""
+A simple utility to work with your personal data collected by the 
+Garmin R10 Approach
+"""
+
+__version__ = "0.1"
---
+++ packages/gappr10/gappr10-0.1.tar.gz/gappr10-0.1/gappr10/gappr10.py
@@ -0,0 +1,3 @@
+""" A simple package to facilitate working with the data logs from the 
+    Garmin R10 Approach Shot Launch Monitor and personal simulator.
+"""
