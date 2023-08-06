0e0426908c323a9665290b4e17a7884e7d83aa7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:46 +0000

    Add package DeepSurrogatepin/0.8/DeepSurrogatepin-0.8.tar.gz

---
+++ packages/DeepSurrogatepin/DeepSurrogatepin-0.8.tar.gz/DeepSurrogatepin-0.8/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+  name = 'DeepSurrogatepin',
+  packages = ['model'],
+  version = '0.8',
+  license='MIT',
+  setup_requires=['wheel'],
+  description = 'Deep surrogate model for the probability of informed trading model',
+  author = 'Guillaume Pavé',
+  author_email = 'guillaumepave@gmail.com',
+  url = 'https://github.com/GuillaumePv/pin_surrogate_model',
+  # download_url = 'https://github.com/AntoineDidisheim/didipack/archive/v0.1.1.tar.gz',    # I explain this later on
+  keywords = ['Machine learning, market microstructure'],
+  install_requires=[            # I get to this in a second
+          'numpy',
+          'pandas',
+          'tensorflow',
+          'tqdm',
+          'tensorflow_probability'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+  include_package_data=True,
+    package_data={'': ['./model_save/Layer_400_400_200_100_swish_Lr0_0005_ADAMoMSE_BATCH_256tr_size_3CM/*.p']},
+)
