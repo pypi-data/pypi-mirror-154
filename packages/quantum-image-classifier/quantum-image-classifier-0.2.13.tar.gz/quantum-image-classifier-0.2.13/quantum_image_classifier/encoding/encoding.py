7b52c84c8912b449ebcd8fd0a314c59a3c47b7f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:29:15 +0000

    Add package quantum-image-classifier/0.2.13/quantum-image-classifier-0.2.13.tar.gz

---
+++ packages/quantum-image-classifier/quantum-image-classifier-0.2.13.tar.gz/quantum-image-classifier-0.2.13/quantum_image_classifier/encoding/encoding.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+class Encoding:
+    """
+    Base class for any encoding of classical data into quantum data.
+    """
+    def __init__(self, name: str) -> None:
+        """
+        Args:
+            name: Name of the encoding.
+        """
+        self.name = name
+
+    def _generate_circuit(self, input_vector: np.ndarray):
+        """
+        Function to generate the circuit with the info of the real data.
+
+        Args:
+            input_vector: Real data
+        Raise:
+            NotImplementedError
+        """
+        raise NotImplementedError()
