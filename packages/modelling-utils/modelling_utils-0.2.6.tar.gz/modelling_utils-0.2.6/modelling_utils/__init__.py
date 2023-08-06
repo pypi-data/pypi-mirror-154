bfba80337fed01354a3fa9bcb1c832e0ed7672fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:25:42 +0000

    Add package modelling-utils/0.2.6/modelling_utils-0.2.6.tar.gz

---
+++ packages/modelling-utils/modelling_utils-0.2.6.tar.gz/modelling_utils-0.2.6/modelling_utils/__init__.py
@@ -0,0 +1,14 @@
+__version__ = '0.2.6'
+__author__ = "Diogo André"
+__date__ = "2022-05-05"
+__annotations__ = "Utility functions and data structure for Analog Integrated Circuit Modelling"
+from .utils import *
+from .read import *
+from .write import *
+from .data import *
+
+def verbose_info():
+    print(f"{__name__}")
+    print(f"Version:        {__version__} ({__date__})")
+    print(f"Author:         {__author__}")
+    print(f"Description:    {__annotations__}")
