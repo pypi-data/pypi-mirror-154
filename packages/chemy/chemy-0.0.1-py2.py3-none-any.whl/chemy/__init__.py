b6cc5e760d497f6090cc861841f06929ecc4ff70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:18 +0000

    Add package chemy/0.0.1/chemy-0.0.1-py2.py3-none-any.whl

---
+++ packages/chemy/chemy-0.0.1-py2.py3-none-any.whl/chemy/__init__.py
@@ -0,0 +1,18 @@
+"""Manage chemical entities.
+
+Import the package::
+
+   import chemy
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   example_function
+   ExampleClass
+"""
+
+__version__ = "0.0.1"  # denote a pre-release for 0.1.0 with 0.1a1
+
+from ._core import ExampleClass, example_function  # noqa
