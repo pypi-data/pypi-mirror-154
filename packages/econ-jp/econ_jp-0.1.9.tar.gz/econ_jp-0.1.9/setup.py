d095a1aa3843c72da1d08eca3b53d940124877ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:59:38 +0000

    Add package econ-jp/0.1.9/econ_jp-0.1.9.tar.gz

---
+++ packages/econ-jp/econ_jp-0.1.9.tar.gz/econ_jp-0.1.9/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+
+
+def _requires_from_file(filename):
+    return open(this_directory / filename).read().splitlines()
+
+
+setup(
+    name="econ_jp",
+    version="0.1.9",
+    license="MIT",
+    install_requirements=_requires_from_file("requirements.txt"),
+    author="HideyukiO",
+    author_email="mazarimono@gmail.com",
+    url="https://github.com/chomoku/econ_jp",
+    description="""This module returns DataFrame of 
+    Japanese economic indicators.""",
+    packages=find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
+
+if __name__ == '__main__':
+    print(_requires_from_file('requirements.txt'))
