f05a7a2cdd813c094816a2d289c884334a934f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:42 +0000

    Add package poetry-demo1507/0.1.1/poetry-demo1507-0.1.1.tar.gz

---
+++ packages/poetry-demo1507/poetry-demo1507-0.1.1.tar.gz/poetry-demo1507-0.1.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poetry_demo1507']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pendulum>=2.1.2,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['add = poetry_demo1507.main:add',
+                     'sub = poetry_demo1507.main:subtract']}
+
+setup_kwargs = {
+    'name': 'poetry-demo1507',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Sarang Mulewa',
+    'author_email': 'sarangmulewa1us@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
