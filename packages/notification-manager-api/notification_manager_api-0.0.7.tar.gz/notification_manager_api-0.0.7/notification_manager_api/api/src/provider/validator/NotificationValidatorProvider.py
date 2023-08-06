3ca1832d83fae32f17919cd436e44add14cd14fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:00 +0000

    Add package notification-manager-api/0.0.7/notification_manager_api-0.0.7.tar.gz

---
+++ packages/notification-manager-api/notification_manager_api-0.0.7.tar.gz/notification_manager_api-0.0.7/notification_manager_api/api/src/provider/validator/NotificationValidatorProvider.py
@@ -0,0 +1,19 @@
+from python_helper import ObjectHelper
+from python_framework import Validator, ValidatorMethod, GlobalException, HttpStatus
+
+
+def buildNotificationValidator():
+
+    @Validator()
+    class NotificationValidator:
+
+        @ValidatorMethod(requestClass=[str])
+        def validateNotificationApiKey(self, notificationApiKey):
+            if ObjectHelper.isNoneOrBlank(notificationApiKey):
+                raise GlobalException(
+                    message = f'Unauthorized',
+                    logMessage = 'Missing current api key',
+                    status = HttpStatus.UNAUTHORIZED
+                )
+
+    return NotificationValidator
