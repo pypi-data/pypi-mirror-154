67a3b995558b77a3cd7f4bbdee8d129bd5218237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:04:37 +0000

    Add package pydacharts/0.1.2/pydacharts-0.1.2-py3-none-any.whl

---
+++ packages/pydacharts/pydacharts-0.1.2-py3-none-any.whl/pydacharts/plugins/datalabels.py
@@ -0,0 +1,54 @@
+from enum import Enum
+from typing import Dict, Optional
+
+from pydantic import BaseModel
+
+from pydacharts.models import Color, Font
+
+Function = str
+
+
+class DataLabelsPlugin(BaseModel):
+    align: str = "center"
+    anchor: str = "center"
+    offset: Optional[int]
+    padding: Optional[int]
+    clip: Optional[bool]
+    font: Optional[Font]
+    labels: Optional[Dict]
+
+    # Found these at https://chartjs-plugin-datalabels.netlify.app/samples/charts/line.html
+    color: Optional[Color]
+    borderRadius: Optional[int]
+
+
+class Label(BaseModel):
+    """
+    Represents a DataLabels Label instance
+    """
+
+    class Anchor(str, Enum):
+        center_ = "center"
+        start = "start"
+        end = "end"
+
+    class Align(str, Enum):
+        center_ = "center"
+        start = "start"
+        end = "end"
+        right = "right"
+        bottom = "bottom"
+        left = "left"
+        top = "top"
+
+    align: Optional[Align]
+    anchor: Optional[Anchor]
+    color: Optional[Color]
+    font: Optional[Font]
+    formatter: Optional[str]
+    offset: Optional[int]
+    opacity: Optional[float]
+
+    padding: Optional[int]
+    borderRadius: Optional[int]
+    backgroundColor: Optional[Color]
