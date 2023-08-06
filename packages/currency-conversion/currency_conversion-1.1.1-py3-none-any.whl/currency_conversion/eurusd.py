3c9afa544d9f7ac90d45e20c11fbbb51ee887be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:29 +0000

    Add package currency-conversion/1.1.1/currency_conversion-1.1.1-py3-none-any.whl

---
+++ packages/currency-conversion/currency_conversion-1.1.1-py3-none-any.whl/currency_conversion/eurusd.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 13 14:27:49 2022
+
+@author: jxu
+"""
+
+from math import sqrt
+from math import isnan
+
+# Define the EURUSD_return class - each instance will store one row from the dataframe
+class EURUSD_return(object):
+    # Variable to store the total number of instantiated objects in this class
+    num = 0
+    # Variable to store the running sum of the return
+    run_sum = 0
+    run_squared_sum = 0
+    run_sum_of_std = 0
+    last_price = -1
+    
+    # Init all the necessary variables when instantiating the class
+    def __init__(self, tick_time, avg_price):
+        
+        # Store each column value into a variable in the class instance
+        self.tick_time = tick_time
+        #self.price = avg_price
+        
+        if EURUSD_return.last_price == -1:
+            hist_return = float('NaN')
+        else:
+            hist_return = (avg_price - EURUSD_return.last_price) / EURUSD_return.last_price
+        
+        self.hist_return = hist_return
+        if isnan(hist_return):
+            EURUSD_return.run_sum = 0
+        else:
+            # Increment the counter
+            if EURUSD_return.num < 5:
+                EURUSD_return.num += 1
+            EURUSD_return.run_sum += hist_return
+        EURUSD_return.last_price = avg_price
+        
+    def add_to_running_squared_sum(self,avg):
+        if isnan(self.hist_return) == False:
+            EURUSD_return.run_squared_sum += (self.hist_return - avg)**2
+    
+    def get_avg(self,pop_value):
+        if isnan(self.hist_return) == False:
+            EURUSD_return.run_sum -= pop_value
+            avg = EURUSD_return.run_sum/(EURUSD_return.num)
+            self.avg_return = avg
+            return avg
+    
+    def get_std(self):
+        if isnan(self.hist_return) == False:
+            std = sqrt(EURUSD_return.run_squared_sum/(EURUSD_return.num))
+            self.std_return = std
+            EURUSD_return.run_sum_of_std += std
+            EURUSD_return.run_squared_sum = 0
+            return std
+    
+    def get_avg_std(self,pop_value):
+        if isnan(self.hist_return) == False:
+            EURUSD_return.run_sum_of_std -= pop_value
+            avg_std = EURUSD_return.run_sum_of_std/(EURUSD_return.num)
+            self.avg_of_std_return = avg_std 
+            return avg_std
---
+++ packages/currency-conversion/currency_conversion-1.1.1-py3-none-any.whl/currency_conversion/usdcny.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 13 14:28:19 2022
+
+@author: jxu
+"""
+from math import sqrt
+from math import isnan
+
+# Define the USDCNY_return class - each instance will store one row from the dataframe
+class USDCNY_return(object):
+    # Variable to store the total number of instantiated objects in this class
+    num = 0
+    # Variable to store the running sum of the return
+    run_sum = 0
+    run_squared_sum = 0
+    run_sum_of_std = 0
+    last_price = -1
+    
+    # Init all the necessary variables when instantiating the class
+    def __init__(self, tick_time, avg_price):
+        
+        # Store each column value into a variable in the class instance
+        self.tick_time = tick_time
+        #self.price = avg_price
+        
+        if USDCNY_return.last_price == -1:
+            hist_return = float('NaN')
+        else:
+            hist_return = (avg_price - USDCNY_return.last_price) / USDCNY_return.last_price
+        
+        self.hist_return = hist_return
+        if isnan(hist_return):
+            USDCNY_return.run_sum = 0
+        else:
+            # Increment the counter
+            if USDCNY_return.num < 5:
+                USDCNY_return.num += 1
+            USDCNY_return.run_sum += hist_return
+        USDCNY_return.last_price = avg_price
+        
+    def add_to_running_squared_sum(self,avg):
+        if isnan(self.hist_return) == False:
+            USDCNY_return.run_squared_sum += (self.hist_return - avg)**2
+    
+    def get_avg(self,pop_value):
+        if isnan(self.hist_return) == False:
+            USDCNY_return.run_sum -= pop_value
+            avg = USDCNY_return.run_sum/(USDCNY_return.num)
+            self.avg_return = avg
+            return avg
+    
+    def get_std(self):
+        if isnan(self.hist_return) == False:
+            std = sqrt(USDCNY_return.run_squared_sum/(USDCNY_return.num))
+            self.std_return = std
+            USDCNY_return.run_sum_of_std += std
+            USDCNY_return.run_squared_sum = 0
+            return std
+    
+    def get_avg_std(self,pop_value):
+        if isnan(self.hist_return) == False:
+            USDCNY_return.run_sum_of_std -= pop_value
+            avg_std = USDCNY_return.run_sum_of_std/(USDCNY_return.num)
+            self.avg_of_std_return = avg_std 
+            return avg_std
