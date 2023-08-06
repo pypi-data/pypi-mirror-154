3e73a88d4a85423a670347cce307a649b957dfa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:18 +0000

    Add package otquickmodule/0.0.1/otquickmodule-0.0.1-py3-none-any.whl

---
+++ packages/otquickmodule/otquickmodule-0.0.1-py3-none-any.whl/otquickmodule/__init__.py
@@ -0,0 +1,10 @@
+"""otquickmodule module"""
+
+from .KernelHerding import KernelHerding
+from .QuadratureWeighting import QuadratureWeighting
+
+__all__ = [
+    "KernelHerding",
+    "QuadratureWeighting",
+]
+__version__ = "0.0.1"
