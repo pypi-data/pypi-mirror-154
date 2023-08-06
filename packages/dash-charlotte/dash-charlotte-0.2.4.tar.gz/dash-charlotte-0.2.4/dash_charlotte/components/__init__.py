826102d506966acc2061bd12e68d483464510846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:12 +0000

    Add package dash-charlotte/0.2.4/dash-charlotte-0.2.4.tar.gz

---
+++ packages/dash-charlotte/dash-charlotte-0.2.4.tar.gz/dash-charlotte-0.2.4/dash_charlotte/components/__init__.py
@@ -0,0 +1,12 @@
+from .box import Box
+from .dashboard import Dashboard
+from .footer import Footer
+from .navbar import Navbar
+
+from .drawer import (
+    Drawer,
+    DrawerSingleItem,
+    DrawerMultiItem,
+    DrawerSubItem,
+    DrawerFooter
+)
---
+++ packages/dash-charlotte/dash-charlotte-0.2.4.tar.gz/dash-charlotte-0.2.4/dash_charlotte/themes/themes.py
@@ -0,0 +1,11 @@
+"""CDN Links for Dash Charlotte Themes.
+
+"""
+
+
+
+ROOT = r'https://gusfurtado.github.io/dash-charlotte/css_themes/'
+
+CHARLOTTE_DARK = ROOT + 'charlotte_dark.min.css'
+CHARLOTTE_LIGHT = ROOT + 'charlotte_light.min.css'
+DRACULA = ROOT + 'dracula.min.css'
