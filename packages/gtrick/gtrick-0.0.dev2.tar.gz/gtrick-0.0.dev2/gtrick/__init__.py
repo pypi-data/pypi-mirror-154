4699831639c00c9c97ff8fe9ba13bfefd8c07dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:38 +0000

    Add package gtrick/0.0.dev2/gtrick-0.0.dev2.tar.gz

---
+++ packages/gtrick/gtrick-0.0.dev2.tar.gz/gtrick-0.0.dev2/gtrick/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# TODO: add version information here
+from .flag import FLAG
+from .fingerprint import ogb2fp, smiles2fp
+from .random_feature import random_feature
---
+++ packages/gtrick/gtrick-0.0.dev2.tar.gz/gtrick-0.0.dev2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="gtrick",
+    version="0.0.dev2",
+    author="sangyx",
+    author_email="sangyunxin@gmail.com",
+    description="Bag of Tricks for Graph Neural Networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sangyx/gtrick",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "torch",
+        "numpy",
+        "networkx"
+    ],
+    python_requires=">=3",
+)
