c94d2cd8ca5b62718f16006c77fe4f813d18cc45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:38 +0000

    Add package mandeintegration/0.1.6/mandeintegration-0.1.6-py3-none-any.whl

---
+++ packages/mandeintegration/mandeintegration-0.1.6-py3-none-any.whl/mandeintegration/data.py
@@ -0,0 +1,114 @@
+data ={
+  "infrastructure": [
+    {
+      "description": "A.1 Irrigation System", 
+      "current_spend_lc": "23", 
+      "spend_to_date": "50500.00"
+    },
+    {
+      "description": "A.2 Fence", 
+      "current_spend_lc": "45", 
+      "spend_to_date": "34000.00"
+    },
+    {
+      "description": "A.3 Office Space & Warehouse", 
+      "current_spend_lc": "300", 
+      "spend_to_date": "100000.00"
+    },
+  ],
+  
+  "equipment purchases": [
+    {
+      "description": "B.1 IT & Office Furniture", 
+      "current_spend_lc": "200", 
+      "spend_to_date": "12800.00"
+    },
+    {
+      "description": "B.2 Small Equipment", 
+      "current_spend_lc": "0", 
+      "spend_to_date": "11000.00"
+    },
+  ],
+  
+  "working capital / input": [
+    {
+      "description": "C.1 Agricultural inputs", 
+      "current_spend_lc": "0", 
+      "spend_to_date": "34490.00"
+    },
+  ],
+  
+  "training": [
+    {
+      "description": "D.1 USADF Reporting", 
+      "current_spend_lc": "0", 
+      "spend_to_date": "456789.00"
+    },
+    {
+      "description": "D.2 Accounting & Internal Control", 
+      "current_spend_lc": "0", 
+      "spend_to_date": "929001.00"
+    },
+    {
+      "description": "D.3 Group Dynamics", 
+      "current_spend_lc": "0", 
+      "spend_to_date": "622298.00"
+    },
+    {
+      "description": "D.4 Organic Market Gardening Techniques", 
+      "current_spend_lc": "0", 
+      "spend_to_date": "37392.00"
+    },
+    {
+      "description": "D.5 Organic Treatment of Crop-Pest", 
+      "current_spend_lc": "0", 
+      "spend_to_date": "98002.00"
+    },
+    {
+      "description": "D.6 Vegetables Processing & Preservation", 
+      "current_spend_lc": "0", 
+      "spend_to_date": "368992.00"
+    },
+    {
+      "description": "D.7 Marketing", 
+      "current_spend_lc": "0", 
+      "spend_to_date": "29983.00"
+    },
+  ],
+  
+  "technical assistance": [
+    {
+      "description": "E.1 Accountant", 
+      "current_spend_lc": "0", 
+      "spend_to_date": "90233.00"
+    },
+    {
+      "description": "E.2 Horticultural Technician", 
+      "current_spend_lc": "0", 
+      "spend_to_date": "5622.00"
+    },
+    {
+      "description": "E.3 Manual of Procedures", 
+      "current_spend_lc": "0", 
+      "spend_to_date": "97772.00"
+    },
+    {
+      "description": "E.4 Sustainability Plan", 
+      "current_spend_lc": "0", 
+      "spend_to_date": "872.00"
+    },
+  ],
+  
+  "administrative support": [
+    {
+      "description": "F.1 Operating Costs", 
+      "current_spend_lc": "0", 
+      "spend_to_date": "29033.00"
+    },
+    {
+      "description": "F.2 Office Supplies", 
+      "current_spend_lc": "0", 
+      "spend_to_date": "12333.00"
+    },
+  ],
+}
