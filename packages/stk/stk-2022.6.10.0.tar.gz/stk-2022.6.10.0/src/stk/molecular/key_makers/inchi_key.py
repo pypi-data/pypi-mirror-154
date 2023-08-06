e3d2ae5509801e773adfb36c3c779b6796b556a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:58 +0000

    Add package stk/2022.6.10.0/stk-2022.6.10.0.tar.gz

---
+++ packages/stk/stk-2022.6.10.0.tar.gz/stk-2022.6.10.0/src/stk/molecular/key_makers/inchi_key.py
@@ -0,0 +1,60 @@
+"""
+InChIKey
+========
+
+"""
+
+from __future__ import annotations
+
+from .molecule import MoleculeKeyMaker
+from .utilities import get_inchi_key
+
+
+class InchiKey(MoleculeKeyMaker):
+    """
+    Used to get the InChIKey of molecules.
+
+    Examples:
+
+        *Adding InChIKey to a Molecule's JSON*
+
+        You want to use the InChIKey as part of a JSON representation
+        of a molecule
+
+        .. testcode:: adding-inchikey-to-a-molecules-json
+
+            import stk
+
+            jsonizer = stk.MoleculeJsonizer(
+                key_makers=(stk.InchiKey(), ),
+            )
+            # Get the JSON representation, including an InChIKey.
+            json = jsonizer.to_json(stk.BuildingBlock('NCCN'))
+
+        .. testcode:: adding-inchikey-to-a-molecules-json
+            :hide:
+
+            assert (
+                json['molecule']['InChIKey']
+                == 'PIICEJLVQHRZGT-UHFFFAOYSA-N'
+            )
+            assert (
+                json['molecule']['InChIKey']
+                == 'PIICEJLVQHRZGT-UHFFFAOYSA-N'
+            )
+
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialize an :class:`.InchiKey` instance.
+
+        """
+
+        super().__init__('InChIKey', get_inchi_key)
+
+    def __str__(self) -> str:
+        return repr(self)
+
+    def __repr__(self) -> str:
+        return 'InchiKey()'
