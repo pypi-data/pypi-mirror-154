91eaee26c52ea721d586316d19ef8c0803ae2930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:00:12 +0000

    Add package aa-srp/1.13.4/aa_srp-1.13.4.tar.gz

---
+++ packages/aa-srp/aa_srp-1.13.4.tar.gz/aa_srp-1.13.4/aasrp/__init__.py
@@ -0,0 +1,6 @@
+"""
+a couple of variable to use throughout the app
+"""
+
+__version__ = "1.13.4"
+__title__ = "Ship Replacement"
---
+++ packages/aa-srp/aa_srp-1.13.4.tar.gz/aa_srp-1.13.4/aasrp/apps.py
@@ -0,0 +1,19 @@
+"""
+app config
+"""
+
+# Django
+from django.apps import AppConfig
+
+# AA SRP
+from aasrp import __version__
+
+
+class AaSrpConfig(AppConfig):
+    """
+    application config
+    """
+
+    name = "aasrp"
+    label = "aasrp"
+    verbose_name = f"AA Ship Replacement v{__version__}"
