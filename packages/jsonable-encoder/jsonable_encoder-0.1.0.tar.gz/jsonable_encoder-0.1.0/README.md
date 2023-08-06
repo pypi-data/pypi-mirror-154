13f9e0dd3a16369eb1e7df117e19f619a724a97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:50 +0000

    Add package jsonable-encoder/0.1.0/jsonable_encoder-0.1.0.tar.gz

---
+++ packages/jsonable-encoder/jsonable_encoder-0.1.0.tar.gz/jsonable_encoder-0.1.0/README.md
@@ -0,0 +1,2 @@
+# JSONable Encoder
+jsonable_encoder provides a pydantic compatible encoder for python objects.
---
+++ packages/jsonable-encoder/jsonable_encoder-0.1.0.tar.gz/jsonable_encoder-0.1.0/setup.py
@@ -0,0 +1,48 @@
+import os
+from setuptools import setup, Extension
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    cythonize = None
+
+
+# https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#distributing-cython-modules
+def no_cythonize(extensions, **_ignore):
+    for extension in extensions:
+        sources = []
+        for sfile in extension.sources:
+            path, ext = os.path.splitext(sfile)
+            if ext in (".pyx", ".py"):
+                if extension.language == "c++":
+                    ext = ".cpp"
+                else:
+                    ext = ".c"
+                sfile = path + ext
+            sources.append(sfile)
+        extension.sources[:] = sources
+    return extensions
+
+
+extensions = [
+    Extension("jsonable_encoder._encoder", ["jsonable_encoder/_encoder.pyx"]),
+]
+
+CYTHONIZE = cythonize is not None
+
+if CYTHONIZE:
+    compiler_directives = {"language_level": 3, "embedsignature": True}
+    extensions = cythonize(extensions, compiler_directives=compiler_directives)
+else:
+    extensions = no_cythonize(extensions)
+
+setup(
+    ext_modules=extensions,
+    install_requires=["pydantic>=1.6.0",],
+    extras_require={
+        "dev": [
+            "cython>=0.29.0,<0.30.0",
+            "pytest>=6.0.0,<7.0.0",
+        ],
+    },
+)
