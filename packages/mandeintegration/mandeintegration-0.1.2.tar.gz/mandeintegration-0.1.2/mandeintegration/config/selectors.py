ded640e8dc5b69b3aef17892a48a370764dfd28b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:34 +0000

    Add package mandeintegration/0.1.2/mandeintegration-0.1.2.tar.gz

---
+++ packages/mandeintegration/mandeintegration-0.1.2.tar.gz/mandeintegration-0.1.2/mandeintegration/config/selectors.py
@@ -0,0 +1,16 @@
+
+
+class BrowserSelector():
+  
+  LOGIN_SUBMIT_BUTTON = "input[type='submit']"
+  LOGIN_USERNAME = "input[name='user']"
+  LOGIN_PASSWORD = "input[name='password']"
+  
+  GRANT_MENU_BAR = 'actiontabbar'
+  GRANT_LIST_CSS_SELECTOR = 'a[id="214"]'
+  GRANT_TABLE_DATA_ROW_CSS_SELECTOR = 'tr'
+  GRANT_TABLE_DATA_COLUMN_CSS_SELECTOR = 'td'
+  GRANT_PROJECT_CODE = "//td[normalize-space()='{}']"
+  
+  
+  
---
+++ packages/mandeintegration/mandeintegration-0.1.2.tar.gz/mandeintegration-0.1.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mandeintegration', 'mandeintegration.config', 'mandeintegration.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['beautifulsoup4>=4.11.1,<5.0.0',
+ 'python-slugify>=6.1.2,<7.0.0',
+ 'selenium>=4.2.0,<5.0.0',
+ 'webdriver-manager>=3.7.0,<4.0.0']
+
+setup_kwargs = {
+    'name': 'mandeintegration',
+    'version': '0.1.2',
+    'description': 'Mande Integration package',
+    'long_description': None,
+    'author': 'Sylvain Kadjo',
+    'author_email': 'sylvain.kadjo@sejen.ci',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
