79c19b724dfbdad5f4a7bc5e6eb04c0928723595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:33 +0000

    Add package ampel-argo/0.1.0a5/ampel_argo-0.1.0a5-py3-none-any.whl

---
+++ packages/ampel-argo/ampel_argo-0.1.0a5-py3-none-any.whl/ampel/argo/models.py
@@ -0,0 +1,11 @@
+from pydantic import Field
+
+from ampel.model.job.JobModel import JobModel
+
+
+class ArgoJobModel(JobModel):
+    name: str = Field(
+        ...,
+        regex="[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*",
+        description="Name of the job template. Must be a lowercase RFC 1123 subdomain name.",
+    )
