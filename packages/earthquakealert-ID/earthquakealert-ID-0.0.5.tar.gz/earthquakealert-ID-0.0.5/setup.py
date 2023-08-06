793b0b1c8f7be7bc33cbd4541390799fd54c983b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:12 +0000

    Add package earthquakealert-ID/0.0.5/earthquakealert-ID-0.0.5.tar.gz

---
+++ packages/earthquakealert-ID/earthquakealert-ID-0.0.5.tar.gz/earthquakealert-ID-0.0.5/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="earthquakealert-ID",
+    version="0.0.5",
+    author="Christopher Lorence",
+    author_email="lorence80@gmail.com",
+    description="Retrieve latest earthquake alert from https://www.bmkg.go.id/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ChristCoding/indonesian-earthquake-alert",
+    project_urls={
+        "Bug Tracker": "https://github.com/ChristCoding/indonesian-earthquake-alert/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
