cdf0c28483db0b870bd30ad32b2c0ecb4366a9c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:48 +0000

    Add package Alto-updateStatus/0.0.1/Alto_updateStatus-0.0.1.tar.gz

---
+++ packages/Alto-updateStatus/Alto_updateStatus-0.0.1.tar.gz/Alto_updateStatus-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="Alto_updateStatus",
+    version="0.0.1",
+    author="Kanokpich",
+    description="Execute PATCH api to update service status",
+    packages=["updateStatus"],
+)
