dc8cbe4a1d3b6e15f483526c7a8d43d4af665c24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:05:02 +0000

    Add package ejerico-harvester/0.0.19/ejerico-harvester-0.0.19.tar.gz

---
+++ packages/ejerico-harvester/ejerico-harvester-0.0.19.tar.gz/ejerico-harvester-0.0.19/src/ejerico/sdk/rdf/impl/organization.py
@@ -0,0 +1,42 @@
+"""TODO doc"""
+
+import sys
+import logging 
+
+from functools import reduce
+
+from rdflib import plugin
+from rdflib import Graph, Literal, URIRef
+from rdflib import RDF, FOAF, XSD, SDO
+from rdflib.store import Store, VALID_STORE
+
+from ejerico.sdk.utils import format_email
+from ejerico.sdk.utils import SEADATANETNameResolver
+
+class OrganizationImpl(object):
+
+    def __init__(self):
+        object.__init__(self)
+        self.address = None
+        self.email = None
+        self.lei_code = None
+        self.name = None
+        self.spatial = None
+        self.phone = None
+        self.url = None
+        self.vcard = None
+
+    def prepare(self):
+        logging.debug("[Organization::prepare] entering method")
+
+        if self.email is not None:
+            self.email = format_email(self.email)
+            for e in [self.email, "mailto:{}" if "mailto:" not in self.email else self.email]:
+                if e not in self.alias: self.alias.append(e)
+
+        is_edmoID =  reduce(lambda x,y: x or "edmo.seadatanet.org" in y, self.alias, False)
+        if not is_edmoID and self.name:
+            edmoID = SEADATANETNameResolver.instance().get_edmoIDByName(self.name)
+            if edmoID is not None:
+                logging.warning("[OrganizationImpl] found edmo")
+            
