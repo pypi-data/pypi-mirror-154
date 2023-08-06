369c11b8c7efc12b23bda42dd462d8f575aa0920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:29:11 +0000

    Add package quantum-image-classifier/0.2.12/quantum-image-classifier-0.2.12.tar.gz

---
+++ packages/quantum-image-classifier/quantum-image-classifier-0.2.12.tar.gz/quantum-image-classifier-0.2.12/quantum_image_classifier/__init__.py
@@ -0,0 +1,16 @@
+"""
+=================================================================
+Quantum image classifier module (:mod:`quantum_image_classifier`)
+=================================================================
+.. currentmodule:: quantum_image_classifier
+
+This is the quantum image classifier implemented by Jorge Vázquez Pérez. The set of algorithms here
+implemented can be used with :mod:`~quantum_image_classifier.NAME_OF_THE_ALGORITHM`.
+"""
+
+from .classifier_algorithms import NearestCentroid
+from .data_treatment import data_generator, data_loader
+from .encoding import UnaryLoader
+from .gates import RBS
+from .visuals import error_graph, variance_error_graph, cloud_point
+from .CESGA_connection import CESGAQPUToBackend
