3a50ac0d98c49afdff448ab789a76e7f34c00c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:34 +0000

    Add package sabana/0.1.1/sabana-0.1.1-py3-none-any.whl

---
+++ packages/sabana/sabana-0.1.1-py3-none-any.whl/sabana/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.1.1"
+
+from .instance import Instance as Instance
+from .instance import InstanceError as InstanceError
---
+++ packages/sabana/sabana-0.1.1-py3-none-any.whl/sabana/common.py
@@ -0,0 +1,6 @@
+def foo_from_bar(name):
+    print("Converting {} to bar".format(name))
+
+
+def bar_from_foo(name):
+    print("Converting {} to foo".format(name))
