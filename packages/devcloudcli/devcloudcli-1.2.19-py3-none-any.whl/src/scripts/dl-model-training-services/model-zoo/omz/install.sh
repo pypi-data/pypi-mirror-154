acbd824cb286c39a92c39d4241cd341c57a6aa28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:24:03 +0000

    Add package devcloudcli/1.2.19/devcloudcli-1.2.19-py3-none-any.whl

---
+++ packages/devcloudcli/devcloudcli-1.2.19-py3-none-any.whl/src/scripts/dl-model-training-services/model-zoo/omz/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+#echo "intel123" | sudo apt-get update
+#sudo apt-get install -y jq
+echo " Download the required models from the DL-model-services"
+
+#wget -r -nH --cut-dirs=6 --no-parent --reject="index.html*"  https://github.com/openvinotoolkit/open_model_zoo/tree/master/models/$1/$2
+
+
+#prerequisite_list=$(jq -r '.eval_configuration | with_entries(select(.value != false)) | keys[]' /home/intel/evalConfig.json)
+#app_list=$(jq -r '.market_configuration | .market_title | .[]' /home/intel/evalConfig.json)
+
+
+#echo ${prerequisite_list}
+#echo ${app_list}
+#omz_downloader --name $1
+
+
