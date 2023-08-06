83357fa8ccada590191a9e2f328ce6efe6a80b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:04:40 +0000

    Add package polars/0.13.46/polars-0.13.46.tar.gz

---
+++ packages/polars/polars-0.13.46.tar.gz/polars-0.13.46/local_dependencies/polars-core/src/series/ops/mod.rs
@@ -0,0 +1,33 @@
+#[cfg(feature = "diff")]
+#[cfg_attr(docsrs, doc(cfg(feature = "diff")))]
+pub mod diff;
+#[cfg(feature = "ewma")]
+#[cfg_attr(docsrs, doc(cfg(feature = "ewma")))]
+mod ewm;
+mod extend;
+#[cfg(feature = "moment")]
+#[cfg_attr(docsrs, doc(cfg(feature = "moment")))]
+pub mod moment;
+mod null;
+#[cfg(feature = "pct_change")]
+#[cfg_attr(docsrs, doc(cfg(feature = "pct_change")))]
+pub mod pct_change;
+#[cfg(feature = "round_series")]
+#[cfg_attr(docsrs, doc(cfg(feature = "round_series")))]
+mod round;
+mod to_list;
+mod unique;
+
+#[derive(Copy, Clone)]
+pub enum NullBehavior {
+    /// drop nulls
+    Drop,
+    /// ignore nulls
+    Ignore,
+}
+
+impl Default for NullBehavior {
+    fn default() -> Self {
+        NullBehavior::Ignore
+    }
+}
