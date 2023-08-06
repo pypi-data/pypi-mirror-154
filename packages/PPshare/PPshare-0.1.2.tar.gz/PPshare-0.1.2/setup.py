415fc94f9e120b7d68a48aeb1d804fcb9cf93533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:55 +0000

    Add package PPshare/0.1.2/PPshare-0.1.2.tar.gz

---
+++ packages/PPshare/PPshare-0.1.2.tar.gz/PPshare-0.1.2/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+
+path = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with open(os.path.join(path, 'README.md')) as f:
+        long_description = f.read()
+except Exception as e:
+    long_description = "Loading macro data based on akshare"
+
+setup(
+    name = "PPshare",
+    version = "0.1.2",
+    author="Lv Yiqing",
+    author_email="344599070@qq.com",
+    description = long_description,
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    python_requires=">=3.5.0",
+    license = "MIT Licence",
+    classifiers=[
+     "Programming Language :: Python :: 3",
+     "License :: OSI Approved :: MIT License",
+     "Operating System :: OS Independent",
+    ],
+    package_data={"": ["*.py", "*.json", "*.pk", "*.js", "*.zip"]},
+    packages =find_packages(),
+    include_package_data = True,
+    install_requires = ['pandas','requests','tqdm','beautifulsoup4','importlib','pathlib','pypinyin','py_mini_racer'],
+    platforms = "any",
+)
