939da3e1255495707fb3c34be837b582a35e1578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:27 +0000

    Add package SynackAPI/0.2.0/SynackAPI-0.2.0.tar.gz

---
+++ packages/SynackAPI/SynackAPI-0.2.0.tar.gz/SynackAPI-0.2.0/src/synack/plugins/targets.py
@@ -0,0 +1,128 @@
+"""plugins/targets.py
+
+Functions related to handling and checking targets
+"""
+
+from .base import Plugin
+
+
+class Targets(Plugin):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for plugin in ['Api', 'Db']:
+            setattr(self,
+                    plugin.lower(),
+                    self.registry.get(plugin)(self.state))
+
+    def build_codename_from_slug(self, slug):
+        """Return a codename for a target given its slug
+
+        Arguments:
+        slug -- Slug of desired target
+        """
+        codename = None
+        targets = self.db.find_targets(slug=slug)
+        if not targets:
+            self.get_registered_summary()
+            targets = self.db.find_targets(slug=slug)
+        if targets:
+            codename = targets[0].codename
+        return codename
+
+    def build_slug_from_codename(self, codename):
+        """Return a slug for a target given its codename"""
+        slug = None
+        targets = self.db.find_targets(codename=codename)
+        if not targets:
+            self.get_registered_summary()
+            targets = self.db.find_targets(codename=codename)
+        if targets:
+            slug = targets[0].slug
+        return slug
+
+    def get_assessments(self):
+        """Check which assessments have been completed"""
+        res = self.api.request('GET', 'assessments')
+        if res.status_code == 200:
+            self.db.add_categories(res.json())
+            return self.db.categories
+
+    def get_credentials(self, **kwargs):
+        """Get Credentials for a target"""
+        target = self.db.find_targets(**kwargs)[0]
+        if target:
+            res = self.api.request('POST',
+                                   'asset/v1/organizations/' +
+                                   target.organization +
+                                   f'/owners/listings/{target.slug}' +
+                                   f'/users/{self.db.user_id}' +
+                                   '/credentials')
+            if res.status_code == 200:
+                return res.json()
+
+    def get_connected(self):
+        """Return information about the currenly selected target"""
+        res = self.api.request('GET', 'launchpoint')
+        if res.status_code == 200:
+            j = res.json()
+            if j['pending_slug'] != '-1':
+                slug = j['pending_slug']
+                status = "Connecting"
+            else:
+                slug = j['slug']
+                status = "Connected"
+            ret = {
+                "slug": slug,
+                "codename": self.build_codename_from_slug(slug),
+                "status": status
+            }
+            return ret
+
+    def get_registered_summary(self):
+        """Get information on your registered targets"""
+        res = self.api.request('GET', 'targets/registered_summary')
+        ret = []
+        if res.status_code == 200:
+            self.db.add_targets(res.json())
+            ret = dict()
+            for t in res.json():
+                ret[t['id']] = t
+        return ret
+
+    def get_unregistered(self):
+        """Get slugs of all unregistered targets"""
+        if not self.db.categories:
+            self.get_assessments()
+        categories = []
+        for c in self.db.categories:
+            if c.passed_practical and c.passed_practical:
+                categories.append(c.id)
+        query = {
+                'filter[primary]': 'unregistered',
+                'filter[secondary]': 'all',
+                'filter[industry]': 'all',
+                'filter[category][]': categories
+        }
+        res = self.api.request('GET', 'targets', query=query)
+        ret = []
+        if res.status_code == 200:
+            self.db.add_targets(res.json(), is_registered=True)
+            for t in res.json():
+                ret.append({'codename': t['codename'], 'slug': t['slug']})
+        return ret
+
+    def set_registered(self, targets=None):
+        """Register all unregistered targets"""
+        if targets is None:
+            targets = self.get_unregistered()
+        data = '{"ResearcherListing":{"terms":1}}'
+        ret = []
+        for t in targets:
+            res = self.api.request('POST',
+                                   f'targets/{t["slug"]}/signup',
+                                   data=data)
+            if res.status_code == 200:
+                ret.append(t)
+        if len(targets) >= 15:
+            ret.extend(self.set_registered())
+        return ret
---
+++ packages/SynackAPI/SynackAPI-0.2.0.tar.gz/SynackAPI-0.2.0/test/test_targets.py
@@ -0,0 +1,262 @@
+"""test_Targets.py
+
+Tests for the Targets Plugin
+"""
+
+import os
+import sys
+import unittest
+
+from unittest.mock import MagicMock
+
+sys.path.insert(0, os.path.abspath(os.path.join(__file__, '../../src')))
+
+import synack  # noqa: E402
+from synack.db.models import Category, Target  # noqa: E402
+
+
+class TargetsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.state = synack._state.State()
+        self.targets = synack.plugins.Targets(self.state)
+        self.targets.api = MagicMock()
+        self.targets.db = MagicMock()
+
+    def test_get_assessments_all_passed(self):
+        """Should return a list of passed assessments"""
+        assessments = [
+            {
+                "category_name": "Cat1",
+                "category_id": "1",
+                "written_assessment": {
+                    "passed": True
+                },
+                "practical_assessment": {
+                    "passed": True
+                }
+            },
+            {
+                "category_name": "Cat2",
+                "category_id": "2",
+                "written_assessment": {
+                    "passed": True
+                },
+                "practical_assessment": {
+                    "passed": True
+                }
+            }
+        ]
+        cat1 = synack.db.models.Category()
+        self.targets.api.request.return_value.status_code = 200
+        self.targets.api.request.return_value.json.return_value = assessments
+        self.targets.db.categories = [cat1]
+        self.assertEqual([cat1], self.targets.get_assessments())
+        self.targets.db.add_categories.assert_called_with(assessments)
+
+    def test_build_slug_from_codename(self):
+        """Should return a slug for a given codename"""
+        ret_targets = [Target(slug="qwerty")]
+        self.targets.db.find_targets.return_value = ret_targets
+        self.assertEqual("qwerty",
+                         self.targets.build_slug_from_codename("qwerty"))
+        self.targets.db.find_targets.assert_called_with(codename="qwerty")
+
+    def test_build_slug_from_codename_no_targets(self):
+        """Should update the targets if empty"""
+        self.targets.db.find_targets.side_effect = [
+            [],
+            [Target(slug="qwerty")]
+        ]
+        calls = [
+            unittest.mock.call(codename="CHONKEYMONKEY"),
+            unittest.mock.call(codename="CHONKEYMONKEY")
+        ]
+        self.targets.get_registered_summary = MagicMock()
+
+        slug = self.targets.build_slug_from_codename("CHONKEYMONKEY")
+        self.assertEqual("qwerty", slug)
+        self.targets.db.find_targets.assert_has_calls(calls)
+        self.targets.get_registered_summary.assert_called_with()
+
+    def test_build_codename_from_slug(self):
+        """Should return a codename for a given slug"""
+        ret_targets = [Target(codename="SLOPPYSLUG")]
+        self.targets.db.find_targets.return_value = ret_targets
+        self.assertEqual("SLOPPYSLUG",
+                         self.targets.build_codename_from_slug("qwfars"))
+        self.targets.db.find_targets.assert_called_with(slug="qwfars")
+
+    def test_build_codename_from_slug_no_targets(self):
+        """Should update the targets if empty"""
+        self.targets.db.find_targets.side_effect = [
+            [],
+            [Target(codename="SLOPPYSLUG")]
+        ]
+        calls = [
+            unittest.mock.call(slug="qwfars"),
+            unittest.mock.call(slug="qwfars")
+        ]
+        self.targets.get_registered_summary = MagicMock()
+        self.assertEqual("SLOPPYSLUG",
+                         self.targets.build_codename_from_slug("qwfars"))
+        self.targets.db.find_targets.assert_has_calls(calls)
+        self.targets.get_registered_summary.assert_called_with()
+
+    def test_get_connected(self):
+        """Should make a request to get the currently selected target"""
+        self.targets.api.request.return_value.status_code = 200
+        self.targets.api.request.return_value.json.return_value = {
+            "pending_slug": "-1",
+            "slug": "qwfars",
+            "status": "connected"
+        }
+        self.targets.build_codename_from_slug = MagicMock()
+        self.targets.build_codename_from_slug.return_value = "SLOPPYSLUG"
+        out = {
+            "slug": "qwfars",
+            "codename": "SLOPPYSLUG",
+            "status": "Connected"
+        }
+        self.assertEqual(out, self.targets.get_connected())
+
+    def test_get_connected_pending(self):
+        """Should return the pending target if one is pending"""
+        self.targets.api.request.return_value.status_code = 200
+        self.targets.api.request.return_value.json.return_value = {
+            "pending_slug": "qwfars",
+            "slug": "",
+            "status": ""
+        }
+        self.targets.build_codename_from_slug = MagicMock()
+        self.targets.build_codename_from_slug.return_value = "SLOPPYSLUG"
+        out = {
+            "slug": "qwfars",
+            "codename": "SLOPPYSLUG",
+            "status": "Connecting"
+        }
+        self.assertEqual(out, self.targets.get_connected())
+
+    def test_get_registered_summary(self):
+        """Should make a request to get basic info about registered targets"""
+        t1 = {
+            "id": "qwfars",
+            "codename": "SLOPPYSLUG",
+            "organization_id": "89yefds",
+            "activated_at": 1633640638,
+            "name": "Bob's Slug Hut",
+            "category": {
+                "id": 1,
+                "name": "Web Application"
+            },
+            "outage_windows": [],
+            "vulnerability_discovery": True
+        }
+        self.targets.api.request.return_value.status_code = 200
+        self.targets.api.request.return_value.json.return_value = [t1]
+        out = {
+            "qwfars": t1
+        }
+        path = 'targets/registered_summary'
+        self.assertEqual(out, self.targets.get_registered_summary())
+        self.targets.api.request.assert_called_with('GET', path)
+
+    def test_get_credentials(self):
+        """Should get credentials for a given target"""
+        target = Target(organization="qwewqe", slug="asdasd")
+        self.targets.db.find_targets = MagicMock()
+        self.targets.api = MagicMock()
+        self.targets.db.find_targets.return_value = [target]
+        self.targets.db.user_id = 'bobby'
+        self.targets.api.request.return_value.status_code = 200
+        self.targets.api.request.return_value.json.return_value = "json_return"
+
+        url = 'asset/v1/organizations/qwewqe/owners/listings/asdasd/users/bobby/credentials'
+
+        self.assertEqual("json_return",
+                         self.targets.get_credentials(codename='SLEEPYSLUG'))
+        self.targets.api.request.assert_called_with('POST', url)
+
+    def test_get_unregistered(self):
+        """Should get a list unregistered targets"""
+        self.targets.db.categories = [
+            Category(id=1, passed_practical=True,  passed_written=True),
+            Category(id=2, passed_practical=True,  passed_written=True),
+            Category(id=3, passed_practical=False, passed_written=False),
+        ]
+        query = {
+            'filter[primary]': 'unregistered',
+            'filter[secondary]': 'all',
+            'filter[industry]': 'all',
+            'filter[category][]': [1, 2]
+        }
+        self.targets.api.request.return_value.status_code = 200
+        unreg = [
+            {
+                "codename": "SLEEPYSLUG",
+                "slug": "1o2h8o"
+            }
+        ]
+        self.targets.api.request.return_value.json.return_value = unreg
+        self.assertEqual(unreg, self.targets.get_unregistered())
+        self.targets.api.request.assert_called_with("GET",
+                                                    "targets",
+                                                    query=query)
+
+    def test_get_unregistered_assessments_empty(self):
+        """Should get a list of unregistered targets"""
+        self.targets.get_assessments = MagicMock()
+        self.targets.db.categories = []
+        query = {
+            'filter[primary]': 'unregistered',
+            'filter[secondary]': 'all',
+            'filter[industry]': 'all',
+            'filter[category][]': []
+        }
+        self.targets.api.request.return_value.status_code = 200
+        unreg = []
+        self.targets.api.request.return_value.json.return_value = unreg
+        self.assertEqual(unreg, self.targets.get_unregistered())
+        self.targets.get_assessments.assert_called_with()
+        self.targets.api.request.assert_called_with("GET",
+                                                    "targets",
+                                                    query=query)
+
+    def test_set_registered(self):
+        """Should register each unregistered target"""
+        self.targets.get_unregistered = MagicMock()
+        unreg = [
+            {
+                "codename": "SLEEPYSLUG",
+                "slug": "1o2h8o"
+            },
+            {
+                "codename": "SLEEPYWALRUS",
+                "slug": "82h934"
+            }
+        ]
+        calls = [
+            unittest.mock.call("POST",
+                               "targets/1o2h8o/signup",
+                               data='{"ResearcherListing":{"terms":1}}'),
+            unittest.mock.call("POST",
+                               "targets/82h934/signup",
+                               data='{"ResearcherListing":{"terms":1}}')
+        ]
+        self.targets.get_unregistered.return_value = unreg
+        self.targets.api.request.return_value.status_code = 200
+        self.assertEqual(unreg, self.targets.set_registered())
+        self.targets.api.request.assert_has_calls(calls)
+
+    def test_set_registered_many(self):
+        """Should call itself again if it has determined the page was full"""
+        self.targets.get_unregistered = MagicMock()
+        t = {
+            "codename": "SLEEPYSLUG",
+            "slug": "1o2h8o"
+        }
+        unreg = []
+        for i in range(0, 15):
+            unreg.append(t)
+        self.targets.get_unregistered.side_effect = [unreg, [t, t]]
+        self.targets.api.request.return_value.status_code = 200
+        self.assertEqual(17, len(self.targets.set_registered()))
