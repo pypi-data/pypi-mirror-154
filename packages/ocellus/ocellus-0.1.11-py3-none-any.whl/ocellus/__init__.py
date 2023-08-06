35e2209953e721b6154e8e4157ae22401a0819ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:38:38 +0000

    Add package ocellus/0.1.11/ocellus-0.1.11-py3-none-any.whl

---
+++ packages/ocellus/ocellus-0.1.11-py3-none-any.whl/ocellus/__init__.py
@@ -0,0 +1,16 @@
+# Copyright © 2022 Byte Motion AB
+import sys
+from os import path
+  
+# Adding the cwd to the path is needed since
+# scripts generated from protos do not take the
+# module name into consideration when importing.
+cwd = path.abspath(path.dirname(__file__))
+sys.path.insert(0, cwd)
+sys.path.insert(0, "{cwd}/api/v1")
+sys.path.insert(0, "{cwd}/google/api")
+sys.path.insert(0, "{cwd}/protoc_gen_swagger/options")
+
+from ocellus.ocellus_types_pb2 import *
+from ocellus.ocellus_module_service_pb2 import *
+from ocellus.ocellus_module_service_pb2_grpc import *
