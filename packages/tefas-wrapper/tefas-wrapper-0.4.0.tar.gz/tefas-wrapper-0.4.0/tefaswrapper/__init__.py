02f3ec5161e731705171f535f9d1da4651dab90e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:52 +0000

    Add package tefas-wrapper/0.4.0/tefas-wrapper-0.4.0.tar.gz

---
+++ packages/tefas-wrapper/tefas-wrapper-0.4.0.tar.gz/tefas-wrapper-0.4.0/tefaswrapper/__init__.py
@@ -0,0 +1,7 @@
+from .wrapper import Wrapper
+from .models import FundType
+from .schema import Fund, History, Allocation
+
+__all__ = ["Wrapper", "Fund", "FundType", "History", "Allocation"]
+
+__version__ = "0.4.0"
