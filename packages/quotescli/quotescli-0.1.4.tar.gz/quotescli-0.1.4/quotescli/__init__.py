7a9b4270828e0d75c85181cbf5a302d2cc8ed633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:05:01 +0000

    Add package quotescli/0.1.4/quotescli-0.1.4.tar.gz

---
+++ packages/quotescli/quotescli-0.1.4.tar.gz/quotescli-0.1.4/quotescli/__init__.py
@@ -0,0 +1,4 @@
+from quotescli import run
+from quotescli import get_celebritie_quote
+from quotescli import show_the_topics
+from quotescli import check_from_topic
---
+++ packages/quotescli/quotescli-0.1.4.tar.gz/quotescli-0.1.4/quotescli/__main__.py
@@ -0,0 +1,4 @@
+import quotescli
+
+if __name__ == "__main__":
+    quotescli.run()
