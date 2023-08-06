5eeac8d7ede19e88031d4b054a09e409ecde979c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:45:41 +0000

    Add package scludam/0.0.2/scludam-0.0.2-py3-none-any.whl

---
+++ packages/scludam/scludam-0.0.2-py3-none-any.whl/scludam/__init__.py
@@ -0,0 +1,8 @@
+"""Scludam.
+
+Star Cluster Detection and Membership Probability Calculation
+
+"""
+
+from .fetcher import Query, search_object, search_table
+from .stat_tests import DipDistTest, HopkinsTest, RipleysKTest
