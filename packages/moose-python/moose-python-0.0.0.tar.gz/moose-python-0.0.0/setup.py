7dbccaf64d7fc11b1ebc5023a75b80ce24114d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:34:02 +0000

    Add package moose-python/0.0.0/moose-python-0.0.0.tar.gz

---
+++ packages/moose-python/moose-python-0.0.0.tar.gz/moose-python-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="moose-python",
+    version="0.0.0",
+    classifiers=['Development Status :: 1 - Planning'],
+    author="The TF Encrypted Authors",
+    author_email="contact@tf-encrypted.io",
+)
