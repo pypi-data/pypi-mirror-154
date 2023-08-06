c880e19ef5a83374eb053f3da1cbcae039c0e0a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:01 +0000

    Add package notification-manager-api/0.0.8/notification_manager_api-0.0.8.tar.gz

---
+++ packages/notification-manager-api/notification_manager_api-0.0.8.tar.gz/notification_manager_api-0.0.8/notification_manager_api/api/src/enumeration/NotificationSeverity.py
@@ -0,0 +1,19 @@
+from python_framework import Enum, EnumItem
+
+
+@Enum()
+class NotificationSeverityEnumeration :
+
+    NONE  = EnumItem(degree=-1)
+
+    DEBUG = EnumItem(degree=0)
+    SETTINGS = EnumItem(degree=1)
+    INFO = EnumItem(degree=2)
+    STATUS = EnumItem(degree=3)
+
+    WARNING = EnumItem(degree=4)
+    FAILURE = EnumItem(degree=5)
+    SUCCESS = EnumItem(degree=6)
+    ERROR = EnumItem(degree=7)
+
+NotificationSeverity = NotificationSeverityEnumeration()
