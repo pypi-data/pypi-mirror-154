b45e38fa1252d3f7f39fcd272c66208120244cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:52 +0000

    Add package bayesml/0.1.1/bayesml-0.1.1.tar.gz

---
+++ packages/bayesml/bayesml-0.1.1.tar.gz/bayesml-0.1.1/bayesml/__init__.py
@@ -0,0 +1,18 @@
+from . import bernoulli
+from . import categorical
+from . import autoregressive
+from . import exponential
+from . import linearregression
+from . import multivariate_normal
+from . import normal
+from . import poisson
+
+__all__ = ['bernoulli',
+           'categorical',
+           'autoregressive',
+           'exponential',
+           'linearregression',
+           'multivariate_normal',
+           'normal',
+           'poisson'
+           ]
