3e76be707dd07e2ed1289630ea94df313cdbefd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:09 +0000

    Add package hspylib/0.11.72/hspylib-0.11.72.tar.gz

---
+++ packages/hspylib/hspylib-0.11.72.tar.gz/hspylib-0.11.72/hspylib/__init__.py
@@ -0,0 +1,14 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v0.11.70
+#
+# Package: main.hspylib
+"""Package initialization."""
+
+__all__ = [
+  '__main__', 
+  'addons', 
+  'core', 
+  'modules'
+]
+__version__ = '0.11.70'
