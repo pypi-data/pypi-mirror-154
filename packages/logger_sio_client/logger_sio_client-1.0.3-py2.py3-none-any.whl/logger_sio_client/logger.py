de4fd0325081c941e33f86f313c27bff5e03900f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:37:27 +0000

    Add package logger_sio_client/1.0.3/logger_sio_client-1.0.3-py2.py3-none-any.whl

---
+++ packages/logger_sio_client/logger_sio_client-1.0.3-py2.py3-none-any.whl/logger_sio_client/logger.py
@@ -0,0 +1,20 @@
+import socketio
+
+class logger:
+    def __init__(self, type, filename = ""):
+        self.type = type
+        if filename == "":
+            self.filename = "mylogger.txt"
+        else:
+            self.filename =  filename
+
+
+    def log(self , data):
+        if self.type == "console":
+            print("console: ", data)
+        elif self.type == "file":
+            f = open(self.filename, "a")
+            f.write(data)
+            f.close()
+        else:
+            print("default: " , data)
