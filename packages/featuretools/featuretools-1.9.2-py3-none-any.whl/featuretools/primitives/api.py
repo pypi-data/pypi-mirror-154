de9c5e22a1c39c551a83d5d2d75a8ed4dc33695c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:39:00 +0000

    Add package featuretools/1.9.2/featuretools-1.9.2-py3-none-any.whl

---
+++ packages/featuretools/featuretools-1.9.2-py3-none-any.whl/featuretools/primitives/api.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+from .standard import *
+from .utils import (
+    get_aggregation_primitives,
+    get_default_aggregation_primitives,
+    get_default_transform_primitives,
+    get_transform_primitives,
+    list_primitives,
+)
