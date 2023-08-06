ec83ef8ce1c5b516cd66039f36ea7ac7775ada4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:28 +0000

    Add package viena-polygon/1.0/viena-polygon-1.0.0.tar.gz

---
+++ packages/viena-polygon/viena-polygon-1.0.0.tar.gz/viena-polygon-1.0.0/src/polygon/cli.py
@@ -0,0 +1,75 @@
+"""This module provides the CLI."""
+# cli-module/cli.py
+
+from pathlib import Path
+from typing import List, Optional
+import typer
+from polygon import __app_name__, __version__, dataset, container,search
+# import dataset
+# import container
+
+app = typer.Typer()
+app.add_typer(dataset.app, name="dataset")
+app.add_typer(container.app, name="container")
+app.add_typer(search.app, name="search")
+
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{__app_name__} v{__version__}")
+        raise typer.Exit()
+
+
+@app.callback()
+
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the application's version and exit.",
+        callback=_version_callback,
+        is_eager=True,
+    )
+) -> None:
+    return
+
+
+@app.command()
+def list(
+    description: List[str] = typer.Argument(...),
+    priority: int = typer.Option(2, "--priority", "-p", min=1, max=3),
+) -> None:
+    """Add a new to-do with a DESCRIPTION."""
+    typer.secho(
+        f"""polygon-cli: list """
+        f"""with priority: {priority}""",
+        fg=typer.colors.GREEN,
+    )
+
+# @app.command()
+# def dataset(
+#     description: List[str] = typer.Argument(...),
+#     priority: int = typer.Option(2, "--priority", "-p", min=1, max=3),
+# ) -> None:
+#     """Add a new to-do with a DESCRIPTION."""
+#     typer.secho(
+#         f"""polygon-cli: list """
+#         f"""with priority: {priority}""",
+#         fg=typer.colors.GREEN,
+#     )
+
+
+
+# @app.command()
+# def container(
+#     description: List[str] = typer.Argument(...),
+#     priority: int = typer.Option(2, "--priority", "-p", min=1, max=3),
+# ) -> None:
+#     """Add a new to-do with a DESCRIPTION."""
+#     typer.secho(
+#         f"""polygon-cli: list """
+#         f"""with priority: {priority}""",
+#         fg=typer.colors.GREEN,
+#     )
+
