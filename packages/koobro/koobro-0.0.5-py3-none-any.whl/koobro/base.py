52a0a74f29c51246282a67bcbd94ccaeec1716ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:16 +0000

    Add package koobro/0.0.5/koobro-0.0.5-py3-none-any.whl

---
+++ packages/koobro/koobro-0.0.5-py3-none-any.whl/koobro/base.py
@@ -0,0 +1,26 @@
+import os
+
+
+def output(value, newline=False):
+    """
+    输出变量
+
+    :param value: 变量
+    :param newline: 是否换行
+    :return:
+    """
+    if newline:
+        print(f'表达式：{repr(value)} \n类型：{type(value)} \n值：{value}')
+    else:
+        print(f'表达式：{repr(value)} ----- 类型：{type(value)} ----- 值：{value}')
+
+
+def create_dir(dir_path):
+    """
+    如果目录不存在，则创建目录
+
+    :param dir_path: 目录路径
+    :return:
+    """
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
