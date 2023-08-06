aab988c0dfb0f23746ed89d027b9005201421ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:35 +0000

    Add package tkt-Toolkit/2.0/tkt-Toolkit-2.0.tar.gz

---
+++ packages/tkt-Toolkit/tkt-Toolkit-2.0.tar.gz/tkt-Toolkit-2.0/README.md
@@ -0,0 +1,14 @@
+# tkt-Toolkit
+****
+## Version : 2.0
+This Package is made and published by the Suprime LLC
+
+Read more about the License in the ``license.txt`` file.
+# Description:
+****
+**Suprime's tkt-Toolkit python package, is a wide collection of packages and scripts, bundled in to
+one package. It is optimized for python 3.8, but it also works with python 3.9,3.7,3.6.
+Not all of the scripts used are made by us.**
+# Contact:
+****
+**Our email support: suprime.sendings@gmail.com**
---
+++ packages/tkt-Toolkit/tkt-Toolkit-2.0.tar.gz/tkt-Toolkit-2.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="tkt-Toolkit",
+    version="2.0",
+    author="Suprime",
+    license = 'MIT',
+    author_email="suprime.sendings@gmail.com",
+    description="Useful collection of python packages and scripts.",
+    long_description="Python package collection, including tracking functionalities.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/MOBSkuchen/tkt-Toolkit",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['httpx','googletrans==3.1.0a0','SpeechRecognition','termcolor','keyboard','piexif','pydub','pygame','pywin32'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
