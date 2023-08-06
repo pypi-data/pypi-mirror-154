3c922e97a990bbb71b3feb5a7a3d550ab4599103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:56:38 +0000

    Add package cdk-monitoring-constructs/0.0.52/cdk_monitoring_constructs-0.0.52-py3-none-any.whl

---
+++ packages/cdk-monitoring-constructs/cdk_monitoring_constructs-0.0.52-py3-none-any.whl/cdk_monitoring_constructs/_jsii/__init__.py
@@ -0,0 +1,25 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import constructs._jsii
+import monocdk._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-monitoring-constructs",
+    "0.0.52",
+    __name__[0:-6],
+    "cdk-monitoring-constructs@0.0.52.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
