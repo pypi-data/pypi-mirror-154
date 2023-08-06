82535483ed8169597694918772633fa6c0f3bf2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:59 +0000

    Add package wsnet/0.0.9/wsnet-0.0.9-py3-none-any.whl

---
+++ packages/wsnet/wsnet-0.0.9-py3-none-any.whl/wsnet/server/wsserver.py
@@ -0,0 +1,58 @@
+import traceback
+import websockets
+import asyncio
+
+from wsnet import logger
+from wsnet.agent.agent import WSNETAgent
+
+
+
+class WSNETWSServer:
+	def __init__(self, listen_ip = '127.0.0.1', listen_port = 8700, ssl_ctx = None):
+		self.listen_ip = listen_ip
+		self.listen_port = listen_port
+		self.ssl_ctx = ssl_ctx
+		self.wsserver = None
+		self.clients = {}
+
+	async def handle_client(self, ws, path):
+		remote_ip, remote_port = ws.remote_address
+		logger.info('Client connected from %s:%d' % (remote_ip, remote_port))
+		client = WSNETAgent(ws)
+		self.clients[client] = 1
+		await client.run()
+		await client.terminate()
+
+	async def run(self):
+		self.wsserver = await websockets.serve(self.handle_client, self.listen_ip, self.listen_port, ssl=self.ssl_ctx)
+		await self.wsserver.wait_closed()
+
+
+async def amain(args):
+	try:
+		server = WSNETWSServer(listen_ip=args.ip, listen_port=args.port)
+		await server.run()
+
+	except:
+		traceback.print_exc()
+
+def main():
+	import argparse
+	import logging
+	parser = argparse.ArgumentParser(description='WSNET proxy server')
+	parser.add_argument('--ip', default='127.0.0.1', help='Listen IP')
+	parser.add_argument('--port', type=int, default=8700, help='Listen port')
+	parser.add_argument('-v', '--verbose', action='count', default=0, help='Increase verbosity, can be stacked')
+
+	args = parser.parse_args()
+	if args.verbose == 1:
+		logger.setLevel(logging.DEBUG)
+			
+	elif args.verbose > 1:
+		logging.basicConfig(level=1)
+		logger.setLevel(logging.DEBUG)
+
+	asyncio.run(amain(args))
+
+if __name__ == '__main__':
+	main()
