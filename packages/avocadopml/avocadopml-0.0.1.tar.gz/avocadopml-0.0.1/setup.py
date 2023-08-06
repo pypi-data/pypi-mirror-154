8ec0d016174f4c0da92813041eed04bb89313fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:38:08 +0000

    Add package avocadopml/0.0.1/avocadopml-0.0.1.tar.gz

---
+++ packages/avocadopml/avocadopml-0.0.1.tar.gz/avocadopml-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='avocadopml',
+      packages=['avocadopml'],
+      version='0.0.1',
+      install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn',
+        'sklearn',
+        'xgboost',
+        'pytest'
+      ],
+)
