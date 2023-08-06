d7190369a86aea4276f029d47166b5af80399e6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:23 +0000

    Add package generalobj/0.10.12/generalobj-0.10.12.tar.gz

---
+++ packages/generalobj/generalobj-0.10.12.tar.gz/generalobj-0.10.12/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+from pathlib import Path
+#this_directory = Path(__file__).parent
+#long_description = (this_directory / "README").read_text()
+long_description = open('README', 'r').read()
+
+
+setup(name='generalobj',
+    version='0.10.12',
+    description='Generalobj is a package for easy form handling for general Form Objects',
+    url='',
+    author='Csaba Saranszky',
+    author_email='alt@256.hu',
+    license='GPL',
+    packages=['generalobj'],
+    install_requires=['XlsxWriter'],
+    zip_safe=False,
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type='text/plain')
