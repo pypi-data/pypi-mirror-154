01df8546fb9208401ed9cabdb6ba5bb4119f98e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:49 +0000

    Add package nlhappy/2022.6.9/nlhappy-2022.6.9-py3-none-any.whl

---
+++ packages/nlhappy/nlhappy-2022.6.9-py3-none-any.whl/nlhappy/components/__init__.py
@@ -0,0 +1,5 @@
+from .span_classifier import make_spancat
+from .token_classifier import make_tokencat
+from .text_classifier import make_text_classification
+from .sentence_segmenter import make_sentence_segmenter
+from .triple_extractor import make_triple_extractor
