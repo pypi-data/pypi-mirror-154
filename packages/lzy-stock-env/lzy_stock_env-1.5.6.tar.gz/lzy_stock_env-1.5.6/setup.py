140c10e81d0756b4048c188ebc04cc66eaeae2b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:24 +0000

    Add package lzy-stock-env/1.5.6/lzy_stock_env-1.5.6.tar.gz

---
+++ packages/lzy-stock-env/lzy_stock_env-1.5.6.tar.gz/lzy_stock_env-1.5.6/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+from setuptools import setup, find_packages
+from pathlib import Path
+setup(
+    name="lzy_stock_env",
+    version="1.5.6",
+    author="luzhenye",
+    author_email="786661074@qq.com",
+    description="机器学习作业用的股票数据环境",
+    #个人主页
+    url="http://ursule.plus/",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+
+    # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+    packages=find_packages(include="lzy_stock_env*"),
+    install_requires=["gym","torch"]
+)
