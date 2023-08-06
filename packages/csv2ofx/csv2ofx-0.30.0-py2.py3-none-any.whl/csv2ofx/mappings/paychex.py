5429a2619d566dd056a929c629618668ef729fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:07 +0000

    Add package csv2ofx/0.30.0/csv2ofx-0.30.0-py2.py3-none-any.whl

---
+++ packages/csv2ofx/csv2ofx-0.30.0-py2.py3-none-any.whl/csv2ofx/mappings/paychex.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# vim: sw=4:ts=4:expandtab
+# pylint: disable=invalid-name
+"""
+csv2ofx.mappings.mintapi
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+Provides a mapping for transactions obtained via the mintapi python script
+"""
+from operator import itemgetter
+
+mapping = {
+    "has_header": True,
+    "is_split": False,
+    "currency": "USD",
+    "delimiter": ",",
+    "date": itemgetter("Date"),
+    "amount": itemgetter("Amount"),
+    "price": itemgetter("Price"),
+    "shares": itemgetter("Shares"),
+    "investment": itemgetter("InvestmentName"),
+    "ticker": itemgetter("Ticker"),
+    "desc": itemgetter("Transaction"),
+}
