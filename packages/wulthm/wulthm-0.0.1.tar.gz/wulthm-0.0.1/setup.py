6e20b66dd52204332f57b6380bef644c2efdc67e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:28 +0000

    Add package wulthm/0.0.1/wulthm-0.0.1.tar.gz

---
+++ packages/wulthm/wulthm-0.0.1.tar.gz/wulthm-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="wulthm",
+    version="0.0.1",
+    license="MIT",
+    author="Kirill Melnikov @wultes",
+    packages=find_packages(),
+    url="https://github.com/wultes/wulthm"
+)
