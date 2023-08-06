3b92ae4c8899bb8c583a989d10a7d47eafb3ee6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:27:29 +0000

    Add package vgs-api-client/0.0.1.dev202206091511/vgs_api_client-0.0.1.dev202206091511-py3-none-any.whl

---
+++ packages/vgs-api-client/vgs_api_client-0.0.1.dev202206091511-py3-none-any.whl/vgs/configuration.py
@@ -0,0 +1,38 @@
+class Configuration:
+    def __init__(
+        self,
+        username,
+        password,
+        vault_id=None,
+        host="https://api.sandbox.verygoodvault.com",
+        configuration_host="https://api.sandbox.verygoodsecurity.com",
+        service_account_name=None,
+        service_account_password=None,
+    ):
+        self.username = username
+        self.password = password
+        self.vault_id = vault_id
+        self.host = host
+        self.configuration_host = configuration_host
+        self.service_account_name = service_account_name
+        self.service_account_password = service_account_password
+
+
+def config(
+    username,
+    password,
+    vault_id=None,
+    host="https://api.sandbox.verygoodvault.com",
+    configuration_host="https://api.sandbox.verygoodsecurity.com",
+    service_account_name=None,
+    service_account_password=None,
+):
+    return Configuration(
+        username=username,
+        password=password,
+        vault_id=vault_id,
+        host=host,
+        configuration_host=configuration_host,
+        service_account_name=service_account_name,
+        service_account_password=service_account_password,
+    )
