7c7187c5e783f320f6b7ec7f9db7671bf531e278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:53 +0000

    Add package databaseci/5.8.1/databaseci-5.8.1.tar.gz

---
+++ packages/databaseci/databaseci-5.8.1.tar.gz/databaseci-5.8.1/README.md
@@ -0,0 +1,3 @@
+## databaseci: Databases and tabular data for people who don't have time to faff about
+
+
---
+++ packages/databaseci/databaseci-5.8.1.tar.gz/databaseci-5.8.1/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['databaseci']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click',
+ 'databaseciservices>=5.2.2',
+ 'docker',
+ 'migra',
+ 'pendulum',
+ 'psycopg2-binary',
+ 'py',
+ 'pyyaml',
+ 'requests',
+ 'schemainspect>=3.1.1648463413']
+
+entry_points = \
+{'console_scripts': ['databaseci = databaseci:command.cli']}
+
+setup_kwargs = {
+    'name': 'databaseci',
+    'version': '5.8.1',
+    'description': 'databaseci.com client',
+    'long_description': "## databaseci: Databases and tabular data for people who don't have time to faff about\n\n\n",
+    'author': 'Robert Lechte',
+    'author_email': 'rob@databaseci.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://databaseci.com',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4',
+}
+
+
+setup(**setup_kwargs)
