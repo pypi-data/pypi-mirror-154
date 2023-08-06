dbf1466328f775099b698763ee349bdf7cae8b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:54 +0000

    Add package rubrix/0.15.0/rubrix-0.15.0-py3-none-any.whl

---
+++ packages/rubrix/rubrix-0.15.0-py3-none-any.whl/rubrix/server/static/_nuxt/0e2d75b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[119],{1129:function(t,n,e){"use strict";e.r(n);var o={mounted:function(){this.$router.push("/datasets")}},r=e(50),component=Object(r.a)(o,(function(){var t=this.$createElement;return(this._self._c||t)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
