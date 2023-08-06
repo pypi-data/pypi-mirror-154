ba8055e7e82a85c84d860f448dbdef0e2c249010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:42:24 +0000

    Add package sienna/0.1.5/sienna-0.1.5.tar.gz

---
+++ packages/sienna/sienna-0.1.5.tar.gz/sienna-0.1.5/sienna/core.py
@@ -0,0 +1,38 @@
+import json
+from typing import Dict, List, Union
+
+
+def __get_file_type(fpath: str) -> str:
+    if fpath.endswith(".jsonl"):
+        return "jsonl"
+    elif fpath.endswith(".json"):
+        return "json"
+    else:
+        return "text"
+
+
+def load(fpath: str) -> Union[List, Dict]:
+    ftype = __get_file_type(fpath)
+    if ftype == "jsonl":
+        with open(fpath, "r") as f:
+            data = [json.loads(line) for line in f.readlines()]
+    elif ftype == "json":
+        with open(fpath, "r") as f:
+            data = json.load(f)
+    else:
+        with open(fpath, "r") as f:
+            data = [line.strip() for line in f.readlines()]
+    return data
+
+
+def save(data: Union[List, Dict], fpath: str) -> None:
+    ftype = __get_file_type(fpath)
+    if ftype == "jsonl":
+        with open(fpath, "w") as f:
+            f.write("\n".join([json.dumps(d) for d in data]))
+    elif ftype == "json":
+        with open(fpath, "w") as f:
+            json.dump(data, f)
+    else:
+        with open(fpath, "w") as f:
+            f.write("\n".join(data))
