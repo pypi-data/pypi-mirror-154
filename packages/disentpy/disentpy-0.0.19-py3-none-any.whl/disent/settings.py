c10dc09785c2c0baf230583a5fdd2665c3ac495e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:41 +0000

    Add package disentpy/0.0.19/disentpy-0.0.19-py3-none-any.whl

---
+++ packages/disentpy/disentpy-0.0.19-py3-none-any.whl/disent/settings.py
@@ -0,0 +1,8 @@
+ENVS = {
+  "local": "http://localhost:8000",
+  "dev":   "https://api-dev.disent.com",
+  "prod":  "https://snpricer-dev.disent.com",
+  "public": "https://snpricer-dev.disent.com",
+}
+
+ENV = 'public'
