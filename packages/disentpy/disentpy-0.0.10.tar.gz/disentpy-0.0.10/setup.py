64a5d0fd750673b3fc022d79d87234b6c37f2691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:36 +0000

    Add package disentpy/0.0.10/disentpy-0.0.10.tar.gz

---
+++ packages/disentpy/disentpy-0.0.10.tar.gz/disentpy-0.0.10/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='disentpy',
+    version='0.0.10',
+    description='Disent API caller',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/disentcorp/disentpy',
+    author='Niels lauritzen',
+    author_email='niels.lauritzen@disent.com',
+    license_files = ('LICENSE.md',),
+    packages=['disent'],
+    install_requires=['requests'],
+    zip_safe=False
+)
