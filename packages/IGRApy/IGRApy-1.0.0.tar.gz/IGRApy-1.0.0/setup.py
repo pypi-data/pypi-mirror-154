4d5b6ed69a42bc8ce0567d35fe36f9c1c21ccc82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:26:42 +0000

    Add package IGRApy/1.0.0/IGRApy-1.0.0.tar.gz

---
+++ packages/IGRApy/IGRApy-1.0.0.tar.gz/IGRApy-1.0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools 
+
+setuptools.setup(
+    name='IGRApy',
+    version='1.0.0',
+    author='Gopikrishnan G S',
+    distribution='Python wrapper for IGRA',
+    packages=['IGRApy'],
+    website=['www.atmoslabiitkgp.com']
+
+)
