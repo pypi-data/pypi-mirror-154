435d92d176fba983e1392a64518010ba6b6a6b8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:24 +0000

    Add package exmachina/0.2.0/exmachina-0.2.0-py3-none-any.whl

---
+++ packages/exmachina/exmachina-0.2.0-py3-none-any.whl/exmachina/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.0"
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+logging.getLogger(__name__).propagate = False
+
+from .core.depends_contoroller import get_depends  # noqa
+from .core.machina import Event, Machina  # noqa
+from .core.params_function import Depends  # noqa
