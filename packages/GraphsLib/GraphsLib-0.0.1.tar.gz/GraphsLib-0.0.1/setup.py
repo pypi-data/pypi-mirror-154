a68c17d6b7873190d40e480b8431c239286e958a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:42:33 +0000

    Add package GraphsLib/0.0.1/GraphsLib-0.0.1.tar.gz

---
+++ packages/GraphsLib/GraphsLib-0.0.1.tar.gz/GraphsLib-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='GraphsLib',
+    version='0.0.1',
+    description='Lib for working with graphs, nodes and verges.',
+    url='https://github.com/Always-prog/GraphsLib',
+    author='AlwaysProg',
+    author_email='always.prog@gmail.com',
+    license='BSD 2-clause',
+    packages=['GraphsLib'],
+    install_requires=['typing>=3.7.4.3'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
