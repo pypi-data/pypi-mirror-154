2ea168aa7ffdc0dedabf4dc72887fa434feac7c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:41 +0000

    Add package offline-model-builder/0.2.5/offline_model_builder-0.2.5-py3-none-any.whl

---
+++ packages/offline-model-builder/offline_model_builder-0.2.5-py3-none-any.whl/offline_model_builder/user_profile/clustering/cluster_generator.py
@@ -0,0 +1,167 @@
+from pandas import DataFrame, Series, get_dummies
+from sklearn.cluster import KMeans, MiniBatchKMeans, \
+    AgglomerativeClustering, DBSCAN, OPTICS, Birch
+from sklearn_extra.cluster import KMedoids
+
+from offline_model_builder.user_profile.clustering.clustering_utils import ClusteringUtils
+from offline_model_builder.user_profile.clustering.config import K_PAYTV, K_NO_PAYTV, MINIBATCH_SIZE, \
+    DBSCAN_EPS, DBSCAN_MINSAMPLES, OPTICS_MINSAMPLES, \
+    BIRCH_NCLUSTERS, KMEDOIDS_INIT
+from offline_model_builder.user_profile.constants import CUSTOMER_ID, MINIBATCH_KMEANS_FEATURE, BIRTHDAY, \
+    CUSTOMER_CREATED_ON, \
+    CUSTOMER_MODIFIED_ON, PAYTVPROVIDER_ID, GENDER, UD_KEY, IS_PAY_TV, GENDER_NAN
+
+
+class ClusterGenerator(ClusteringUtils):
+
+    def __init__(
+            self,
+            data=DataFrame
+    ):
+        data[CUSTOMER_ID] = data[CUSTOMER_ID].astype(str)
+        ClusteringUtils.__init__(self, data=data)
+        self.clusters = DataFrame()
+
+    def get_kmeans(
+            self,
+            features: DataFrame,
+            paytv: bool
+    ) -> Series:
+        """
+        Generate KMeans Clusters
+        :param features: user features
+        :return: list of assigned cluster values
+        """
+        if paytv:
+            K = K_PAYTV
+        else:
+            K = K_NO_PAYTV
+        model = KMeans(n_clusters=K, random_state=0)
+        return model.fit_predict(features)
+
+    def get_minibatch_kmeans(
+            self,
+            features: DataFrame,
+            paytv: bool
+    ) -> Series:
+        """
+        Generate MiniBatch-KMeans clusters
+        :param features: user features
+        :return: list of assigned cluster values
+        """
+        if paytv:
+            K = K_PAYTV
+        else:
+            K = K_NO_PAYTV
+        model = MiniBatchKMeans(n_clusters=K, random_state=0,
+                                batch_size=MINIBATCH_SIZE)
+        return model.fit_predict(features)
+
+    def get_wards_hierarchical(
+            self,
+            features: DataFrame,
+            paytv: bool
+    ) -> Series:
+        """
+        Generate clusters using Ward's Hierarchical
+        Clustering
+        :param features: user features
+        :return: list of assigned cluster values
+        """
+        if paytv:
+            K = K_PAYTV
+        else:
+            K = K_NO_PAYTV
+        model = AgglomerativeClustering(n_clusters=K)
+        return model.fit_predict(features)
+
+    def get_kmedoids(
+            self,
+            features: DataFrame,
+            paytv: bool
+    ) -> Series:
+        """
+        Generate KMedoids Clusters
+        :param features: user features
+        :return: list of assigned cluster values
+        """
+        if paytv:
+            K = K_PAYTV
+        else:
+            K = K_NO_PAYTV
+        model = KMedoids(n_clusters=K, random_state=0,
+                         init=KMEDOIDS_INIT)
+        return model.fit_predict(features)
+
+    def get_dbscan(
+            self,
+            features: DataFrame
+    ) -> Series:
+        """
+        Generate DBSCAN Clusters
+        :param features: user features
+        :return: list of assigned cluster values
+        """
+        model = DBSCAN(eps=DBSCAN_EPS,
+                       min_samples=DBSCAN_MINSAMPLES)
+        return model.fit_predict(features)
+
+    def get_optics(
+            self,
+            features: DataFrame
+    ) -> Series:
+        """
+        Generate OPTICS clusters
+        :param features: user features
+        :return: list of assigned cluster values
+        """
+        model = OPTICS(min_samples=OPTICS_MINSAMPLES)
+        return model.fit_predict(features)
+
+    def get_birch(
+            self,
+            features: DataFrame
+    ) -> Series:
+        """
+        Generate BIRCH subclusters if n_clusters
+        is set to None, else merge subclusters to
+        obtain n_clusters using Agglomerative
+        Clustering.
+        :param features: user features
+        :return: list of assigned cluster values
+        """
+        model = Birch(n_clusters=BIRCH_NCLUSTERS)
+        return model.fit_predict(features)
+
+    def controller(
+            self,
+            paytv: bool
+    ):
+        """
+        Driver function for Cluster based feature
+        processing and results generation
+        :return: None
+        """
+        features = self.drop_sparse_features()
+        # save intermediate results here, if required
+
+        self.clusters[CUSTOMER_ID] = features[CUSTOMER_ID]
+
+        features = get_dummies(
+            features,
+            columns=[GENDER, PAYTVPROVIDER_ID]
+        )
+
+        # features.to_pickle("features_without_paytv.pkl")
+
+        # since we do not want to pass an identifier as a clustering feature
+        features = self.remove_attributes(attributes=features,
+                                          to_drop=[CUSTOMER_ID,
+                                                   BIRTHDAY,
+                                                   CUSTOMER_CREATED_ON,
+                                                   CUSTOMER_MODIFIED_ON,
+                                                   IS_PAY_TV,
+                                                   UD_KEY,
+                                                   GENDER_NAN])
+
+        self.clusters[MINIBATCH_KMEANS_FEATURE] = self.get_minibatch_kmeans(features=features, paytv=paytv)
