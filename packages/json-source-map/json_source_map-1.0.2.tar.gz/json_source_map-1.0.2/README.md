471d936ba2757a9f8645174c4533df2a1e5af97f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:58:47 +0000

    Add package json-source-map/1.0.2/json_source_map-1.0.2.tar.gz

---
+++ packages/json-source-map/json_source_map-1.0.2.tar.gz/json_source_map-1.0.2/README.md
@@ -0,0 +1,43 @@
+# JsonSourceMap
+
+Calculate JSON Pointers to each value within a JSON document along with the
+line, column and character position for the start and end of that value. For
+more information see: <https://github.com/open-alchemy/json-source-map/wiki>.
+
+For example:
+
+```bash
+python -m pip install json_source_map
+```
+
+```Python
+from json_source_map import calculate
+
+
+print(calculate('{"foo": "bar"}'))
+```
+
+The above prints:
+
+```Python
+{
+    '': Entry(
+        value_start=Location(line=0, column=0, position=0),
+        value_end=Location(line=0, column=14, position=14),
+        key_start=None,
+        key_end=None,
+    ),
+    '/foo': Entry(
+        value_start=Location(line=0, column=8, position=8),
+        value_end=Location(line=0, column=13, position=13),
+        key_start=Location(line=0, column=1, position=1),
+        key_end=Location(line=0, column=6, position=6),
+    ),
+}
+```
+
+The following features have been implemented:
+
+- support for primitive types (`strings`, `numbers`, `booleans` and `null`),
+- support for structural types (`array` and `object`) and
+- support for space, tab, carriage and return whitespace.
---
+++ packages/json-source-map/json_source_map-1.0.2.tar.gz/json_source_map-1.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['json_source_map']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['hypothesis>=6.12.0,<7.0.0']
+
+setup_kwargs = {
+    'name': 'json-source-map',
+    'version': '1.0.2',
+    'description': 'Calculate the source map for a JSON document.',
+    'long_description': '# JsonSourceMap\n\nCalculate JSON Pointers to each value within a JSON document along with the\nline, column and character position for the start and end of that value. For\nmore information see: <https://github.com/open-alchemy/json-source-map/wiki>.\n\nFor example:\n\n```bash\npython -m pip install json_source_map\n```\n\n```Python\nfrom json_source_map import calculate\n\n\nprint(calculate(\'{"foo": "bar"}\'))\n```\n\nThe above prints:\n\n```Python\n{\n    \'\': Entry(\n        value_start=Location(line=0, column=0, position=0),\n        value_end=Location(line=0, column=14, position=14),\n        key_start=None,\n        key_end=None,\n    ),\n    \'/foo\': Entry(\n        value_start=Location(line=0, column=8, position=8),\n        value_end=Location(line=0, column=13, position=13),\n        key_start=Location(line=0, column=1, position=1),\n        key_end=Location(line=0, column=6, position=6),\n    ),\n}\n```\n\nThe following features have been implemented:\n\n- support for primitive types (`strings`, `numbers`, `booleans` and `null`),\n- support for structural types (`array` and `object`) and\n- support for space, tab, carriage and return whitespace.\n',
+    'author': 'David Andersson',
+    'author_email': 'nderssonpublic@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/open-alchemy/json-source-map/wiki',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
