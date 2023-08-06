75b25ccc8a783a61f8316266486c803475cf0a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:21 +0000

    Add package praxis/0.0.dev1/praxis-0.0.dev1.tar.gz

---
+++ packages/praxis/praxis-0.0.dev1.tar.gz/praxis-0.0.dev1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='praxis',
+    version='0.0.dev1',
+    description='',
+    long_description='',
+    url='https://github.com/google/praxis',
+    author='Praxis authors',
+    author_email='shafey@google.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
