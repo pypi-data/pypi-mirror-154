6d7a04ef8ecad4f9733fabdb90d547dfc122be7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:25:29 +0000

    Add package anuga/3.1.5/anuga-3.1.5.tar.gz

---
+++ packages/anuga/anuga-3.1.5.tar.gz/anuga-3.1.5/anuga/revision.py
@@ -0,0 +1,9 @@
+"""Stored git revision info.
+
+This file provides the git sha id and commit date for the installed revision of ANUGA.
+The file is automatically generated and should not be modified manually.
+"""
+
+__git_sha__ = "a9d9181727e6defa7c2f6a38f2f964c49f13698d"
+__git_committed_datetime__ = "2022-06-09 16:51:59+09:18"
+__version__ = "3.1.5"
