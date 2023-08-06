242f92832f3287a1095f9947a8f10275b706b08e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:17 +0000

    Add package gsg-utils-stg/0.0.3/gsg-utils-stg-0.0.3.tar.gz

---
+++ packages/gsg-utils-stg/gsg-utils-stg-0.0.3.tar.gz/gsg-utils-stg-0.0.3/gsg_utils/constants.py
@@ -0,0 +1,18 @@
+# TODO: Need to make it singleton, one class per service
+class Constants:
+
+    def __init__(self):
+        self.__parameter_map = {
+            "SLACK_URL": "/SLACK/WEBHOOK_URL/ALERTS",
+            "ENV": "STG",
+            "REGION": "us-west-2"
+        }
+
+    def set_key_value(self, key, value):
+        self.__parameter_map[key] = value
+
+    def get_value_by_key(self, key):
+        self.__parameter_map.get(key, None)
+
+
+CONSTANTS_INSTANCE = Constants()
