30c35f07095f217e2936cbc1cb0de69f30aecf1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:01 +0000

    Add package pg-python-sdk/0.0.10/pg-python-sdk-0.0.10.tar.gz

---
+++ packages/pg-python-sdk/pg-python-sdk-0.0.10.tar.gz/pg-python-sdk-0.0.10/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pg-python-sdk",
+    version="0.0.10",
+    author="Abdulmejid Shemsu",
+    author_email="abdudlh1@gmail.com",
+    description="Python SDK for the Payment Gateway",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/abdulmejidshemsu/pg-python-sdk",
+    project_urls={
+        "Bug Tracker": "https://github.com/abdulmejidshemsu/pg-python-sdk/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        "requests",
+    ],
+)
