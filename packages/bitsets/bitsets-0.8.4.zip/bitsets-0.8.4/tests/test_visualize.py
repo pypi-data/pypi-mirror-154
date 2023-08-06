5b7845b75fac91fe7ef5bb197e6493d97a52c9b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:43 +0000

    Add package bitsets/0.8.4/bitsets-0.8.4.zip

---
+++ packages/bitsets/bitsets-0.8.4.zip/bitsets-0.8.4/tests/test_visualize.py
@@ -0,0 +1,7 @@
+import bitsets.visualize
+
+
+def test_label_func(Four):  # noqa: N803
+    dot = bitsets.visualize.bitset(Four,
+                                   member_label=lambda b: f'spam{b.real}spam')
+    assert 'spam1spam' in str(dot)
