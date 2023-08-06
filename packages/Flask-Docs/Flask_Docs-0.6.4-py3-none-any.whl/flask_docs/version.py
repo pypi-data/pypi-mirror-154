4f289619548c6d19fe9bbd943bdd074be2697a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:30:10 +0000

    Add package Flask-Docs/0.6.4/Flask_Docs-0.6.4-py3-none-any.whl

---
+++ packages/Flask-Docs/Flask_Docs-0.6.4-py3-none-any.whl/flask_docs/version.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+Program:
+    Flask-Docs Version
+History:
+    Created on 2021/06/06
+    Last modified on 2022/06/09
+Author:
+    kwkw
+"""
+
+__version__ = "0.6.4"
