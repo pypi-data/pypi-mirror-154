8c1e267090c5e9e1f651202925a50b0c1130e2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:56 +0000

    Add package Jianmu/0.0.6/Jianmu-0.0.6.tar.gz

---
+++ packages/Jianmu/Jianmu-0.0.6.tar.gz/Jianmu-0.0.6/jianmu/__init__.py
@@ -0,0 +1,3 @@
+from . import exceptions, info
+
+__all__ = ['exceptions', 'info']
---
+++ packages/Jianmu/Jianmu-0.0.6.tar.gz/Jianmu-0.0.6/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+import setuptools
+
+from jianmu.info import version
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='Jianmu',
+    version=version,
+    description=
+    'A simple desktop app development framework combining Python, Vue.js, Element Plus and Electron.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Zhaoji Wang',
+    author_email='hwoam@outlook.com',
+    url='https://github.com/frederick-wang/jianmu',
+    project_urls={
+        "Bug Tracker": "https://github.com/frederick-wang/jianmu/issues",
+    },
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'Flask',
+        'requests',
+    ],
+    packages=setuptools.find_packages(include=['jianmu', 'jianmu.*']),
+    entry_points={
+        'console_scripts': ['jianmu=jianmu.cli:parse'],
+    },
+    python_requires=">=3.6",
+)
