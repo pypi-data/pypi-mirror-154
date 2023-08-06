64271d3ed5def546760e7780eb3daa7f24c65e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:54 +0000

    Add package django-painless-redirects/0.7.1/django_painless_redirects-0.7.1-py2.py3-none-any.whl

---
+++ packages/django-painless-redirects/django_painless_redirects-0.7.1-py2.py3-none-any.whl/painless_redirects/tests/urls.py
@@ -0,0 +1,15 @@
+"""URLs to run the tests."""
+from django.conf.urls.i18n import i18n_patterns
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns = urlpatterns + i18n_patterns(
+    path('test/', TemplateView.as_view(template_name='test.html')),
+    path('test/no-slash', TemplateView.as_view(template_name='test_no_slash.html')),
+)
