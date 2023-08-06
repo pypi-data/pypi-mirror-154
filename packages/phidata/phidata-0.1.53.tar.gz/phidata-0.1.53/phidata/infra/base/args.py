5cf06ffe79ea82bac6d6e07bd7a8a99a42325e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:40 +0000

    Add package phidata/0.1.53/phidata-0.1.53.tar.gz

---
+++ packages/phidata/phidata-0.1.53.tar.gz/phidata-0.1.53/phidata/infra/base/args.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+from typing import Optional, Dict, Union
+
+from phidata.base import PhidataBaseArgs
+
+
+class InfraArgs(PhidataBaseArgs):
+    env: Optional[str] = None
+
+    # -*- Path parameters
+    # Path to the workspace root directory
+    workspace_root_path: Optional[Path] = None
+    # Path to the workspace config file
+    workspace_config_file_path: Optional[Path] = None
+    # Path to important directories relative to the workspace_root
+    # These directories are joined to the workspace_root dir
+    #   to build paths depending on the environments (local, docker, k8s)
+    # defaults are set by WorkspaceConfig.__init__()
+    scripts_dir: Optional[str] = None
+    storage_dir: Optional[str] = None
+    meta_dir: Optional[str] = None
+    products_dir: Optional[str] = None
+    notebooks_dir: Optional[str] = None
+    workspace_config_dir: Optional[str] = None
+
+    # -*- Environment parameters
+    # Env vars added to local env
+    local_env: Optional[Dict[str, str]] = None
+    # Load local env using env file
+    local_env_file: Optional[Union[str, Path]] = None
+    # Env vars added to docker env
+    docker_env: Optional[Dict[str, str]] = None
+    # Load docker env using env file
+    docker_env_file: Optional[Union[str, Path]] = None
+    # Env vars added to k8s env
+    k8s_env: Optional[Dict[str, str]] = None
+    # Load k8s env using env file
+    k8s_env_file: Optional[Union[str, Path]] = None
+
+    # -*- AWS parameters
+    # Common aws params used by apps, resources and data assets
+    aws_region: Optional[str] = None
+    aws_profile: Optional[str] = None
+    aws_config_file: Optional[str] = None
+    aws_shared_credentials_file: Optional[str] = None
