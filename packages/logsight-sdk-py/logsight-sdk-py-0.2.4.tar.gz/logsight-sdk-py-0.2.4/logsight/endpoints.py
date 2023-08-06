2acda615204677b7fcee248c486e37b83380b7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:38 +0000

    Add package logsight-sdk-py/0.2.4/logsight-sdk-py-0.2.4.tar.gz

---
+++ packages/logsight-sdk-py/logsight-sdk-py-0.2.4.tar.gz/logsight-sdk-py-0.2.4/logsight/endpoints.py
@@ -0,0 +1,18 @@
+PATH_USERS = "users"
+PATH_USERS_DELETE = "users/{userId}"
+
+PATH_AUTH_LOGIN = "auth/login"
+
+PATH_APP_CREATE = "users/{userId}/applications"
+PATH_APP_LST = "users/{userId}/applications"
+PATH_APP_DELETE = "users/{userId}/applications/{applicationId}"
+
+PATH_LOGS_SINGLES = "logs/singles"
+
+PATH_GET_COMPARE = "logs/compare"
+PATH_POST_COMPARE = "logs/compare"
+PATH_GET_COMPARE_ID = "logs/compare/{compareId}"
+PATH_DELETE_COMPARE = "logs/compare/{compareId}"
+PATH_POST_STATUS = "logs/compare/status"
+
+PATH_LOGS_INCIDENTS = "logs/incidents"
