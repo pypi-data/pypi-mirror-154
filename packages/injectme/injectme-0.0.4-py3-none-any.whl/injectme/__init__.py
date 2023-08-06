fb17e4a69e83ffd0fab4f07c6d165d35e2d89daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:55 +0000

    Add package injectme/0.0.4/injectme-0.0.4-py3-none-any.whl

---
+++ packages/injectme/injectme-0.0.4-py3-none-any.whl/injectme/__init__.py
@@ -0,0 +1,35 @@
+from .decorator import inject
+from .global_registry import (
+    register,
+    register_factory,
+    clear_dependencies,
+    set_registry,
+    get_registry,
+)
+from .registry import DependenciesRegistry
+from .errors import (
+    DependencyNotFound,
+    DependencyAlreadyRegistered,
+    InjectionNotSupported,
+    InjectionFailure,
+    InjectmeException,
+)
+
+
+__all__ = [
+    "inject",
+    "register",
+    "register_factory",
+    "clear_dependencies",
+    "set_registry",
+    "get_registry",
+    "DependenciesRegistry",
+    "DependencyNotFound",
+    "DependencyAlreadyRegistered",
+    "InjectionNotSupported",
+    "InjectionFailure",
+    "InjectmeException",
+]
+
+__version__ = "0.0.4"
+
