66b1cf39ab2fb52c82e9a9df561d2ed2c116e45e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:28 +0000

    Add package pyvda/0.2.7/pyvda-0.2.7-py3-none-any.whl

---
+++ packages/pyvda/pyvda-0.2.7-py3-none-any.whl/pyvda/utils.py
@@ -0,0 +1,59 @@
+import threading
+from ctypes import POINTER
+from comtypes import (
+    CoInitialize,
+    CoCreateInstance,
+    CLSCTX_LOCAL_SERVER,
+)
+from .com_defns import (
+    CLSID_VirtualDesktopPinnedApps,
+    IVirtualDesktopManager,
+    IVirtualDesktopManagerInternal,
+    IVirtualDesktopManagerInternal2,
+    IVirtualDesktopPinnedApps,
+    IApplicationViewCollection,
+    IServiceProvider,
+    CLSID_ImmersiveShell,
+    CLSID_VirtualDesktopManagerInternal,
+    BUILD_OVER_20231,
+    BUILD_OVER_21313,
+)
+
+
+def _get_object(cls, clsid = None):
+    CoInitialize()
+    pServiceProvider = CoCreateInstance(
+        CLSID_ImmersiveShell, IServiceProvider, CLSCTX_LOCAL_SERVER
+    )
+    pObject = POINTER(cls)()
+    pServiceProvider.QueryService(
+        clsid or cls._iid_,
+        cls._iid_,
+        pObject,
+    )
+    return pObject
+
+def get_vd_manager():
+    return _get_object(IVirtualDesktopManager)
+
+def get_vd_manager_internal():
+    return _get_object(IVirtualDesktopManagerInternal, CLSID_VirtualDesktopManagerInternal)
+
+def get_vd_manager_internal2():
+    return _get_object(IVirtualDesktopManagerInternal2, CLSID_VirtualDesktopManagerInternal)
+
+def get_view_collection():
+    return _get_object(IApplicationViewCollection)
+
+def get_pinned_apps():
+    return _get_object(IVirtualDesktopPinnedApps, CLSID_VirtualDesktopPinnedApps)
+
+class Managers(threading.local):
+    def __init__(self):
+        self.manager_internal = get_vd_manager_internal()
+        self.view_collection = get_view_collection()
+        self.pinned_apps = get_pinned_apps()
+
+        # Old interface only used for SetName
+        if not BUILD_OVER_21313:
+            self.manager_internal2 = get_vd_manager_internal2()
