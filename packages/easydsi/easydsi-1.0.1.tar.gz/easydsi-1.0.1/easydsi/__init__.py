7bb1ab192e521fe1681574af07b04df244ccb098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:15 +0000

    Add package easydsi/1.0.1/easydsi-1.0.1.tar.gz

---
+++ packages/easydsi/easydsi-1.0.1.tar.gz/easydsi-1.0.1/easydsi/__init__.py
@@ -0,0 +1,9 @@
+# import sys
+# sys.path.append(r'D:/Projects/Python/easydsi')
+
+from easydsi.linked_list import *
+from easydsi.array import *
+from easydsi.stack import *
+from easydsi.queue import *
+
+__version__ = "1.0.1"
