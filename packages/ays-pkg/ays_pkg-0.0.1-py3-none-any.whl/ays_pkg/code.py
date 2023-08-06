46ba3394afc2b34c0ee17f69514cd11a7f410480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:08 +0000

    Add package ays-pkg/0.0.1/ays_pkg-0.0.1-py3-none-any.whl

---
+++ packages/ays-pkg/ays_pkg-0.0.1-py3-none-any.whl/ays_pkg/code.py
@@ -0,0 +1,15 @@
+class Output:
+    def __init__(self,name,age):
+        self.name= name
+        self.age=age
+
+    def start(self):
+        print("you have entered")
+        print("Directory name:package")
+        print("Module name:code.py")
+
+    def display(self):
+        print("you name is",self.name)
+        print("your age is",self.age)
+        print("Thank you")
+
---
+++ packages/ays-pkg/ays_pkg-0.0.1-py3-none-any.whl/ays_pkg/code2.py
@@ -0,0 +1,14 @@
+class Output2:
+    def __init__(self,country,lang):
+        self.country= country
+        self.language=lang
+
+    def start(self):
+        print("you have entered")
+        print("Directory name:package")
+        print("Module name:code2.py")
+
+    def display(self):
+        print("you are living in",self.country)
+        print("your age is",self.language)
+        print("Thank you")
