6feadfa900f0d212f7d0b96f4e2fdb74940b8a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:34 +0000

    Add package mgz/1.7.7/mgz-1.7.7.tar.gz

---
+++ packages/mgz/mgz-1.7.7.tar.gz/mgz-1.7.7/mgz/summary/__init__.py
@@ -0,0 +1,25 @@
+from mgz.fast.header import decompress, parse_version
+from mgz.summary.full import FullSummary
+from mgz.model.compat import ModelSummary
+from mgz.util import Version
+import logging
+
+logger = logging.getLogger(__name__)
+
+class SummaryStub:
+
+    def __call__(self, data, playback=None, fallback=False):
+        header = decompress(data)
+        version, game, save, log = parse_version(header, data)
+        data.seek(0)
+        if version is Version.DE and save > 13.34 and not fallback:
+            logger.info("using model summary")
+            try:
+                return ModelSummary(data, playback)
+            except RuntimeError as e:
+                logger.warning(f"could not fast parse; falling back: {e}")
+        logger.info("using full summary")
+        return FullSummary(data, playback)
+
+
+Summary = SummaryStub()
