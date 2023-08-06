2ac1f3ce36989e93b9570cbcde8194460d4c1499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:16 +0000

    Add package aws-iam-utils/1.6.0/aws-iam-utils-1.6.0.tar.gz

---
+++ packages/aws-iam-utils/aws-iam-utils-1.6.0.tar.gz/aws-iam-utils-1.6.0/aws_iam_utils/__init__.py
@@ -0,0 +1,6 @@
+import aws_iam_utils.checks
+import aws_iam_utils.generator
+import aws_iam_utils.combiner
+import aws_iam_utils.util
+import aws_iam_utils.simplifier
+import aws_iam_utils.action_data_overrides
