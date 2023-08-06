f746d4e3ba42d32d247a61f6367cdbd140c74218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:57 +0000

    Add package aiida-optimize/1.0.0/aiida_optimize-1.0.0-py3-none-any.whl

---
+++ packages/aiida-optimize/aiida_optimize-1.0.0-py3-none-any.whl/aiida_optimize/wrappers/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Defines helper workchains that can wrap existing processes into
+a format compatible with the optimization procedure.
+"""
+
+from ._create_evaluate import CreateEvaluateWorkChain
+from ._add_inputs import AddInputsWorkChain
+from ._concatenate import ConcatenateWorkChain
+
+__all__ = ['CreateEvaluateWorkChain', 'AddInputsWorkChain', "ConcatenateWorkChain"]
