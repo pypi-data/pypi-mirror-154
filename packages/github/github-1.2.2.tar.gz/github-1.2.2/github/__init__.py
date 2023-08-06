59644b5d10cfcd6f971f2a895ce04947dfba6f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:26 +0000

    Add package github/1.2.2/github-1.2.2.tar.gz

---
+++ packages/github/github-1.2.2.tar.gz/github-1.2.2/github/__init__.py
@@ -0,0 +1,13 @@
+# == __init__.py ==#
+
+__title__ = 'Github-Api-Wrapper'
+__authors__ = 'VarMonke', 'sudosnok'
+__version__ = '1.2.2'
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2020 VarMonke & sudosnok'
+
+from .client import *
+from .objects import *
+from .http import *
+from .urls import *
+from .exceptions import *
