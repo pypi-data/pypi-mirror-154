327ffdfb899bc4b632bf47a185c5c8e402c56e68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:51 +0000

    Add package pg-python-sdk/0.0.4/pg_python_sdk-0.0.4-py3-none-any.whl

---
+++ packages/pg-python-sdk/pg_python_sdk-0.0.4-py3-none-any.whl/pg_python_sdk/payment_gateway_request.py
@@ -0,0 +1,35 @@
+
+
+import json
+from requests import Response, Session, Request
+from pg_python_sdk.payment_gateway_response import PaymentGatewayResponse
+
+
+class PaymentGatewayRequest:
+
+    def __init__(self, key: str):
+        self.key = key
+
+    def send_request(self, method: str, uri: str, data: dict = None) -> Response:
+        _session = Session()
+        _request = Request(
+            method,
+            f'http://127.0.0.1:8050/api/v1/{uri}',
+            data=json.dumps(data),
+            headers={
+                'Authorization': f'Bearer {self.key}',
+                'Content-Type': 'application/json'
+            }
+        )
+
+        _prepared = _request.prepare()
+        _response = _session.send(
+            _prepared, verify=True, allow_redirects=False)
+
+        return _response
+
+    def get(self, uri: str, data: dict = None) -> Response:
+        return PaymentGatewayResponse(self.send_request('GET', uri, data))
+
+    def post(self, uri: str, data: dict = None) -> Response:
+        return PaymentGatewayResponse(self.send_request('POST', uri, data))
