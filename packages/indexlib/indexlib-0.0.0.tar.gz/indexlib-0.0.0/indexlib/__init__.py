3012ba12d21dcd7e9872ce8a59a59692f5b21592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:32:30 +0000

    Add package indexlib/0.0.0/indexlib-0.0.0.tar.gz

---
+++ packages/indexlib/indexlib-0.0.0.tar.gz/indexlib-0.0.0/indexlib/__init__.py
@@ -0,0 +1,8 @@
+"""indexlib name claim
+
+Dull package only used to claim the project name on pypi.org
+
+Please contact me on wouter.koorn@cern.ch if this project remains unused, and you want the name.
+"""
+
+raise RuntimeError(__doc__)
