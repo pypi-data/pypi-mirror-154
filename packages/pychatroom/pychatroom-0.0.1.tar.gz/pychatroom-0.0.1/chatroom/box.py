dff7aea2840a62be3bdeee3ee339915968493ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:18:42 +0000

    Add package pychatroom/0.0.1/pychatroom-0.0.1.tar.gz

---
+++ packages/pychatroom/pychatroom-0.0.1.tar.gz/pychatroom-0.0.1/chatroom/box.py
@@ -0,0 +1,22 @@
+import socket
+import client
+import readline
+
+
+def cli():
+    ...
+
+
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.connect((client.HOST_SEND, client.PORT_SEND))
+        except ConnectionRefusedError:
+            print('\033[1;31mText box connection failed.\033[0m')
+            exit()
+        while True:
+            msg = input()
+            print('\033[1A\033[K', end='')
+            s.send(msg.encode('utf-8'))
+except (KeyboardInterrupt, EOFError):
+    print()
