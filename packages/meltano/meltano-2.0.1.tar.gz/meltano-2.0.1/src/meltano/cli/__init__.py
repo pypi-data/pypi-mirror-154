af1b4f4ccf841b8147905b29351cc24d483d6e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:50 +0000

    Add package meltano/2.0.1/meltano-2.0.1.tar.gz

---
+++ packages/meltano/meltano-2.0.1.tar.gz/meltano-2.0.1/src/meltano/cli/__init__.py
@@ -0,0 +1,62 @@
+"""Main entry point for the meltano CLI."""
+import logging
+import os
+import sys
+
+from meltano.core.logging import setup_logging
+from meltano.core.project import ProjectReadonly
+
+from .utils import CliError
+
+# TODO: Importing the cli.cli module breaks other cli module imports
+# This suggests a cyclic dependency or a poorly structured interface.
+# This should be investigated and resolved to avoid implicit behavior
+# based solely on import order.
+from .cli import cli  # isort:skip
+from . import (  # isort:skip # noqa: F401, WPS235
+    add,
+    config,
+    discovery,
+    dragon,
+    elt,
+    environment,
+    initialize,
+    install,
+    invoke,
+    remove,
+    repl,
+    schedule,
+    schema,
+    select,
+    state,
+    ui,
+    upgrade,
+    user,
+    run,
+    validate,
+    job,
+)
+
+setup_logging()
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    """Entry point for the meltano cli."""
+    # mark the current process as executed via the `cli`
+    os.environ["MELTANO_JOB_TRIGGER"] = os.getenv("MELTANO_JOB_TRIGGER", "cli")
+    try:
+        try:  # noqa: WPS505
+            cli(obj={"project": None})
+        except ProjectReadonly as err:
+            raise CliError(
+                f"The requested action could not be completed: {err}"
+            ) from err
+        except KeyboardInterrupt:  # noqa: WPS329
+            raise
+        except Exception as err:
+            raise CliError(str(err)) from err
+    except CliError as cli_error:
+        cli_error.print()
+        sys.exit(1)
