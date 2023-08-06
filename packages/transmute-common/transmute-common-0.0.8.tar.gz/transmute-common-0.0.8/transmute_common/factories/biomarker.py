179dba88ee9347b4b74cdf36878d10d17ae99275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:19:11 +0000

    Add package transmute-common/0.0.8/transmute-common-0.0.8.tar.gz

---
+++ packages/transmute-common/transmute-common-0.0.8.tar.gz/transmute-common-0.0.8/transmute_common/factories/biomarker.py
@@ -0,0 +1,15 @@
+def create(values = None):
+  record = {
+    "test": None,
+    "value": None,
+    "units": None,
+    "result": None,
+    "interpretation": None,
+    "threshold": None,
+    "component": None,
+    "genomicSource": None,
+    "therapyRecommendation": None,
+  }
+  if values!=None:
+    record.update(values)
+  return record
