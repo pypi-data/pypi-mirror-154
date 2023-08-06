743804a5243e970d07cc8e2e1a8bf78ab5f8cdf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:23 +0000

    Add package deepsmirud/0.0.0.0.7/deepsmirud-0.0.0.0.7.tar.gz

---
+++ packages/deepsmirud/deepsmirud-0.0.0.0.7.tar.gz/deepsmirud-0.0.0.0.7/deepsmirud/Main.py
@@ -0,0 +1,36 @@
+__author__ = "Jianfeng Sun"
+__version__ = "v1.0"
+__copyright__ = "Copyright 2022"
+__license__ = "MIT"
+__email__ = "jianfeng.sunmt@gmail.com"
+__maintainer__ = "Jianfeng Sun"
+
+import urllib.request
+import click
+
+
+
+# @click.group()
+def main():
+    # pass
+    print(1)
+
+# @click.command()
+# @click.option('-u', '--url', default='https://github.com/2003100127/deepsmirud/releases/download/model/model.zip', help='URL of deepsmirud models')
+# @click.option('-o', '--output_path', default='./model.zip', help='output path of deepsmirud models')
+def download(url, fpn):
+    print('downloading...')
+    urllib.request.urlretrieve(
+        url=url,
+        filename=fpn
+    )
+    print('downloaded!')
+    return
+
+#
+# if __name__ == '__main__':
+#     main()
+download(
+    'https://github.com/2003100127/deepsmirud/releases/download/model/model.zip',
+    './asds.zip'
+)
