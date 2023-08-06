99397453f2be15772734e0bcab1817f4730c69df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:25 +0000

    Add package DJQ-Distributions/1.0/DJQ_Distributions-1.0.tar.gz

---
+++ packages/DJQ-Distributions/DJQ_Distributions-1.0.tar.gz/DJQ_Distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='DJQ_Distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['DJQ_Distributions'],
+      author = 'Darin Quintana',
+      author_email = 'djquintana889@gmail.com',
+      zip_safe=False)
