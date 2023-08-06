6a979f17e4e935de081b1101145e1669b8ae582f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:22:39 +0000

    Add package whine/0.1.0/whine-0.1.0-py3-none-any.whl

---
+++ packages/whine/whine-0.1.0-py3-none-any.whl/whine/__init__.py
@@ -0,0 +1,10 @@
+"""
+`whine` is a very lightweight package for custom implementations of subscribing and
+dispatching event messages, using an observer-like pattern.
+"""
+
+
+from whine.event_handler import EventHandler
+
+__version__ = "0.1.0"
+__all__ = ["EventHandler"]
