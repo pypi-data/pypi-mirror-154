c0631e86024dcaa0b5dfb838cb11c976efef6f57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:31 +0000

    Add package openconcept/0.4.1/openconcept-0.4.1-py3-none-any.whl

---
+++ packages/openconcept/openconcept-0.4.1-py3-none-any.whl/openconcept/components/__init__.py
@@ -0,0 +1,6 @@
+from .battery import SimpleBattery
+from .generator import SimpleGenerator
+from .motor import SimpleMotor
+from .propeller import SimplePropeller
+from .splitter import PowerSplit, FlowSplit, FlowCombine
+from .turboshaft import SimpleTurboshaft
---
+++ packages/openconcept/openconcept-0.4.1-py3-none-any.whl/openconcept/utilities/math/__init__.py
@@ -0,0 +1,4 @@
+from .add_subtract_comp import AddSubtractComp
+from .combine_split_comp import VectorConcatenateComp, VectorSplitComp
+from .multiply_divide_comp import ElementMultiplyDivideComp
+from .sum_comp import SumComp
