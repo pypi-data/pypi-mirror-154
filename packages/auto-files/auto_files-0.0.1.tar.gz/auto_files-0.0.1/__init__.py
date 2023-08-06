fa1763867297d6518170b072b28013acf9261d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:30:43 +0000

    Add package auto-files/0.0.1/auto_files-0.0.1.tar.gz

---
+++ packages/auto-files/auto_files-0.0.1.tar.gz/auto_files-0.0.1/__init__.py
@@ -0,0 +1,27 @@
+__version__ = '0.0.1'
+import os
+def create_file(**key):
+    for i in key:
+        if i == "Filename":
+            b = key[i]
+            break
+        else:
+            raise ValueError("No filename specified, please type a filename like this 'Filename='filename.txt'")
+    for i in key:
+        if i == "Content":
+            a = key[i]
+            break
+    with open(b, "w") as f:
+        f.write(a)
+    print("File created successfully!")
+def write(Filename, Content):
+    with open(Filename, "a") as f:
+        f.write(Content + "\n")
+    print("File written successfully!")
+def read(Filename):
+    with open(Filename, "r") as f:
+        print(f.read())
+def delete(Filename):
+    os.remove(Filename)
+    print("File deleted successfully!")
+    
---
+++ packages/auto-files/auto_files-0.0.1.tar.gz/auto_files-0.0.1/setup.py
@@ -0,0 +1,23 @@
+
+   
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='auto_files',
+  version='0.0.1',
+  description='This is a very basic package that just let you create files and more things to do very easily ',
+  url='',  
+  author='Joshua Lowe',
+  author_email='gamerproia555@gmail.com',
+  classifiers=classifiers,  
+  keywords='', 
+  packages=find_packages(),
+)
