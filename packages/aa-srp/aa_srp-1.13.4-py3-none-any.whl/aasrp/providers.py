e86c7a758c8a1cd83c15adf7777fea792ad86a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:00:11 +0000

    Add package aa-srp/1.13.4/aa_srp-1.13.4-py3-none-any.whl

---
+++ packages/aa-srp/aa_srp-1.13.4-py3-none-any.whl/aasrp/providers.py
@@ -0,0 +1,11 @@
+"""
+providers
+"""
+
+# Alliance Auth
+from esi.clients import EsiClientProvider
+
+# AA SRP
+from aasrp.constants import USERAGENT
+
+esi = EsiClientProvider(app_info_text=USERAGENT)
