1ad13510b61d0f43c153f0372850eac17af34103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:25:50 +0000

    Add package filescreator/0.0.3/filescreator-0.0.3.tar.gz

---
+++ packages/filescreator/filescreator-0.0.3.tar.gz/filescreator-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='filescreator',
+  version='0.0.3',
+  description='A very basic package',
+  url='',  
+  author='me',
+  author_email='gamerproia555@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
