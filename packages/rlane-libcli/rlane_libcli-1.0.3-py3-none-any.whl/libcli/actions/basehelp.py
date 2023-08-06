fd3dc528e0a87d3d5333bc2eb2a847fddb892a7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:30 +0000

    Add package rlane-libcli/1.0.3/rlane_libcli-1.0.3-py3-none-any.whl

---
+++ packages/rlane-libcli/rlane_libcli-1.0.3-py3-none-any.whl/libcli/actions/basehelp.py
@@ -0,0 +1,9 @@
+"""Base class for our `HelpAction`s."""
+
+import argparse
+
+
+class BaseHelpAction(argparse._HelpAction):
+    # pylint: disable=protected-access
+    # pylint: disable=too-few-public-methods
+    """Base class for our `HelpAction`s."""
