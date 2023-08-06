5d2a286646d5e6f228d10b387cb4a7f0715efb70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:20 +0000

    Add package filbuktools/0.1/filbuktools-0.1.tar.gz

---
+++ packages/filbuktools/filbuktools-0.1.tar.gz/filbuktools-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='filbuktools',
+      version='0.1',
+      description='filbuktools',
+      url='http://github.com/filbuk/filbuktools',
+      author='filbuk',
+      author_email='filbuk.wen@gmail.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
