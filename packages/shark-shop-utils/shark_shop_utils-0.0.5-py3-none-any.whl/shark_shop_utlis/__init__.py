9dd639200e82d1500a4d195fd549e137e3619af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:45:54 +0000

    Add package shark-shop-utils/0.0.5/shark_shop_utils-0.0.5-py3-none-any.whl

---
+++ packages/shark-shop-utils/shark_shop_utils-0.0.5-py3-none-any.whl/shark_shop_utlis/__init__.py
@@ -0,0 +1,12 @@
+from .depends import CurrentUserBase, CurrentUser, CurrentSuperUser, ApplicationToken
+from .ormar_custom_router import CustomOrmarCRUDRouter
+
+__version__ = "0.0.5"
+
+__all__ = [
+    "CurrentUserBase",
+    "CurrentUser",
+    "CurrentSuperUser",
+    "CustomOrmarCRUDRouter",
+    "ApplicationToken"
+]
