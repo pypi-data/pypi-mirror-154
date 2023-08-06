dc4adbb6c6412a028ff7a00a49277d8ac524ce4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:29:30 +0000

    Add package segmind/0.1.9b1/segmind-0.1.9b1.tar.gz

---
+++ packages/segmind/segmind-0.1.9b1.tar.gz/segmind-0.1.9b1/README.md
@@ -0,0 +1,53 @@
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/segmind?style=plastic)
+![GitHub Workflow Status (event)](https://img.shields.io/github/workflow/status/segmind/segmind/CI?event=push)
+![GitHub Workflow Status (event)](https://img.shields.io/github/workflow/status/segmind/segmind/Upload%20Python%20Package?event=publish)
+![PyPI - Downloads](https://img.shields.io/pypi/dm/segmind)
+![PyPI](https://img.shields.io/pypi/v/segmind)
+![GitHub tag (latest SemVer)](https://img.shields.io/github/v/tag/segmind/segmind)
+## Installation
+
+`pip3 install -r requirements.txt`
+
+## Code style
+
+### Python
+We adopt [PEP8](https://www.python.org/dev/peps/pep-0008/) as the preferred code style.
+
+We use the following tools for linting and formatting:
+- [flake8](http://flake8.pycqa.org/en/latest/): linter
+- [yapf](https://github.com/google/yapf): formatter
+- [isort](https://github.com/timothycrosley/isort): sort imports
+
+Style configurations of yapf and isort can be found in [setup.cfg](../setup.cfg).
+
+We use [pre-commit hook](https://pre-commit.com/) that checks and formats for `flake8`, `yapf`, `isort`, `trailing whitespaces`,
+ fixes `end-of-files`, sorts `requirments.txt` automatically on every commit.
+The config for a pre-commit hook is stored in [.pre-commit-config](../.pre-commit-config.yaml).
+
+After you clone the repository, you will need to install initialize pre-commit hook.
+
+```
+pip install -U pre-commit
+```
+
+From the repository folder
+```
+pre-commit install
+```
+
+After this on every commit check code linters and formatter will be enforced.
+
+
+
+# Run locally
+
+`cd .`
+
+`pip install virtualenv`
+
+`virtualenv venv`
+
+`source ./venv/bin/activate`
+
+pip install -r requirements.txt
+pip install -e .
---
+++ packages/segmind/segmind-0.1.9b1.tar.gz/segmind-0.1.9b1/segmind/cli.py
@@ -0,0 +1,106 @@
+import getpass
+import os
+import sys
+import time
+
+import click
+
+from segmind.data.public import upload as upload_data
+from segmind.data.public import sync as sync_data
+from segmind.utils import cyan_print, green_print, red_print
+from segmind.lite_extensions.client_utils import get_user_info_with_new_token
+from segmind.version import VERSION
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option(
+    "-a",
+    "--access_token",
+    required=False,
+    type=click.STRING,
+    help="String, Segmind Access Token",
+)
+def config(access_token):
+    if not access_token:
+        access_token = input("Enter Access token: ")
+
+    try:
+        user_info = get_user_info_with_new_token(access_token=access_token)
+    except Exception as err:
+        red_print(f"Log-In failed !!! Error: {err}")
+        sys.exit()
+
+    green_print(f"Welcome {user_info['username']}, Log-In Successful !!!")
+
+
+@cli.command()
+@click.option(
+    "-p",
+    "--path",
+    required=True,
+    type=click.Path(exists=True),
+    help="String, path to the file/folder",
+)
+@click.option(
+    "--destination_path",
+    required=False,
+    type=str,
+    help="String, example README.md OR new_folder/README.md OR old_folder/new_folder/README.md",
+)
+@click.option(
+    "--datastore_name",
+    required=True,
+    type=str,
+    help="String, name of your datastore, created via Segmind UI from https://cloud.segmind.com/",
+)
+def upload(path, destination_path, datastore_name, *args, **kwargs):
+    upload_data(
+        path=path,
+        destination_path=destination_path,
+        datastore_name=datastore_name,
+        via_cli=True,
+    )
+    green_print(
+        "Success! To view your file/folder, login to your account in https://cloud.segmind.com"
+    )
+
+@cli.command()
+@click.option(
+    "-p",
+    "--path",
+    required=True,
+    type=click.Path(exists=True),
+    help="String, path to the file/folder",
+)
+@click.option(
+    "--destination_path",
+    required=False,
+    type=str,
+    help="String, example README.md OR new_folder/README.md OR old_folder/new_folder/README.md",
+)
+@click.option(
+    "--datastore_name",
+    required=True,
+    type=str,
+    help="String, name of your datastore, created via Segmind UI from https://cloud.segmind.com/",
+)
+def sync(path, destination_path, datastore_name, *args, **kwargs):
+    sync_data(
+        path=path,
+        destination_path=destination_path,
+        datastore_name=datastore_name,
+        via_cli=True,
+    )
+    green_print(
+        "Success! To view your file/folder, login to your account in https://cloud.segmind.com"
+    )
+
+
+@cli.command()
+def version(*args, **kwargs):
+    green_print(f"Segmind {VERSION}")
