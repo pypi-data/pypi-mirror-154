48b5bc8c4014e0f30c7bd93492bddd204175d480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:17 +0000

    Add package sivadathhistory/1.0/sivadathhistory-1.0.tar.gz

---
+++ packages/sivadathhistory/sivadathhistory-1.0.tar.gz/sivadathhistory-1.0/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+from pathlib import Path
+setuptools.setup(
+    name = "sivadathhistory",
+    version = 1.0,
+    long_description = Path("README.md").read_text(),
+    packages = setuptools.setuptools.find_packages(exclude = ["data", "tests"])
+)
---
+++ packages/sivadathhistory/sivadathhistory-1.0.tar.gz/sivadathhistory-1.0/sivadathhistory/family.py
@@ -0,0 +1,14 @@
+def father():
+    print("my father is biju")
+def mother():
+    print("my mther is anitha")
+def sister():
+    print("my sister is chandralekha")
+def pet():
+    print("my pet is monkuttan")
+def housename():
+    print("sivasailam")
+def plcae():
+    print("keralapuram, kollam, kerala")
+def personalno():
+    print("8089156390")
