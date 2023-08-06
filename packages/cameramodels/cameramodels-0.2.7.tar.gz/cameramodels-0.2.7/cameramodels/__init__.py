b213bf353d11b33b89a703fb1b1d44171b593a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:02 +0000

    Add package cameramodels/0.2.7/cameramodels-0.2.7.tar.gz

---
+++ packages/cameramodels/cameramodels-0.2.7.tar.gz/cameramodels-0.2.7/cameramodels/__init__.py
@@ -0,0 +1,15 @@
+# flake8: noqa
+
+import pkg_resources
+
+
+__version__ = pkg_resources.get_distribution('cameramodels').version
+
+
+from cameramodels.pinhole_camera import PinholeCameraModel
+
+import cameramodels.data
+import cameramodels.io
+import cameramodels.models
+
+from cameramodels.align import align_depth_to_rgb
