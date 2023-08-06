21732887feb6d30bae9d1a90b0757d01bd4a007d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:09 +0000

    Add package current/0.4.1/current-0.4.1-py3-none-any.whl

---
+++ packages/current/current-0.4.1-py3-none-any.whl/current.py
@@ -0,0 +1,70 @@
+"""Temporarily change sys.path for imports relative to the current module."""
+
+import contextlib
+import os
+import sys
+
+__all__ = ['current_path', 'inserted_path', 'caller_path']
+
+__title__ = 'current'
+__version__ = '0.4.1'
+__author__ = 'Sebastian Bank <sebastian.bank@uni-leipzig.de>'
+__license__ = 'CC0, see LICENSE.TXT'
+
+
+def current_path(*names):
+    """Return the path to names relative to the current module."""
+    depth = 0 if __name__ == '__main__' else 1
+
+    frame = sys._getframe(depth)
+
+    try:
+        path = os.path.dirname(frame.f_code.co_filename)
+    finally:
+        del frame
+
+    if names:
+        path = os.path.join(path, *names)
+
+    return os.path.realpath(path)
+
+
+@contextlib.contextmanager
+def inserted_path(directory=os.pardir, index: int = 1):
+    """Temporarily insert directory (os.pardir) to sys.path at index (1)."""
+    depth = 1 if __name__ == '__main__' else 2
+
+    frame = sys._getframe(depth)
+
+    try:
+        path = os.path.dirname(frame.f_code.co_filename)
+    finally:
+        del frame
+
+    path = os.path.realpath(os.path.join(path, directory))
+
+    oldpath = list(sys.path)
+    sys.path.insert(index, path)
+
+    try:
+        yield
+    finally:
+        sys.path = oldpath
+
+
+def caller_path(steps=1, names=None):
+    """Return the path to the file of the current frames' caller."""
+    frame = sys._getframe(steps + 1)
+
+    try:
+        path = os.path.dirname(frame.f_code.co_filename)
+    finally:
+        del frame
+
+    if not path:
+        path = os.getcwd()
+
+    if names is not None:
+        path = os.path.join(path, *names)
+
+    return os.path.realpath(path)
