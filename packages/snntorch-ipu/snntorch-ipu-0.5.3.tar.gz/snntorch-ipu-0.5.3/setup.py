8f0b7a83ec6613723615c3d8e70cac040a3f90f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:41:34 +0000

    Add package snntorch-ipu/0.5.3/snntorch-ipu-0.5.3.tar.gz

---
+++ packages/snntorch-ipu/snntorch-ipu-0.5.3.tar.gz/snntorch-ipu-0.5.3/setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open("README.rst", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
+# with open("HISTORY.rst") as history_file:
+#     history = history_file.read()
+
+# fmt: off
+__version__ = '0.5.3'
+# fmt: on
+
+requirements = [
+    "torch>=1.1.0",
+    "pandas",
+    "matplotlib",
+    "numpy>=1.17",
+]
+
+
+test_requirements = ["pytest>=3"]
+
+version = __version__
+
+setup(
+    author="Jason K. Eshraghian & Vincent Sun",
+    author_email="jasonesh@umich.edu",
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    description="Deep learning with spiking neural networks on IPUs.",
+    # entry_points={
+    #     "console_scripts": [
+    #         "snntorch=snntorch.cli:main",
+    #     ],
+    # },
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    long_description=readme,
+    include_package_data=True,
+    keywords="snntorch",
+    name="snntorch-ipu",
+    packages=find_packages(include=["snntorch-ipu", "snntorch", "snntorch.*"]),
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/vinniesun/snntorch-ipu",
+    version=__version__,
+    zip_safe=False,
+)
