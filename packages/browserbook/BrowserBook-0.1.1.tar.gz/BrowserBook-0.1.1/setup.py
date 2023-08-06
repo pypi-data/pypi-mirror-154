88b268b20d0c69c04f4d8d6100c5d43a21ce225d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:33 +0000

    Add package browserbook/0.1.1/BrowserBook-0.1.1.tar.gz

---
+++ packages/browserbook/BrowserBook-0.1.1.tar.gz/BrowserBook-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['browserbook', 'browserbook.tools']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'browserbook',
+    'version': '0.1.1',
+    'description': 'A web browser backend. Gets all data needed from a url. Multiple sessions at once is possible',
+    'long_description': None,
+    'author': 'DragonHunter',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
