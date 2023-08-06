de30add4495aab77e0da0671e31a05d43a832c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:17 +0000

    Add package disentpy/0.0.18/disentpy-0.0.18-py3-none-any.whl

---
+++ packages/disentpy/disentpy-0.0.18-py3-none-any.whl/disent/settings.py
@@ -0,0 +1,6 @@
+ENVS = {
+  "local": ["http", "localhost:8000"],
+  "dev": ["https", "api-dev.disent.com"],
+  "prod": ["https", "snpricer-dev.disent.com"],
+  "disent-cloud": ["https", "snpricer-dev.disent.com"]
+}
