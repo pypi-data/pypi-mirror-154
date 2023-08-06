d6689321d9112d0314c4a1cf1aa861d665f3fe99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:44:01 +0000

    Add package simstring-fast/0.1.0/simstring-fast-0.1.0.tar.gz

---
+++ packages/simstring-fast/simstring-fast-0.1.0.tar.gz/simstring-fast-0.1.0/dev/company_names.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+
+import os, sys
+sys.path.append(os.getcwd())
+import numpy as np
+
+from simstring.feature_extractor.character_ngram import CharacterNgramFeatureExtractor
+from simstring.measure import CosineMeasure, OverlapMeasure, LeftOverlapMeasure
+# from simstring.database.mongo import MongoDatabase
+from simstring.database.dict import DictDatabase
+from simstring.searcher import Searcher
+import numpy as np
+
+from pyinstrument import Profiler
+
+profiler = Profiler()
+
+
+def output_similar_strings_of_each_line(path, measure):
+    strings = []
+    with open(path, 'r') as lines:
+        for line in lines:
+            strings.append(line.rstrip('\r\n'))
+
+
+    db = DictDatabase(CharacterNgramFeatureExtractor(2))
+    for string in strings:
+        db.add(string)
+
+    db.save("companies.db")
+
+    dbl = DictDatabase.load("companies.db")
+
+            
+    searcher = Searcher(dbl, measure)
+    profiler.start()
+
+
+    for string in strings:
+        result = searcher.search(string, 0.8)
+        # result = [str(np.round(x[0], 5)) + ' ' + x[1] for x in searcher.ranked_search(string, 0.8)]
+        # print("\t".join([string, ",".join(result)]))
+
+    profiler.stop()
+
+    profiler.print()
+    profiler.open_in_browser()
+    
+
+measure = CosineMeasure()
+output_similar_strings_of_each_line('./data/company_names.txt', measure)
+
+measure = OverlapMeasure()
+output_similar_strings_of_each_line('./data/company_names.txt', measure)
+
+measure = LeftOverlapMeasure()
+output_similar_strings_of_each_line('./data/company_names.txt', measure)
+
---
+++ packages/simstring-fast/simstring-fast-0.1.0.tar.gz/simstring-fast-0.1.0/tests/measure/test_cosine.py
@@ -0,0 +1,37 @@
+# -*- coding:utf-8 -*-
+
+from unittest import TestCase
+from simstring.measure.cosine import CosineMeasure
+
+class TestCosine(TestCase):
+    measure = CosineMeasure()
+
+    def test_min_feature_size(self):
+        self.assertEqual(self.measure.min_feature_size(5, 1.0), 5)
+        self.assertEqual(self.measure.min_feature_size(5, 0.5), 2)
+
+    def test_max_feature_size(self):
+        self.assertEqual(self.measure.max_feature_size(5, 1.0), 5)
+        self.assertEqual(self.measure.max_feature_size(5, 0.5), 20)
+
+    def test_minimum_common_feature_count(self):
+        self.assertEqual(self.measure.minimum_common_feature_count(5, 5, 1.0), 5)
+        self.assertEqual(self.measure.minimum_common_feature_count(5, 20, 1.0), 10)
+        self.assertEqual(self.measure.minimum_common_feature_count(5, 5, 0.5), 3)
+
+    def test_similarity(self):
+        x = ["a", "ab", "bc", "c"]
+        y = ["a", "ab", "bc", "cd", "e"]
+        self.assertEqual(round(self.measure.similarity(x, x), 2), 1.0)
+        self.assertEqual(round(self.measure.similarity(x, y), 2), 0.67)
+        
+        z = ["a", "ab", "ba", "ab", "a"]
+        self.assertEqual(round(self.measure.similarity(z, z), 2), 1.0)
+        self.assertEqual(round(self.measure.similarity(x, z), 2), 0.58)
+        self.assertEqual(round(self.measure.similarity(x, y), 2), 0.67)
+
+        # Test as per paper trigrams with quotes of methyl sulphone and methyl sulfone
+        a = [' "m', '"me', 'met', 'eth', 'thy', 'hyl', 'yl ', 'l s', ' su', 'sul', 'ulf', 'lfo', 'fon', 'one', 'ne"', 'e" ']
+        b = [' "m', '"me', 'met', 'eth', 'thy', 'hyl', 'yl ', 'l s', ' su', 'sul', 'ulp', 'lph', 'pho', 'hon', 'one', 'ne"', 'e" ']
+        self.assertEqual(round(self.measure.similarity(a, b), 3), 0.788) #BUG? Disagrees with paper that claims should be 0.788
+
