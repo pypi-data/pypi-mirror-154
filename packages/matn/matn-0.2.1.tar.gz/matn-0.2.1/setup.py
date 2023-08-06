6709055cd974b770e7eeb32e2f02abd72ea4d927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:42 +0000

    Add package matn/0.2.1/matn-0.2.1.tar.gz

---
+++ packages/matn/matn-0.2.1.tar.gz/matn-0.2.1/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import re
+from pathlib import Path
+
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+readme_text = (this_directory / "README.md").read_text()
+long_description = re.sub(re.compile("<picture?>"), "", readme_text)
+
+setup(
+    name="matn",
+    version="0.2.1",
+    packages=["matn"],
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "matn = matn.cli:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development",
+    ],
+    url="https://github.com/iamjazzar/matn",
+    license="MIT License",
+    author="Ahmed Jazzar",
+    author_email="me@ahmedjazzar.com",
+    description="Arabic text processor tool.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
