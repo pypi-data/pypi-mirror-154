f566271c15df905b7831908b7753c5289bfb0709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:23:05 +0000

    Add package loggerpackage/0.0.1/loggerpackage-0.0.1.tar.gz

---
+++ packages/loggerpackage/loggerpackage-0.0.1.tar.gz/loggerpackage-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'My first Python package'
+LONG_DESCRIPTION = 'My first Python package with a slightly longer description'
+setup(name='loggerpackage',
+        version=VERSION,
+        description=DESCRIPTION,
+        author='Havacs Peter',
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'loggerpackage'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
