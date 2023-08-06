114b312a4e0875cd8b7d11736612418d5d397a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:34:17 +0000

    Add package nwunity/0.2.7/nwunity-0.2.7.tar.gz

---
+++ packages/nwunity/nwunity-0.2.7.tar.gz/nwunity-0.2.7/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from os import path
+
+DIR = path.dirname(path.abspath(__file__))
+# INSTALL_PACKAGES = open(path.join(DIR, 'requirements.txt')).read().splitlines()
+
+with open(path.join(DIR, 'README.md')) as f:
+    README = f.read()
+
+setup(
+    name='nwunity',
+    packages=['nwunity'],
+    entry_points = {
+        'console_scripts': [
+            'nwunity = nwunity.cli:main',
+        ],
+    },
+    description="NW-Unity is a tool for auto packing your Unity WebGL output files to a NW.js executable file.",
+    long_description=README,
+    long_description_content_type='text/markdown',
+    # install_requires=INSTALL_PACKAGES,
+    version='0.2.7',
+    url='https://github.com/zzxzzk115/NW-Unity',
+    author='Lazy_V',
+    author_email='954294627@qq.com',
+    keywords=['NW.js', 'GameShell', 'Unity', 'WebGL'],
+    # tests_require=[
+    #     'pytest',
+    #     'pytest-cov',
+    #     'pytest-sugar'
+    # ],
+    include_package_data=True,
+    python_requires='>=3'
+)
