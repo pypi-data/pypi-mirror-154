22e5d6ab37e10b4c42a1f3ba1fe808267ac2c43e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:54 +0000

    Add package ehelply-microservice-library/1.9.8/eHelply-Microservice-Library-1.9.8.tar.gz

---
+++ packages/ehelply-microservice-library/eHelply-Microservice-Library-1.9.8.tar.gz/eHelply-Microservice-Library-1.9.8/ehelply_microservice_library/cli/cli_utils.py
@@ -0,0 +1,28 @@
+import typer
+import base64
+from typing import List
+
+cli = typer.Typer()
+
+
+RESERVED_WORDS: List[str] = ["playground", "test", "testing", "privileged"]
+
+@cli.command()
+def encode_json_header(
+        json: str = typer.Option(..., prompt=True, help="JSON string to encrypt"),
+):
+    json_bytes = json.encode('utf-8')
+    base64_bytes = base64.b64encode(json_bytes)
+    base64_str = base64_bytes.decode('utf-8')
+    typer.echo(base64_str)
+
+
+def prune_reserved_words(openapi_data: dict):
+    for endpoint_path, endpoints in openapi_data['paths'].items():
+        remove: List[str] = []
+        for endpoint_verb, endpoint in endpoints.items():
+            for reserved_word in RESERVED_WORDS:
+                if 'tags' in endpoint and reserved_word in endpoint['tags']:
+                    remove.append(endpoint_verb)
+        for remove_verb in remove:
+            del endpoints[remove_verb]
