454187fd7d2710392bcf2176dc89ff7f2612f899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:41 +0000

    Add package paxml/0.0.dev1/paxml-0.0.dev1.tar.gz

---
+++ packages/paxml/paxml-0.0.dev1.tar.gz/paxml-0.0.dev1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='paxml',
+    version='0.0.dev1',
+    description='',
+    long_description='',
+    url='https://github.com/google/paxml',
+    author='Paxml authors',
+    author_email='shafey@google.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
