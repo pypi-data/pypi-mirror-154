a2cdcad0b6597b54141c74e95524d343fdcd8ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:19:20 +0000

    Add package hspylib/0.11.66/hspylib-0.11.66-py3-none-any.whl

---
+++ packages/hspylib/hspylib-0.11.66-py3-none-any.whl/hspylib/__init__.py
@@ -0,0 +1,14 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v0.11.66
+#
+# Package: main.hspylib
+"""Package initialization."""
+
+__all__ = [
+  '__main__', 
+  'addons', 
+  'core', 
+  'modules'
+]
+__version__ = '0.11.66'
---
+++ packages/hspylib/hspylib-0.11.66-py3-none-any.whl/hspylib/core/tools/__init__.py
@@ -0,0 +1,16 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib v0.11.66
+#
+# Package: main.hspylib.core.tools
+"""Package initialization."""
+
+__all__ = [
+  'commons', 
+  'constants', 
+  'git_tools', 
+  'preconditions', 
+  'text_tools', 
+  'validator'
+]
+__version__ = '0.11.66'
