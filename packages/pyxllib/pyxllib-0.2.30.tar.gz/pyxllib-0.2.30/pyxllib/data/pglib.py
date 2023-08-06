1a149d2b62e2a0eb9e31e3c284ea08dcb1fd6eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:17 +0000

    Add package pyxllib/0.2.30/pyxllib-0.2.30.tar.gz

---
+++ packages/pyxllib/pyxllib-0.2.30.tar.gz/pyxllib-0.2.30/pyxllib/data/pglib.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author : 陈坤泽
+# @Email  : 877362867@qq.com
+# @Date   : 2022/06/09 16:26
+
+"""
+针对PostgreSQL封装的工具
+"""
+
+from pyxllib.prog.pupil import check_install_package
+
+check_install_package('psycopg')
+
+import psycopg
+
+
+# import psycopg2
+# psycopg2.extensions.connection
+
+
+class Connection(psycopg.Connection):
+    pass
