92c3fb98ad0bdf1d4a2472e732af8884d5fa8661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:38:25 +0000

    Add package laboro/0.0.42/laboro-0.0.42.tar.gz

---
+++ packages/laboro/laboro-0.0.42.tar.gz/laboro-0.0.42/src/laboro/context/__init__.py
@@ -0,0 +1,78 @@
+import os
+import logging
+from laboro.error import LaboroError
+from laboro.history import History
+from laboro.config.manager import Manager as ConfigMgr
+from laboro.module.manager import Manager as ModuleMgr
+from laboro.error.handler import Handler as ErrorHandler
+
+
+class Context:
+  def __init__(self, logger):
+    self.logger = logger
+    self.memory = dict()
+    self.modules = dict()
+    self.history = None
+    self.modulemgr = ModuleMgr()
+    self.configmgr = ConfigMgr("/etc/laboro/laboro.yml")
+    self.workspacedir = self.configmgr.get_parameter("main",
+                                                     "$.laboro.workspacedir")
+    self.histdir = self.configmgr.get_parameter("main",
+                                                "$.laboro.histdir")
+    self.workflowdir = self.configmgr.get_parameter("main",
+                                                    "$.laboro.workflowdir")
+    self.logdir = self.configmgr.get_parameter("main",
+                                               "$.laboro.log.dir")
+    self.loglevel = self.configmgr.get_parameter("main",
+                                                 "$.laboro.log.level")
+    logging.getLogger().vault.clear()
+
+  def reset(self, workflow_name, session):
+    logging.getLogger().vault.clear()
+    self.memory = dict()
+    self._configure_logger(workflow_name, session)
+    self.set_workflow_config(workflow_name)
+    self.history = History(filename=os.path.join(self.histdir,
+                                                 f"{workflow_name}.db"),
+                           workflow=workflow_name,
+                           session=session,
+                           params=self.configmgr.workflow_config)
+    return self
+
+  def _configure_logger(self, workflow_name, session):
+    self.logger.add_file_handler(self.logdir, workflow_name, session)
+    self.logger.set_log_level(self.loglevel)
+
+  def set_workflow_config(self, workflow_name):
+    config_file_name = os.path.join(self.workflowdir, f"{workflow_name}.yml")
+    logging.info(f"[+] Loading workflow_config: {config_file_name}")
+    self.configmgr.workflow_config = config_file_name
+
+  def install_packages(self):
+    """Install all packages listed in the workflow configuration file.
+    """
+    packages = self.configmgr.get_parameter("workflow",
+                                            "$.laboro.workflow.packages")
+    for pkg in packages:
+      self.modulemgr.install_package(pkg)
+
+  def register_class(self, module, cls):
+    self.modulemgr.register_class_from_module(cls, module)
+
+  def register_instance_secrets(self, instance, args):
+    class_args = instance.specification["args"]
+    secret_keys = [arg["name"] for arg in class_args if arg["secret"]]
+    secrets = [args[key] for key in args.keys() if key in secret_keys]
+    list(map(logging.getLogger().vault.add, secrets))
+
+  def register_method_secrets(self, instance, action, args):
+    try:
+      method_args = [meth for meth in instance.specification["methods"] if meth["name"] == action][0]["args"]
+      secret_keys = [arg["name"] for arg in method_args if arg["secret"]]
+      secrets = [args[key] for key in args.keys() if key in secret_keys]
+      list(map(logging.getLogger().vault.add, secrets))
+    except IndexError as err:
+      raise LaboroError(f"UnknownMethodError: {action}") from err
+
+  def handle_error(self, kind, value, traceback):
+    ErrorHandler().handle_error(kind, value, traceback)
---
+++ packages/laboro/laboro-0.0.42.tar.gz/laboro-0.0.42/src/laboro/workflow/__init__.py
@@ -0,0 +1,82 @@
+import uuid
+import logging
+from laboro.workspace import Workspace
+
+
+class Workflow:
+  """The ``laboro.workflow.Workflow`` object is the main class for the workflow representation.
+  It load all configuration needed, set alk objects such History, Vault and workspace and run according to its configuration.
+
+  The Workflow object provides a runtime context that will handle log, history, vault, and workspace, etc.
+
+  Arguments:
+    name: A string representing the workflow name.
+    logger: A laboro.logger.manager.Manager instance.
+
+  Returns:
+    ``laboro.workflow.Workflow``: A Workflow object.
+
+  ..  code-block:: python
+
+      from laboro.workflow import Workflow
+      from laboro.logger.manager import Manager as LogMgr
+
+      logmgr = LogMgr()
+      logmgr.set_default()
+      with Workflow(name="my_workflow", logger=logmgr) as wkf:
+        wkf.run()
+        ...
+  """
+  def __init__(self, name, context):
+    self.name = name
+    self.session = str(uuid.uuid4())
+    self.context = context.reset(self.name, self.session)
+    self.workspace = Workspace(workspacedir=self.context.workspacedir,
+                               workflow=self.name,
+                               session=self.session)
+
+  def __enter__(self):
+    logging.getLogger().log_section("WORKFLOW",
+                                    f"Started {self.name} / {self.session}")
+    self.context.history.enter()
+    self.context.install_packages()
+    return self
+
+  def __exit__(self, kind, value, traceback):
+    self.workspace.delete()
+    self.context.history.exit(kind, value)
+    exit_code = 0
+    if kind == SystemExit:
+      if value.code is not None:
+        exit_code = value.code
+    elif kind is not None:
+      exit_code = f"{kind.__name__}: {value}"
+    logging.info(f"[+] Workflow ended with code: {exit_code}")
+    self.context.handle_error(kind, value, traceback)
+    self.context.logger.remove_file_handler(self.session)
+
+  def _instantiate(self, module, cls, args):
+    logging.info(f"[+] Object instantiation: {module}.{cls}")
+    return self.context.modulemgr.get_class_from_module(cls=cls,
+                                                        module=module)(args=args)
+
+  def run(self):
+    """Run the workflow."""
+    for step in self.context.configmgr.get_parameter("workflow",
+                                                     "$.laboro.workflow.steps"):
+      logging.getLogger().log_section("STEP", step['name'])
+      module = step["module"]["name"]
+      cls_args = step["module"]["args"]
+      cls = step["module"]["class"]
+      self.context.register_class(module, cls)
+      instance = self._instantiate(module, cls, cls_args)
+      self.context.register_instance_secrets(instance, cls_args)
+      for action in step["module"]["actions"]:
+        action_args = action["args"]
+        self.context.register_method_secrets(instance,
+                                             action["method"],
+                                             action_args)
+        logging.getLogger().log_section("ACTION",
+                                        f"{step['name']} / {action['method']}")
+        getattr(instance, action["method"])(**action_args)
+    logging.getLogger().log_section("WORKFLOW", "No more steps to run")
