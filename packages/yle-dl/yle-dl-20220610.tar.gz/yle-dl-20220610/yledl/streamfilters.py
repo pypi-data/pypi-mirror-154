333a767a51ed5bc8562641dc381ee2f09bea4d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:00 +0000

    Add package yle-dl/20220610/yle-dl-20220610.tar.gz

---
+++ packages/yle-dl/yle-dl-20220610.tar.gz/yle-dl-20220610/yledl/streamfilters.py
@@ -0,0 +1,15 @@
+import attr
+from .backends import Backends
+
+
+@attr.s
+class StreamFilters:
+    """Parameters for deciding which of potentially multiple available stream
+    versions to download.
+    """
+    latest_only = attr.ib(default=False)
+    maxbitrate = attr.ib(default=None)
+    maxheight = attr.ib(default=None)
+    enabled_backends = attr.ib(default=attr.Factory(
+        lambda: list(Backends.default_order)
+    ))
---
+++ packages/yle-dl/yle-dl-20220610.tar.gz/yle-dl-20220610/yledl/streamflavor.py
@@ -0,0 +1,21 @@
+import attr
+from .backends import FailingBackend
+
+
+@attr.s
+class StreamFlavor:
+    media_type = attr.ib()
+    height = attr.ib(default=None, converter=attr.converters.optional(int))
+    width = attr.ib(default=None, converter=attr.converters.optional(int))
+    bitrate = attr.ib(default=None, converter=attr.converters.optional(int))
+    streams = attr.ib(factory=list)
+
+
+class FailedFlavor(StreamFlavor):
+    def __init__(self, error_message):
+        StreamFlavor.__init__(self,
+                              media_type='unknown',
+                              height=None,
+                              width=None,
+                              bitrate=None,
+                              streams=[FailingBackend(error_message)])
