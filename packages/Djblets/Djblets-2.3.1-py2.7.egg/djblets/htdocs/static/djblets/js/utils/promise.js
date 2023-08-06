a2b69633c9bc92d9d947c33ed6edbb40838cf691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:52:23 +0000

    Add package Djblets/2.3.1/Djblets-2.3.1-py2.7.egg

---
+++ packages/Djblets/Djblets-2.3.1-py2.7.egg/djblets/htdocs/static/djblets/js/utils/promise.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * Return a new promise with its resolver.
+ *
+ * Returns:
+ *     array:
+ *     An array of the promise and its resolver.
+ */
+Promise.withResolver = function withResolver() {
+  var resolver;
+  var promise = new Promise(function (resolve, reject) {
+    resolver = resolve;
+  });
+  return [promise, resolver];
+};
+
+//# sourceMappingURL=promise.js.map
