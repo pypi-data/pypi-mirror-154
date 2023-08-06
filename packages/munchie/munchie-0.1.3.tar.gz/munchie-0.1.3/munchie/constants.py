20e233484c36a6ee5f95b35923df33a65da25a1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:35 +0000

    Add package munchie/0.1.3/munchie-0.1.3.tar.gz

---
+++ packages/munchie/munchie-0.1.3.tar.gz/munchie-0.1.3/munchie/constants.py
@@ -0,0 +1,7 @@
+SUPPORTED_FILE_MODES = ["r", "w", "x", "a", "t", "b", "+"]
+SUPPORTED_FILE_TYPES = {
+    "csv": ["csv"],
+    "json": ["json"],
+    "toml": ["cfg", "ini", "toml"],
+    "yaml": ["yaml", "yml"],
+}
