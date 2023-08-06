33e78f3ff57fb49c2b5a3895831b90f51efe44aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:45:19 +0000

    Add package oblivious-robots-target-searching/0.1.5/oblivious_robots_target_searching-0.1.5.tar.gz

---
+++ packages/oblivious-robots-target-searching/oblivious_robots_target_searching-0.1.5.tar.gz/oblivious_robots_target_searching-0.1.5/README.md
@@ -0,0 +1,9 @@
+# Oblivious Robots Target Searching
+
+## Current Version
+
+0.1.5
+
+## Changelog
+
+- Fixed GUI in windows
---
+++ packages/oblivious-robots-target-searching/oblivious_robots_target_searching-0.1.5.tar.gz/oblivious_robots_target_searching-0.1.5/src/oblivious_robots_target_searching/__main__.py
@@ -0,0 +1,8 @@
+import sys
+
+from ._terminal import terminal
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    terminal(args)
+    
