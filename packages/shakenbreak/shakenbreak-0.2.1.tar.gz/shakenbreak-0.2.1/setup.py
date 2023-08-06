557b2e17a94a7ff6ffe2fdd0f18f1a7afce23af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:30:18 +0000

    Add package shakenbreak/0.2.1/shakenbreak-0.2.1.tar.gz

---
+++ packages/shakenbreak/shakenbreak-0.2.1.tar.gz/shakenbreak-0.2.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+This is a setup.py script to install ShakeNBreak
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="shakenbreak",
+    version="0.2.1",
+    description="Package to generate and analyse distorted defect structures, in order to "
+    "identify ground-state and metastable defect configurations.",
+    author="Irea Mosquera, Seán Kavanagh",
+    author_email="irea.lois.20@ucl.ac.uk",
+    packages=find_packages(),
+    license="MIT",
+    install_requires=[
+        "doped>=0.0.5",
+        "numpy",
+        "pymatgen",
+        "matplotlib",
+        "ase",
+        "pandas",
+        "seaborn",
+        "hiphive",
+        "monty",
+    ],
+    extras_require={"tests": ["pytest", "pytest-mpl"]},
+    include_package_data=True
+)
