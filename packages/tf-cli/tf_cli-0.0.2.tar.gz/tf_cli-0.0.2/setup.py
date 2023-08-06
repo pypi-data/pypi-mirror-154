579fcc612f7f705c325e5dc7ad79a217b83e2e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:02 +0000

    Add package tf-cli/0.0.2/tf_cli-0.0.2.tar.gz

---
+++ packages/tf-cli/tf_cli-0.0.2.tar.gz/tf_cli-0.0.2/setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt", "r") as f:
+    install_requires = f.readlines()
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="tf_cli",
+    version="0.0.2",
+    description="This contains a CLI for AWS terraform",
+    author="Rooklyn Kline",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rkline2/tf_cli",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    package_data={'':['scripts/*']},
+    include_package_data=True,
+    install_requires=["click==8.1.3", "wheel==0.37.0"],
+    entry_points = {
+        'console_scripts':[
+            'tf = tf.cli:main',
+        ],
+    },
+    python_requires=">=3.7",
+    extras_require={
+        "dev": [
+            "pytest==6.2.5",
+            "wheel==0.37.0",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: POSIX :: Linux",
+    ]
+)
