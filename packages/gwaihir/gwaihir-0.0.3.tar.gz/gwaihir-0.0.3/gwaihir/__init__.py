661916df4f09a3fa7cdd773637e0b1b63b1364dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:20 +0000

    Add package gwaihir/0.0.3/gwaihir-0.0.3.tar.gz

---
+++ packages/gwaihir/gwaihir-0.0.3.tar.gz/gwaihir-0.0.3/gwaihir/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+"""Welcome to gwaihir, there is one submodule used for the gui.
+
+The terminal scripts used for quick analysis are in the terminal_scripts
+folder
+"""
+
+__all__ = ["gui", "scripts", "support", "plot", "utilities", "version"]
