24689024184d3a5b349a5074d37c6ce79fc92f99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:10 +0000

    Add package pytorch-functional/0.1.5/pytorch-functional-0.1.5.tar.gz

---
+++ packages/pytorch-functional/pytorch-functional-0.1.5.tar.gz/pytorch-functional-0.1.5/pytorch-functional/__init__.py
@@ -0,0 +1,2 @@
+from .src import layers, tools
+from .src.functional_model import Input, FunctionalModel
---
+++ packages/pytorch-functional/pytorch-functional-0.1.5.tar.gz/pytorch-functional-0.1.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+# NOT NEEDED!
+# setup(
+#    name='pytorch-functional',
+#    version='0.1.0',
+#    description='Provides functional API for model creation in PyTorch',
+#    author='gahaalt',
+#    author_email='gahaalt@gmail.com',
+#    packages=['pytorch_functional'],
+#    url='https://github.com/gahaalt/pytorch-functional',
+#    license='MIT',
+# )
+
+setup()
