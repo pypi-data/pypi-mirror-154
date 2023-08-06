f914ed945cd1293bf91276c6f96fa8ebd871fc08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:27:50 +0000

    Add package planemo/0.74.11/planemo-0.74.11.tar.gz

---
+++ packages/planemo/planemo-0.74.11.tar.gz/planemo-0.74.11/planemo/galaxy/__init__.py
@@ -0,0 +1,18 @@
+"""Entry-point for Galaxy specific functionality in Planemo."""
+
+
+from .config import galaxy_config
+from .run import (
+    run_galaxy_command,
+    setup_venv,
+)
+from .serve import serve as galaxy_serve
+from .serve import shed_serve
+
+__all__ = (
+    "galaxy_config",
+    "setup_venv",
+    "run_galaxy_command",
+    "galaxy_serve",
+    "shed_serve",
+)
---
+++ packages/planemo/planemo-0.74.11.tar.gz/planemo-0.74.11/planemo/galaxy/test/__init__.py
@@ -0,0 +1,14 @@
+"""Entry point and interface for ``planemo.galaxy.test`` package."""
+from .actions import (
+    handle_reports,
+    handle_reports_and_summary,
+    run_in_config,
+)
+from .structures import StructuredData
+
+__all__ = (
+    "handle_reports",
+    "handle_reports_and_summary",
+    "run_in_config",
+    "StructuredData",
+)
