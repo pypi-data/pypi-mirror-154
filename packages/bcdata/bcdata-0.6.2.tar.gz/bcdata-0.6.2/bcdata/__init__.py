fb50ee4d314629d4203093d1cc545c5c09992c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:52 +0000

    Add package bcdata/0.6.2/bcdata-0.6.2.tar.gz

---
+++ packages/bcdata/bcdata-0.6.2.tar.gz/bcdata-0.6.2/bcdata/__init__.py
@@ -0,0 +1,17 @@
+from .wfs import get_table_name
+from .wfs import get_data
+from .wfs import get_features
+from .wfs import get_count
+from .wfs import list_tables
+from .wfs import validate_name
+from .wfs import define_request
+from .wfs import get_type
+from .wcs import get_dem
+
+__version__ = "0.6.2"
+
+
+BCDC_API_URL = "https://catalogue.data.gov.bc.ca/api/3/action/"
+WFS_URL = "https://openmaps.gov.bc.ca/geo/pub/wfs"
+OWS_URL = "http://openmaps.gov.bc.ca/geo/ows"
+WCS_URL = "https://openmaps.gov.bc.ca/om/wcs"
