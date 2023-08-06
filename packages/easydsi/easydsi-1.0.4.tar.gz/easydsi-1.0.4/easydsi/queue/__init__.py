9bea2c05d11b09047de092f04a2302851afc9f9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:26 +0000

    Add package easydsi/1.0.4/easydsi-1.0.4.tar.gz

---
+++ packages/easydsi/easydsi-1.0.4.tar.gz/easydsi-1.0.4/easydsi/queue/__init__.py
@@ -0,0 +1,54 @@
+from multipledispatch import dispatch
+
+class Queue():
+
+    def __init__(self, size, elements):
+        self.size = size
+        self.elements = elements
+        if(any(self.elements)):
+            self.length = len(elements)
+        else:
+            self.elements = []
+            self.length = 0
+    
+    def index(self, position):
+        return self.elements[position]
+
+    def add(self, element):
+        if(self.size == self.length):
+            print("Queue is full")
+        else:
+            self.elements.append(element)
+            self.length += 1
+
+    def remove(self):
+        if(self.length == 0):
+            print("Stack is empty")
+        else:
+            elementToReturn = self.elements.pop(0)
+            self.length -= 1
+            return elementToReturn
+            
+    def display(self):
+        print(self.elements)
+
+    def getElements(self):
+        return self.elements
+    
+    def getSize(self):
+        return self.size
+
+    def getLength(self):
+        return self.length
+
+
+def queue(size):
+
+    if(isinstance(size, int)):
+        elements = [0] * size
+        return Queue(size, elements)
+
+    elif(isinstance(size, list)):
+        elements = size
+        size = len(elements)
+        return Queue(size, elements)
