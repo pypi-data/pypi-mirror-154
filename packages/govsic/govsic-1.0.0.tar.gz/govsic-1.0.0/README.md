ae1f086371400cd6a9805ee5345d2130298eb8eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:43:51 +0000

    Add package govsic/1.0.0/govsic-1.0.0.tar.gz

---
+++ packages/govsic/govsic-1.0.0.tar.gz/govsic-1.0.0/README.md
@@ -0,0 +1,18 @@
+# gov-sic
+
+![Tests](https://github.com/ellsphillips/sic/actions/workflows/config.yml/badge.svg)
+
+A lightweight library to parse and interface Standard Industrial Classification instances using the current UK SIC 2007 methodology.
+
+`gov-sic` supports the UK SIC 2007 framework by unifying the classification of business establishments and other statistical units by their type of economic activity/engagement into a common structure.
+
+## Data
+
+Data used has been transformed using the [Office for National Statistics](https://www.ons.gov.uk/methodology/classificationsandstandards/ukstandardindustrialclassificationofeconomicactivities/uksic2007)' published opensource SIC structure and volume datasets.
+
+## Installation
+
+```bash
+pip install -U pip setuptools wheel
+pip install git+https://github.com/ellsphillips/sic
+```
---
+++ packages/govsic/govsic-1.0.0.tar.gz/govsic-1.0.0/src/govsic/sic.py
@@ -0,0 +1,46 @@
+import string
+from dataclasses import dataclass
+from typing import Union
+
+from govsic.constants import Component, SectionBoundaries
+from govsic.data.glossary import SIC_GLOSSARY
+from govsic.exceptions import InvalidSICCodeError
+
+SICCode = Union[str, int]
+
+@dataclass
+class SIC:
+
+    code: SICCode = ""
+
+    def __post_init__(self) -> None:
+        if len(str(self.code)) > 5:
+            raise InvalidSICCodeError(
+                message="SIC codes should be at most 5 digits long."
+            )
+
+        self.code = str(self.code).zfill(5)
+
+    @property
+    def is_valid(self) -> bool:
+        return self.code in SIC_GLOSSARY
+
+    @property
+    def component(self) -> str:
+        for index, character in enumerate(str(self.code)[::-1]):
+            if int(character) > 0:
+                return list(Component)[len(str(self.code)) - index - 1].name
+        return "DIVISION"
+
+    @property
+    def section(self) -> str:
+        bounds = [int(b.value) for b in SectionBoundaries]
+        bucket = next(x[0] for x in enumerate(bounds) if x[1] > int(self.code))
+        return string.ascii_uppercase[bucket - 1]
+
+    def __repr__(self) -> str:
+        div, grp_cls, sub_cls = [str(self.code)[i:i+2] for i in range(0, len(str(self.code)), 2)]
+        return (
+            f"[{self.section}] "
+            f"{div}.{grp_cls}/{sub_cls}"
+        )
