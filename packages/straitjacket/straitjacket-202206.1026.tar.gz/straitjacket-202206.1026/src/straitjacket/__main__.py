c7c784e1c05a7f1e5dd643adf2c895004f8210f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:31 +0000

    Add package straitjacket/202206.1026/straitjacket-202206.1026.tar.gz

---
+++ packages/straitjacket/straitjacket-202206.1026.tar.gz/straitjacket-202206.1026/src/straitjacket/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# This file is part of the straitjacket project
+# https://github.com/mbarkhau/straitjacket
+#
+# Copyright (c) 2018-2021 Manuel Barkhau (mbarkhau@gmail.com) - MIT License
+# SPDX-License-Identifier: MIT
+
+import sys
+
+from . import sjfmt
+
+if __name__ == '__main__':
+    sjfmt.main()
+    sys.exit(0)
