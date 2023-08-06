5aecd73fe529ee26ee39200f48499af0d9c0c51d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:56 +0000

    Add package jina-now/0.0.12.dev10/jina-now-0.0.12.dev10.tar.gz

---
+++ packages/jina-now/jina-now-0.0.12.dev10.tar.gz/jina-now-0.0.12.dev10/now/apps/image_to_text/app.py
@@ -0,0 +1,50 @@
+from typing import Dict, List
+
+from docarray import DocumentArray
+from now_common import options
+
+from now.apps.base.app import JinaNOWApp
+from now.constants import CLIP_USES, IMAGE_MODEL_QUALITY_MAP, Apps, Modalities
+from now.dataclasses import UserInput
+from now.run_backend import finetune_flow_setup
+
+
+class ImageToText(JinaNOWApp):
+    @property
+    def app(self) -> str:
+        return Apps.IMAGE_TO_TEXT
+
+    @property
+    def is_enabled(self) -> bool:
+        return True
+
+    @property
+    def description(self) -> str:
+        return 'Image to text search app'
+
+    @property
+    def input_modality(self) -> Modalities:
+        return Modalities.IMAGE
+
+    @property
+    def output_modality(self) -> Modalities:
+        return Modalities.TEXT
+
+    @property
+    def options(self) -> List[Dict]:
+        return [options.QUALITY_CLIP]
+
+    def setup(self, da: DocumentArray, user_config: UserInput, kubectl_path) -> Dict:
+        return finetune_flow_setup(
+            self,
+            da,
+            user_config,
+            kubectl_path,
+            encoder_uses=CLIP_USES,
+            encoder_uses_with={
+                'pretrained_model_name_or_path': IMAGE_MODEL_QUALITY_MAP[
+                    user_config.quality
+                ][1]
+            },
+            indexer_uses='DocarrayIndexer',
+        )
