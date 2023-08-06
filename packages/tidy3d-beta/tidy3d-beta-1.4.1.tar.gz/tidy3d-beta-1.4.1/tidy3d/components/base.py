9e9fd7175df9f76906af5e5485c135eaf662e058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:08 +0000

    Add package tidy3d-beta/1.4.1/tidy3d-beta-1.4.1.tar.gz

---
+++ packages/tidy3d-beta/tidy3d-beta-1.4.1.tar.gz/tidy3d-beta-1.4.1/tidy3d/components/base.py
@@ -0,0 +1,352 @@
+"""global configuration / base class for pydantic models used to make simulation."""
+
+import json
+from functools import wraps
+
+import rich
+import pydantic
+import yaml
+import numpy as np
+from pydantic.fields import ModelField
+
+from .types import ComplexNumber, NumpyArray, Literal
+from ..log import FileError
+
+# default indentation (# spaces) in files
+INDENT = 4
+
+# type tag default name
+TYPE_TAG_STR = "type"
+
+
+class Tidy3dBaseModel(pydantic.BaseModel):
+    """Base pydantic model that all Tidy3d components inherit from.
+    Defines configuration for handling data structures
+    as well as methods for imporing, exporting, and hashing tidy3d objects.
+    For more details on pydantic base models, see:
+    `Pydantic Models <https://pydantic-docs.helpmanual.io/usage/models/>`_
+    """
+
+    def __init_subclass__(cls):
+        """Things that are done to each of the models."""
+
+        add_type_field(cls)
+        cls.__doc__ = generate_docstring(cls)
+
+    class Config:  # pylint: disable=too-few-public-methods
+        """Sets config for all :class:`Tidy3dBaseModel` objects.
+
+        Configuration Options
+        ---------------------
+        allow_population_by_field_name : bool = True
+            Allow properties to stand in for fields(?).
+        arbitrary_types_allowed : bool = True
+            Allow types like numpy arrays.
+        extra : str = 'forbid'
+            Forbid extra kwargs not specified in model.
+        json_encoders : Dict[type, Callable]
+            Defines how to encode type in json file.
+        validate_all : bool = True
+            Validate default values just to be safe.
+        validate_assignment : bool
+            Re-validate after re-assignment of field in model.
+        """
+
+        arbitrary_types_allowed = True
+        validate_all = True
+        extra = "forbid"
+        validate_assignment = True
+        allow_population_by_field_name = True
+        json_encoders = {
+            np.ndarray: lambda x: NumpyArray(data_list=x.tolist()),
+            complex: lambda x: ComplexNumber(real=x.real, imag=x.imag),
+        }
+
+    def copy(self, deep: bool = True, **kwargs) -> "Self":
+        """Copy a Tidy3dBaseModel.  With ``deep=True`` as default."""
+        return super().copy(deep=deep, **kwargs)
+
+    def help(self, methods: bool = False) -> None:
+        """Prints message describing the fields and methods of a :class:`Tidy3dBaseModel`.
+
+        Parameters
+        ----------
+        methods : bool = False
+            Whether to also print out information about object's methods.
+
+        Example
+        -------
+        >>> simulation.help(methods=True)
+        """
+        rich.inspect(self, methods=methods)
+
+    @classmethod
+    def from_file(cls, fname: str, **parse_kwargs):
+        """Loads a :class:`Tidy3dBaseModel` from .yaml or .json file.
+
+        Parameters
+        ----------
+        fname : str
+            Full path to the .yaml or .json file to load the :class:`Tidy3dBaseModel` from.
+        **parse_kwargs
+            Keyword arguments passed to either pydantic's ``parse_file`` or ``parse_raw`` methods
+            for ``.json`` and ``.yaml`` file formats, respectively.
+        Returns
+        -------
+        :class:`Tidy3dBaseModel`
+            An instance of the component class calling `load`.
+
+        Example
+        -------
+        >>> simulation = Simulation.from_file(fname='folder/sim.json')
+        """
+        if ".json" in fname:
+            return cls.from_json(fname=fname, **parse_kwargs)
+        if ".yaml" in fname:
+            return cls.from_yaml(fname=fname, **parse_kwargs)
+        raise FileError(f"File must be .json or .yaml, given {fname}")
+
+    def to_file(self, fname: str) -> None:
+        """Exports :class:`Tidy3dBaseModel` instance to .yaml or .json file
+
+        Parameters
+        ----------
+        fname : str
+            Full path to the .yaml or .json file to save the :class:`Tidy3dBaseModel` to.
+
+        Example
+        -------
+        >>> simulation.to_file(fname='folder/sim.json')
+        """
+        if ".json" in fname:
+            return self.to_json(fname=fname)
+        if ".yaml" in fname:
+            return self.to_yaml(fname=fname)
+        raise FileError(f"File must be .json or .yaml, given {fname}")
+
+    @classmethod
+    def from_json(cls, fname: str, **parse_file_kwargs):
+        """Load a :class:`Tidy3dBaseModel` from .json file.
+
+        Parameters
+        ----------
+        fname : str
+            Full path to the .json file to load the :class:`Tidy3dBaseModel` from.
+
+        Returns
+        -------
+        :class:`Tidy3dBaseModel`
+            An instance of the component class calling `load`.
+        **parse_file_kwargs
+            Keyword arguments passed to pydantic's ``parse_file`` method.
+
+        Example
+        -------
+        >>> simulation = Simulation.from_json(fname='folder/sim.json')
+        """
+        return cls.parse_file(fname, **parse_file_kwargs)
+
+    def to_json(self, fname: str) -> None:
+        """Exports :class:`Tidy3dBaseModel` instance to .json file
+
+        Parameters
+        ----------
+        fname : str
+            Full path to the .json file to save the :class:`Tidy3dBaseModel` to.
+
+        Example
+        -------
+        >>> simulation.to_json(fname='folder/sim.json')
+        """
+        json_string = self._json_string()
+        with open(fname, "w", encoding="utf-8") as file_handle:
+            file_handle.write(json_string)
+
+    @classmethod
+    def from_yaml(cls, fname: str, **parse_raw_kwargs):
+        """Loads :class:`Tidy3dBaseModel` from .yaml file.
+
+        Parameters
+        ----------
+        fname : str
+            Full path to the .yaml file to load the :class:`Tidy3dBaseModel` from.
+        **parse_raw_kwargs
+            Keyword arguments passed to pydantic's ``parse_raw`` method.
+
+        Returns
+        -------
+        :class:`Tidy3dBaseModel`
+            An instance of the component class calling `from_yaml`.
+
+        Example
+        -------
+        >>> simulation = Simulation.from_yaml(fname='folder/sim.yaml')
+        """
+        with open(fname, "r", encoding="utf-8") as yaml_in:
+            json_dict = yaml.safe_load(yaml_in)
+        json_raw = json.dumps(json_dict, indent=INDENT)
+        return cls.parse_raw(json_raw, **parse_raw_kwargs)
+
+    def to_yaml(self, fname: str) -> None:
+        """Exports :class:`Tidy3dBaseModel` instance to .yaml file.
+
+        Parameters
+        ----------
+        fname : str
+            Full path to the .yaml file to save the :class:`Tidy3dBaseModel` to.
+
+        Example
+        -------
+        >>> simulation.to_yaml(fname='folder/sim.yaml')
+        """
+        json_string = self._json_string()
+        json_dict = json.loads(json_string)
+        with open(fname, "w+", encoding="utf-8") as file_handle:
+            yaml.dump(json_dict, file_handle, indent=INDENT)
+
+    def __hash__(self) -> int:
+        """Hash a :class:`Tidy3dBaseModel` objects using its json string."""
+        return hash(self.json())
+
+    def __lt__(self, other):
+        """define < for getting unique indices based on hash."""
+        return hash(self) < hash(other)
+
+    def __gt__(self, other):
+        """define > for getting unique indices based on hash."""
+        return hash(self) > hash(other)
+
+    def __le__(self, other):
+        """define <= for getting unique indices based on hash."""
+        return hash(self) <= hash(other)
+
+    def __ge__(self, other):
+        """define >= for getting unique indices based on hash."""
+        return hash(self) >= hash(other)
+
+    def __eq__(self, other):
+        """define == for checking whether two base models are equal unique indices based on hash."""
+        return hash(self) == hash(other)
+
+    def _json_string(self, include_unset: bool = True) -> str:
+        """Returns string representation of a :class:`Tidy3dBaseModel`.
+
+        Parameters
+        ----------
+        include_unset : bool = True
+            Whether to include default fields in json string.
+
+        Returns
+        -------
+        str
+            Json-formatted string holding :class:`Tidy3dBaseModel` data.
+        """
+        exclude_unset = not include_unset
+
+        # put infinity and -infinity in quotes
+        tmp_string = "<<TEMPORARY_INFINITY_STRING>>"
+        json_string = self.json(indent=INDENT, exclude_unset=exclude_unset)
+        json_string = json_string.replace("-Infinity", tmp_string)
+        json_string = json_string.replace("Infinity", '"Infinity"')
+        json_string = json_string.replace(tmp_string, '"-Infinity"')
+
+        return json_string
+
+
+def add_type_field(cls):
+    """Automatically place "type" field with model name in the model field dictionary."""
+
+    value = cls.__name__
+    annotation = Literal[value]
+
+    tag_field = ModelField.infer(
+        name=TYPE_TAG_STR,
+        value=value,
+        annotation=annotation,
+        class_validators=None,
+        config=cls.__config__,
+    )
+    cls.__fields__[TYPE_TAG_STR] = tag_field
+
+
+def generate_docstring(cls) -> str:
+    """Generates a docstring for a Tidy3D model."""
+
+    # store the docstring in here
+    doc = ""
+
+    # if the model already has a docstring, get the first lines and save the rest
+    original_docstrings = []
+    if cls.__doc__:
+        original_docstrings = cls.__doc__.split("\n\n")
+        class_description = original_docstrings.pop(0)
+        doc += class_description
+    original_docstrings = "\n\n".join(original_docstrings)
+
+    # create the list of parameters (arguments) for the model
+    doc += "\n\n    Parameters\n    ----------\n"
+    for field_name, field in cls.__fields__.items():
+
+        # ignore the type tag
+        if field_name == TYPE_TAG_STR:
+            continue
+
+        # get data type
+        data_type = field._type_display()  # pylint:disable=protected-access
+
+        # get default values
+        default_val = field.get_default()
+        if "=" in str(default_val):
+            # handle cases where default values are pydantic models
+            default_val = f"{default_val.__class__.__name__}({default_val})"
+            default_val = (", ").join(default_val.split(" "))
+
+        # make first line: name : type = default
+        default_str = f" = {default_val}" if default_val != ... else ""
+        doc += f"    {field_name} : {data_type}{default_str}\n"
+
+        # get field metadata
+        field_info = field.field_info
+        doc += "        "
+
+        # add units (if present)
+        units = field_info.extra.get("units")
+        if units is not None:
+            doc += f"[units = {units}].  "
+
+        # add description
+        description_str = field_info.description
+        if description_str is not None:
+            doc += f"{description_str}\n"
+
+    # add in remaining things in the docs
+    if original_docstrings:
+        doc += "\n"
+        doc += original_docstrings
+
+    doc += "\n"
+
+    return doc
+
+
+def cache(prop):
+    """Decorates a property to cache the previously computed values based on a hash of self."""
+
+    stored_values = {}
+
+    @wraps(prop)
+    def cached_property(self):
+        """The new property method to be returned by decorator."""
+
+        hash_key = hash(self)
+
+        # if the value has been computed before, we can simply return the stored value
+        if hash_key in stored_values:
+            return stored_values[hash_key]
+
+        # otherwise, we need to recompute the value, store it in the storage dict, and return
+        return_value = prop(self)
+        stored_values[hash_key] = return_value
+        return return_value
+
+    return cached_property
---
+++ packages/tidy3d-beta/tidy3d-beta-1.4.1.tar.gz/tidy3d-beta-1.4.1/tidy3d/plugins/smatrix/smatrix.py
@@ -0,0 +1,295 @@
+"""Tools for generating an S matrix automatically from tidy3d simulation and port definitions."""
+
+from typing import List, Tuple, Dict
+
+import pydantic as pd
+import numpy as np
+
+from ...constants import HERTZ, C_0
+from ...components.simulation import Simulation
+from ...components.geometry import Box
+from ...components.mode import ModeSpec
+from ...components.monitor import ModeMonitor
+from ...components.source import ModeSource, GaussianPulse
+from ...components.data import SimulationData
+from ...components.types import Direction, Ax
+from ...components.viz import add_ax_if_none, equal_aspect
+from ...components.base import Tidy3dBaseModel
+from ...log import SetupError, log
+from ...web.container import Batch
+
+# fwidth of gaussian pulse in units of central frequency
+FWIDTH_FRAC = 1.0 / 10
+DEFAULT_DATA_DIR = "data"
+
+
+class Port(Box):
+    """Specifies a port in the scattering matrix."""
+
+    direction: Direction = pd.Field(
+        ...,
+        title="Direction",
+        description="'+' or '-', defining which direction is considered 'input'.",
+    )
+    mode_spec: ModeSpec = pd.Field(
+        ModeSpec(),
+        title="Mode Specification",
+        description="Specifies how the mode solver will solve for the modes of the port.",
+    )
+    mode_indices: Tuple[pd.NonNegativeInt] = pd.Field(
+        None,
+        title="Mode Indices.",
+        description="Indices into modes returned by the mode solver to use in the port. "
+        "If ``None``, all modes returned by the mode solver are used in the scattering matrix. "
+        "Otherwise, the scattering matrix will include elements for each supplied mode index.",
+    )
+    name: str = pd.Field(
+        ...,
+        title="Name",
+        description="Unique name for the port.",
+        min_length=1,
+    )
+
+    @pd.validator("mode_indices", always=True)
+    def evaluate_mode_indices(cls, val, values):
+        """Evaluates mode indices based on number of modes in mode spec."""
+        if val is None:
+            num_modes = values.get("mode_spec").num_modes
+            val = tuple(range(num_modes))
+        return val
+
+
+"""
+s_matrix[port_name_in][port_name_out] gives a numpy array of shape (m, n)
+relating the coupling amplitudes between the m and n mode orders in the two ports, respectively.
+"""
+SMatrixType = Dict[str, np.ndarray]
+
+
+class ComponentModeler(Tidy3dBaseModel):
+    """Tool for modeling devices and computing scattering matrix elements."""
+
+    simulation: Simulation = pd.Field(
+        ...,
+        title="Simulation",
+        description="Simulation describing the device without any sources or monitors present.",
+    )
+    ports: List[Port] = pd.Field(
+        [],
+        title="Ports",
+        description="Collection of ports describing the scattering matrix elements. "
+        "For each port, one simulation will be run with a modal source.",
+    )
+    freq: float = pd.Field(
+        ...,
+        title="Frequency",
+        description="Frequency at which to evaluate the scattering matrix.",
+        units=HERTZ,
+    )
+
+    batch: Tidy3dBaseModel = pd.Field(
+        None,
+        title="Batch",
+        description="Batch of task used to compute S matrix. Set internally.",
+    )
+
+    @pd.validator("simulation", always=True)
+    def _sim_has_no_sources(cls, val):
+        """Make sure simulation has no sources as they interfere with tool."""
+        if len(val.sources) > 0:
+            raise SetupError("Simulation must not have sources.")
+        return val
+
+    def _to_monitor(self, port: Port) -> ModeMonitor:
+        """Creates a mode monitor from a given port."""
+        return ModeMonitor(
+            center=port.center,
+            size=port.size,
+            freqs=[self.freq],
+            mode_spec=port.mode_spec,
+            name=port.name,
+        )
+
+    def _to_sources(self, port: Port) -> List[ModeSource]:
+        """Creates a list of mode sources from a given port."""
+        return [
+            ModeSource(
+                center=port.center,
+                size=port.size,
+                source_time=GaussianPulse(freq0=self.freq, fwidth=self.freq * FWIDTH_FRAC),
+                mode_spec=port.mode_spec,
+                mode_index=mode_index,
+                direction=port.direction,
+                name=port.name,
+            )
+            for mode_index in port.mode_indices
+        ]
+
+    @equal_aspect
+    @add_ax_if_none
+    def plot_sim(self, x: float = None, y: float = None, z: float = None, ax: Ax = None) -> Ax:
+        """Plot a :class:`Simulation` with all sources added for each port, for troubleshooting."""
+
+        sim_plot = self.simulation.copy(deep=True)
+        for port_source in self.ports:
+            mode_source_0 = self._to_sources(port_source)[0]
+            sim_plot.sources.append(mode_source_0)
+        return sim_plot.plot(x=x, y=y, z=z, ax=ax)
+
+    def _shift_value_signed(self, port: Port) -> float:
+        """How far (signed) to shift the source from the monitor."""
+
+        # get the grid boundaries and sizes along port normal from the simulation
+        normal_axis = port.size.index(0.0)
+        grid = self.simulation.grid
+        grid_boundaries = grid.boundaries.to_list[normal_axis]
+        grid_centers = grid.centers.to_list[normal_axis]
+
+        # get the index of the grid cell where the port lies
+        port_position = port.center[normal_axis]
+        port_index = np.argwhere(port_position > grid_boundaries)[-1]
+
+        # shift the port to the left
+        if port.direction == "+":
+            shifted_index = port_index - 2
+            if shifted_index < 0:
+                raise SetupError(
+                    f"Port {port.name} normal is too close to boundary "
+                    f"on -{'xyz'[normal_axis]} side."
+                )
+
+        # shift the port to the right
+        else:
+            shifted_index = port_index + 2
+            if shifted_index >= len(grid_centers):
+                raise SetupError(
+                    f"Port {port.name} normal is too close to boundary "
+                    f"on +{'xyz'[normal_axis]} side."
+                )
+
+        new_pos = grid_centers[shifted_index]
+        return new_pos - port_position
+
+    def _shift_port(self, port: Port) -> Port:
+        """Generate a new port shifted by the shift amount in normal direction."""
+
+        shift_value = self._shift_value_signed(port)
+        center_shifted = list(port.center)
+        center_shifted[port.size.index(0.0)] += shift_value
+        port_shifted = port.copy(deep=True)
+        port_shifted.center = center_shifted
+        return port_shifted
+
+    def _task_name(self, port_source: Port, mode_index: int) -> str:
+        """The name of a task, determined by the port of the source and mode index."""
+        return f"smatrix_port{port_source.name}_mode{mode_index}"
+
+    def _make_sims(self) -> Dict[str, Simulation]:
+        """Generate all the :class:`Simulation` objects for the S matrix calculation."""
+
+        mode_monitors = [self._to_monitor(port) for port in self.ports]
+        sim_dict = {}
+        for port_source in self.ports:
+            port_source = self._shift_port(port_source)
+            for mode_source in self._to_sources(port_source):
+                sim_copy = self.simulation.copy(deep=True)
+                sim_copy.sources = [mode_source]
+                sim_copy.monitors += mode_monitors
+                task_name = self._task_name(port_source, mode_source.mode_index)
+                sim_dict[task_name] = sim_copy
+        return sim_dict
+
+    def _run_sims(
+        self, sim_dict: Dict[str, Simulation], folder_name: str, path_dir: str
+    ) -> "BatchData":
+        """Run :class:`Simulations` for each port and return the batch after saving."""
+
+        self.batch = Batch(simulations=sim_dict, folder_name=folder_name)
+        self.batch.upload()
+        self.batch.start()
+        self.batch.monitor()
+        batch_data = self.batch.load(path_dir=path_dir)
+        return batch_data
+
+    def _normalization_factor(self, port_source: Port, sim_data: SimulationData) -> complex:
+        """Compute the normalization amplitude based on the measured input mode amplitude."""
+
+        port_monitor_data = sim_data[port_source.name]
+        mode_index = sim_data.simulation.sources[0].mode_index
+
+        normalize_amp = port_monitor_data.amps.sel(
+            f=self.freq,
+            direction=port_source.direction,
+            mode_index=mode_index,
+        ).values
+
+        normalize_n_eff = port_monitor_data.n_eff.sel(f=self.freq, mode_index=mode_index).values
+
+        k0 = 2 * np.pi * C_0 / self.freq
+        k_eff = k0 * normalize_n_eff
+        shift_value = self._shift_value_signed(port_source)
+        return normalize_amp * np.exp(1j * k_eff * shift_value)
+
+    def _construct_smatrix(self, batch_data: "BatchData") -> SMatrixType:
+        """Post process batch to generate scattering matrix."""
+
+        # load all data
+        s_matrix_dict = {
+            port_source.name: {port_monitor.name: [] for port_monitor in self.ports}
+            for port_source in self.ports
+        }
+
+        # loop through source ports
+        for port_source in self.ports:
+
+            source_name = port_source.name
+
+            # loop through all source injection indices
+            for mode_index in port_source.mode_indices:
+
+                # get the data for this source and compute normalization by injection
+                task_name = self._task_name(port_source, mode_index)
+                sim_data = batch_data[task_name]
+                norm_factor = self._normalization_factor(port_source, sim_data)
+
+                # loop through all monitors
+                for port_monitor in self.ports:
+
+                    monitor_name = port_monitor.name
+
+                    # compute the mode amplitude data
+                    dir_out = "-" if port_monitor.direction == "+" else "+"
+                    mode_data = sim_data[monitor_name].amps.sel(f=self.freq, direction=dir_out)
+                    amps_normalized = np.array(mode_data.values) / norm_factor
+                    s_matrix_dict[source_name][monitor_name].append(amps_normalized)
+
+            # convert to an array
+            for port_monitor in self.ports:
+                monitor_name = port_monitor.name
+                mode_matrix = np.array(s_matrix_dict[source_name][monitor_name])
+                s_matrix_dict[source_name][monitor_name] = mode_matrix
+
+        return s_matrix_dict
+
+    def solve(self, folder_name: str = "default", path_dir: str = DEFAULT_DATA_DIR) -> SMatrixType:
+        """Solves for the scattering matrix of the system."""
+        log.warning(
+            "`ComponentModeler.solve()` is renamed to `ComponentModeler.run()` "
+            "'and will be removed in a later version."
+        )
+        return self.run(folder_name=folder_name, path_dir=path_dir)
+
+    def run(self, folder_name: str = "default", path_dir: str = DEFAULT_DATA_DIR) -> SMatrixType:
+        """Solves for the scattering matrix of the system."""
+
+        sim_dict = self._make_sims()
+        batch_data = self._run_sims(sim_dict=sim_dict, folder_name=folder_name, path_dir=path_dir)
+        return self._construct_smatrix(batch_data=batch_data)
+
+    def load(self, path_dir: str = DEFAULT_DATA_DIR) -> SMatrixType:
+        """Load an Smatrix from saved BatchData object."""
+
+        if self.batch is None:
+            raise SetupError("Component modeler has no batch saved. Run .solve() to generate.")
+        batch_data = self.batch.load(path_dir=path_dir)
+        return self._construct_smatrix(batch_data=batch_data)
