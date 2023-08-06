d562a0cbd146ae3bfdb0c17a14559bcc7486a587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:14 +0000

    Add package tidify/0.0.1/tidify-0.0.1.tar.gz

---
+++ packages/tidify/tidify-0.0.1.tar.gz/tidify-0.0.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['tidify']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pandas>=1.4.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'tidify',
+    'version': '0.0.1',
+    'description': 'Convert nested objects into tidy data',
+    'long_description': None,
+    'author': 'Austin Howard',
+    'author_email': 'austin@tangibleintelligence.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
