253564ac3af58b858d5f1ce6a604f343741856ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:52:24 +0000

    Add package ashley/1.2.3/ashley-1.2.3-py2.py3-none-any.whl

---
+++ packages/ashley/ashley-1.2.3-py2.py3-none-any.whl/ashley/machina_extensions/forum/urls.py
@@ -0,0 +1,36 @@
+"""
+This module defines URL patterns associated with the django-machina``forum``
+application.
+"""
+
+from django.urls import path
+from machina.apps.forum.urls import (
+    ForumURLPatternsFactory as BaseForumURLPatternsFactory,
+)
+from machina.core.loading import get_class
+
+
+class ForumURLPatternsFactory(BaseForumURLPatternsFactory):
+    """Allows to generate the URL patterns of the ``forum`` application."""
+
+    rename_view = get_class("forum.views", "ForumRenameView")
+    archive_view = get_class("forum.views", "ForumArchiveView")
+    lock_course_view = get_class("forum.views", "ForumLockCourseView")
+
+    def get_urlpatterns(self):
+        """Returns the URL patterns managed by the considered factory / application."""
+
+        return super().get_urlpatterns() + [
+            path("admin/rename/<int:pk>/", self.rename_view.as_view(), name="rename"),
+            path(
+                "admin/archive/<int:pk>/", self.archive_view.as_view(), name="archive"
+            ),
+            path(
+                "admin/lock-course/<int:pk>/",
+                self.lock_course_view.as_view(),
+                name="lock_course",
+            ),
+        ]
+
+
+urlpatterns_factory = ForumURLPatternsFactory()
