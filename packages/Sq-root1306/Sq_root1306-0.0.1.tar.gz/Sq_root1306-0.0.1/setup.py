52a667057eb98aed6bd7ad8b8f362b3b9040230c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:55 +0000

    Add package Sq-root1306/0.0.1/Sq_root1306-0.0.1.tar.gz

---
+++ packages/Sq-root1306/Sq_root1306-0.0.1.tar.gz/Sq_root1306-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A basic sqroot function'
+
+# Setting up
+setup(
+    name="Sq_root1306",
+    version=VERSION,
+    author="NeuralNine (Florian Dedov)",
+    author_email="<mail@neuralnine.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
