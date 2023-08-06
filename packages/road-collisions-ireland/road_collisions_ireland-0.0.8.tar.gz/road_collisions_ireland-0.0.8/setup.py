c60794097b83be570b44081794232db8d92d5f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:27:02 +0000

    Add package road-collisions-ireland/0.0.8/road_collisions_ireland-0.0.8.tar.gz

---
+++ packages/road-collisions-ireland/road_collisions_ireland-0.0.8.tar.gz/road_collisions_ireland-0.0.8/setup.py
@@ -0,0 +1,31 @@
+from setuptools import (
+    find_packages,
+    setup
+)
+
+INSTALL_REQUIRES = (
+    'road-collisions-base'
+)
+
+setup(
+    name='road_collisions_ireland',
+    version='0.0.8',
+    python_requires='>=3.6',
+    description='Road collision data for Ireland',
+    author='Robert Lucey',
+    url='https://github.com/RobertLucey/road-collisions-ireland',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=INSTALL_REQUIRES,
+    package_data={
+        'road_collisions_ireland': [
+            'resources/ireland/ireland.csv.tgz'
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'raw_cleaner = road_collisions_ireland.bin.raw_cleaner:main',
+            'load_road_collisions_ireland = road_collisions_ireland.bin.load:main',
+        ]
+    }
+)
