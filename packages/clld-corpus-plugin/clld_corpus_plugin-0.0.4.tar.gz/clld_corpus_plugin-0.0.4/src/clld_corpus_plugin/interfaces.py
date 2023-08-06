c940fe9f9fe4a2af9abcd0670e7b093768d271ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:09 +0000

    Add package clld-corpus-plugin/0.0.4/clld_corpus_plugin-0.0.4.tar.gz

---
+++ packages/clld-corpus-plugin/clld_corpus_plugin-0.0.4.tar.gz/clld_corpus_plugin-0.0.4/src/clld_corpus_plugin/interfaces.py
@@ -0,0 +1,25 @@
+from zope.interface import Interface
+
+
+class IText(Interface):
+    """
+    text interface
+    """
+
+
+class IMeaning(Interface):
+    """
+    meaning interface
+    """
+
+
+class ITag(Interface):
+    """
+    tag interface
+    """
+
+
+class IWordform(Interface):
+    """
+    form interface
+    """
