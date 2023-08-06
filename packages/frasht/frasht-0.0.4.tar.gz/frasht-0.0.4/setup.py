ead66c211e5cc021ab2e3cbfe2d1479fbe597458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:46 +0000

    Add package frasht/0.0.4/frasht-0.0.4.tar.gz

---
+++ packages/frasht/frasht-0.0.4.tar.gz/frasht-0.0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'frasht',
+    version = '0.0.4',
+    description = 'A library that makes Python development easier',
+    license = 'MIT License',
+    url = 'https://github.com/',
+    author = 'Frasht',
+    author_email = '3357099132@qq.com',
+    packages = find_packages(),
+    platforms = 'any',
+    install_requires = [],
+)
