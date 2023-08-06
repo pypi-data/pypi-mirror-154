e50e7750bac0f052b4677bd121be1f99098d6520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:15 +0000

    Add package tf-gpu/2.9.1/tf_gpu-2.9.1-cp38-cp38-win_amd64.whl

---
+++ packages/tf-gpu/tf_gpu-2.9.1-cp38-cp38-win_amd64.whl/tensorflow/python/platform/build_info.py
@@ -0,0 +1,19 @@
+
+# Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Auto-generated module providing information about the build."""
+import collections
+
+build_info = collections.OrderedDict([('cpu_compiler', 'C:/Program Files (x86)/Microsoft Visual Studio/2019/Professional/VC/Tools/MSVC/14.29.30133/bin/HostX64/x64/cl.exe'), ('cuda_compute_capabilities', ['compute_50', 'compute_52', 'compute_60', 'compute_61', 'compute_70', 'compute_75', 'compute_86']), ('cuda_version', '64_116'), ('cudart_dll_name', 'cudart64_116.dll'), ('cudnn_dll_name', 'cudnn64_8.dll'), ('cudnn_version', '64_8'), ('is_cuda_build', True), ('is_rocm_build', False), ('is_tensorrt_build', False), ('msvcp_dll_names', 'msvcp140.dll,msvcp140_1.dll'), ('nvcuda_dll_name', 'nvcuda.dll')])
