1a3e7cf7e1bc0427f40bbc0d17c9de66bf944970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:55 +0000

    Add package bigc/0.1.4/bigc-0.1.4.tar.gz

---
+++ packages/bigc/bigc-0.1.4.tar.gz/bigc-0.1.4/bigc/resources/__init__.py
@@ -0,0 +1,7 @@
+from .carts import BigCommerceCartsAPI
+from .categories import BigCommerceCategoriesAPI
+from .customers import BigCommerceCustomersAPI
+from .orders import BigCommerceOrdersAPI
+from .product_variants import BigCommerceProductVariantsAPI
+from .products import BigCommerceProductsAPI
+from .webhooks import BigCommerceWebhooksAPI
