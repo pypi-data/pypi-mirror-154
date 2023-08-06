6a395accf92333e7fe1d2f9bfe58c41b320ba247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:48 +0000

    Add package rrshare/4.2.4/rrshare-4.2.4.tar.gz

---
+++ packages/rrshare/rrshare-4.2.4.tar.gz/rrshare-4.2.4/rrshare/rqFactor/__init__.py
@@ -0,0 +1,4 @@
+    
+from rrshare.rqFactor.stock_RS_OH_MA import update_stock_PRS_day, update_stock_PRS_new
+from rrshare.rqFactor.swl_rs_valuation_pg import update_swl_rs_valuation, update_swl_all_main
+
