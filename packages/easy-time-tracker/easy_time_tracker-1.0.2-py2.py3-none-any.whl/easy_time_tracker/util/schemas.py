f5a19a83abaef93f72f5e2c757c21e65be4da740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:37:02 +0000

    Add package easy-time-tracker/1.0.2/easy_time_tracker-1.0.2-py2.py3-none-any.whl

---
+++ packages/easy-time-tracker/easy_time_tracker-1.0.2-py2.py3-none-any.whl/easy_time_tracker/util/schemas.py
@@ -0,0 +1,26 @@
+"""
+Data schemas
+"""
+from typing import List, Optional
+from pydantic import BaseModel, Field  # pylint: disable=no-name-in-module
+
+
+class StartTimeRecordSchema(BaseModel):  # pylint: disable=too-few-public-methods
+    """Class schema to start a record"""
+    description: str
+    people: list
+    project: Optional[str] = Field(default='na')
+    time_zone: str
+    start_time: str
+
+
+class EndTimeRecordSchema(StartTimeRecordSchema):  # pylint: disable=too-few-public-methods
+    """Class schema to end a record"""
+    end_time: str
+    ending_comments: Optional[str] = Field(default='no comments added')
+    total_time_worked: str
+
+
+class CompletedTimeRecordsSchema(BaseModel):  # pylint: disable=too-few-public-methods
+    """Class schema for records archive"""
+    records: List[EndTimeRecordSchema]
