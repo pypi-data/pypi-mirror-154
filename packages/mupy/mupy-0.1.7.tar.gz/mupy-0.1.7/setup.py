d2f62afcd89c7314114a59aa1382d124626c6484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:19 +0000

    Add package mupy/0.1.7/mupy-0.1.7.tar.gz

---
+++ packages/mupy/mupy-0.1.7.tar.gz/mupy-0.1.7/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='mupy',
+    version='0.1.7',
+    author='Michael Cranston Ryan',
+    author_email='spacetime.engineer@gmail.com',
+    url='http://pypi.python.org/pypi/mupy/',
+    license='LICENSE.txt',
+    description='Manufacturing Utility',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Intended Audience :: Education',
+        'Intended Audience :: Manufacturing',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: BSD License',  
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Other', # Openscad programming language in particular.
+    ]
+)
