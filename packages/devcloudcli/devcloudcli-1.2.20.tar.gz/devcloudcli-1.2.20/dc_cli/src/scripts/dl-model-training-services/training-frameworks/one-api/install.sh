f064b235457e26a833cc9b3565de45c2202f9d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:26:49 +0000

    Add package devcloudcli/1.2.20/devcloudcli-1.2.20.tar.gz

---
+++ packages/devcloudcli/devcloudcli-1.2.20.tar.gz/devcloudcli-1.2.20/dc_cli/src/scripts/dl-model-training-services/training-frameworks/one-api/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget https://registrationcenter-download.intel.com/akdlm/irc_nas/18486/l_AIKit_p_2022.1.2.135.sh
+sudo sh ./l_AIKit_p_2022.1.2.135.sh
+sudo sh ./l_AIKit_p_2022.1.2.135.sh -a --silent --eula accept
+/opt/intel/oneapi
