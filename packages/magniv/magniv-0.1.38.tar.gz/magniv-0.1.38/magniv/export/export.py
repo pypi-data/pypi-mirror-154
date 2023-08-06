3d8f4770c3ff0a6ce1042fc43f8117686484add2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:01 +0000

    Add package magniv/0.1.38/magniv-0.1.38.tar.gz

---
+++ packages/magniv/magniv-0.1.38.tar.gz/magniv-0.1.38/magniv/export/export.py
@@ -0,0 +1,22 @@
+from magniv.export.airflow import export_to_airflow
+from magniv.utils.utils import _get_tasks_json
+
+
+def export(
+    gcp=False,
+    gcp_project_id=None,
+    gcp_dag_folder=None,
+    callback_hook=None,
+    kubernetes_startup_timeout=None,
+    env_file_path=None,
+):
+    task_list = _get_tasks_json("./dump.json")
+    export_to_airflow(
+        task_list,
+        gcp=gcp,
+        gcp_project_id=gcp_project_id,
+        gcp_dag_folder=gcp_dag_folder,
+        callback_hook=callback_hook,
+        kubernetes_startup_timeout=kubernetes_startup_timeout,
+        env_file_path=env_file_path,
+    )
