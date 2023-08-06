583e6b4b113f90dc65176f0c116e25c3c1332ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:55 +0000

    Add package Sq-root1306/0.0.1/Sq_root1306-0.0.1-py3-none-any.whl

---
+++ packages/Sq-root1306/Sq_root1306-0.0.1-py3-none-any.whl/Sq_root1306/sq.py
@@ -0,0 +1,13 @@
+def sq(n):
+        x = n
+        y = 1
+
+        e = 0.000001
+        while(x - y > e):
+     
+            x = (x + y)/2
+            y = n / x
+     
+        return x
+
+print(sq(2))
