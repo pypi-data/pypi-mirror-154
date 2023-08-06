a7e598712e29dbd03d0278e36eaa2a5bae07b4b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:18 +0000

    Add package liiatools/0.1.1/liiatools-0.1.1.tar.gz

---
+++ packages/liiatools/liiatools-0.1.1.tar.gz/liiatools-0.1.1/liiatools/datasets/shared_functions/common.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+import re
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def flip_dict(some_dict):
+    """
+    Potentially a temporary function which switches keys and values in a dictionary.
+    May need to rewrite LA-codes YAML file to avoid this step
+
+    :param some_dict: A config dictionary
+    :return: a reversed dictionary with keys as values and vice versa
+    """
+    return {value: key for key, value in some_dict.items()}
+
+
+def check_postcode(postcode):
+    """
+    Checks that the postcodes are in the right format
+    :param postcode: A string with a UK-style post code
+    :return: a post code, or if incorrect a blank string
+    """
+    if postcode:
+        match = re.search(
+            r"^[A-Z]{1,2}\d[A-Z\d]? *\d[A-Z]{2}$", postcode.strip(), re.IGNORECASE
+        )
+        return match.group(0)
+    return ""
+
+
+def to_short_postcode(postcode):
+    """
+    Remove whitespace from the beginning and end of postcodes and the last two digits for anonymity
+    return blank if not in the right format
+    :param postcode: A string with a UK-style post code
+    :return: a shortened post code with the area, district, and sector. The units is removed
+    """
+    try:
+        match = re.search(
+            r"([A-Z]+\d\d?[A-Z]?)\s*(\d+)([A-Z]{2})", postcode, re.IGNORECASE
+        )
+        postcode = match.group(1) + " " + match.group(2)
+    except (AttributeError, TypeError, ValueError):
+        postcode = ""
+    return postcode
+
+
+def to_month_only_dob(dob):
+    """
+    Convert dates of birth into month and year of birth starting from 1st of each month for anonymity
+    return blank if not in the right format
+    :param dob: A date of birth datetime object
+    :return: A date of birth datetime object with the month rounded to the first day
+    """
+    try:
+        dob = dob.replace(day=1)
+    except (AttributeError, TypeError, ValueError):
+        dob = ""
+    return dob
