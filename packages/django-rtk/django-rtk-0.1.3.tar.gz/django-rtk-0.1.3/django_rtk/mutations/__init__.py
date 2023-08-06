670efabd63cfd3f0106cfcb1e207a46d27e0f27b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:22:46 +0000

    Add package django-rtk/0.1.3/django-rtk-0.1.3.tar.gz

---
+++ packages/django-rtk/django-rtk-0.1.3.tar.gz/django-rtk-0.1.3/django_rtk/mutations/__init__.py
@@ -0,0 +1,6 @@
+from .activateaccount import ActivateAccount  # noqa
+from .changepassword import ChangePassword  # noqa
+from .obtainjsonwebtoken import ObtainJSONWebToken  # noqa
+from .registeraccount import RegisterAccount, send_activation_email  # noqa
+from .requestpasswordreset import RequestPasswordReset  # noqa
+from .resetpassword import ResetPassword  # noqa
---
+++ packages/django-rtk/django-rtk-0.1.3.tar.gz/django-rtk-0.1.3/django_rtk/signals.py
@@ -0,0 +1,7 @@
+from django.dispatch import Signal
+
+account_registered = Signal()
+account_activated = Signal()
+password_reset_requested = Signal()
+password_reset = Signal()
+password_changed = Signal()
