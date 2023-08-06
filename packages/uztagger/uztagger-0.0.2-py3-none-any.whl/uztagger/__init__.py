586f2e05aa11cad0fcf47ec247928bc343b93c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:28 +0000

    Add package uztagger/0.0.2/uztagger-0.0.2-py3-none-any.whl

---
+++ packages/uztagger/uztagger-0.0.2-py3-none-any.whl/uztagger/__init__.py
@@ -0,0 +1,11 @@
+class Tagger:
+    def predict_pos(self, sentence: str):
+        return [('maktabimda', 'N'), ('kitoblar', 'N')]
+
+    def help(self):
+        print("help of usage of metoding")
+
+    def pos_tags(self):
+        print("all list of pos")
+
+
