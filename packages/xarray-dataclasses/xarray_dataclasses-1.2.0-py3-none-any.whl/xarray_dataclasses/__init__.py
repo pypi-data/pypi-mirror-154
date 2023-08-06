c8d256bca24c7656ef32d67d879eb920e7b9f4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:15 +0000

    Add package xarray-dataclasses/1.2.0/xarray_dataclasses-1.2.0-py3-none-any.whl

---
+++ packages/xarray-dataclasses/xarray_dataclasses-1.2.0-py3-none-any.whl/xarray_dataclasses/__init__.py
@@ -0,0 +1,14 @@
+# type: ignore
+__version__ = "1.2.0"
+
+
+# submodules
+from .dataarray import *
+from .dataset import *
+from .datamodel import *
+from .dataoptions import *
+from .typing import *
+
+
+# for Sphinx docs
+__all__ = dir()
