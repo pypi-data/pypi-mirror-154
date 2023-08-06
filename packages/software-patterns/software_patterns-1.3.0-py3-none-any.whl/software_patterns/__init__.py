d0a1d86ea3f2001651d87b76db0b0f5390a22659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:42:30 +0000

    Add package software-patterns/1.3.0/software_patterns-1.3.0-py3-none-any.whl

---
+++ packages/software-patterns/software_patterns-1.3.0-py3-none-any.whl/software_patterns/__init__.py
@@ -0,0 +1,17 @@
+__version__ = '1.3.0'
+
+from .memoize import ObjectsPool
+from .notification import Observer, Subject
+from .proxy import Proxy, ProxySubject
+from .singleton import Singleton
+from .subclass_registry import SubclassRegistry
+
+__all__ = [
+    'Observer',
+    'Subject',
+    'ObjectsPool',
+    'SubclassRegistry',
+    'ProxySubject',
+    'Proxy',
+    'Singleton',
+]
