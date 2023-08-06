58057b097138d61d30d6f09a392871aaa4b3ecbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:57:40 +0000

    Add package splitmovieaiyc/1.1.0/SplitMovieAIYC-1.1.0.tar.gz

---
+++ packages/splitmovieaiyc/SplitMovieAIYC-1.1.0.tar.gz/SplitMovieAIYC-1.1.0/README.md
@@ -0,0 +1,5 @@
+# split
+
+This is a simple example package. You can use
+[AI悦创·Github](https://github.com/AndersonHJB)
+to write your content.
---
+++ packages/splitmovieaiyc/SplitMovieAIYC-1.1.0.tar.gz/SplitMovieAIYC-1.1.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/6/13 1:28
+# @Author  : AI悦创
+# @FileName: setup.py.py
+# @Software: PyCharm
+# @Blog    ：https://bornforthis.cn/
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SplitMovieAIYC",
+    version="1.1.0",
+    author="Bornforthis",
+    author_email="bornforthis@bornforthis.cn",
+    description="bornforthis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
