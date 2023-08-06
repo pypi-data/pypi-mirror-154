caceecfec718c078d335192c8784bb16a2a2bbbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:58:23 +0000

    Add package pdme/0.8.6/pdme-0.8.6.tar.gz

---
+++ packages/pdme/pdme-0.8.6.tar.gz/pdme-0.8.6/pdme/model/__init__.py
@@ -0,0 +1,25 @@
+from pdme.model.model import DipoleModel
+from pdme.model.fixed_magnitude_model import SingleDipoleFixedMagnitudeModel
+from pdme.model.multidipole_fixed_magnitude_model import (
+	MultipleDipoleFixedMagnitudeModel,
+)
+from pdme.model.random_count_multidipole_fixed_magnitude_model import (
+	RandomCountMultipleDipoleFixedMagnitudeModel,
+)
+
+from pdme.model.log_spaced_random_choice_model import (
+	LogSpacedRandomCountMultipleDipoleFixedMagnitudeModel,
+)
+
+from pdme.model.log_spaced_random_choice_fixed_orientation_model import (
+	LogSpacedRandomCountMultipleDipoleFixedMagnitudeFixedOrientationModel,
+)
+
+__all__ = [
+	"DipoleModel",
+	"SingleDipoleFixedMagnitudeModel",
+	"MultipleDipoleFixedMagnitudeModel",
+	"RandomCountMultipleDipoleFixedMagnitudeModel",
+	"LogSpacedRandomCountMultipleDipoleFixedMagnitudeModel",
+	"LogSpacedRandomCountMultipleDipoleFixedMagnitudeFixedOrientationModel",
+]
