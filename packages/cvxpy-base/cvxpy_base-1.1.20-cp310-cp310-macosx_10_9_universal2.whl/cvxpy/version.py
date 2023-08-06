75ea8033b320b1b3b9c29f0cf7d3125494917f81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:58:32 +0000

    Add package cvxpy-base/1.1.20/cvxpy_base-1.1.20-cp310-cp310-macosx_10_9_universal2.whl

---
+++ packages/cvxpy-base/cvxpy_base-1.1.20-cp310-cp310-macosx_10_9_universal2.whl/cvxpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CVXPY SETUP.PY
+short_version = '1.1.20'
+version = '1.1.20'
+full_version = '1.1.20'
+git_revision = 'db1e75e'
+commit_count = '0'
+release = True
+if not release:
+    version = full_version
