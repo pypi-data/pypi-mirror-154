2b600054509abd98831222f15bbcfa1c120b20c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:12 +0000

    Add package nekonohako/0.0.2.0/nekonohako-0.0.2.0-py3-none-any.whl

---
+++ packages/nekonohako/nekonohako-0.0.2.0-py3-none-any.whl/nekonohako/nekonohako.py
@@ -0,0 +1,15 @@
+from .core.catbox import Catbox
+
+
+class NekoNoHako:
+    instances = {}
+
+    @staticmethod
+    def get(user_hash='') -> Catbox:
+        if user_hash not in NekoNoHako.instances:
+            config = {
+                'user_hash': user_hash
+            }
+            NekoNoHako.instances[user_hash] = Catbox(config)
+
+        return NekoNoHako.instances[user_hash]
---
+++ packages/nekonohako/nekonohako-0.0.2.0-py3-none-any.whl/nekonohako/test.py
@@ -0,0 +1,4 @@
+from src.nekonohako.nekonohako import NekoNoHako
+
+nknh = NekoNoHako.get('7e99f099b43f518af9ca4eda1')
+print(nknh.upload_file('C:\\Users\\images64\\Downloads\\umniy.gif'))
