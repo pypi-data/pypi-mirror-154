09a335eb8fd45970f3f347ab1b468df2cc5a9823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:47 +0000

    Add package namegeneratormodule/0.0.3/namegeneratormodule-0.0.3.tar.gz

---
+++ packages/namegeneratormodule/namegeneratormodule-0.0.3.tar.gz/namegeneratormodule-0.0.3/namegeneratormodule/name.py
@@ -0,0 +1,12 @@
+from database import get_first_name, get_last_name
+
+class Name:
+    def __init__(self):
+        pass 
+
+    def __call__(self, nationality='default'):
+        return f"{get_first_name()} {get_last_name()}"
+
+
+if __name__ == "__main__":
+    pass
